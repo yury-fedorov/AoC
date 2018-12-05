dcf27b629f6bb9be4f2f6989ad3886556b4bb5d2
FEDOROV Yury <fedoy@chzrhmac030.local>
2018-12-05 06:05:30 +0100

    Day 5 start

---
+++ AdventOfCode2018.Tests/Day5Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+namespace AdventOfCode2018.Tests
+{
+    public class Day5Test
+    {
+        private readonly Day5.Day5 day5 = new Day5.Day5();
+
+        [TestCase("Day5Input.txt")]
+        public void TestStar1(string file)
+        {
+            var lines = File.ReadAllLines(Path.Combine(Day1Test.Directory, file));
+            Assert.IsNotNull(lines, "file not read");
+        }
+    }
+}
---
+++ AdventOfCode2018/Day5.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode2018
+{
+    namespace Day5
+    {
+        public class Day5
+        {
+
+        }
+    }
+}
