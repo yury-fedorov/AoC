147fa66dcca0b6dbaa4f61373af555eeb1589cd4
FEDOROV Yury <fedoy@chzrhmac030.local>
2018-12-06 06:26:58 +0100

    Day 6 start

---
+++ AdventOfCode2018.Tests/Day6Test.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+// {}
+namespace AdventOfCode2018.Tests
+{
+    public class Day6Test
+    {
+   
+        [Test]
+        //[TestCase("Day5Input.txt")]
+        public void TestCase1() { }
+    }
+}
