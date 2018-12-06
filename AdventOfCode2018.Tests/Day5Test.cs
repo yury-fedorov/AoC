945b6663c687cf06c8a9cc995652d24543ff1d88
FEDOROV Yury <fedoy@chzrhmac030.local>
2018-12-06 06:14:41 +0100

    solved

--- AdventOfCode2018.Tests/Day5Test.cs
+++ AdventOfCode2018.Tests/Day5Test.cs
@@ -8,11 +8,33 @@ namespace AdventOfCode2018.Tests
     {
         private readonly Day5.Day5 day5 = new Day5.Day5();
 
+        [Ignore("takes 25 seconds")]
         [TestCase("Day5Input.txt")]
-        public void TestStar1(string file)
+        public void FileTest(string file)
         {
             var lines = File.ReadAllLines(Path.Combine(Day1Test.Directory, file));
-            Assert.IsNotNull(lines, "file not read");
+            var polimer = lines.First();
+            Assert.IsNotNull(polimer, "polimer not read");
+            var output = day5.OptimizePolimer(polimer);
+            // File.WriteAllText("/Users/fedoy/projects/AoC18/Day5-1.txt", output);
+            Assert.AreEqual(9822,output.Length, "result1");
+
+            var d = polimer.Select(c => Char.ToLower(c))
+                .Distinct().ToDictionary(c=>c,c=> day5.OptimizePolimer(
+                    String.Concat( polimer.Where(a=>Char.ToLower(a) != c ) ) ).Length );
+            var min = d.Values.Min();
+            Assert.AreEqual(5726, min,"result2");
+        }
+
+        [TestCase("aA","")]
+        [TestCase("abBA", "")]
+        [TestCase("abAB", "abAB")]
+        [TestCase("aabAAB", "aabAAB")]
+        [TestCase("dabAcCaCBAcCcaDA", "dabCBAcaDA")]
+
+        public void TestCase(string polimer, string optimized)
+        {
+            Assert.AreEqual(optimized, day5.OptimizePolimer(polimer), "not well optimized");
         }
     }
 }
--- AdventOfCode2018/Day5.cs
+++ AdventOfCode2018/Day5.cs
@@ -4,13 +4,34 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 
+// {} []
 namespace AdventOfCode2018
 {
     namespace Day5
     {
         public class Day5
         {
+            public bool IsOpposite(char a, char b) {
+                return a != b && Char.ToLower(a) == Char.ToLower(b);
+            }
 
+            public string OptimizePolimer(string polimer) 
+            {
+                var list = polimer.ToList();
+                for ( int i = 1; i < list.Count ; i++) {
+                    char b = list[i];
+                    char a = list[i - 1];
+                    if (IsOpposite(a, b))
+                    {
+                        // we destroy them
+                        list.RemoveAt(i);
+                        list.RemoveAt(i - 1);
+                    }
+                }
+                
+                var shorter = string.Concat(list);
+                return polimer.Length > shorter.Length ? OptimizePolimer(shorter) : polimer;
+            }
         }
     }
 }
