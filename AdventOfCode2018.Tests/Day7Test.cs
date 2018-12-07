1cd57e38d6a1cb70b3f7b5a3a9dead7966da1b61
FEDOROV Yury <fedoy@chzrhmac030.local>
2018-12-07 07:06:51 +0100

    day 7 - task 1 solved

--- AdventOfCode2018.Tests/Day7Test.cs
+++ AdventOfCode2018.Tests/Day7Test.cs
@@ -3,109 +3,63 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NUnit.Framework;
+using System.Text.RegularExpressions;
 
 // {}
 // []
 namespace AdventOfCode2018.Tests
 {
 
-    public class Point { 
-        public int X;  public int Y; 
-
-        public static Distance GetDistance(Point a, Point b)
-        {
-            return new Distance { X = Math.Abs(a.X - b.X), Y = Math.Abs(a.Y - b.Y) };
-        }
-
-        public override string ToString()
-        {
-            return string.Format("({0},{1})", X,Y);
-        }
-
-        public override bool Equals(object obj)
-        {
-            return ToString().Equals(obj.ToString());
-        }
-
-        public override int GetHashCode()
-        {
-            return ToString().GetHashCode();
-        }
-    }
-
-    public class PointId : Point { public int Id;
-
-        public override string ToString()
-        {
-            return string.Format("(({0},{1}),{2})", X, Y, Id);
-        }
-    }
-
-    public class Distance {
-        public int X; public int Y;
-
-        public int GetManhattan() {
-            return X + Y;
-        }
-    }
-
-    public class Day6Test
+    public class Day7Test
     {
-        [TestCase("Day6Sample.txt", 17,32,16)]
-        [TestCase("Day6Input.txt",3223,10000, 40495)]
-        public void TestCase1(string file, int expectedLargestArea, int maxDistance, int maxDistanceArea) {
+        // A,B
+        (char,char) ParseLine(string line) {
+            var pattern = @"Step ([A-Z]) must be finished before step ([A-Z]) can begin.";
+            var matches = Regex.Matches(line, pattern);
+            foreach (Match match in matches)
+            {
+                return (
+                    match.Groups[1].Value.Single(), // first
+                    match.Groups[2].Value.Single() );
+            }
+            throw new ArgumentException(line);
+
+        }
+
+
+        public string t1(IEnumerable<string>lines) {
+            var dpl = lines.Select(l => ParseLine(l))
+                .OrderBy( a=> string.Concat<char>( new[] { a.Item1, a.Item2 } ) )
+                .ToList();
+            var set = dpl.Select(a=>a.Item1).Concat(dpl.Select(a=>a.Item2)).ToHashSet();
+
+            var r = new List<char>();
+
+            while (set.Any()) {
+                // all elements without dependency from left ordered by alphabet
+                var set1 = set.Where(e => !dpl.Any(t => t.Item2 == e))
+                    .OrderBy(a => a).ToArray();
+                foreach(var e1 in set1)
+                {
+                    var dpl1 = dpl.Where(a => a.Item1 != e1 ).ToList();
+                    var isInternalDependency = dpl1.Any(t => t.Item2 == e1);
+                    if (isInternalDependency) continue;
+                    r.Add(e1);
+                    set.Remove(e1); // this element not necessary any more
+                    dpl = dpl1;
+                    break;
+                }
+            }
+
+            return string.Concat(r); 
+        }
+
+        [TestCase("Day7Sample.txt", "CABDFE")]
+        [TestCase("Day7Input.txt", "BGJCNLQUYIFMOEZTADKSPVXRHW")]
+        public void TestCase1(string file, string er1) {
             var lines = File.ReadAllLines(Path.Combine(Day1Test.Directory, file));
-            var pId = 0;
-            var points = lines.Select(l => {
-                var a = l.Split(',');
-                return new PointId { X = Convert.ToInt32(a[0]), 
-                    Y = Convert.ToInt32(a[1]), Id = pId++ }; }).ToArray();
-            var maxX = points.Max(p => p.X);
-            var maxY = points.Max(p => p.Y);
-            const int NoId = -1;
-
-            var map = new Dictionary<Point, int>();
-
-            for (int x = 0; x <= maxX; x++)
-            {
-                for (int y = 0; y <= maxY; y++)
-                {
-                    var p = new Point { X = x, Y = y };
-                    // if (map[p] == ND) 
-                    {
-                        var pd = points.Select(a => Tuple.Create(a,Point.GetDistance(a, p).GetManhattan())).ToArray();
-                        var min = pd.Select(a => a.Item2).Min();
-                        var minPd = pd.Where(a => a.Item2 == min).ToArray();
-                        map[p] = minPd.Length == 1 ? minPd.Single().Item1.Id : NoId;
-                    }
-                }
-            }
-            var idArea = points.ToDictionary(a => a.Id, a=>  map.Count(b => b.Value == a.Id));
-            for (int x = 0; x <= maxX; x++)
-            {
-                for (int y = 0; y <= maxY; y++)
-                {
-                    if (x == 0 || x == maxX || y == 0 || y == maxY ) {
-                        var p = new Point { X = x, Y = y };
-                        var id = map[p];
-                        idArea.Remove(id);
-                    }
-                }
-            }
-            var max = idArea.Max(a => a.Value);
-            Assert.AreEqual(expectedLargestArea, max); // answer 1
-
-            int area = 0;
-            for (int x = 0; x <= maxX; x++)
-            {
-                for (int y = 0; y <= maxY; y++)
-                {
-                    var p = new Point { X = x, Y = y };
-                    var isOut = points.Select(a => Point.GetDistance(p, a).GetManhattan()).Sum() >= maxDistance;
-                    area += isOut ? 0 : 1;
-                }
-            }
-            Assert.AreEqual(maxDistanceArea, area, "wrong area");
+            var r1 = t1(lines);
+            Assert.AreEqual(er1,r1, "wrong result 1");
         }
     }
 }
