b34cdc6789c33d3f03b8770311498c60ce7e7a1d
FEDOROV Yury <fedoy@chzrhmac030.local>
2018-12-10 06:24:15 +0100

    day 10 task 1 beginning

---
+++ AdventOfCode2018/Main.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+// {} []
+namespace AdventOfCode2018.App
+{
+
+
+    public class App
+    {
+        public static void Main(string[] args )
+        {
+        }
+    }
+}
+// {} []
