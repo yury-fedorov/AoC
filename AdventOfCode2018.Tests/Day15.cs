45d39af718e7a2af7c5c7d7a7856dad4608d6e55
FEDOROV Yury <fedoy@chzrhmac030.local>
2018-12-15 09:34:49 +0100

    get destination of man

--- AdventOfCode2018.Tests/Day15.cs
+++ AdventOfCode2018.Tests/Day15.cs
@@ -6,9 +6,9 @@ using NUnit.Framework;
 
 namespace AdventOfCode2018.Day15
 {
-    public class Point : Tuple<int,int>
+    public class Point : Tuple<int, int>
     {
-        public Point(int x, int y) : base (x,y) { }
+        public Point(int x, int y) : base(x, y) { }
         public int X => Item1;
         public int Y => Item2;
 
@@ -16,7 +16,7 @@ namespace AdventOfCode2018.Day15
 
         public Point Go(Direction direction) {
             var dx = direction == Direction.Left ? -1 : direction == Direction.Right ? 1 : 0;
-            var dy = direction == Direction.Up   ? -1 : direction == Direction.Down  ? 1 : 0;
+            var dy = direction == Direction.Up ? -1 : direction == Direction.Down ? 1 : 0;
             return new Point(X + dx, Y + dy);
         }
     }
@@ -60,17 +60,17 @@ namespace AdventOfCode2018.Day15
             }
         }
 
-        public IEnumerable<Tuple<Race,Point>> FindAllMen() 
-        { 
-            for ( int x = 0; x < Width; x++)
+        public IEnumerable<Tuple<Race, Point>> FindAllMen()
+        {
+            for (int x = 0; x < Width; x++)
             {
-                for ( int y = 0; y < Height; y++)
+                for (int y = 0; y < Height; y++)
                 {
                     var cell = Map[x, y];
-                    if ( cell == Goblin || cell == Elf)
+                    if (cell == Goblin || cell == Elf)
                     {
                         var race = cell == Goblin ? Race.Goblin : Race.Elf;
-                        yield return Tuple.Create(race, new Point(x,y));
+                        yield return Tuple.Create(race, new Point(x, y));
                     }
                 }
             }
@@ -88,7 +88,7 @@ namespace AdventOfCode2018.Day15
 
         public Direction Opposite(Direction direction)
         {
-            switch(direction)
+            switch (direction)
             {
                 case Direction.Up: return Direction.Down;
                 case Direction.Down: return Direction.Up;
@@ -101,15 +101,35 @@ namespace AdventOfCode2018.Day15
         public IEnumerable<Direction> Directions(Point start)
         {
             // in which directions we may go from here?
-            return AllDirections.Where( d => At(start.Go(d)) == Path );
+            return AllDirections.Where(d => At(start.Go(d)) == Path);
         }
 
-        public void Move( Man man, Direction direction)
+        public void Move(Man man, Direction direction)
         {
             var newPosition = man.Position.Go(direction);
             Map[man.Position.X, man.Position.Y] = Path;
             man.Position = newPosition;
             Map[man.Position.X, man.Position.Y] = man.Race == Race.Elf ? Elf : Goblin;
+        }
+
+        public bool IsDirectPath(Point p0, Point p1)
+        {
+            var distance = p0.Distance(p1);
+            if (distance < 2) return true;
+            var dx = p1.X - p0.X;
+            var options = new List<Point>();
+            if (dx != 0)
+            {
+                // we move closer by X
+                options.Add( new Point(p0.X + Math.Sign(dx),p0.Y));
+            }
+            var dy = p1.Y - p0.Y;
+            if (dy != 0)
+            {
+                // we move closer by Y
+                options.Add(new Point(p0.X, p0.Y + Math.Sign(dy)));
+            }
+            return options.Where(p => At(p) == Path).Any(p => IsDirectPath(p, p1));
         }
     }
 
@@ -129,7 +149,7 @@ namespace AdventOfCode2018.Day15
         public Race Enemy(Race race) => race == Race.Elf ? Race.Goblin : Race.Elf;
         public bool IsOver => !MenOfRace(Race.Elf).Any() || !MenOfRace(Race.Goblin).Any();
 
-        public void A(Man man)
+        public Point GetDestination(Man man)
         {
             var enemies = MenOfRace(Enemy(man.Race));
 
@@ -141,7 +161,31 @@ namespace AdventOfCode2018.Day15
             // simplest strategy: select path with minimal distance
             var minDistance = inRange.Select( man.Position.Distance ).Min();
 
+            // not sure they are shortest indeed to be prooved
             var nearestByAir = inRange.Where(p => man.Position.Distance(p) == minDistance);
+
+            var nearestByAirIndeed = 
+                nearestByAir.Where(p => _map.IsDirectPath(p, man.Position)).ToArray();
+
+            if (nearestByAirIndeed.Any())
+            {
+                // check how many
+                if ( nearestByAirIndeed.Length == 1)
+                {
+                    // we have found the destination
+                    return nearestByAirIndeed.Single();
+                }
+                else
+                {
+                    // we need to choose among equal options
+                    throw new Exception("choose among equal");
+                }
+            }
+            else
+            {
+                // more difficult task
+                throw new Exception("longer then by air distance optimization"); 
+            }
         }
     }
 
