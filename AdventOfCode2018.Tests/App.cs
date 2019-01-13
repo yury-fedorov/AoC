aa9d1a24baa3f3328e0b08dacf00c26013b5e868
Yury Fedorov <yury.fedorov@gmail.com>
2019-01-13 10:48:27 +0100

    refactoring - now single project

---
+++ AdventOfCode2018.Tests/App.cs
@@ -0,0 +1,9 @@
+﻿namespace AdventOfCode2018.App
+{
+    public class App
+    {
+        public const string Directory = //
+			"D:\\projects\\leonteq\\AoC18\\AdventOfCode2018.Tests";
+    // "/Users/fedoy/projects/AoC18/AdventOfCode2018.Tests";
+    }
+}
