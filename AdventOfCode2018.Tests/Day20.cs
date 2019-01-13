e7c44011283d52899739b20835a2d1d0550f4aa8
Yury Fedorov <yury.fedorov@gmail.com>
2019-01-13 23:47:57 +0100

    now all starting files and classes defined

---
+++ AdventOfCode2018.Tests/Day20.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode2018
+{
+    public class Day20
+    {
+        [TestCase()]
+        public void Test()
+        {
+            Assert.Fail("to be implemented");
+        }
+    }
+}
---
+++ AdventOfCode2018.Tests/Day25.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace AdventOfCode2018
+{
+    public class Day25
+    {
+        [TestCase()]
+        public void Test()
+        {
+            Assert.Fail("to be implemented");
+        }
+    }
+}
