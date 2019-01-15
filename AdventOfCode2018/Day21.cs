f531d4a5be15cfa47cdfe3a5369b295bded4f682
Yury Fedorov <yury.fedorov@gmail.com>
2019-01-15 12:03:42 +0100

    #5 - while instead of goto

--- AdventOfCode2018/Day21.cs
+++ AdventOfCode2018/Day21.cs
@@ -5,6 +5,46 @@ namespace AdventOfCode2018
 {
     public class Day21
     {
+        void CodeV2(long r0)
+        {
+            long r2, r3, r4, r5;
+            r2 = r3 = r4 = r5 = 0;
+
+            r5 = 0;                             // 05: seti 0 4 5
+
+            while (true)
+            {
+                r3 = r5 | 0x10000;              // 06: bori 5 65536 3            
+                r5 = 521363;     // 7F493	    // 07: seti 521363 7 5            
+                while(true)
+                {
+                    r4 = r3 & 0xFF;             // 08: bani 3 255 4
+                    r5 += r4;                   // 09: addr 5 4 5
+                    r5 &= 0xFFFFFF;             // 10: bani 5 16777215 5
+                    r5 *= 65899;    // 1016B    // 11: muli 5 65899 5
+                    r5 &= 0xFFFFFF;             // 12: bani 5 16777215 5
+                    r4 = 256 > r3 ? 1 : 0;      // 13: gtir 256 3 4
+                    if (r4 != 0)                // 14: addr 4 1 1
+                        break;                  // 16: seti 27 1 1            
+
+                    r4 = 0;                     // 17: seti 0 2 4
+                    while (true)
+                    {
+                        r2 = r4 + 1;            // 18: addi 4 1 2                                    			
+                        r2 = r2 << 8;           // 19: muli 2 256 2            // r2 *= 256;
+                        r2 = r2 > r3 ? 1 : 0;   // 20: gtrr 2 3 2
+                        if (r2 != 0)            // 21: addr 2 1 1
+                            break;              // 23: seti 25 2 1
+                        r4++;                   // 24: addi 4 1 4
+                    }                           // 25: seti 17 3 1
+
+                    r3 = r4;                    // 26: setr 4 2 3
+                }                               // 27: seti 7 1 1
+                if (r5 == r0)                   // 28: eqrr 5 0 4
+                    return;                     // 29: addr 4 1 1
+            }                                   // 30: seti 5 8 1
+        }	    
+	    
         void CodeV1(long r0) 
         {
             long r2, r3, r4, r5;
