60d72aa1965962fd5d54744c51709f6a33ca9fbe
Yury Fedorov <yury.fedorov@gmail.com>
2019-01-16 17:54:24 +0100

    #6 - Day 22 - Task 1 - solved

--- AdventOfCode2018/Day22.cs
+++ AdventOfCode2018/Day22.cs
@@ -18,6 +18,8 @@ namespace AdventOfCode2018
             _target = target;
         }
 
+        public int ToErosionLevel(BigInteger geologicalIndex) => (int)((geologicalIndex + _depth) % 20183);
+
         public BigInteger GeologicalIndex( (int,int) point )
         {
             BigInteger value;
@@ -28,18 +30,19 @@ namespace AdventOfCode2018
             else if (point == _target) value = 0;
             else if (y == 0) value = (x * 16807);
             else if (x == 0) value = (y * 48271);
-            else value = GeologicalIndex( (x-1, y) ) * GeologicalIndex( (x, y-1) );
+            else value = ToErosionLevel(GeologicalIndex( (x-1, y) )) * ToErosionLevel(GeologicalIndex( (x, y-1) ));
             _pointIndexMap.Add(point, value);
             return value;
         }
 
-        public Material ToMaterial(BigInteger geologicalIndex) 
-            => (Material)( ( (int)( (geologicalIndex + _depth) % 20183 ) ) % 3 );
+        public Material ToMaterial(BigInteger geologicalIndex) => (Material)( ToErosionLevel(geologicalIndex) % 3 );
 
         public Material At((int, int) point) => ToMaterial(GeologicalIndex(point));
 
         public string Draw()
         {
+            // Rocky, Wet, Narrow - Drawing this same cave system with rocky as ., wet as =, narrow as |
+            char[] mapping = { '.', '=', '|' }; 
             var (tx, ty) = _target;
             var map = new StringBuilder();
             for (var y = 0; y <= ty; y++)
@@ -47,7 +50,7 @@ namespace AdventOfCode2018
                 for (var x = 0; x <= tx; x++)
                 {
                     var material = At((x, y));
-                    char ch = material == Material.Wet ? '=' : material == Material.Rocky ? '.' : '|';
+                    char ch = mapping[(int)material];
                     map.Append(ch);
                 }
                 map.AppendLine();
@@ -62,10 +65,11 @@ namespace AdventOfCode2018
         // depth: 7863
         // target: 14,760
         [TestCase(510,10,10,114)]
-        [TestCase(7863, 14, 760, 786)] // 786 - too low
+        [TestCase(7863, 14, 760, 11462)] // 786 - too low
         public void Test(int depth, int tx, int ty, int eRiskLevel)
         {
             var map = new Map(depth, (tx,ty) );
+            var a =  map.At((1, 1));
             int riskLevel = 0;
             for (var x = 0; x <= tx; x++ )
             {
