bb66abca5aa84385a16fe5a51745a51bde3cd6d8
Yury Fedorov <yury.fedorov@gmail.com>
2019-01-21 15:24:02 +0100

    #8 - Day 24 - Task 1 - solved

--- AdventOfCode2018/Day24.cs
+++ AdventOfCode2018/Day24.cs
@@ -67,79 +67,6 @@ namespace AdventOfCode2018.Day24
 
     public class Day24
     {
-        // real task
-        public IEnumerable<Group> Input()
-        {
-            var nill = Enumerable.Empty<Damage>();
-            return new[] {
-                // Immune System
-                //457 units each with 4941 hit points with an attack that does 98 slashing damage at initiative 14
-                new Group(false,457,4941,98,Damage.Slashing,14,nill,nill),
-                //35 units each with 3755 hit points (weak to cold; immune to fire, bludgeoning) with an attack that does 1021 bludgeoning damage at initiative 1
-                new Group(false,35,3755,1021,Damage.Bludgeoning,1, new [] { Damage.Cold }, new [] { Damage.Fire, Damage.Bludgeoning }),
-                //6563 units each with 7155 hit points(immune to radiation, bludgeoning) with an attack that does 10 cold damage at initiative 11
-                new Group(false,6563,7155,10,Damage.Cold,11, nill, new [] { Damage.Radiation, Damage.Bludgeoning }),
-                //5937 units each with 1868 hit points(weak to slashing) with an attack that does 2 fire damage at initiative 2
-                new Group(false,5937,1868,2,Damage.Fire,2, new [] { Damage.Slashing }, nill ),
-                //422 units each with 7279 hit points(immune to radiation, cold) with an attack that does 170 bludgeoning damage at initiative 16
-                new Group(false,422,7279,170,Damage.Bludgeoning,16, nill, new [] { Damage.Radiation, Damage.Cold } ),
-                //279 units each with 9375 hit points(weak to cold) with an attack that does 333 slashing damage at initiative 10
-                new Group(false,279,9375,333,Damage.Slashing,10, new [] { Damage.Cold }, nill ),
-                //4346 units each with 3734 hit points(weak to cold; immune to fire, slashing) with an attack that does 8 bludgeoning damage at initiative 9
-                new Group(false,4346,3734,8,Damage.Bludgeoning,9, new [] { Damage.Cold }, new [] { Damage.Fire, Damage.Slashing } ),
-                //1564 units each with 1596 hit points with an attack that does 8 bludgeoning damage at initiative 19
-                new Group(false,1564,1596,8,Damage.Bludgeoning,19, nill, nill ),
-                //361 units each with 1862 hit points(weak to bludgeoning, slashing) with an attack that does 40 bludgeoning damage at initiative 3
-                new Group(false,361,1862,40,Damage.Bludgeoning,3, new [] { Damage.Bludgeoning, Damage.Slashing }, nill ),
-                //427 units each with 8025 hit points(immune to fire, slashing; weak to cold) with an attack that does 139 bludgeoning damage at initiative 7
-                new Group(false,427,8025,139,Damage.Bludgeoning,7, new [] { Damage.Cold}, new [] { Damage.Fire, Damage.Slashing } ),
-
-                //Infection:
-                //27 units each with 24408 hit points(weak to bludgeoning, fire) with an attack that does 1505 cold damage at initiative 5
-                new Group(true,27,24408,1505,Damage.Cold,5,new []{ Damage.Bludgeoning, Damage.Fire }, nill),
-                //137 units each with 29784 hit points(immune to slashing) with an attack that does 423 cold damage at initiative 12
-                new Group(true,137,29784,423,Damage.Cold,12, nill, new []{ Damage.Slashing } ),
-                //1646 units each with 15822 hit points(weak to slashing, cold; immune to fire) with an attack that does 16 fire damage at initiative 6
-                new Group(true,1646,15822,16,Damage.Fire,6,new []{ Damage.Slashing, Damage.Cold }, new [] { Damage.Fire }),
-                //1803 units each with 10386 hit points(immune to cold, radiation) with an attack that does 11 bludgeoning damage at initiative 17
-                new Group(true,1803,10386,11,Damage.Bludgeoning,17,nill, new []{ Damage.Cold, Damage.Radiation }),
-                //2216 units each with 39081 hit points with an attack that does 28 bludgeoning damage at initiative 20
-                new Group(true,2216,39081,28,Damage.Bludgeoning,20,nill, nill),
-                //3192 units each with 40190 hit points(weak to radiation, cold; immune to slashing) with an attack that does 22 radiation damage at initiative 15
-                new Group(true,3192,40190,22,Damage.Radiation,15,new []{ Damage.Radiation, Damage.Cold }, new [] { Damage.Slashing}),
-                //1578 units each with 69776 hit points with an attack that does 63 cold damage at initiative 4
-                new Group(true,1578,69776,63,Damage.Cold,4, nill, nill),
-                //2950 units each with 40891 hit points(weak to fire) with an attack that does 25 slashing damage at initiative 8
-                new Group(true,2950,40891,25,Damage.Slashing,8,new []{ Damage.Fire }, nill),
-                //70 units each with 56156 hit points(weak to radiation) with an attack that does 1386 radiation damage at initiative 13
-                new Group(true,70,56156,1386,Damage.Radiation,13,new []{ Damage.Radiation }, nill),
-                //1149 units each with 48840 hit points(weak to slashing) with an attack that does 63 slashing damage at initiative 18
-                new Group(true,1149,48840,63,Damage.Slashing,18,new []{ Damage.Slashing }, nill),
-            };
-        }
-
-        public IEnumerable<Group> TestInput()
-        {
-            var nill = Enumerable.Empty<Damage>();
-            return new[] {
-                //Immune System:
-                //17 units each with 5390 hit points (weak to radiation, bludgeoning) with
-                // an attack that does 4507 fire damage at initiative 2
-                new Group(false, 17,5390,4507,Damage.Fire,2, new [] { Damage.Radiation, Damage.Bludgeoning }, nill ),
-                //989 units each with 1274 hit points (immune to fire; weak to bludgeoning,
-                // slashing) with an attack that does 25 slashing damage at initiative 3
-                new Group(false, 989,1274,25,Damage.Slashing,3, new [] { Damage.Bludgeoning, Damage.Slashing }, new [] { Damage.Fire } ),
-
-                //Infection:
-                //801 units each with 4706 hit points(weak to radiation) with an attack
-                //that does 116 bludgeoning damage at initiative 1
-                new Group(true, 801,4706,116,Damage.Bludgeoning,1, new [] { Damage.Radiation }, nill ),
-                //4485 units each with 2961 hit points(immune to radiation; weak to fire,
-                // cold) with an attack that does 12 slashing damage at initiative 4
-                new Group(true, 4485,2961,12,Damage.Slashing,4, new [] { Damage.Fire, Damage.Cold }, new [] { Damage.Radiation } ),
-            };
-        }
-
         public IEnumerable<Group> Alive(IEnumerable<Group> groups)
             => groups.Where(g => g.Units > 0);  
 
@@ -153,7 +80,7 @@ namespace AdventOfCode2018.Day24
         public int EstimateDamage(Group attacking, Group target, bool selection)
         {
             var maxHit = target.DamageK(attacking.AttackType) * attacking.EffectivePower;
-            if (maxHit < target.HitPoints) return 0; // if we can not kill even one enemy unit, no demage here
+            // if (maxHit < target.HitPoints) return 0; // if we can not kill even one enemy unit, no demage here
             return selection ? maxHit : Math.Min(maxHit, target.PotentialDamage);
         }
 
@@ -247,8 +174,8 @@ namespace AdventOfCode2018.Day24
             }
         }
 
-        // [TestCase("Day24Sample.txt", 5216)]
-        [TestCase("Day24.txt", -1)] // 13731 - too low
+        [TestCase("Day24Sample.txt", 5216)]
+        [TestCase("Day24.txt", 14000)]
         public void Test(string file, int expected)
         {
             // var groups = Input().ToArray();
@@ -292,7 +219,6 @@ namespace AdventOfCode2018.Day24
                 }
             }
             var result = Alive(groups).Sum(g => g.Units);
-            Assert.Greater(result, 13731); // to low
             Assert.AreEqual(expected, result);
         }
     }
