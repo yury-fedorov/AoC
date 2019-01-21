0de7b5b6ec179e5fe33dfb264adccb4eab126239
Yury Fedorov <yury.fedorov@gmail.com>
2019-01-21 23:18:10 +0100

    #6 - Day 22 - Task 2 - first approximation

--- AdventOfCode2018/Day22.cs
+++ AdventOfCode2018/Day22.cs
@@ -1,11 +1,15 @@
 using NUnit.Framework;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Numerics;
 using System.Text;
 
 namespace AdventOfCode2018
 {
     public enum Material { Rocky, Wet, Narrow }
+
+    public enum Tool { Neither, Torch, ClimbingGear }
 
     public class Map
     {
@@ -59,12 +63,59 @@ namespace AdventOfCode2018
         }
     }
 
+    public class AirMap : Dictionary<(int, int, Tool), int> { }
+
     public class Day22
     {
+        // Tools can only be used in certain regions:
+        public Tool NoGo(Material material)
+        {
+            switch(material)
+            {
+                // In narrow regions, you can use the torch or neither tool. 
+                // You cannot use the climbing gear (it's too bulky to fit).
+                case Material.Narrow: return Tool.ClimbingGear;
+
+                // In rocky regions, you can use the climbing gear or the torch.
+                // You cannot use neither(you'll likely slip and fall).
+                case Material.Rocky: return Tool.Neither;
+
+                // In wet regions, you can use the climbing gear or neither tool.
+                // You cannot use the torch (if it gets wet, you won't have a light source).
+                case Material.Wet: return Tool.Torch;
+            }
+            throw new Exception("not expected material");
+        }
+        
+        public IEnumerable<Tool> Go(Material material) => OtherTools(NoGo(material));
+
+        public IEnumerable<Tool> OtherTools(Tool tool) => Enum.GetValues(typeof(Tool)).Cast<Tool>().Where(t => t != tool);
+    
+
+        const int SameTool = 1;
+        const int OtherTool = 7;
+
+        // get the wishing tool, if absent, get 2 other tools and select minimal of them + other tool
+        public int CostTill(AirMap map, int x, int y, Tool tool)
+        {
+            if (x < 0 || y < 0) return int.MaxValue;
+            int cost;
+            if (!map.TryGetValue((x, y, tool), out cost))
+            {
+                // this tool is no go on the region
+                var others = OtherTools(tool).Where(t => map.ContainsKey( (x,y,t) ) );
+                cost = others.Select(t => map[(x, y, t)]).Min() + OtherTool;
+            }
+            return cost;
+        }
+
+        public int MinimalCost( AirMap map, int x, int y, Tool tool )
+            => Math.Min( CostTill(map,x-1,y,tool), CostTill(map,x,y-1,tool) ) + SameTool;
+
         // Puzzle input
         // depth: 7863
         // target: 14,760
-        [TestCase(510,10,10,114)]
+        // [TestCase(510,10,10,114)]
         [TestCase(7863, 14, 760, 11462)] // 786 - too low
         public void Test(int depth, int tx, int ty, int eRiskLevel)
         {
@@ -80,7 +131,37 @@ namespace AdventOfCode2018
                 }
             }
             var image = map.Draw();
-            Assert.AreEqual(eRiskLevel, riskLevel); 
+            Assert.AreEqual(eRiskLevel, riskLevel);
+
+            var mapAirPath = new AirMap();
+            // You start at 0,0 (the mouth of the cave) with the torch equipped
+            mapAirPath.Add((0, 0, Tool.Torch), 0);
+
+            Assert.AreEqual(Material.Rocky, map.At((tx, ty)));
+
+            var maxAirPath = tx + ty;
+            for ( var diagonal = 1; diagonal <= maxAirPath; diagonal++ )
+            {
+                for ( var x = 0; x <= diagonal; x++ )
+                {
+                    var y = diagonal - x;
+                    var material = map.At((x, y));
+                    var tools = Go(material); // this tools can be used in this region
+                    foreach ( var tool in tools )
+                    {
+                        var cost = MinimalCost(mapAirPath, x, y, tool);
+                        Assert.GreaterOrEqual(cost, x + y);
+                        mapAirPath.Add( (x,y,tool), cost );
+                    }
+                }
+            }
+            // Finally, once you reach the target, you need the torch equipped before you can find him in the dark. 
+            // The target is always in a rocky region, so if you arrive there with climbing gear equipped, 
+            // you will need to spend seven minutes switching to your torch.
+            var airDistance = mapAirPath[(tx,ty,Tool.Torch)];
+            // 1656 - this is the upper bond (simplest algorithm)
+            // 774 - this is the lower bond (no change of tool at all)
+            Assert.AreEqual(-1, airDistance);
         }
     }
 }
