1bb464fb7659b1fd33a60be51607cf05f3ef05e2
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-02 15:35:05 +0100

    Day2 2019 solved

--- AoC19/Day02.fs
+++ AoC19/Day02.fs
@@ -1,5 +1,8 @@
 ﻿module Day02
 
+open System.IO
+open Xunit
+
 let Sample = "1,9,10,3,2,3,11,0,99,30,40,50"
 
 let IntArr (s : string) = s.Split ',' |> Array.map int
@@ -35,6 +38,86 @@ let ExecOp pos (code: array<int>) : Operation =
         let res = (f arg1 arg2)
         Math { Result = res; ResultIndex = resIndex; Position = (pos + 4); }
 
+let rec Exec pos (code: array<int>) =
+    let op = ExecOp pos code
+    match op with
+    | End -> code
+    | Math m ->
+        code.[(m.ResultIndex)] <- m.Result
+        Exec m.Position code
+
 [<Theory>]
-[<InlineData(0,70,3,3)>]
-let SampleTest pos0 
+[<InlineData(0,70,3,4)>]
+let SampleTest pos0 expR expRI expPos1 = 
+    let op = ExecOp pos0 (IntArr Sample)
+    match op with 
+    | End -> Assert.True(false)
+    | Math m -> 
+        Assert.Equal(expR, m.Result)
+        Assert.Equal(expRI, m.ResultIndex)
+        Assert.Equal(expPos1, m.Position)
+
+[<Theory>]
+[<InlineData("1,0,0,0,99","2,0,0,0,99")>]
+[<InlineData("2,3,0,3,99","2,3,0,6,99")>]
+[<InlineData("2,4,4,5,99,0","2,4,4,5,99,9801")>]
+[<InlineData("1,1,1,4,99,5,6,0,99","30,1,1,4,2,5,6,0,99")>]
+let SampleRun code0 code1 =
+    let c0 = IntArr code0
+    let c1 : array<int> = Exec 0 c0
+    let expC1 : array<int> = IntArr code1
+    let i = [| 0 .. (Array.length c1) - 1 |]
+    i |> Array.map ( fun i -> Assert.Equal(c1.[i],expC1.[i]) )
+
+
+let fileName = "../../../Day02.txt"
+
+[<Theory>]
+[<InlineData(3716250)>]
+let Answer1 exp =
+    let cs = File.ReadAllText(fileName)
+    let code = IntArr cs
+    code.[1] <- 12
+    code.[2] <- 2
+    let r = Exec 0 code
+    Assert.Equal(r.[0],exp)
+
+
+let Result2 = 19690720
+
+let Calc (code: array<int>) noun verb =
+    code.[1] <- noun
+    code.[2] <- verb
+    let r = Exec 0 code
+    r.[0]
+
+let Search (code: array<int>) = 
+    let l = (Array.length code) - 1
+    seq { 
+        for n in 0..l do
+            for v in 0..l do
+                let r = Calc (Array.copy code) n v
+                yield (n,v,r) 
+    }
+
+let Find (code: array<int>) =
+    Seq.find (fun (n,v,r) -> r = Result2) (Search code)
+
+let A2 n v = (n * 100) + v
+    
+[<Theory>]
+[<InlineData(6472)>] // 6471 - no
+let Answer2 er =
+    let cs = File.ReadAllText(fileName)
+    let code = IntArr cs
+    let (n,v,r) = Find code
+    Assert.Equal(er, (A2 n v))
+
+
+[<Theory>]
+[<InlineData(64,72)>] // 6471 - no
+let Test2 n v =
+    let cs = File.ReadAllText(fileName)
+    let code = IntArr cs
+    let r = Calc code n v
+    Assert.Equal(Result2, r) 
