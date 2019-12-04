c9e5d25b70494148ca1bbbfd23e3884abe6e216c
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-04 06:50:45 +0100

    draft day 4 question 1

---
+++ AoC19/Day04.fs
@@ -0,0 +1,28 @@
+﻿module Day04
+
+open System.IO
+open Xunit
+
+let IsValid ( pwd : int ) = 
+    let c = pwd.ToString().ToCharArray()
+    if (Array.length c <> 6 ) then false
+    else
+        let shifted = Array.tail c 
+        true
+
+[<Theory>]
+[<InlineData(111111, true)>]
+[<InlineData(223450, false)>]
+[<InlineData(123789, false)>] // does not meet these criteria (no double)
+let IsValidTest pwd exp = Assert.Equal( ( IsValid pwd ), exp )
+
+let Question1 from till = 
+    seq { from .. till } |> Seq.length
+
+[<Theory>]
+[<InlineData(111111,111113, 3)>]
+[<InlineData(136760,595730, -1)>]
+let Question1Test from till exp =
+    Assert.Equal( (Question1 from till), exp )
+
+
