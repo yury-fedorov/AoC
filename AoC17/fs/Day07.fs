053114459ef4d9bb4f2ed97d5363d6958ad5503f
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-04 19:49:48 +0100

    code review fsharp 2017

--- AoC17/fs/Day07.fs
+++ AoC17/fs/Day07.fs
@@ -7,3 +7,22 @@ let fileName = "../../../Day07.txt"
 
 let GetData = File.ReadAllLines(fileName)
 
+
+
+
+(*
+    using System.Text.RegularExpressions;
+
+    var patternOperation = @"([0-9]+) ([0-9]+) ([0-9]+) ([0-9]+)";
+
+    var matches = Regex.Matches(line, pattern);
+    foreach (Match match in matches)
+    {
+        return new[] {
+            Convert.ToInt32(match.Groups[1].Value),
+            Convert.ToInt32(match.Groups[2].Value),
+            Convert.ToInt32(match.Groups[3].Value),
+            Convert.ToInt32(match.Groups[4].Value) };
+    }
+*)
+
