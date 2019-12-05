006d9e63cafd1ee7c71594d5ca4be4bc6b3c6ba1
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-05 07:52:37 +0100

    draft

--- AoC19/Day05.fs
+++ AoC19/Day05.fs
@@ -14,9 +14,35 @@ let In : int =
 
 let Out arg = printf "%d" arg 
 
-let GetOperation (instruction:int) : int = instruction % 100 // last 2 digits
+let GetOpCode (instruction:int) : int = instruction % 100 // last 2 digits
 
 [<Theory>]
 [<InlineData(1002, 2)>]
 let TestGetOperation instruction expectedOperation = 
-    Assert.Equal( expectedOperation, GetOperation instruction )
+    Assert.Equal( expectedOperation, GetOpCode instruction )
+
+type Operation = 
+    | End
+    | Add of a: int * b: int * r: int
+    | Mul of a: int * b: int * r: int
+    | In of a: int
+    | Out of a: int
+
+let OperationLength (o:Operation) =
+    match o with 
+    | End -> 1
+    | Add (_,_,_) -> 4
+    | Mul (_,_,_) -> 4
+    | In _ -> 2
+    | Out _-> 2
+
+let GetArgsCount (o:Operation) =
+    let rawCount = ( OperationLength o ) - 2 // codop and result
+    max 0 rawCount // for End rawCount is negative
+
+let ReadOp pos (code: array<int>) : Operation = 
+    let instruction = code.[pos]
+    let opcode = GetOpCode instruction
+    match opcode with
+    | CodeEnd -> End
+    | CodeAdd -> Add( code.[pos+1], code.[pos+2], code.[pos+3] )
