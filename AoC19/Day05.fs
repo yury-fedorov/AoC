b1383d58927c61b0f28be3c2d8bbb6061a4669db
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-05 06:52:56 +0100

    draft day 5 2019

--- AoC19/Day05.fs
+++ AoC19/Day05.fs
@@ -1,1 +1,22 @@
-﻿
+﻿module Day05
+
+open System
+open System.IO
+open Xunit
+open Day02 // initial impletementation
+
+let CodeIn = 3
+let CodeOut = 4
+
+let In : int = 
+    printf "Input:"
+    Console.ReadLine() |> int
+
+let Out arg = printf "%d" arg 
+
+let GetOperation (instruction:int) : int = instruction % 100 // last 2 digits
+
+[<Theory>]
+[<InlineData(1002, 2)>]
+let TestGetOperation instruction expectedOperation = 
+    Assert.Equal( expectedOperation, GetOperation instruction )
