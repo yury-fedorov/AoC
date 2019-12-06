4afab9270069006b0c8703898857926b41503d57
FEDOROV Yury <fedoy@chzrhmac030.local>
2019-12-06 07:06:52 +0100

    draft day 6

--- AoC19/Day06.fs
+++ AoC19/Day06.fs
@@ -3,4 +3,25 @@
 open System
 open System.IO
 open Xunit
+ 
+type Tree = 
+    | Node of Value:string * Parent:Tree
+    | None
+
+let sampleFileName = "../../../Day06Sample.txt"
+
+let fileName = "../../../Day06.txt"
+
+let RootNode = Node(Value = "COM", Parent = None) 
+
+let Init file =
+    let lines = File.ReadAllLines file |> Array.map (fun line -> line.Split ')')
+    let first = lines |> Array.find (fun a -> a.[0] = RootNode.Value)
+    let mutable map = [ RootNode.Value, RootNode ] |> Map.ofList
+    let parent = RootNode
+    let small = first.[1]
+    let node = Node(Value=small, Parent = parent)
+    map <- map.Add( small, node )
+
+
 
