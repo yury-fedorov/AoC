e82de342096ac4c7a3dd67f3dab1069754db7b2d
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-11-07 01:27:39 +0100

    AoC15 Day07 start

---
+++ AoC15/07/day.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <fstream>
+#include <sstream>
+#include <boost/regex.hpp>
+
+using namespace std;
+using namespace boost;
+
+int main() {
+
+    const bool isFirstAnswer = false;
+
+    ifstream f("input.txt");
+    
+    regex re("");
+    smatch what;
+
+    string line;
+    while (getline(f, line)) {
+        if( regex_match( line, what, re, match_extra )) {
+
+        } else {
+            cerr << "Failed to parse line: " << line;
+        }
+    }
+
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << "" << endl; 
+
+    return 0;
+}
