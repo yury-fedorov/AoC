d1787c19d58b7e617a525a27a2e3d11e152012f7
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-11-08 22:22:15 +0100

    AoC15 Day08 - failed approach

--- AoC15/08/day.cpp
+++ AoC15/08/day.cpp
@@ -3,29 +3,51 @@
 #include <map>
 #include <fstream>
 #include <sstream>
-#include <boost/regex.hpp>
+#include <regex>
 
 using namespace std;
-using namespace boost;
+
+string replaceAll( string_view str, string_view oldText, string_view newText ) {
+    string s(str);
+    const auto oldSize = oldText.length();
+    while (true) {
+        const auto i = s.find(oldText);
+        if ( i == string::npos ) return s;
+        s.replace(i, oldSize, newText );
+    }
+}
+
+string replaceAllRegEx( const string & s, const string & oldRegEx, const string & newText ) {
+    regex re(oldRegEx);
+    return regex_replace(s,re,newText);
+}
 
 int main() {
 
     const bool isFirstAnswer = false;
 
     ifstream f("input.txt");
-    
-    regex re("(.+) -> (\\w+)");
-    smatch what;
+
+    cout << "\x1k" << endl;
+
+    auto codeLength = 0;
+    auto textLength = 0;
 
     string line;
     while (getline(f, line)) {
-        if( regex_match( line, what, re, match_extra )) {
-        } else {
-            cerr << "Failed to parse line: " << line;
-        }
+        const auto l = line.length();
+        codeLength += l; 
+
+        auto s = line.substr( 1, l - 2 ); // remove first and last
+        s = replaceAll(s, "\\\"", "\"" );
+        s = replaceAll(s, "\\\\", "\\" );
+        s = replaceAllRegEx(s, "(.*)(\\\\x[0-9a-f]{2})(.*)", "$1?$3" ); // does not matter what is the character but only the size
+        const auto l1 = s.length();
+        textLength += l1;
+        cout << "[" << line << "] - {" << s << "} = " << l << " - " << l1 << " = " << (l - l1) <<  endl; 
     }
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << "" << endl; 
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << (codeLength - textLength) << "" << endl; // 21026 - too high
 
     return 0;
 }
