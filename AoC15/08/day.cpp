71f12bd9c1ab8b492d3dfd8051c30baf1f32a4fa
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-11-09 23:23:39 +0100

    AoC15 Day08 solved

--- AoC15/08/day.cpp
+++ AoC15/08/day.cpp
@@ -1,25 +1,61 @@
 #include <iostream>
+#include <deque>
 #include <vector>
-#include <map>
 #include <fstream>
 #include <sstream>
-#include <regex>
+#include <string_view>
 
 using namespace std;
 
-string replaceAll( string_view str, string_view oldText, string_view newText ) {
-    string s(str);
-    const auto oldSize = oldText.length();
-    while (true) {
-        const auto i = s.find(oldText);
-        if ( i == string::npos ) return s;
-        s.replace(i, oldSize, newText );
-    }
+template<class T>
+char pop(deque<T> & c) {
+    const T ch = c[0];
+    c.pop_front();
+    return ch;
 }
 
-string replaceAllRegEx( const string & s, const string & oldRegEx, const string & newText ) {
-    regex re(oldRegEx);
-    return regex_replace(s,re,newText);
+// question 1
+string toMemory( string_view line ) {
+    const auto l = line.length();
+    const auto d = line.data();
+    deque<char> c( d + 1, d + l - 1 ); // remove first and last
+    vector<char> v;
+    while (!c.empty()) {
+        const auto next = pop(c);
+        if ( next != '\\' ) {
+            v.push_back(next);
+        } else {
+            const auto next1 = pop(c);
+            if ( next1 != 'x' ) {
+                v.push_back(next1);
+            } else {
+                const auto next2 = pop(c);
+                const auto next3 = pop(c);
+                v.push_back('?'); // we care only about size
+            }
+        }
+    }
+    return string(v.begin(), v.end());
+}
+
+// question 2
+string fromMemory(string_view line) {
+    const auto l = line.length();
+    const auto d = line.data();
+    deque<char> c( d, d + l );
+    vector<char> v;
+    v.push_back('\"');
+    while (!c.empty()) {
+        const auto next = pop(c);
+        if ( next == '\\' ) {
+            v.push_back( '\\' );
+        } else if ( next == '"' ) {
+            v.push_back( '\\' );
+        }
+        v.push_back(next);
+    }
+    v.push_back('\"');
+    return string(v.begin(), v.end());
 }
 
 int main() {
@@ -28,26 +64,20 @@ int main() {
 
     ifstream f("input.txt");
 
-    cout << "\x1k" << endl;
-
-    auto codeLength = 0;
-    auto textLength = 0;
+    int codeLength = 0;
+    int textLength = 0;
 
     string line;
     while (getline(f, line)) {
-        const auto l = line.length();
-        codeLength += l; 
-
-        auto s = line.substr( 1, l - 2 ); // remove first and last
-        s = replaceAll(s, "\\\"", "\"" );
-        s = replaceAll(s, "\\\\", "\\" );
-        s = replaceAllRegEx(s, "(.*)(\\\\x[0-9a-f]{2})(.*)", "$1?$3" ); // does not matter what is the character but only the size
-        const auto l1 = s.length();
+        const int l = line.length();
+        codeLength += l;
+        const string s = isFirstAnswer ? toMemory(line) : fromMemory(line);
+        const int l1 = s.length();
         textLength += l1;
-        cout << "[" << line << "] - {" << s << "} = " << l << " - " << l1 << " = " << (l - l1) <<  endl; 
+        cout << "[" << line << "] [" << s << "] = " << l << " - " << l1 << " = " << (l - l1) <<  endl; 
     }
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << (codeLength - textLength) << "" << endl; // 21026 - too high
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << (codeLength - textLength) << endl;
 
     return 0;
 }
