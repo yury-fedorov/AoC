9937c59b1f4a70dc5112f117dd11d0cfc53852f7
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-11-29 22:51:29 +0100

    AoC20 Day 1 - draft

---
+++ AoC20/01/day.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <regex>
+#include <numeric>
+#include <assert.h>
+#include <climits>
+
+using namespace std;
+
+int main() {
+
+    const bool isFirstAnswer = false;
+
+    ifstream f("input.txt");
+
+    string line;
+    while (getline(f, line)) {
+        /* regex draft
+        smatch what;
+        if( regex_match( line, what, re )) {
+            const auto & a = what[1];
+        } else {
+            cerr << "Unexpected line: " << line << endl;
+        }
+        */
+    }
+
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
+
+    return 0;
+}
