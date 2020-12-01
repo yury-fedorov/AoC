0c714ab446eee387a8d0c8a9b43e0a1ea63178e4
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-01 09:15:55 +0100

    AoC20 day 1 solve and optimized

--- AoC20/01/day.cpp
+++ AoC20/01/day.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
-#include <map>
-#include <set>
 #include <vector>
-#include <algorithm>
 #include <fstream>
-#include <sstream>
-#include <regex>
-#include <numeric>
-#include <assert.h>
-#include <climits>
 
 using namespace std;
 
-int main() {
+long long answer1( const vector<int> & numbers ) {
+    const auto n = numbers.size();
+    for ( auto i = 0; i < n; i++ ) {
+        const auto a = numbers[i];
+        for ( auto j = 0; j < i; j++ ) {
+            const auto b = numbers[j];
+            if ( ( a + b ) == 2020 ) {
+                return a * b;
+            }
+        }
+    }
+    throw domain_error( "answer is not found" );
+}
 
-    const bool isFirstAnswer = false;
+long long answer2( const vector<int> & numbers ) {
+    const auto n = numbers.size();
+    for ( auto i = 0; i < n; i++ ) {
+        const auto a = numbers[i];
+        for ( auto j = 0; j < i; j++ ) {
+            const auto b = numbers[j];
+            for ( auto k = 0; k < j; k++ ) {
+                const auto c = numbers[k];
+                if ( ( a + b + c ) == 2020 ) {
+                    return a * b * c;
+                }
+            }
+        }
+    }
+    throw domain_error( "answer is not found" );
+}
+
+int main() {
+    const bool isFirstAnswer = true;
 
     ifstream f("input.txt");
-
+    vector<int> numbers;
     string line;
     while (getline(f, line)) {
-        /* regex draft
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
-        }
-        */
+        numbers.push_back( stoi(line) );
     }
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
-
+    const auto answer = isFirstAnswer ? answer1(numbers) : answer2(numbers);
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " <<  answer << endl;
     return 0;
 }
