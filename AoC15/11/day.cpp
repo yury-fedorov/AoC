ae1d42210ecb0f3d01ef6a25adf09662ff114d07
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-01 20:42:13 +0100

    AoC15 Day 11 solved

---
+++ AoC15/11/day.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <sstream>
+#include <string_view>
+
+using namespace std;
+
+bool containsIncreasing3Letters(string_view pwd) {
+    const auto n = pwd.length();
+    for ( auto i = 2; i < n; i++ ) {
+        if ( ( pwd[i] - pwd[i-1] ) == 1 && ( pwd[i-1] - pwd[i-2] ) == 1 )
+          return true;
+    }
+    return false;
+}
+
+bool noIol(string_view pwd) {
+    return !any_of( begin(pwd), end(pwd), [] (char c) { return c == 'i' || c == 'o' || c == 'l'; } );
+}
+
+bool contains2Pairs(string_view pwd) {
+    auto pairs = 0;
+    char last = '?';
+    auto count = 0;
+    string s(pwd);
+    s +="$"; // to simply algorithm
+    for ( auto ch : s ) {
+        if ( ch == last ) count++;
+        else { 
+            pairs += count >= 2 ? 1 : 0;
+            last = ch;
+            count = 1;
+        }
+    }
+    return pairs >= 2;
+}
+
+bool isValidPassword(string_view pwd) {
+    return containsIncreasing3Letters(pwd) && noIol(pwd) && contains2Pairs(pwd);
+}
+
+string next(string_view pwd) {
+    const auto n = pwd.length();
+    string result(pwd);
+    auto i = n - 1;
+    while ( i >= 0 ) {
+        if ( result[i] != 'z' ) {
+            result[i]++;
+            break;
+        } else {
+            result[i--] = 'a';
+        }
+    }
+    return result;
+}
+
+int main() {
+    string input = "hepxcrrq";
+    for ( ; !isValidPassword(input); input = next(input) ) {}
+    cout << "Answer 1: " << input << endl;
+    do {
+        input = next(input);
+    } while (!isValidPassword(input));
+    cout << "Answer 2: " << input << endl;
+    return 0;
+}
