908e39bd174c5d2dbd20ec223226e822edec0c5a
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-06 06:45:13 +0100

    AoC20 Day06 solved

--- AoC20/06/day.cpp
+++ AoC20/06/day.cpp
@@ -16,22 +16,39 @@ int main() {
 
     const bool isFirstAnswer = false;
 
+    auto sumUniqueAnswers = 0;
+    set<char> uniqueInGroup;
+    bool isNewSet = true;
     ifstream f("input.txt");
-
     string line;
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
+        if ( line.empty() ) {
+            // group is over
+            sumUniqueAnswers += uniqueInGroup.size();
+            uniqueInGroup.clear();
+            isNewSet = true; // for second question
         } else {
-            cerr << "Unexpected line: " << line << endl;
+            set<char> one;
+            const bool isOriginalSet = isFirstAnswer || isNewSet;
+            set<char> & s = isOriginalSet ? uniqueInGroup : one;
+            for ( char ch : line ) {
+                s.insert(ch);
+            }
+            if (!isOriginalSet) {
+                set <char> u;
+                for ( const char ch : one ) {
+                    if ( uniqueInGroup.find(ch) != uniqueInGroup.end() ) {
+                        u.insert(ch);
+                    }
+                }
+                swap( uniqueInGroup, u );
+            }
+            isNewSet = false;
         }
-        */
     }
+    sumUniqueAnswers += uniqueInGroup.size();
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << sumUniqueAnswers << endl; // 3471 - not right 
 
     return 0;
 }
