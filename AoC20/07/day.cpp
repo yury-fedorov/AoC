89a2b5b37e4ad306a2cce0680faace2c7f7bbf3e
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-07 06:59:55 +0100

    AoC20 Day07 solved and optimized

--- AoC20/07/day.cpp
+++ AoC20/07/day.cpp
@@ -1,37 +1,32 @@
 #include <iostream>
 #include <map>
-#include <set>
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <regex>
-#include <numeric>
-#include <assert.h>
-#include <climits>
 
 using namespace std;
 
 typedef map<string, vector< pair<string,int> > > Rules;
 
 bool canContain( const Rules & rules, const string & out, const string & in ) {
-    const auto found = rules.find(out);
-    if (found == rules.end()) {
-        cerr << out << " - " << in << endl;
-        assert(false);
-    } 
-    for ( const auto & p : found->second ) {
-        if ( p.first == in ) return true;
-        if ( canContain(rules, p.first, in ) ) return true;
+    for ( const auto & p : rules.at(out) ) {
+        if ( ( p.first == in ) || canContain(rules, p.first, in ) ) return true;
     }
     return false;
 }
 
+int countIn( const Rules & rules, const string & out ) {
+    int result = 0;
+    for ( const auto & p : rules.at(out) ) {
+        result += p.second * ( 1 + countIn(rules, p.first) );
+    }
+    return result;
+}
+
 int main() {
-
-    const bool isFirstAnswer = true;
-
     ifstream f("input.txt");
+    // ifstream f("test.txt");
 
     string line;
 
@@ -61,15 +56,13 @@ int main() {
         }
     }
 
-    cout << rules.size() << endl;
-
     auto count = 0;
     const string color = "shiny gold";
     for ( auto & p : rules ) {
         count += canContain( rules, p.first, color);
     }
-
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? count : 2 ) << endl;
+    cout << "Answer 1: " << count << endl;
+    cout << "Answer 2: " << countIn(rules, color) << endl;
 
     return 0;
 }
