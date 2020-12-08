0e387c6c2392b8c33da3ac0ee9eb4c61cc88a145
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-08 06:30:27 +0100

    AoC20 Day08 parsing is ready

--- AoC20/08/day.cpp
+++ AoC20/08/day.cpp
@@ -12,25 +12,33 @@
 
 using namespace std;
 
+const vector<string> operations { "nop", "acc", "jmp" };
+enum Operation { NOP, ACC, JMP };
+typedef pair<Operation, int> Instruction;
+
 int main() {
-
-    const bool isFirstAnswer = false;
-
     ifstream f("input.txt");
-
+    regex re("^(\\w{3}) ([-+]\\d+)$");
+    smatch what;
     string line;
+    vector<Instruction> code;
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
         if( regex_match( line, what, re )) {
-            const auto & a = what[1];
+            const string op = what[1];
+            const int arg = stoi(what[2]);
+            const auto i = find( operations.cbegin(), operations.cend(), op);
+            assert( i != operations.end() );
+            code.emplace_back( (Operation)(int)(i - operations.cbegin()), arg );
         } else {
             cerr << "Unexpected line: " << line << endl;
         }
-        */
     }
 
+    for ( auto e : code ) {
+        cout << e.first << " " << e.second << endl;
+    }
+
+    const bool isFirstAnswer = true;
     cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
 
     return 0;
