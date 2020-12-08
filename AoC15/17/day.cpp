441297e09640328ee872450b030e1ffff0c4e929
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-09 00:17:08 +0100

    AoC15 Day15 not solved

--- AoC15/17/day.cpp
+++ AoC15/17/day.cpp
@@ -1,37 +1,84 @@
 #include <iostream>
 #include <map>
-#include <set>
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <sstream>
-#include <regex>
-#include <numeric>
-#include <assert.h>
-#include <climits>
 
 using namespace std;
 
+int sum( const vector<int> & v, int b ) {
+    auto result = 0;
+    for ( auto i = 0; b; b >>= 1, i++ ) {
+        result += ( b & 1 ) ? v[i] : 0;
+    }
+    return result;
+}
+
+void init( const vector<int> & v, int b, map<int,int> & m ) {
+    for ( ; b > 0; b-- ) {
+        const auto n = sum( v, b );
+        if ( n == 120 ) cout << b << endl;
+        m[n] += 1;
+    }
+}
+
+int maxCounter( int n ) {
+    return ( 1 << n ) - 1; // sets all valid bits to 1
+}
+
+auto split( const vector<int> & containers, const int target ) {
+    auto low = 0;
+    auto it = containers.cbegin();
+    for ( ; it != containers.cend(); it++ ) {
+        const auto next = low + *it;
+        if ( next >= target ) { 
+            break;
+        }
+        low = next;
+    }
+    return make_pair( it, low );
+}
+
 int main() {
+    const bool isFirstAnswer = true;
 
-    const bool isFirstAnswer = false;
-
+    vector<int> containers;
+    /* {20, 15, 10, 5, 5};
+    const int TARGET = 25; */
+    
     ifstream f("input.txt");
-
     string line;
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
-        }
-        */
+        containers.push_back( stoi(line) );
     }
+    const int TARGET = 150;
+    
+    sort( containers.begin(), containers.end() );
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
+    const auto sp = split(containers, TARGET);
+    const auto h = sp.first - containers.cbegin();
+    const auto firstHigh = *sp.first;
+    // cout << sp.second << " " << h << " " << firstHigh << endl;
+
+    map<int, int> lowMap;
+    vector<int> lowCo(containers.cbegin(), sp.first);
+    init( lowCo, maxCounter(h), lowMap );
+
+    map<int, int> highMap;
+    const vector<int> highCo( sp.first, containers.end() );
+    init( highCo, maxCounter( highCo.size() ), highMap );
+
+    long long counter = 0;
+    for ( auto cl : lowMap ) {
+        const auto ch = highMap.find( TARGET - cl.first );
+        if ( ch != highMap.end() ) {
+            cout << cl.first << " + " << ch->first << " -> " << cl.second << " * " << ch->second << " = " << (cl.second * ch->second) << endl;
+            counter += ( cl.second * ch->second );
+        }
+    }
+    // 6732 - too high
+    // 4369 - is not the right
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? counter : 2 ) << endl;
 
     return 0;
 }
