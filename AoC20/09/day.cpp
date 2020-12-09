d37c39b15cd6da3afadd0ba185923101f97067d5
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-09 07:26:23 +0100

    AoC20 Day09 solved & optimized

--- AoC20/09/day.cpp
+++ AoC20/09/day.cpp
@@ -1,37 +1,64 @@
 #include <iostream>
-#include <map>
-#include <set>
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <sstream>
-#include <regex>
-#include <numeric>
-#include <assert.h>
-#include <climits>
 
 using namespace std;
 
-int main() {
+typedef long long lint;
 
-    const bool isFirstAnswer = false;
+bool isSum( const vector<lint> & v, lint n ) {
+    auto rit = v.rbegin();
+    std::advance(rit, 25);
+    vector<lint> v1 (v.rbegin(), rit);
+    assert( v1.size() == 25 );
+    sort( v1.begin(), v1.end() );
+    const auto min = *v1.begin();
+    const auto max = *v1.rbegin();
+    for ( auto r : v1 ) {
+        const auto d = n - r;
+        if ( d <= 0 || d < min || d > max || d == r ) continue;
+        if ( binary_search( v1.begin(), v1.end(), d ) ) return true;
+    }
+    return false;
+}
+
+int main() {
+    vector<lint> numbers;
+
+    lint answer1 = 0;
 
     ifstream f("input.txt");
-
     string line;
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
+        const auto n = stoll(line);
+        if ( numbers.size() >= 25 ) {
+            if ( !isSum( numbers, n ) ) {
+                answer1 = n;
+                cout << "Answer 1: " << n << endl; // 29221323 - answer 1
+            }
         }
-        */
+        numbers.push_back(n);
     }
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
+    const auto n = numbers.size();
+    for ( int i = 0; i < n; i++ ) {
+        lint sum = answer1;
+        for ( int j = i; j < n; j++ ) {
+            sum -= numbers[j];
+            if ( sum == 0 && ( i < j ) ) { // at least 2 numbers
+                // found
+                lint mi = numbers[i];
+                lint ma = numbers[j]; // XXX - minmax and even max for some reason did not work well
+                for ( auto k = i + 1; k <= j; k++ ) {
+                    const auto a = numbers[k];
+                    mi = a < mi ? a : mi;
+                    ma = a > ma ? a : ma;
+                }
+                cout << "Answer 2: " << ( mi + ma ) << endl;
+            } else if ( sum < 0 ) break;
+        }
+    }
 
     return 0;
 }
