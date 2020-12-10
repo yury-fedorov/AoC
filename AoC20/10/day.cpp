2a07a87aabf4e778d864ddec43ce649c8c409158
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-10 07:19:32 +0100

    AoC20 Day10 optimized

--- AoC20/10/day.cpp
+++ AoC20/10/day.cpp
@@ -1,37 +1,76 @@
 #include <iostream>
 #include <map>
-#include <set>
 #include <vector>
-#include <algorithm>
 #include <fstream>
-#include <sstream>
-#include <regex>
-#include <numeric>
 #include <assert.h>
-#include <climits>
 
 using namespace std;
 
+// 3 3 - no alternative
+// 3 1 3 - no alternative
+// 3 1 1 3 - and 3 2 3 (2)
+// 3 1 1 1 3 - and 3 3 3, 3 2 1 3, 3 1 2 3 (4) 
+// 3 1 1 1 1 3 - 3 3 1 3, 3 1 3 3, 3 2 2 3, 3 1 2 1 3, 3 2 1 1 3, 3 1 1 2 3 (7)
+int options( int digit, int length ) {
+    assert( digit == 1 );
+    assert( length > 0 );
+    switch ( length ) {
+        case 1: return 1;
+        case 2: return 2;
+        case 3: return 4;
+        case 4: return 7;
+        default:
+            cerr << length << endl;
+            assert( false);
+    }
+}
+
 int main() {
-
-    const bool isFirstAnswer = false;
-
     ifstream f("input.txt");
+
+    vector<int> adapters;
 
     string line;
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
+        adapters.push_back(stoi(line));
+    }
+    const auto builtin = *max_element(adapters.begin(), adapters.end()) + 3;
+    adapters.push_back( 0 );
+    adapters.push_back( builtin );
+    const auto n = adapters.size();
+    sort(adapters.begin(), adapters.end());
+
+    map<int,int> differences;
+    vector<int> seq;
+    for ( auto i = 1; i < n; i++ ) {
+        const auto diff = adapters[i]-adapters[i-1];
+        differences[diff]++;
+        seq.push_back(diff);
+    }
+    cout << "Answer 1: " << differences[1] * differences[3] << endl;
+
+    vector<pair<int,int>> grseq;
+    int prev = 0;
+    int length = 0;
+    for ( const int se : seq ) {
+        if ( se == prev ) {
+            length++;
         } else {
-            cerr << "Unexpected line: " << line << endl;
+            if ( length > 0 ) {
+                grseq.emplace_back( prev, length );
+            }
+            prev = se;
+            length = 1;
         }
-        */
+    }
+    grseq.emplace_back( prev, length );
+
+    long long combinations = 1;
+    for ( const auto p : grseq ) {
+        combinations *= p.first == 3 ? 1 : options( p.first, p.second );
     }
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
+    cout << "Answer 2: " << combinations << endl; // right - 9256148959232
 
     return 0;
 }
