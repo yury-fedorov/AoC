087b24a903ddce0aa6149c8ea26484fad7c93ca3
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-12 18:34:15 +0100

    #10 - AoC15 Day 19: Medicine for Rudolph - Part 2 draft

--- AoC15/19/day.cpp
+++ AoC15/19/day.cpp
@@ -4,57 +4,92 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
-#include <sstream>
 #include <regex>
-#include <numeric>
 #include <assert.h>
 #include <climits>
 
 using namespace std;
 
-int main() {
-    const bool isFirstAnswer = true;
+typedef map< string, vector<string> > Rules;
+typedef map< string, string > ToFromRules;
+typedef set<string> Combinations;
 
+void addAll( const string & input, const Rules & rules, Combinations & combinations ) {
+    for ( const auto & p : rules ) {
+        const string & from = p.first;
+        const auto fromLen = from.length();
+        for ( const auto & to : p.second ) {
+            auto i = input.find( from );
+            while ( i != string::npos ) {
+                string product { input };
+                product.replace( i, fromLen, to );
+                combinations.insert( product );
+                i = input.find(from, i + 1 );
+            }
+        }
+    }
+}
+const string START { "e" };
+const int MAX_DISTANCE = 505;
+int globalMin = MAX_DISTANCE;
+int distance( const string & input, const ToFromRules & rules, int depth ) {
+    if ( input == START ) return 0;
+    if ( depth >= MAX_DISTANCE ) return MAX_DISTANCE;
+    int minLength = MAX_DISTANCE;
+    for ( auto & p : rules ) {
+        // we go back from end to the beginning
+        const string & to = p.first;
+        const string & from = p.second;
+        auto i = input.find( to );
+        const int len = to.length();
+        while ( i != string::npos ) {
+            string product { input };
+            product.replace( i, len, from );
+            if ( product.length() < input.length() ) {
+                const int d = 1 + distance( product, rules, depth + 1 );
+                if ( d < minLength ) {
+                    minLength = d;
+                    const int localMin = depth + d;
+                    if ( localMin < globalMin ) {
+                        globalMin = localMin;
+                        cout << globalMin << endl;
+                    }
+                }
+            }
+            i = input.find(to, i + 1 );
+        }
+    }
+    return minLength;
+}
+
+int main() {
     ifstream f("input.txt");
 
-    map< string, vector<string> > rules;
+    Rules rules;
+    map<string,string> toFrom;
+    string input;
+
     regex reRule("^(\\w+) => (\\w+)$");
     smatch what;
     string line;
-
-    string input;
-
     while (getline(f, line)) {
         if( regex_match( line, what, reRule )) {
             const string from = what[1];
             const string to = what[2];
             rules[from].push_back(to);
+            toFrom.insert( make_pair( to, from ) );
         } else if ( !line.empty() ) {
             assert(input.empty());
             input = line;
         }
     }
 
-    cout << rules.size() << endl;
+    Combinations combinations;
+    addAll( input, rules, combinations );
+    cout << "Answer 1: " << combinations.size() << endl; // 535
 
-    set<string> replacements;
-    for ( const auto & p : rules ) {
-        const string & from = p.first;
-        cout << from << " -> " << endl;
-        const auto fromLen = from.length();
-        for ( const auto & to : p.second ) {
-            cout << to << endl;
-            auto i = input.find( from );
-            while ( i != string::npos ) {
-                string product { input };
-                product.replace( i, fromLen, to );
-                replacements.insert( product );
-                i = input.find(from, i + 1 );
-            }
-        }
-    }
+    cout << "Answer 2: " << distance(input, toFrom, 0 ) << endl;
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? replacements.size() : 2 ) << endl;
 
     return 0;
 }
