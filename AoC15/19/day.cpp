610e0b70bcd3511ba8fe6940f17dd4fa949665f7
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-12 08:07:26 +0100

    AoC15 Day19 Part 1 solved

--- AoC15/19/day.cpp
+++ AoC15/19/day.cpp
@@ -13,25 +13,48 @@
 using namespace std;
 
 int main() {
-
-    const bool isFirstAnswer = false;
+    const bool isFirstAnswer = true;
 
     ifstream f("input.txt");
 
+    map< string, vector<string> > rules;
+    regex reRule("^(\\w+) => (\\w+)$");
+    smatch what;
     string line;
+
+    string input;
+
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
+        if( regex_match( line, what, reRule )) {
+            const string from = what[1];
+            const string to = what[2];
+            rules[from].push_back(to);
+        } else if ( !line.empty() ) {
+            assert(input.empty());
+            input = line;
         }
-        */
     }
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
+    cout << rules.size() << endl;
+
+    set<string> replacements;
+    for ( const auto & p : rules ) {
+        const string & from = p.first;
+        cout << from << " -> " << endl;
+        const auto fromLen = from.length();
+        for ( const auto & to : p.second ) {
+            cout << to << endl;
+            auto i = input.find( from );
+            while ( i != string::npos ) {
+                string product { input };
+                product.replace( i, fromLen, to );
+                replacements.insert( product );
+                i = input.find(from, i + 1 );
+            }
+        }
+    }
+
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? replacements.size() : 2 ) << endl;
 
     return 0;
 }
