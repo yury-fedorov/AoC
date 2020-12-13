66f7407791f5f882f4142ca3adac1b63f3348b35
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-13 06:41:53 +0100

    AoC20 Day13 Part1 solved

--- AoC20/13/day.cpp
+++ AoC20/13/day.cpp
@@ -17,19 +17,36 @@ int main() {
     const bool isFirstAnswer = false;
 
     ifstream f("input.txt");
-
     string line;
-    while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
-        }
-        */
+    f >> line;
+    const int t0 = stoi(line);
+    cout << "t0="  << t0 << endl;
+    f >> line;
+    vector<int> buses;
+    regex re("(\\d+)");
+    smatch res;
+    string::const_iterator searchStart( line.cbegin() );
+    while ( regex_search( searchStart, line.cend(), res, re ) )
+    {
+        const int busId = stoi(res[1]);
+        buses.push_back( busId );
+        searchStart = res.suffix().first;
+        cout << busId << endl;
     }
+    cout << "CALC" << endl;
+    int bestBus = 0;
+    int bestTime = INT_MAX;
+    for ( const int b : buses ) {
+        const int wait = b - (t0 % b);
+        cout << "Bus " << b << " " << wait << endl;
+        if (bestTime > wait) {
+            cout << "Better" << endl;
+            bestTime = wait;
+            bestBus = b;
+        }
+    }
+
+    cout << bestBus << ',' << bestTime << '=' << bestBus * bestTime << endl; // 296 - answer 1
 
     cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
 
