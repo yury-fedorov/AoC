a5f53a632bbeb68c64a8cbb4ce66b84d945737a7
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-14 06:43:36 +0100

    AoC20 Day14 Part1 solved

--- AoC20/14/day.cpp
+++ AoC20/14/day.cpp
@@ -12,6 +12,20 @@
 
 using namespace std;
 
+typedef unsigned long long Int;
+
+Int applyValue( const string & mask, Int v ) {
+    const int n = mask.size();
+    bitset<36> result( v );
+    for ( int i = 1; i <= n; i++ ) {
+        const char mv = mask[n - i];
+        if ( mv != 'X') {
+            result.set( i - 1, mv == '1' );
+        }
+    }
+    return result.to_ullong();
+}
+
 int main() {
 
     const bool isFirstAnswer = false;
@@ -19,17 +33,40 @@ int main() {
     ifstream f("input.txt");
 
     string line;
+    // mask = 1000XX0X0X0X0011XX11110110X101101X01
+    // mem[17353] = 91550
+    const regex reMask("^mask = ([10X]+)$");
+    const regex reMem("^mem\\[(\\d+)\\] = (\\d+)$");
+    smatch what;
+
+    string mask;
+    map<Int,Int> mem;
+
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
+        const bool isMask = line[1] == 'a';
+        if (isMask) {
+            if( regex_match( line, what, reMask )) {
+                mask = what[1];
+            } else {
+                cerr << "Unexpected line: " << line << endl;
+            }
         } else {
-            cerr << "Unexpected line: " << line << endl;
+            if( regex_match( line, what, reMem )) {
+                const string a = what[1];
+                const string v = what[2];
+                const auto mv = applyValue( mask, stoull(v) );
+                mem[stoull(a)] = mv;
+            } else {
+                cerr << "Unexpected line: " << line << endl;
+            }
         }
-        */
     }
+    Int sum = 0;
+    for ( const auto [ a, v ] : mem ) {
+        sum += v;
+    }
+    cout << "Answer 1: " << sum << endl;
+    assert( sum == 13105044880745 );
 
     cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
 
