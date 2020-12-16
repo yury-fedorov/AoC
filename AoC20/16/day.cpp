ba70be85a6829825f9c574e13584e3a2ecdaf85c
Yury Fedorov <yury.fedorov@gmail.com>
2020-12-16 06:56:38 +0100

    AoC20 Day16 parsing input data written

--- AoC20/16/day.cpp
+++ AoC20/16/day.cpp
@@ -9,26 +9,66 @@
 #include <numeric>
 #include <assert.h>
 #include <climits>
+#include <optional>
 
 using namespace std;
 
-int main() {
+typedef vector<int> Ticket;
+typedef vector<Ticket> Tickets;
+typedef pair<int, int> Range;
+typedef vector<Range> Rule;
+typedef map<string, Rule> Rules;
 
+smatch parse( const string & line, const regex & re ) {
+    smatch what;
+    if( regex_match( line, what, re )) {
+        return what;
+    } 
+    cerr << "Unexpected line: " << line << endl;
+    throw domain_error("Unexpected format");
+}
+
+vector<int> parseList( const string & line, const regex & re ) {
+    vector<int> result;
+    smatch res;
+    string::const_iterator searchStart( line.cbegin() );
+    while ( regex_search( searchStart, line.cend(), res, re ) ) {
+        const auto n = stoi(res[1]);
+        result.push_back( n );
+        searchStart = res.suffix().first;
+    }
+    return result;
+} 
+
+int main() {
     const bool isFirstAnswer = false;
 
-    ifstream f("input.txt");
+    Rules rules;
 
+    ifstream f("input");
     string line;
-    while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
-        }
-        */
+
+    const regex reRule("^([a-z ]+): (\\d+)-(\\d+) or (\\d+)-(\\d+)");
+    while (getline(f, line) && !line.empty() ) {
+        auto && m = parse(line, reRule);
+        const Rule rule = { { stoi(m[2]), stoi(m[3]) }, { stoi(m[4]), stoi(m[5]) } };
+        rules.insert( make_pair( m[1], rule ) );
+    }
+    getline(f, line);
+    assert(line == "your ticket:"); 
+    const regex reTicket("(\\d+)");
+    getline(f, line);
+    const Ticket && myTicket = parseList(line, reTicket);
+    
+    getline(f, line);
+    assert( line.empty() );
+
+    getline(f, line);
+    assert( line == "nearby tickets:" );
+    Tickets tickets;
+    while (getline(f, line) && !line.empty() ) {
+        Ticket && ticket = parseList(line, reTicket);
+        tickets.push_back( ticket );
     }
 
     cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
