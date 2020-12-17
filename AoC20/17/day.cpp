d65961de883f4646b5e25c56ed7d1dcf71ccee99
Yury Fedorov <yury.fedorov@gmail.com>
2020-12-17 07:49:04 +0100

    AoC20 Day17 solved and optimized

--- AoC20/17/day.cpp
+++ AoC20/17/day.cpp
@@ -1,26 +1,24 @@
 #include <iostream>
-#include <map>
 #include <set>
-#include <vector>
-#include <algorithm>
 #include <fstream>
-#include <sstream>
-#include <regex>
-#include <numeric>
 #include <assert.h>
-#include <climits>
+#include <tuple>
 
 using namespace std;
 
-typedef tuple<int,int,int> Point;
+typedef tuple<int,int,int,int> Point;
 typedef set<Point> Config; 
+
+const bool isFirstAnswer = true;
 
 inline int & gx( Point & p ) { return get<0>(p); }
 inline int & gy( Point & p ) { return get<1>(p); }
 inline int & gz( Point & p ) { return get<2>(p); }
+inline int & gw( Point & p ) { return get<3>(p); }
 inline int gx( const Point & p ) { return get<0>(p); }
 inline int gy( const Point & p ) { return get<1>(p); }
 inline int gz( const Point & p ) { return get<2>(p); }
+inline int gw( const Point & p ) { return get<3>(p); }
 
 inline bool isOn( const Config & c, const Point & p ) { return c.find( p ) != c.end(); }
 
@@ -29,28 +27,27 @@ int countOn( const Config & c, const Point & p ) {
     for ( int x = gx(p) - 1; x <= gx(p) + 1; x++ ) {
         for ( int y = gy(p) - 1; y <= gy(p) + 1; y++ ) {
             for ( int z = gz(p) - 1; z <= gz(p) + 1; z++ ) {
-                const Point p1 {x, y, z};
-                if ( p == p1 ) continue;
-                count += isOn(c, p1);
+                for ( int w = gw(p) - 1; w <= gw(p) + 1; w++ ) {
+                    const Point p1 {x, y, z, w};
+                    if ( p == p1 ) continue;
+                    count += isOn(c, p1);
+                }
             }
         }
     }
-    assert(count <= 26);
+    assert(count <= ( isFirstAnswer ? 26 : 80 ) );
     return count;
 }
 
 int main() {
-
     Config configOn;
-
-    const bool isFirstAnswer = true;
 
     ifstream f("input");
     string line;
     for ( int y = 0; getline(f, line); y++ ) {
         for ( int x = line.length(); --x >= 0; ) {
             const char ch = line[x];
-            if ( ch == '#' ) configOn.insert( { x, y, 0 } );
+            if ( ch == '#' ) configOn.insert( { x, y, 0, 0 } );
         }
     }
 
@@ -62,33 +59,40 @@ int main() {
             gx(mi) = min( gx(mi), gx(p) );
             gy(mi) = min( gy(mi), gy(p) );
             gz(mi) = min( gz(mi), gz(p) );
+            gw(mi) = min( gw(mi), gw(p) );
 
             gx(ma) = max( gx(ma), gx(p) );
             gy(ma) = max( gy(ma), gy(p) );
             gz(ma) = max( gz(ma), gz(p) );
+            gw(ma) = max( gw(ma), gw(p) );
         }
+        const int D = 1;
+        const int wmi = isFirstAnswer ? 0 : gw(mi) - D;
+        const int wma = isFirstAnswer ? 0 : gw(ma) + D;
 
         Config newConfig {};
-        for ( int x = gx(mi) - 2; x <= gx(ma) + 2; x++ ) {
-            for ( int y = gy(mi) - 2; y <= gy(ma) + 2; y++ ) {
-                for ( int z = gz(mi) - 2; z <= gz(ma) + 2; z++ ) {
-                    const Point p { x, y, z };
-                    const int count = countOn( configOn, p );
-                    bool on = isOn( configOn, p );
-                    // if ( on ) cout << gx(p) << ',' << gy(p) << ',' << gz(p) << " -> " << count << endl;
-                    if ( on ) {
-                        on = count == 2 || count == 3;
-                    } else {
-                        on = count == 3;
+        for ( int x = gx(mi) - D; x <= gx(ma) + D; x++ ) {
+            for ( int y = gy(mi) - D; y <= gy(ma) + D; y++ ) {
+                for ( int z = gz(mi) - D; z <= gz(ma) + D; z++ ) {
+                    for ( int w = wmi; w <= wma; w++ ) {
+                        const Point p { x, y, z, w };
+                        const int count = countOn( configOn, p );
+                        bool on = isOn( configOn, p );
+                        // if ( on ) cout << gx(p) << ',' << gy(p) << ',' << gz(p) << " -> " << count << endl;
+                        if ( on ) {
+                            on = count == 2 || count == 3;
+                        } else {
+                            on = count == 3;
+                        }
+                        if ( on ) newConfig.insert(p);
                     }
-                    if ( on ) newConfig.insert(p);
                 }
             }
         }
         configOn = newConfig;
     }
-    // 249 - too low
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? configOn.size() : 2 ) << endl;
-
+    const auto answer = configOn.size();
+    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << answer << endl;
+    assert( answer == ( isFirstAnswer ? 286 : 960 ) );
     return 0;
 }
