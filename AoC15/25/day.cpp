343a290ee4a2d373d7de8f3e64c278c6259cb2c3
Yury Fedorov <yury.fedorov@gmail.com>
2020-12-18 10:42:54 +0100

    AoC15 Day25 solved and optimized

--- AoC15/25/day.cpp
+++ AoC15/25/day.cpp
@@ -1,37 +1,24 @@
 #include <iostream>
-#include <map>
-#include <set>
-#include <vector>
-#include <algorithm>
-#include <fstream>
-#include <sstream>
-#include <regex>
-#include <numeric>
 #include <assert.h>
-#include <climits>
 
 using namespace std;
 
+typedef long long Int;
+typedef pair<int,int> Position;
+
+inline Int next( Int value ) { return ( value * 252533 ) % 33554393; }
+inline Position next( const Position & p ) { 
+    return ( p.second == 1 ) ? Position { 1,  p.first + 1 } : Position { p.first + 1, p.second -1 };
+};
+
 int main() {
-
-    const bool isFirstAnswer = false;
-
-    ifstream f("input.txt");
-
-    string line;
-    while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
-        }
-        */
+    const Position target { 3075, 2981 }; // row 2981, column 3075
+    Int value { 20151125 };
+    for ( Position position { 1, 1 }; position != target; position = next(position) ) {
+        value = next(value);
+        assert( value >= 0 );
     }
-
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
-
+    cout << "Answer 1: " << value << endl;
+    assert( value == 9132360 );
     return 0;
 }
