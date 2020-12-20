6d41b6095d90c982e695b13953dadb38660f8459
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-20 15:25:48 +0100

    AoC20 Day20 good reverse, good order of borders

--- AoC20/20/day.cpp
+++ AoC20/20/day.cpp
@@ -12,8 +12,33 @@
 
 using namespace std;
 
+enum Direction { UP, RIGHT, DOWN, LEFT }; // clockwise rotation 
+
 typedef vector<string> Tile;
 typedef map<int, Tile> Tiles;
+typedef string Border;
+typedef vector<Border> Borders;
+typedef map<int, Borders> TileBordersMap;
+
+Border reverse( const Border & b ) {
+    Border result(b);
+    reverse( result.begin(), result.end() );
+    return result;
+}
+
+auto minmax( const Border & b ) { return minmax(b, reverse(b)); } 
+
+Borders borders( const Tile & tile ) {
+    Borders result { tile[0], "", reverse( *tile.rbegin() ), "" };
+    string right, left;
+    for ( const auto & line : tile ) {
+        right +=  *line.rbegin();
+        left += line[0];
+    }
+    result[RIGHT] = right;
+    result[LEFT] = reverse( left );
+    return result;
+}
 
 int main() {
     const bool isFirstAnswer = false;
@@ -40,6 +65,24 @@ int main() {
     }
 
     cout << tiles.size() << endl;
+
+    TileBordersMap borderMap;
+    set<Border> allBoarders;
+    for ( const auto [ id, tile ] : tiles ) {
+        const Borders && bs = borders( tile );
+        borderMap.emplace( id, bs );
+        cout << id << endl;
+        for ( const auto & b : bs ) {
+            /*
+            allBoarders.emplace( b );
+            allBoarders.emplace( reverse(b));
+            */
+           // allBoarders.emplace( minmax(b).first );
+           cout << b << endl;
+        }
+    }
+    cout << allBoarders.size() << endl;
+
 
     cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
 
