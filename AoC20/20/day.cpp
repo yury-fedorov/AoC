1313f4bc5881fbbe7acd360a9badf6c31841da85
FEDOROV Yury <fedoy@chzrhmac030.local>
2020-12-21 01:41:02 +0100

    AoC20 Day20 Part2 - first corner and two sides

--- AoC20/20/day.cpp
+++ AoC20/20/day.cpp
@@ -14,11 +14,16 @@ using namespace std;
 
 enum Direction { UP, RIGHT, DOWN, LEFT }; // clockwise rotation 
 
+const int SIDE = 10; // assumption that it is a square
+
 typedef vector<string> Tile;
 typedef map<int, Tile> Tiles;
 typedef string Border;
 typedef vector<Border> Borders;
 typedef map<int, Borders> TileBordersMap;
+typedef pair<int,int> Point;
+typedef pair<Border,Border> TwoBorders;
+typedef map<Point,int> Image;
 
 Border reverse( const Border & b ) {
     Border result(b);
@@ -28,29 +33,37 @@ Border reverse( const Border & b ) {
 
 inline Direction opposite( Direction d ) { return (Direction)( ( d + 2 ) % 4 ); }
 
-pair<Border,Border> minmax( const Border & b ) { return minmax(b, reverse(b)); } 
+TwoBorders minmax( const Border & b ) { return minmax(b, reverse(b)); } 
 
-Borders borders( const Tile & tile ) {
-    Borders result { tile[0], "", reverse( *tile.rbegin() ), "" };
-    string right, left;
-    for ( const auto & line : tile ) {
-        right +=  *line.rbegin();
-        left += line[0];
+Border border( const Tile & tile, Direction d ) {
+    switch (d) {
+        case UP: return tile[0];
+        case DOWN: return reverse( *tile.rbegin() );
+        case RIGHT: 
+        case LEFT: {
+            Border border;
+            for ( const auto & line : tile ) {
+                border += ( d == RIGHT) ? *line.rbegin() : line[0];
+            }
+            return ( d == RIGHT ) ? border : reverse( border );
+        }
+        default: assert(false);
     }
-    result[RIGHT] = right;
-    result[LEFT] = reverse( left );
-    return result;
 }
 
-optional< pair<Border,Border> > connection( const Tiles & tiles, int tile1, int tile2 ) {
+Borders borders( const Tile & t ) { return { border(t,UP), border(t,RIGHT), border(t,DOWN), border(t,LEFT) }; }
+
+Border border( const Tiles & tiles, int id, Direction d ) { return border( tiles.at(id), d ); }
+
+optional< TwoBorders > connection( const Tiles & tiles, int tile1, int tile2 ) {
     const Tile & t1 = tiles.at(tile1);
     const Tile & t2 = tiles.at(tile2);
     const Borders && b1 = borders(t1);
     const Borders && b2 = borders(t2);
     for ( const Border & b1i : b1 ) {
         for ( const Border & b2i : b2 ) {
-            if ( b1i == reverse(b2i) ) {
-                return make_pair(b1i, b2i);
+            if ( b1i == b2i || b1i == reverse(b2i) ) {
+                return TwoBorders { b1i, b2i };
             }
         }
     }
@@ -64,14 +77,8 @@ Direction where( const Tiles & tiles, int id, const Border & b ) {
     return (Direction)( i - bs.cbegin() );
 }
 
-inline Border border( const Tiles & tiles, int id, Direction d ) {
-    return borders( tiles.at(id) ).at(d);
-}
-
-int main() {
-
+Tiles init() {
     Tiles tiles;
-
     ifstream f("input.txt");
     regex re("^Tile (\\d+):$");
     string line;
@@ -90,10 +97,58 @@ int main() {
             cerr << "Unexpected line: " << line << endl;
         }
     }
+    return tiles;
+}
 
+// ideal is one result - this one is faster than connection
+vector< pair<int,Border> > next( const Tiles & tiles, int tile, const vector<int> & scope, Border border ) {
+    vector< pair<int,Border> > result;
+    const auto && br = reverse(border);
+    for ( const int tile2 : scope ) {
+        if ( tile2 == tile ) continue;
+        const auto & t2 = tiles.at(tile2);
+        const Borders && b2 = borders(t2);
+        for ( const Border & b2i : b2 ) {
+            if ( br == b2i ) {
+                result.emplace_back( tile2, b2i );
+                break;
+            }
+        }
+    }
+    return result;
+}
+
+void initSide( Point p, int & value(Point & p), const Tiles & tiles, const vector<int> & scope,
+        Image & image ) {
+    for ( ; value(p) <= SIDE; ) {
+        const int t1 = image.at( p );
+        Point p0(p);
+        value(p0) -= 1;
+        const int t0 = image.at( p0 );
+        const auto & op = connection(tiles, t0, t1 );
+        assert( op.has_value() );
+        const auto [ b1, b2 ] = op.value();
+        const auto d2 = where( tiles, t1, b2 );
+        const auto d2o = opposite(d2); // good for going through side but not for corner
+        const auto b2o = border( tiles, t1, d2o );
+        cout << "Searching " << t1 << " " << b2o << endl;
+        auto && close = next( tiles, t1, scope, b2o );
+        if ( close.empty() ) close = next( tiles, t1, scope, reverse(b2o) );
+        const int t2 = close.cbegin()->first;
+        value(p) += 1;
+        cout << t2 << " " << p.first << "-" << p.second << endl;
+        image.emplace( p, t2 );
+    }
+}
+
+int & x( Point & p ) { return p.first; }
+int & y( Point & p ) { return p.second; }
+
+int main() {
+    Tiles && tiles = init();
     cout << tiles.size() << endl;
 
-    // TileBordersMap borderMap;
+    // normalized border to tile IDs
     map< Border, set<int> > borderIdsMap;
     for ( const auto [ id, tile ] : tiles ) {
         const Borders && bs = borders( tile );
@@ -105,6 +160,7 @@ int main() {
     }
     cout << borderIdsMap.size() << endl;
 
+    // unique tile ID for border (sign of outer side)
     map<int, int> idCount;
     for ( const auto [ b, ids ] : borderIdsMap ) {
         if ( ids.size() == 1 ) { 
@@ -114,8 +170,9 @@ int main() {
         }
     }
     unsigned long long answer1 = 1;
-    vector<int> corners;
-    vector<int> sides;
+    vector<int> corners; // 4 corners
+    vector<int> sides; // all sides (tile IDs) with one border out
+    vector<int> sidesAndCorners;
     for ( const auto [ id, count ] : idCount ) {
         if ( count == 1 ) { sides.push_back(id); }
         else if ( count == 2 ) {
@@ -123,6 +180,7 @@ int main() {
             answer1 *= id;
             corners.push_back(id);
         } else assert(false);
+        sidesAndCorners.push_back(id);
     }
 
     cout << "Answer 1: " << answer1 << endl;
@@ -132,21 +190,23 @@ int main() {
     assert( corners.size() == 4 );
     assert( sides.size() == 40 );
 
-    const int SIDE = 10; // assumption that it is a square
-
-    const int firstCorner = corners[0];
-    for ( const int sideId : sides ) {
-        const auto & op = connection(tiles, firstCorner, sideId );
-        if ( op.has_value() ) {
-            const auto [ b1, b2 ] = op.value();
-            const auto d2 = where( tiles, sideId, b2 );
-            const auto d2o = opposite(d2);
-            const auto b2o = border( tiles, sideId, d2o );
-            cout << b1 << " " << firstCorner << " " << sideId << " " 
-                << where( tiles, firstCorner, b1 ) << " " 
-                << d2 << " " << d2o << " " << b2o << endl;
-        }
+    Image image;
+    const int corner0 = corners[0];
+    image.emplace( Point{0,0}, corner0 );
+    const auto && corner0borders = borders( tiles.at(corner0) );
+    for ( const auto & b : corner0borders ) {
+        const auto && close = next( tiles, corner0, sides, b );
+        if ( close.size() == 1 ) {
+            const int sideId = close.cbegin()->first;
+            cout << sideId << " " << b << endl;
+            if ( image.size() == 1 ) image.emplace( Point{1,0}, sideId );
+            else image.emplace( Point{0,1}, sideId );
+        } 
     }
+
+    initSide( Point {1,0}, x, tiles, sidesAndCorners, image );
+    initSide( Point {0,1}, y, tiles, sidesAndCorners, image );
+    // till now we defined: first corner and its neighbour sides
 
     return 0;
 }
