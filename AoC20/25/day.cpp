5d2958ce8deec9690734b81b9fcca81036e4d8c8
Yury Fedorov <yury.fedorov@gmail.com>
2020-12-25 06:42:50 +0100

    AoC20 Day25 solved

--- AoC20/25/day.cpp
+++ AoC20/25/day.cpp
@@ -12,26 +12,52 @@
 
 using namespace std;
 
-int main() {
+int loops( const int pk ) {
+    const int subjectNumber = 7;
+    int value = 1;
+    for ( int loop = 0; true; loop++ ) {
+        if ( value == pk ) return loop;
+        value *= subjectNumber;
+        value %= 20201227;
+    }
+    return -1;
+}
 
-    const bool isFirstAnswer = false;
+long encryptionKey( const int subjectNumber, const int loops ) {
+    long value = 1;
+    for ( int loop = 0; loop < loops; loop++ ) {
+        value *= subjectNumber;
+        value %= 20201227;
+    }
+    return value;
+}
+
+int main() {
+    vector<int> input;
 
     ifstream f("input.txt");
-
     string line;
     while (getline(f, line)) {
-        /* regex draft
-        regex re("(\\d+)-(\\d+) ([a-z]): ([a-z]+)");
-        smatch what;
-        if( regex_match( line, what, re )) {
-            const auto & a = what[1];
-        } else {
-            cerr << "Unexpected line: " << line << endl;
-        }
-        */
+        input.push_back( stoi(line) );
+        cout << stoi(line) << endl;
     }
 
-    cout << "Answer " << ( isFirstAnswer ? 1 : 2 ) << ": " << ( isFirstAnswer ? 1 : 2 ) << endl;
+    // const int pk1 = 5764801; // input[0];
+    const int pk1 = input[0];
+    const int l1 = loops( pk1 );
+
+    const int pk2 = input[1];
+    const int l2 = loops( pk2 );
+
+    cout << "8? -> " << loops( 5764801 ) << endl;
+    cout << "11? -> " << loops( 17807724 ) << endl;
+    cout << l1 << endl;
+    cout << l2 << endl;
+    cout << encryptionKey(17807724, 8) << endl;
+    cout << encryptionKey(pk1, l2) << endl;
+    cout << encryptionKey(pk2, l1) << endl;
+
+    cout << "Answer 1: " << encryptionKey(pk1, l2) << endl;
 
     return 0;
 }
