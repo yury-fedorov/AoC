6c856c361d7cbe0738ccaa15feeab2eaca0e1497
Yury Fedorov <yury.fedorov@gmail.com>
2020-12-28 14:48:45 +0100

    AoC16 Day04 Part1 refactored

--- AoC16/src/test/java/aoc16/d04/Day04Test.java
+++ AoC16/src/test/java/aoc16/d04/Day04Test.java
@@ -1,5 +1,6 @@
 package aoc16.d03;
 
+import org.javatuples.Triplet;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -13,7 +14,7 @@ import static org.junit.Assert.assertTrue;
 
 public class Day04Test {
 
-    static int validRoomSectorId( String line ) {
+    static Triplet<String, Integer, String> parse( String line ) {
         final var patternString = "([a-z\\-]+)(\\d+)\\[([a-z]+)\\]$";
         final var pattern = Pattern.compile(patternString);
         final var matcher = pattern.matcher(line);
@@ -21,38 +22,44 @@ public class Day04Test {
             final var p1 = matcher.group(1);
             final var p2 = Integer.parseInt( matcher.group(2) );
             final var p3 = matcher.group(3);
-            final var frequency = new HashMap<Character,Integer>();
-            for ( final var ch : p1.toCharArray() ) {
-                if ( ch == '-' ) continue;
-                final var counter = frequency.getOrDefault( ch, 0 );
-                frequency.put(ch, counter + 1 );
-            }
-            int lastFrequency = p1.length() + 1;
-            for ( final var ch : p3.toCharArray() ) {
-                final var f = frequency.getOrDefault( ch, 0 );
-                if ( ( f == 0 ) || ( f > lastFrequency ) ) return 0;
-                frequency.remove(ch);
-                lastFrequency = Math.min( f, lastFrequency );
-            }
-            for ( final var p : frequency.entrySet() ) {
-                if ( p.getValue() > lastFrequency ) return 0;
-            }
-            // TODO
-            // System.out.println("found: " + p1 + " -> " + p2 +" -> " + p3 );
-            return p2;
+            return Triplet.with( p1, p2, p3 );
         }
         System.out.println(line);
         throw new IllegalArgumentException();
     }
+
+    static int validRoomSectorId( Triplet<String,Integer,String> p ) {
+        final var p1 = p.getValue0();
+        final var p2 = p.getValue1();
+        final var p3 = p.getValue2();
+        final var frequency = new HashMap<Character,Integer>();
+        for ( final var ch : p1.toCharArray() ) {
+            if ( ch == '-' ) continue;
+            final var counter = frequency.getOrDefault( ch, 0 );
+            frequency.put(ch, counter + 1 );
+        }
+        int lastFrequency = p1.length() + 1;
+        for ( final var ch : p3.toCharArray() ) {
+            final var f = frequency.getOrDefault( ch, 0 );
+            if ( ( f == 0 ) || ( f > lastFrequency ) ) return 0;
+            frequency.remove(ch);
+            lastFrequency = Math.min( f, lastFrequency );
+        }
+        for ( final var e : frequency.entrySet() ) {
+            if ( e.getValue() > lastFrequency ) return 0;
+        }
+        return p2;
+    }
     
     @Test
     public void samples() {
+
         // is a real room because the most common letters are a (5), b (3), and then a tie between x, y, and z, which are listed alphabetically.
-        assertEquals("sample 1", 123, validRoomSectorId("aaaaa-bbb-z-y-x-123[abxyz]") );
+        assertEquals("sample 1", 123, validRoomSectorId(parse( "aaaaa-bbb-z-y-x-123[abxyz]") ) );
         // is a real room because although the letters are all tied (1 of each), the first five are listed alphabetically.
-        assertEquals("sample 2", 987, validRoomSectorId("a-b-c-d-e-f-g-h-987[abcde]") );
-        assertEquals("sample 3", 404, validRoomSectorId("not-a-real-room-404[oarel]") );
-        assertEquals("sample 4", 0,   validRoomSectorId("totally-real-room-200[decoy]") );
+        assertEquals("sample 2", 987, validRoomSectorId(parse( "a-b-c-d-e-f-g-h-987[abcde]") ) );
+        assertEquals("sample 3", 404, validRoomSectorId(parse("not-a-real-room-404[oarel]") ) );
+        assertEquals("sample 4", 0,   validRoomSectorId(parse("totally-real-room-200[decoy]") ) );
     }
 
     @Test
@@ -61,7 +68,8 @@ public class Day04Test {
         final var input = Files.readAllLines( Path.of(url.getPath()) );
         long answer1 = 0;
         for ( final var l : input ) {
-            answer1 += validRoomSectorId(l);
+            final var p = parse(l);
+            answer1 += validRoomSectorId(p);
         }
         assertEquals("answer 1", 278221, answer1 );
         // assertEquals("answer 2", 1921, answer2 );
