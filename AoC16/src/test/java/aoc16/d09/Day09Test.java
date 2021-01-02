b752c25babb1a0249ef019220322377721fdf2fe
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-02 17:27:41 +0100

    AoC16 Day09 Part02 attempt failed - need run tests

--- AoC16/src/test/java/aoc16/d09/Day09Test.java
+++ AoC16/src/test/java/aoc16/d09/Day09Test.java
@@ -10,11 +10,42 @@ import java.util.regex.Pattern;
 
 public class Day09Test {
 
+    final static Pattern patternInstruction = Pattern.compile("^(\\d+)x(\\d+)$");
+
+    static long decodedLength( CharSequence s ) {
+        long result = 0;
+        boolean isInstruction = false;
+        final var instruction = new StringBuilder();
+
+        for ( int i = 0; i < s.length(); ) {
+            final char ch = s.charAt(i);
+            if ( ch == '(' ) {
+                // starts instruction
+                assertEquals( "instruction == false", false, isInstruction );
+                isInstruction = true;
+                instruction.setLength(0);
+                i++;
+            } else if ( ch == ')' ) {
+                // ends instruction
+                isInstruction = false;
+                final var matcher = patternInstruction.matcher( instruction );
+                assertTrue( "matcher.find()", matcher.find() );
+                final var length = Integer.parseInt( matcher.group(1) );
+                var repeated = Integer.parseInt( matcher.group(2) );
+                result += length * (repeated - 1);
+                i++;
+            } else {
+                if ( isInstruction ) instruction.append(ch);
+                else result++;
+                i++;
+            }
+        }
+        return result;
+    }
     static CharSequence decode( CharSequence s ) {
         final var out = new StringBuilder();
         boolean isInstruction = false;
         final var instruction = new StringBuilder();
-        final var patternInstruction = Pattern.compile("^(\\d+)x(\\d+)$");
 
         for ( int i = 0; i < s.length(); ) {
             final char ch = s.charAt(i);
@@ -59,9 +90,13 @@ public class Day09Test {
         final var url = getClass().getClassLoader().getResource( "d09/input.txt" );
         final var input = Files.readAllLines( Path.of(url.getPath()) );
         int answer1 = 0;
+        long answer2 = 0;
         for ( final var l : input ) {
             answer1 += decode( l ).length();
+            answer2 += decodedLength(l);
+            assertTrue( "answer2 > 0", answer2 > 0 );
         }
-        assertEquals("answer 1", -1, answer1 );
+        assertEquals("answer 1", 150914, answer1 );
+        assertEquals("answer 2", -1, answer2 ); // too low - 501035
     }
 }
