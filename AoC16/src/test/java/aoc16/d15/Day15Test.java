461658e7478dfbae96cadf8e2e67d4bff6824c27
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-07 23:43:57 +0100

    AoC16 Day15 start

---
+++ AoC16/src/test/java/aoc16/d15/Day15Test.java
@@ -0,0 +1,13 @@
+package aoc16.d15;
+
+import aoc16.common.IOUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+public class Day15Test {
+    @Test
+    public void solution() throws IOException {
+        final var input = IOUtil.input("d15");
+    }
+}
