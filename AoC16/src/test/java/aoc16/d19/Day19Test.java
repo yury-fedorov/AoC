e495b55ed2b092ec0302fc5b77a00485473fac68
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-08 15:44:54 +0100

    AoC16 Day19 Part2 second version finishes in 1 h 8 minutes but not correct

--- AoC16/src/test/java/aoc16/d19/Day19Test.java
+++ AoC16/src/test/java/aoc16/d19/Day19Test.java
@@ -1,9 +1,12 @@
 package aoc16.d19;
 
+import aoc16.common.Config;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.ListIterator;
 
 public class Day19Test {
@@ -37,6 +40,19 @@ public class Day19Test {
         }
     }
 
+    static int detectWinner21( final int initialSize ) {
+        final List<Integer> list = new ArrayList<>( init(initialSize) );
+        for (  int position = 0; true; ) {
+            final int n = list.size();
+            if ( n == 1 ) return list.get(0);
+            if ( ( n % 10000 ) == 0 ) System.out.println( n );
+            final int half = ( n >> 1 ); // divide by 2
+            final int outIndex = ( position + half ) % n;
+            list.remove( outIndex );
+            if ( outIndex > position ) position++;
+        }
+    }
+
     private static LinkedList<Integer> init(int n) {
         final var list = new LinkedList<Integer>();
         for (int i = 1; i <= n; i++ ) {
@@ -51,6 +67,7 @@ public class Day19Test {
         Assert.assertEquals( "test 1", 3, detectWinner(5));
         Assert.assertEquals( "answer 1", 1842613, detectWinner(INPUT));
         Assert.assertEquals( "test 2", 2, detectWinner2(5));
-        Assert.assertEquals( "answer 2", -1, detectWinner2(INPUT));
+        if (Config.isFast() ) return; // 1 h 8 minutess 16 seconds
+        Assert.assertEquals( "answer 2", -1, detectWinner21(INPUT)); // 259778 - too low
     }
 }
