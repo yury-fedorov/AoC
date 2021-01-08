ebff35e7bbee53efc3f7b6941a708682db19f278
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-08 09:59:15 +0100

    AoC16 Day19 Part1 solved

---
+++ AoC16/src/test/java/aoc16/d19/Day19Test.java
@@ -0,0 +1,36 @@
+package aoc16.d19;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.LinkedList;
+import java.util.ListIterator;
+
+public class Day19Test {
+
+    static ListIterator<Integer> next( LinkedList<Integer> list, ListIterator<Integer> it ) {
+        if ( !it.hasNext() ) it = list.listIterator();
+        it.next();
+        return it;
+    }
+
+    static int detectWinner( final int n ) {
+        final var list = new LinkedList<Integer>();
+        for ( int i = 1; i <= n; i++ ) {
+            list.addLast(i);
+        }
+        var it = next(list, list.listIterator() );
+        for ( ; list.size() > 1;  ) {
+            it = next(list, it);
+            it.remove();
+            it = next(list, it);
+        }
+        return list.get(0);
+    }
+
+    @Test
+    public void solution() {
+        Assert.assertEquals( "test", 3, detectWinner(5));
+        Assert.assertEquals( "answer 1", 1842613, detectWinner(3018458));
+    }
+}
