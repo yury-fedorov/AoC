b04f9f3c11ceef2f93d6a15f90b55ae9a147fa52
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-08 13:09:50 +0100

    AoC16 Day19 Part2 second version slower than 1 hour

--- AoC16/src/test/java/aoc16/d19/Day19Test.java
+++ AoC16/src/test/java/aoc16/d19/Day19Test.java
@@ -14,20 +14,6 @@ public class Day19Test {
         return it;
     }
 
-    static ListIterator<Integer> prev( LinkedList<Integer> list, ListIterator<Integer> it ) {
-        if ( !it.hasPrevious() ) {
-            while ( it.hasNext() ) it.next();
-        }
-        it.previous();
-        return it;
-    }
-
-    static ListIterator<Integer> next( LinkedList<Integer> list, ListIterator<Integer> it, int n ) {
-        final boolean isNext = n > 0;
-        for ( int i = Math.abs(n); --i >= 0; ) { it = isNext ? next(list, it) : prev( list, it ); }
-        return it;
-    }
-
     static int detectWinner( final int n ) {
         final LinkedList<Integer> list = init(n);
         var it = next(list, list.listIterator() );
@@ -36,19 +22,19 @@ public class Day19Test {
             it.remove();
             it = next(list, it); // remove() call brings us to the previous element
         }
-        return list.get(0);
+        return list.peek();
     }
 
-    static int detectWinner2( final int n ) {
-        final LinkedList<Integer> list = init(n);
-        var it = next(list, list.listIterator() );
-        while (list.size() > 1) {
-            final var half = ( list.size() >> 1 ); // divide by 2
-            it = next(list, it, half);
-            it.remove();
-            it = next(list, it, -( half - 2 ) ); // we moved one back and we need to move to the next element
+    static int detectWinner2( final int initialSize ) {
+        final LinkedList<Integer> list = init(initialSize);
+        for (  int position = 0; true; ) {
+            final int n = list.size();
+            if ( n == 1 ) return list.peek();
+            final int half = ( n >> 1 ); // divide by 2
+            final int outIndex = ( position + half ) % n;
+            list.remove( outIndex );
+            if ( outIndex > position ) position++;
         }
-        return list.get(0);
     }
 
     private static LinkedList<Integer> init(int n) {
@@ -65,6 +51,6 @@ public class Day19Test {
         Assert.assertEquals( "test 1", 3, detectWinner(5));
         Assert.assertEquals( "answer 1", 1842613, detectWinner(INPUT));
         Assert.assertEquals( "test 2", 2, detectWinner2(5));
-        // Assert.assertEquals( "answer 2", -1, detectWinner2(INPUT));
+        Assert.assertEquals( "answer 2", -1, detectWinner2(INPUT));
     }
 }
