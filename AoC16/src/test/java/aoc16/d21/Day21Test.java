e498296ed51c45c8701d444dcde47f5b192cdfd5
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-10 00:36:01 +0100

    AoC16 Day21 draft

--- AoC16/src/test/java/aoc16/d21/Day21Test.java
+++ AoC16/src/test/java/aoc16/d21/Day21Test.java
@@ -7,16 +7,25 @@ import org.junit.Test;
 public class Day21Test {
 
     // swap position 4 with position 0
-    static StringBuilder swap( CharSequence t, int a, int b ) {
-        final var t1 = new StringBuilder( t );
+    static StringBuilder swapPosition( CharSequence t, int a, int b ) {
+        final var t1 = new StringBuilder(t);
         t1.setCharAt( a, t.charAt(b) );
         t1.setCharAt( b, t.charAt(a) );
         return t1;
     }
 
+    // swap letter d with letter b
+    static StringBuilder swapLetter( CharSequence t, Character a, Character b ) {
+        final var s = new StringBuilder(t);
+        final int ai = s.indexOf( a.toString() );
+        final int bi = s.indexOf( b.toString() );
+        return swapPosition( t, ai, bi );
+    }
+
     @Test
     public void test() {
-        Assert.assertEquals( "ebcda", swap( "abcde", 0, 4 ).toString() );
+        Assert.assertEquals( "ebcda", swapPosition( "abcde", 0, 4 ).toString() );
+        Assert.assertEquals( "edcba", swapLetter( "ebcda", 'b', 'd' ).toString() );
     }
 
     @Test
