9aeb50e2b85a018de96f33754c6147e713781e6d
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-09 21:25:13 +0100

    AoC16 Day21 start

---
+++ AoC16/src/test/java/aoc16/d21/Day21Test.java
@@ -0,0 +1,21 @@
+package aoc16.d21;
+
+import aoc16.common.IOUtil;
+import org.junit.Test;
+
+public class Day21Test {
+
+    /*
+rotate based on position of letter a
+swap letter g with letter d
+move position 1 to position 5
+reverse positions 6 through 7
+move position 5 to position 4
+    */
+
+    @Test
+    public void solution() {
+        final var operations = IOUtil.input( "d21" );
+        final var input = "abcdefgh";
+    }
+}
