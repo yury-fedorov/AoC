f2e198cf4ec0d4514504f99b6a6333ebf52528cf
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-10 01:10:49 +0100

    AoC16 Day21 draft

--- AoC16/src/test/java/aoc16/d21/Day21Test.java
+++ AoC16/src/test/java/aoc16/d21/Day21Test.java
@@ -4,9 +4,13 @@ import aoc16.common.IOUtil;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Collection;
+import java.util.regex.Pattern;
+
 public class Day21Test {
 
     // swap position 4 with position 0
+    final static Pattern SWAP_POS = Pattern.compile( "^swap position (\\d+) with position (\\d+)$" );
     static StringBuilder swapPosition( CharSequence t, int a, int b ) {
         final var t1 = new StringBuilder(t);
         t1.setCharAt( a, t.charAt(b) );
@@ -15,6 +19,7 @@ public class Day21Test {
     }
 
     // swap letter d with letter b
+    final static Pattern SWAP_LETTER = Pattern.compile( "^swap letter (\\w) with letter (\\w)$" );
     static StringBuilder swapLetter( CharSequence t, Character a, Character b ) {
         final var s = new StringBuilder(t);
         final int ai = s.indexOf( a.toString() );
@@ -22,7 +27,7 @@ public class Day21Test {
         return swapPosition( t, ai, bi );
     }
 
-    // reverse positions 0 through 4
+    final static Pattern REV_POS = Pattern.compile( "^reverse positions (\\d+) through (\\d+)$" );
     static StringBuilder reversePosition( CharSequence t, int a, int b ) {
         final var result = new StringBuilder();
         for ( int i = 0; i < a; i++ ) result.append( t.charAt(i) );
@@ -32,6 +37,7 @@ public class Day21Test {
     }
 
     // rotate left 1 step
+    final static Pattern ROTATE_LEFT = Pattern.compile( "^rotate left (\\d+) step" );
     static StringBuilder rotateLeft( CharSequence t, int s ) {
         final var result = new StringBuilder(t);
         while ( --s >= 0 ) {
@@ -42,6 +48,7 @@ public class Day21Test {
         return result;
     }
 
+    final static Pattern ROTATE_RIGHT = Pattern.compile( "^rotate right (\\d+) step" );
     static StringBuilder rotateRight( CharSequence t, int s ) {
         final int lastIndex = t.length() - 1;
         final var result = new StringBuilder(t);
@@ -54,6 +61,7 @@ public class Day21Test {
     }
 
     // rotate based on position of letter b
+    final static Pattern ROTATE_BY_POS = Pattern.compile( "^rotate based on position of letter (\\w)$" );
     static StringBuilder rotateByPosition( CharSequence t, Character c ) {
         final var s = new StringBuilder(t);
         final int i = s.indexOf( c.toString() );
@@ -69,6 +77,20 @@ public class Day21Test {
         Assert.assertEquals( "deabc", rotateRight( "abcde", 2).toString() );
         Assert.assertEquals( "ecabd", rotateByPosition( "abdec", 'b').toString() );
         Assert.assertEquals( "decab", rotateByPosition( "ecabd", 'd').toString() );
+
+        Assert.assertEquals( "decab", scramble( "abcde", IOUtil.inputByPath( "d21/sample.txt" ) ) );
+    }
+
+    static StringBuilder apply( String operation, StringBuilder input ) {
+        return input;
+    }
+
+    static String scramble( String input, Collection<String> operations ) {
+        var result = new StringBuilder(input);
+        for ( final var o : operations ) {
+            result = apply( o, result );
+        }
+        return result.toString();
     }
 
     @Test
