fbbb12364cd2b2f70e0a37b9f961186576237b9a
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-12 23:21:46 +0100

    AoC16 Day24 start

---
+++ AoC16/src/test/java/aoc16/d24/Day24Test.java
@@ -0,0 +1,13 @@
+package aoc16.d24;
+
+import aoc16.common.IOUtil;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Day24Test {
+    @Test
+    public void solution() {
+        final var input = IOUtil.input("d24");
+        Assert.fail("to implement");
+    }
+}
