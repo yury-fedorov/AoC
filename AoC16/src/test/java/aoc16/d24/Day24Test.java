8eb2bacc38344b20413b440dac50d6522a273e21
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-13 07:20:59 +0100

    AoC16 Day24 draft

--- AoC16/src/test/java/aoc16/d24/Day24Test.java
+++ AoC16/src/test/java/aoc16/d24/Day24Test.java
@@ -1,13 +1,30 @@
 package aoc16.d24;
 
 import aoc16.common.IOUtil;
+import org.javatuples.Pair;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.HashMap;
+
 public class Day24Test {
+
+    final static char WALL = '#';
+    final static char SPACE = '.';
+
     @Test
     public void solution() {
         final var input = IOUtil.input("d24");
+        final var sizeY = input.size();
+        final var sizeX = input.get(0).length();
+        final var m = new HashMap<Character, Pair<Integer,Integer>>();
+        for ( var y = 0; y < sizeY; y++ ) {
+            final var l = input.get(y);
+            for ( var x = 0; x < sizeX; x++ ) {
+                final var ch = l.charAt(x);
+                if ( ch != WALL && ch != SPACE ) m.put( ch, Pair.with(x,y) );
+            }
+        }
         Assert.fail("to implement");
     }
 }
