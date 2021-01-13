fd26e18f6cc11f4357b1d6657df8bddbd5d448d5
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-13 22:51:12 +0100

    AoC16 Point refactoring

--- AoC16/src/test/java/aoc16/d02/Day02Test.java
+++ AoC16/src/test/java/aoc16/d02/Day02Test.java
@@ -1,6 +1,7 @@
 package aoc16.d02;
 
 import aoc16.common.IOUtil;
+import aoc16.common.Point;
 import org.junit.Test;
 import java.util.*;
 import org.javatuples.*;
@@ -23,29 +24,26 @@ public class Day02Test {
     final static Map<Character,Direction> TO_DIR = Map.of(
             'U', Direction.UP, 'D', Direction.DOWN, 'L', Direction.LEFT, 'R', Direction.RIGHT );
 
-    static char at( char [][] keypad, Pair<Integer,Integer> p ) { return keypad[p.getValue1()][p.getValue0()]; }
-
-    static Pair<Integer,Integer> xy( int x, int y ) { return Pair.with( x, y ); }
+    static char at( char [][] keypad, Point p ) { return keypad[p.y()][p.x()]; }
 
     static int bound( char [][] keypad, int v ) { return Math.min( Math.max( 0, v ), keypad.length -1 ); }
 
-    static Pair<Integer,Integer> move( char [][] keypad, Pair<Integer, Integer> p, Direction d ) {
+    static Point move( char [][] keypad, Point p, Direction d ) {
         final int dx = d == Direction.LEFT ? -1 : d == Direction.RIGHT ? 1 : 0;
         final int dy = d == Direction.UP   ? -1 : d == Direction.DOWN  ? 1 : 0;
-        int x1 = bound( keypad, p.getValue0() + dx );
-        int y1 = bound( keypad, p.getValue1() + dy );
-        final var p1 = xy(x1,y1);
-        return at( keypad, p1 ) == ' ' ? p : Pair.with( x1, y1 );
+        int x1 = bound( keypad, p.x() + dx );
+        int y1 = bound( keypad, p.y() + dy );
+        return at( keypad, Point.with(x1,y1) ) == ' ' ? p : Point.with( x1, y1 );
     }
 
-    public static Pair<Integer,Integer> processLine( char [][] keypad, String input, Pair<Integer,Integer> p ) {
+    public static Point processLine( char [][] keypad, String input, Point p ) {
         for ( char d : input.toCharArray() ) {
             p = move( keypad, p, TO_DIR.get( d ) );
         }
         return p;
     }
 
-    public static String answer( List<String> input, Pair<Integer,Integer> p, char [][] keypad ) {
+    public static String answer( List<String> input, Point p, char [][] keypad ) {
         assertEquals( '5', at( keypad, p ) );
         var result = new StringBuilder();
         for ( var line : input ) {
@@ -58,9 +56,9 @@ public class Day02Test {
     @Test
     public void solution() {
         final var input = IOUtil.input( "d02" );
-        var answer1 = answer( input, xy(1,1), KEYPAD );
+        var answer1 = answer( input, Point.with(1,1), KEYPAD );
         assertEquals( "answer 1", "38961", answer1 );
-        var answer2 = answer( input, xy(0,2), KEYPAD2 );
+        var answer2 = answer( input, Point.with(0,2), KEYPAD2 );
         assertEquals( "answer 2", "46C92", answer2 );
     }
 }
