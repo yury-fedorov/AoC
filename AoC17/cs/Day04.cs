e8d7113ce9ea0e6f1704f80233b93909e6674a17
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-21 23:47:24 +0100

    AoC17 code review

--- AoC17/cs/Day04.cs
+++ AoC17/cs/Day04.cs
@@ -4,9 +4,6 @@ using NUnit.Framework;
 
 namespace AoC17
 {
-    // alt 5 - square brackets
-    // alt 7 - or pipe
-    // alt 8-9 - {}
     public class Day04
     {
         public const string Directory = "/Users/fedoy/projects/AoC/AoC17/cs";
