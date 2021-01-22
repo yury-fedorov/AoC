1096dee67c3af6bcddc64f84c84587fa829f8d55
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-23 00:03:02 +0100

    AoC17 Rust solution is now parsing input data

--- AoC17/rust/src/day07.rs
+++ AoC17/rust/src/day07.rs
@@ -11,8 +11,11 @@ pub fn task1() -> String {
    lazy_static! {
         static ref re: Regex = Regex::new(r"(\w{4}) \((\d+)\)(.+)").unwrap();
    }
+   // https://doc.rust-lang.org/rust-by-example/primitives/tuples.html
    type Data = (String, i32);
+   // https://doc.rust-lang.org/book/ch08-01-vectors.html
    let mut v: Vec<Data> = Vec::new();
+   // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
    for line in lines {
       for cap in re.captures_iter(line) {
          v.push( (cap[1].to_string(), i32::from_str(&cap[2]).unwrap() ) );
