1194c29686c685f0c275503dd6f17670f7edf2dc
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-23 01:26:40 +0100

    AoC17 Day07 Part1 solved

--- AoC17/rust/src/common.rs
+++ AoC17/rust/src/common.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 pub fn input( short_path: &str ) -> String {
-    let dir = Path::new("/Users/fedoy/projects/AoC/AoC17/scala/src/test/resources");
+    let dir = Path::new("/Users/fedoy/projects/AoC/AoC17/data");
     let filename = dir.join( short_path );
     // https://doc.rust-lang.org/book/ch12-02-reading-a-file.html
     std::fs::read_to_string(filename)
