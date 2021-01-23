119f3b856c81cd02faa17b68ff938d50afe7c04e
David Peklak <dpeklak@gmail.com>
2021-01-23 14:04:27 +0100

    get more functional in fn tail(...)

--- AoC17/rust/src/day07.rs
+++ AoC17/rust/src/day07.rs
@@ -9,12 +9,12 @@ fn tail( t : &str ) -> HashSet<String> {
         static ref RE_TAIL: Regex = Regex::new(r" -> (.+)").unwrap();
         static ref RE_ELEMENT: Regex = Regex::new(r"([a-z]+),?").unwrap();
    }
+
    for cap_tail in RE_TAIL.captures_iter(t) {
-      let mut set = HashSet::new();
-      for cap_element in RE_ELEMENT.captures_iter( &cap_tail[1] ) {
-         set.insert( cap_element[1].to_string() );
-      }
-      return set;
+      return RE_ELEMENT
+          .captures_iter( &cap_tail[1] )
+          .map(|cap_element| cap_element[1].to_string())
+          .collect() // collect() collects the iterator into the target type (the HashSet<String>)
    }
    HashSet::new()
 }
@@ -29,6 +29,7 @@ pub fn task1( file : &str ) -> String {
    // https://doc.rust-lang.org/rust-by-example/primitives/tuples.html
    type Data = (String, i32, HashSet<String>);
    // https://doc.rust-lang.org/book/ch08-01-vectors.html
+
    let mut v: Vec<Data> = Vec::new();
    // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
    for line in lines {
@@ -40,9 +41,11 @@ pub fn task1( file : &str ) -> String {
    for (n,_,_) in &v {
       result.push(n.to_string());
    }
+
    for (_,_,s) in &v {
       result.retain( |x| !s.contains(x) );
    }
+
    if result.len() != 1 {
       let mut error = String::new();
       error.push_str("Unexpected content: ");
