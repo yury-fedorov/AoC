749185c1d71ebef716dc67694d3daf1237c2340d
David Peklak <dpeklak@gmail.com>
2021-01-23 19:03:52 +0100

    construct v in fn task1(...) in functional way

--- AoC17/rust/src/day07.rs
+++ AoC17/rust/src/day07.rs
@@ -21,22 +21,23 @@ fn tail( t : &str ) -> HashSet<String> {
 
 pub fn task1( file : &str ) -> String {
    let text = common::input(file);
-   let lines = text.lines();
+   // https://doc.rust-lang.org/rust-by-example/primitives/tuples.html
+   type Data = (String, i32, HashSet<String>);
    // https://docs.rs/regex/1.4.3/regex/
    lazy_static! {
         static ref RE_LINE: Regex = Regex::new(r"([a-z]+) \((\d+)\)(.+)").unwrap();
    }
-   // https://doc.rust-lang.org/rust-by-example/primitives/tuples.html
-   type Data = (String, i32, HashSet<String>);
+
    // https://doc.rust-lang.org/book/ch08-01-vectors.html
 
-   let mut v: Vec<Data> = Vec::new();
-   // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
-   for line in lines {
-      for cap in RE_LINE.captures_iter(line) {
-         v.push( (cap[1].to_string(), i32::from_str(&cap[2]).unwrap(), tail(&cap[3]) ) );
-      }
-   }
+   let v: Vec<Data> = text
+       // https://doc.rust-lang.org/rust-by-example/std_misc/file/read_lines.html
+       .lines()
+       .into_iter()
+       .flat_map(|line| RE_LINE.captures_iter(line))
+       .map(|cap| (cap[1].to_string(), i32::from_str(&cap[2]).unwrap(), tail(&cap[3]) ))
+       .collect();
+   
    let mut result : Vec<String> = Vec::new();
    for (n,_,_) in &v {
       result.push(n.to_string());
