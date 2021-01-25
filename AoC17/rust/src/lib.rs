f42227dec73ddb628162f5b36509b3333dacfbfd
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-25 01:16:29 +0100

    AoC17 Day08 solved

--- AoC17/rust/src/lib.rs
+++ AoC17/rust/src/lib.rs
@@ -2,6 +2,7 @@
 pub mod common;
 
 pub mod day07;
+pub mod day08;
 
 #[cfg(test)]
 // https://doc.rust-lang.org/book/ch11-02-running-tests.html#controlling-how-tests-are-run
--- AoC17/rust/src/tests.rs
+++ AoC17/rust/src/tests.rs
@@ -1,4 +1,3 @@
-
 fn day07( file : &str, answer1 : &str, answer2 : i32 ) {
     use crate::day07;
     let input = day07::parse( file );
@@ -12,3 +11,20 @@ fn test_day07() {
     day07( "07/sample.txt", "tknk", 60 );
     day07( "07/input.txt", "azqje", 646 );
 }
+
+fn day08( file : &str, answer1 : i32, answer2 : i32 ) {
+    use crate::common;
+    use crate::day08;
+    let text = common::input(file);
+    let (a1,a2) = day08::tasks( &text );
+    assert_eq!( answer1, a1 );
+    assert_eq!( answer2, a2 );
+}
+
+#[test]
+fn test_day08() {
+    day08( "08/sample.txt", 1, 10 );
+    day08( "08/input.txt", 5849, 6702 );
+}
+
+
