62d4c7e1e35c62332822f9a74f1bdc8e3b7e1bb9
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-29 07:11:52 +0100

    AoC17 Day15 code review (still 7 seconds)

--- AoC17/rust/src/day15.rs
+++ AoC17/rust/src/day15.rs
@@ -1,17 +1,20 @@
 type Input = (u32,u32);
 
-fn next_a( a : u64 ) -> u64 { ( a * 16807 ) % 2147483647 }
-fn next_b( b : u64 ) -> u64 { ( b * 48271 ) % 2147483647 }
+const BASIS : u64 = 2147483647;
 
-pub fn task1( input : Input ) -> i32 {
-    let n = 40000000; // 40 millions
+type FnNext = fn(u64) -> u64;
+
+fn next_a( a : u64 ) -> u64 { ( a * 16807 ) % BASIS }
+fn next_b( b : u64 ) -> u64 { ( b * 48271 ) % BASIS }
+
+fn task( input : Input, na : FnNext, nb : FnNext, n : i32 ) -> i32 {
     let mask = 0xFFFF; // 16 bits - 2 bytes
     let mut count = 0;
     let mut ca = input.0 as u64;
     let mut cb = input.1 as u64;
     for _ in 0 .. n {
-        ca = next_a( ca );
-        cb = next_b( cb );
+        ca = na( ca );
+        cb = nb( cb );
         if ( ca & mask ) == ( cb & mask ) {
             count += 1;
         }
@@ -19,7 +22,12 @@ pub fn task1( input : Input ) -> i32 {
     count
 }
 
-fn next_2( v : u64, next : fn(u64) -> u64, base : u64 ) -> u64 {
+pub fn task1( input : Input ) -> i32 {
+    let n = 40000000; // 40 millions
+    task( input, next_a, next_b, n )
+}
+
+fn next_2( v : u64, next : FnNext, base : u64 ) -> u64 {
     let mut u = v;
     loop {
         u = next(u);
@@ -32,16 +40,5 @@ fn next_b2( b : u64 ) -> u64 { next_2( b, next_b, 8 - 1 ) }
 
 pub fn task2( input : Input ) -> i32 {
     let n = 5000000; // 5 millions
-    let mask = 0xFFFF; // 16 bits - 2 bytes
-    let mut count = 0;
-    let mut ca = input.0 as u64;
-    let mut cb = input.1 as u64;
-    for _ in 0 .. n {
-        ca = next_a2( ca );
-        cb = next_b2( cb );
-        if ( ca & mask ) == ( cb & mask ) {
-            count += 1;
-        }
-    }
-    count
+    task( input, next_a2, next_b2, n )
 }
