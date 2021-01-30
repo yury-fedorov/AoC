5da63b94d146a4b1322acf88123ffb04a0caec53
Yury Fedorov <yury.fedorov@gmail.com>
2021-01-30 23:54:13 +0100

    AoC17 Day17 code review - still takes 30 minutes

--- AoC17/rust/src/day17.rs
+++ AoC17/rust/src/day17.rs
@@ -1,20 +1,14 @@
-extern crate linked_list;
-/*
-fn next(p : usize, dp: usize, n : usize ) -> usize { ( p + dp ) % n }
+extern crate linked_list; // LinkedList in standard library is not usable,
+// the solution on Vector does not finish even in 6 hours
 
-fn task_vec( steps : usize, last : i32 ) -> ( Vec<i32>, i32) {
-    let mut buffer : Vec<i32> = vec![0];
-    let mut position : usize = 0;
-    let last1 = last + 1;
-    for i in  1 .. last1 {
-        position = next( position, steps, buffer.len() ) + 1;
-        buffer.insert( position, i );
+fn forward( c : &mut linked_list::Cursor<i32>, times : usize ) -> i32 {
+    let mut value : Option<&mut i32> = None;
+    for _j in 0 .. times {
+        value = c.next();
+        if value.is_none() { value = c.next(); }
     }
-    position = next( position, 1, buffer.len() );
-    let next_element = buffer[position];
-    ( buffer, next_element )
+    *value.unwrap()
 }
-*/
 
 fn task( steps : usize, last : i32 ) -> ( Vec<i32>, i32 ) {
     let mut buffer : linked_list::LinkedList<i32> = linked_list::LinkedList::new();
@@ -23,17 +17,10 @@ fn task( steps : usize, last : i32 ) -> ( Vec<i32>, i32 ) {
     let last1 = last + 1;
     let steps1 = steps + 1;
     for i in  1 .. last1 {
-        for _j in 0 .. steps1 {
-            if c.next().is_none() { c.next(); }
-        }
+        forward( &mut c, steps1 );
         c.insert( i );
     }
-    let mut next_element = None;
-    for _j in 0 .. 2 {
-        next_element = c.next();
-        if next_element.is_none() { next_element = c.next(); }
-    }
-    let r_next_element = *next_element.unwrap();
+    let r_next_element = forward( &mut c,2 );
     let r_vec : Vec<i32> = buffer.into_iter().collect();
     ( r_vec, r_next_element )
 }
@@ -44,7 +31,7 @@ pub fn task1( steps : usize ) -> i32 {
 }
 
 pub fn task2( steps : usize ) -> i32 {
-    let last = 50000000; // fifty millionth
+    let last = 50000000; // fifty million
     let buffer = task( steps, last ).0;
     let n = buffer.len();
     for i in 0 .. n {
