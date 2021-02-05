c5ef4d3b576fe1448cf71144b64936c9592d8632
Yury Fedorov <yury.fedorov@gmail.com>
2021-02-05 22:01:29 +0100

    AoC17 Day18 Part2 refactoring (still draft)

--- AoC17/rust/src/day18.rs
+++ AoC17/rust/src/day18.rs
@@ -24,7 +24,8 @@ fn get_v( value :&str, registers : &Registers ) -> Int {
     get_r( registers, to_r( value ) )
 }
 
-type Command = Box< dyn Fn ( &mut Registers, &mut Music ) -> Int >;
+type Command = Box< dyn Fn ( &mut Registers, &mut Music, &mut Music ) -> Int >;
+type Code = Vec<Command>;
 type Music = Vec<Int>;
 
 fn op_r_v( a : char, b : &str, f : fn( a : Int, b : Int ) -> Int, r : &mut Registers ) -> Int {
@@ -45,43 +46,43 @@ fn compile( line : &str ) -> Command {
     }
     for cap in RE_SND.captures_iter( line ) {
         let value = cap[1].to_string();
-        return Box::new( move | r, m | {
-            m.push( get_v( &value, r ) ); 1 } )
+        return Box::new( move | r, _mi, mo | {
+            mo.push( get_v( &value, r ) ); 1 } )
     }
     for cap in RE_SET.captures_iter( line ) {
         let a = to_r(&cap[1] );
         let b = cap[2].to_string();
-        return Box::new( move |r, _m|
+        return Box::new( move |r, _mi, _mo |
             { set_r( r, a, get_v( &b, r ) ); 1 } )
     }
     for cap in RE_ADD.captures_iter( line ) {
         let a = to_r(&cap[1] );
         let b = cap[2].to_string();
-        return Box::new( move |r, _m|
+        return Box::new( move |r, _mi, _mo |
             op_r_v( a, &b, |a,b| a + b, r ) )
     }
     for cap in RE_MUL.captures_iter( line ) {
         let a = to_r(&cap[1] );
         let b = cap[2].to_string();
-        return Box::new( move |r, _m|
+        return Box::new( move |r, _mi, _mo |
             op_r_v( a, &b, |a,b| a * b, r ) )
     }
     for cap in RE_MOD.captures_iter( line ) {
         let a = to_r(&cap[1] );
         let b = cap[2].to_string();
-        return Box::new( move |r, _m|
+        return Box::new( move |r, _mi, _mo |
             op_r_v( a, &b, |a,b| a % b, r ) )
     }
     for cap in RE_RCV.captures_iter( line ) {
         let a = to_r(&cap[1] );
-        return Box::new( move |r, _m|
+        return Box::new( move |r, _mi, _mo|
              { if get_r( r, a ) != 0 { return 1000000 }
                1 } )
     }
     for cap in RE_JGZ.captures_iter( line ) {
         let a = to_r(&cap[1] );
         let b = cap[2].to_string();
-        return Box::new( move |r, _m|
+        return Box::new( move |r, _mi, _mo |
             { if get_r( r, a ) > 0 { return get_v( &b, r ) }
                 1 } )
     }
@@ -89,21 +90,37 @@ fn compile( line : &str ) -> Command {
 }
 
 pub fn task1(script: &str) -> Int {
-    let code : Vec<Command> = script.lines().into_iter().map( compile ).collect();
+    let code : Code = script.lines().into_iter().map( compile ).collect();
     let n = code.len() as Int;
     let mut registers : HashMap<char,Int> = HashMap::new();
-    let mut music : Music = Vec::new();
+    let mut m_in : Music = Vec::new();
+    let mut m_out : Music = Vec::new();
     let mut index :  Int = 0;
     while index >= 0 && index < n {
         let cmd = code.get(index as usize );
         if cmd.is_none() { break; }
         let f = cmd.unwrap();
-        let di = f( &mut registers, &mut music );
+        let di = f( &mut registers, &mut m_in, &mut m_out );
         index += di;
     }
-    *music.last().unwrap() // too high: 8039
+    *m_out.last().unwrap() // too high: 8039
+}
+
+fn run_duet( p_id: Int, m_in : &mut Music, m_out : &mut Music, code : &Code ) {
+    let mut registers : HashMap<char,Int> = HashMap::new();
+    registers.insert( 'p', p_id );
+    let n = code.len() as Int;
+    let mut index :  Int = 0;
+    while index >= 0 && index < n {
+        let cmd = code.get(index as usize );
+        if cmd.is_none() { break; }
+        let f = cmd.unwrap();
+        let di = f( &mut registers, m_in, m_out );
+        index += di;
+    }
 }
 
 pub fn task2(script: &str) -> i32 {
+
     0
 }
