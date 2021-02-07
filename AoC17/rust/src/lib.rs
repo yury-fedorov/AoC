fac4ff0c752c6298afebfe39369b08c6be8b7a01
David Peklak <dpeklak@gmail.com>
2021-02-07 09:09:28 +0100

    solve day17 with red-black tree datastructure

--- AoC17/rust/src/lib.rs
+++ AoC17/rust/src/lib.rs
@@ -12,9 +12,11 @@ pub mod day14;
 pub mod day15;
 pub mod day16;
 pub mod day17;
+pub mod day17david;
 pub mod day18;
 mod red_black_bst_2;
 
 #[cfg(test)]
 // https://doc.rust-lang.org/book/ch11-02-running-tests.html#controlling-how-tests-are-run
 mod tests;
+
