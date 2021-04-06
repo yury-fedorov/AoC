6002d50fe085179f03c92d589ffc2c943697a7fc
Yury Fedorov <yury.fedorov@gmail.com>
2021-04-06 20:22:19 +0200

    AoC15 Day17 removed commented code

--- AoC15/17/day.cpp
+++ AoC15/17/day.cpp
@@ -27,30 +27,7 @@ int maxCounter( int n ) {
     return ( 1 << n ) - 1; // sets all valid bits to 1
 }
 
-/* commented code is at least 2 times faster to solve the task and is more scalable
-void init( const vector<int> & v, int b, CountMinMap & m ) {
-    for ( ; b >= 0; b-- ) {
-        const CountMin r = sum( v, b );
-        const auto n = r.first;
-        CountMin prev = m[n];
-        if ( prev.first == 0 ) prev.second = r.second;
-        m[n] = make_pair( prev.first + 1, min( prev.second, r.second ) );
-    }
-}
-
-auto split( const vector<int> & containers, const int target ) {
-    auto low = 0;
-    auto it = containers.cbegin();
-    for ( ; it != containers.cend(); it++ ) {
-        const auto next = low + *it;
-        if ( next >= target ) { 
-            break;
-        }
-        low = next;
-    }
-    return make_pair( it, low );
-}
-*/
+// deleted code is at least 2 times faster to solve the task and is more scalable but this solution runs in 0.3 seconds
 
 TEST_CASE( "Day17", "[17]" ) {
     vector<int> containers;
@@ -61,32 +38,7 @@ TEST_CASE( "Day17", "[17]" ) {
         containers.push_back( stoi(line) );
     }
     const int TARGET = 150;
-    /*
-    sort( containers.begin(), containers.end() );
-
-    const auto sp = split(containers, TARGET);
-    const auto h = sp.first - containers.cbegin();
-    const auto firstHigh = *sp.first;
-
-    CountMinMap lowMap;
-    vector<int> lowCo(containers.cbegin(), sp.first);
-    init( lowCo, maxCounter(h), lowMap );
-
-    CountMinMap highMap;
-    const vector<int> highCo( sp.first, containers.end() );
-    init( highCo, maxCounter( highCo.size() ), highMap );
-
-    int minSum = INT_MAX;
-    long long answer1 = 0;
-    for ( auto cl : lowMap ) {
-        const auto ch = highMap.find( TARGET - cl.first );
-        if ( ch != highMap.end() ) {
-            minSum = min(minSum, cl.second.second + ch->second.second);
-            answer1 += ( cl.second.first * ch->second.first );
-        }
-    }
-    */
-
+    
     auto answer1 = 0;
     int minSum = INT_MAX;
     for ( auto c = maxCounter(containers.size()); c > 0; c-- ) {
