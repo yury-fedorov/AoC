7ca6ee1d376c8d522f72fed5f3e173edb204a4e7
Yury Fedorov <yury.fedorov@gmail.com>
2021-06-20 18:33:06 +0200

    AoC17 - Day19 - Part 1 - still draft

--- AoC17/rust/src/day19.rs
+++ AoC17/rust/src/day19.rs
@@ -1,33 +1,87 @@
+#[derive(PartialEq, Copy, Clone)]
 enum Direction {
     Down, Up, Left, Right
 }
 
-type Point = (i32,i32);
+#[derive(PartialEq)]
+enum Part {
+    Cable, Letter, Space
+}
 
-fn next( p : Point, d : Direction ) -> Point {
+type Point = (i32,i32);
+type Map = Vec<String>;
+
+fn next( p : &Point, d : &Direction ) -> Point {
     let (x,y) = p;
     match d {
-        Down => ( x, y + 1 ),
-        Up => ( x, y - 1 ),
-        Left => ( x - 1, y ),
-        Rigth => ( x + 1, y )
+        Direction::Down => ( *x, *y + 1 ),
+        Direction::Up => ( *x, *y - 1 ),
+        Direction::Left => ( *x - 1, *y ),
+        Direction::Right => ( *x + 1, *y )
     }
-} 
+}
+
+fn at( m : &Map, p : &Point ) -> char {
+    let (x,y) = p;
+    let empty = ' ';
+    if *x < 0 || *y < 0 { return empty; }
+    let sy = m.len() as i32;
+    if *y >= sy { return empty; }
+    let row : &str = m.get(*y as usize).unwrap();
+    let sx = row.len() as i32;
+    if *x >= sx { return empty; }
+    return row.chars().nth( *x as usize).unwrap();
+}
+
+fn other_directions( d: &Direction ) -> Vec<Direction> {
+    let mut r = vec![Direction::Down, Direction::Left, Direction::Right, Direction::Up];
+    let i = r.iter().position( |e| *e == *d ).unwrap();
+    r.remove(i);
+    r
+}
+
+fn is_cable( c : char ) -> bool {
+    match c {
+        '-' | '|' | '+' => true,
+        _ => false
+    }
+}
+
+fn is_space( c : char ) -> bool { c == ' ' }
+
+fn what( c : char ) -> Part {
+    if is_cable(c) { return Part::Cable }
+    if is_space( c ) { return Part::Space }
+    Part::Letter
+}
+
+fn next_point( m: &Map, p : &Point, d : &Direction ) -> Option<( Point, Direction )> {
+    let nd = other_directions(d);
+    for d1 in nd {
+        let p1 = next(&p, &d1);
+        let c1 = at(&m, &p1);
+        if !is_space(c1 ) {
+            return Some(  ( p1, d1 ) );
+        }
+    }
+    None
+}
 
 pub fn task1(map : &str) -> String {
-    let ml : Vec<&str> = map.lines().collect();
+    let m : Map = map.lines().map( |s| s.to_string() ).collect();
     let mut answer = String::new();
-    let sx = ml.get(0).unwrap().len();
-    let sy = ml.len();
-    let start = ml.get(0).unwrap().find('|').unwrap() as i32;
+    let start = m.get(0).unwrap().find('|').unwrap() as i32;
     let mut p : Point = (start, 0);
     let mut d = Direction::Down;
-    while true {
-        let p1 = next(p, d.clone() );
+    loop {
+        let c = at(&m, &p);
+        println!( "p {} {} d {} c {}", p.0, p.1, d.clone() as i32, c );
+        if what(c) == Part::Letter { answer.push(c) }
+        let o1 = next_point( &m,&p, &d);
+        match o1 {
+            None => break,
+            Some( (p1, d1) ) => { p = p1; d = d1 }
+        }
     }
-    map.chars()
-    // .into_iter()
-    .filter( |c| match c { ' ' | '-' | '|' | '+' | '\r' | '\n' => true, _ => false } );
-    // .as_str()
-    start.to_string()
+    answer
 }
