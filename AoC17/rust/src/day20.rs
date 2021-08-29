2340b60901b9b1664f1328a63860f230954d4952
Yury Fedorov <yury.fedorov@gmail.com>
2021-08-29 13:33:53 +0200

    AoC17 Day20 parse started

--- AoC17/rust/src/day20.rs
+++ AoC17/rust/src/day20.rs
@@ -1,3 +1,24 @@
+struct Point3 {
+    pub x: i64,
+    pub y: i64,
+    pub z: i64,
+}
+
+fn parse( line : &str ) -> () {
+    lazy_static! {
+        // p=<-833,-499,-1391>, v=<84,17,61>, a=<-4,1,1>
+        static ref RE_SND: Regex = Regex::new(r"^snd (\w+)$").unwrap();         // snd X
+        static ref RE_SET: Regex = Regex::new(r"^set (\w+) (\S+)$").unwrap();   // set X Y
+    }
+    for cap in RE_SND.captures_iter( line ) {
+        let value = cap[1].to_string();
+        return Box::new(move |r, _mi, mo| {
+            mo.push(get_v(&value, r));
+            1
+        })
+    }
+}
+
 pub fn task12(data : &str) -> ( i32, i32 ) {
     // data.lines();
     (1,-1)
