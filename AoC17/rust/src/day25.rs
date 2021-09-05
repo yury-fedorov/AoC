11fba3bc499f74f8ffdf7622960329cbdb5d8725
Yury Fedorov <yury.fedorov@gmail.com>
2021-09-05 10:38:01 +0200

    AoC17 Day25 init

--- AoC17/rust/src/day25.rs
+++ AoC17/rust/src/day25.rs
@@ -3,6 +3,7 @@ use std::collections::HashSet;
 enum Direction { Left, Right }
 enum State { A, B, C, D, E, F }
 type Instruction = ( bool, Direction, State ); // write_value, move_slot_to_x, continue_with_state
+type StateCases = (Instruction, Instruction);
 
 pub fn task1() -> usize {
     let steps = 12861455;
