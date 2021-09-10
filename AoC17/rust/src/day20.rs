5d2f0570a85801d9e8ca8adc68a78750ca4ba4bd
Yury Fedorov <yury.fedorov@gmail.com>
2021-09-10 06:26:23 +0200

    AoC17 Day20 Part 2 solution but not write and it is also not particularly fast

--- AoC17/rust/src/day20.rs
+++ AoC17/rust/src/day20.rs
@@ -41,6 +41,68 @@ fn add( a: Point, b: Point ) -> Point {
     ( (a.0 + b.0), (a.1 + b.1), (a.2 + b.2) )
 }
 
+// x = x0 + (v0+0.5)*t + (a*0.5)*t*t
+// based on Ramanujan summation (1+2+3...)
+// rough approximation just to get till when to run the simulation
+// Some(0) - means the solution is  valid for any number
+fn collisions_time_on_axis_inner( pva0: Point, pva1: Point ) -> Vec<f64> {
+    let dp : f64 = ( pva0.0 - pva1.0 ) as f64;
+    let dv : f64 = ( pva0.1 - pva1.1 ) as f64;
+    let da : f64 = ( pva0.2 - pva1.2 ) as f64;
+    let c = dp;
+    let b = dv;
+    if da == 0.0 {
+        // b * t = -c => t = -c / b
+        return [ match b { 0.0 => 0.0, _ => ( -c / b  ) } ].to_vec();
+    }
+    let a = da * 0.5;
+    let det = ( b * b ) - ( 4.0 * a * c );
+    if det < 0.0 { return [].to_vec(); }
+    let a2 = 2.0 * a;
+    if det == 0.0 { return [ ( -b / a2 ) ].to_vec(); }
+    let sr = det.sqrt();
+    let t0 = ( (-b+sr) / a2 );
+    let t1 = ( (-b-sr) / a2 );
+    [t0, t1].to_vec()
+}
+
+fn collisions_time_on_axis( pva0: Point, pva1: Point ) -> Vec<i64> { 
+    collisions_time_on_axis_inner(pva0,  pva1).iter()
+        .filter( |i| **i >= 0.0 ).map( |i| i.ceil() as i64 ).collect()
+}
+
+fn approx_collision_time( a: PVA, b : PVA ) -> Option<i64> {
+    let x = collisions_time_on_axis(( a.0.0, a.1.0, a.2.0 ), ( b.0.0, b.1.0, b.2.0 ) );
+    if x.len() == 0 { return None; }
+    let y = collisions_time_on_axis(( a.0.1, a.1.1, a.2.1 ), ( b.0.1, b.1.1, b.2.1 ) );
+    if y.len() == 0 { return None; }
+    let z = collisions_time_on_axis(( a.0.2, a.1.2, a.2.2 ), ( b.0.2, b.1.2, b.2.2 ) );
+    if z.len() == 0 { return None; }
+    let mut result : HashSet<i64> = HashSet::new();
+    for xi in &x {
+        for yi in &y  {
+            for zi in &z {
+                let i = [xi,yi,zi];
+                let min_i : i64 = **i.iter().min().unwrap();
+                let max_i : i64 = **i.iter().max().unwrap();
+                if min_i == max_i { result.insert( max_i ); }
+                else if min_i > 0 && max_i > min_i { }
+                else if min_i == 0 && max_i > min_i {
+                    let is_other = i.iter().any(|j| j > &&min_i && j < &&max_i );
+                    if !is_other { result.insert( max_i ); }
+                } else {
+                    panic!("unexpected {} {} {}", xi, yi, zi)
+                }
+            }
+        }
+    }
+    match result.len() {
+        0 => None,
+        1 => Some( *result.iter().nth(0).unwrap() ),
+        _  => panic!( "too many results" )
+    }
+}
+
 fn get_collision_time( a: PVA, b: PVA ) -> Option<usize> {
     let mut pa = a.0;
     let mut pb = b.0;
@@ -48,71 +110,41 @@ fn get_collision_time( a: PVA, b: PVA ) -> Option<usize> {
     let mut vb = b.1;
     let aa = a.2;
     let ab = b.2;
-    let mut prev_distance = distance(pa, pb);
-    let mut when : usize = 0;
-    if prev_distance == 0 { return Some(when) };
-    loop {
+    let max_t = approx_collision_time( a, b );
+    if max_t.is_none() { return None; }
+    for t in 1 .. ( max_t.unwrap() + 2 ) as usize {
         // update velocities
         va = add( va, aa );
         vb = add( vb, ab );
 
         // update positions
-        when += 1;
         pa = add( pa, va );
         pb = add( pb, vb );
-        let new_distance = distance(pa, pb);
-        if new_distance >= prev_distance {return None;}
-        if new_distance == 0 {return Some(when);}
-        prev_distance = new_distance;
-    }
-}
-/*
-fn round_time(t:f64) -> f64 {
-    const N : f64 = 1000.0;
-    return ( N * t ).round() / N;
-}
 
-// x = x0 + (v0+0.5)*t + (a*0.5)*t*t
-// based on Ramanujan summation (1+2+3...)
-fn collision_time_on_axis( pva0: Point, pva1: Point ) -> Vec<f64> {
-    let dp : f64 = ( pva0.0 - pva1.0 ) as f64;
-    let dv : f64 = ( pva0.1 - pva1.1 ) as f64;
-    let da : f64 = ( pva0.2 - pva1.2 ) as f64;
-    let c = dp;
-    let b = dv;
-    let a = da * 0.5;
-    let det = b*b - 4.0*a*c;
-    if det < 0.0 { return [].to_vec(); }
-    if det == 0.0 { return [ round_time( -b / (2.0*a) ) ].to_vec(); }
-    let sr = det.sqrt();
-    let t0 = round_time((-b+sr) / (2.0*a) );
-    let t1 = round_time((-b-sr) / (2.0*a) );
-    if t0 > 0.0 && t1 > 0.0 { return [t0,t1].to_vec(); }
-    if t0 > 0.0 { return [t0].to_vec(); }
-    return [t1].to_vec();
-}
-
-fn get_collision_time1( a: PVA, b : PVA ) -> Option<f64> {
-    let x = collision_time_on_axis(( a.0.0, a.1.0, a.2.0 ), ( b.0.0, b.1.0, b.2.0 ) );
-    if x.len() == 0 { return None; }
-    let y = collision_time_on_axis(( a.0.1, a.1.1, a.2.1 ), ( b.0.1, b.1.1, b.2.1 ) );
-    if y.len() == 0 { return None; }
-    let z = collision_time_on_axis(( a.0.2, a.1.2, a.2.2 ), ( b.0.2, b.1.2, b.2.2 ) );
-    if z.len() == 0 { return None; }
-    if x.len() == 1 && y.len() == 1 && z.len() == 1 {
-        let t = x[0];
-        if t == y[0] && t == z[0] { return Some(t); }
-        return None;
+        if distance(pa, pb) == 0 { return Some(t); }
     }
-    for t in x.iter() {
-        if y.iter().any( |i| *i == *t ) && z.iter().any( |i| *i == *t ) { return Some(*t); }
-    }
-    return None;
+    None
 }
-*/
 
 pub fn to_pva(data : &str) -> Vec<PVA> {
     return data.lines().map( parse ).collect();
+}
+
+fn min_time( d : &Vec<PVA>, all : &HashSet<usize> ) -> Option<usize> {
+    let mut t : Option<usize> = None;
+    for i in all {
+        for j  in all {
+            if i > j {
+                let ti = get_collision_time( d[*i], d[*j] );
+                t = match ( t, ti ) {
+                    ( Some(a), Some(b) ) => Some( a.min(b) ),
+                    ( None, Some(b) ) => Some(b),
+                    ( a, _ ) => a
+                }
+            }
+        }
+    }
+    t
 }
 
 pub fn task12(data : &str) -> ( usize, usize ) {
@@ -127,13 +159,30 @@ pub fn task12(data : &str) -> ( usize, usize ) {
     // XXX dirty - for clean way we need to sort filter also by velocity and position
     let a1 = a.last().unwrap().0;
 
-    // let p0_unique : HashSet<Point> = d.iter().map(|pva| pva.0 ).collect();
-
     let all : HashSet<usize> = id.iter().map( |i| i.0 ).collect();
+    let mut left = all.clone();
+    loop {
+        let min_collision_t: Option<usize> = min_time( &d, &left );
+        if min_collision_t.is_none() { break; }
+        let t : usize = min_collision_t.unwrap();
+        let after : HashSet<usize> = left.iter()
+            .filter( |i| !left.iter().any(|j|
+                 {
+                     if *j == **i { return false; }
+                     let tj = get_collision_time( d[**i], d[*j] );
+                     if tj.is_none() { return false; }
+                     tj.unwrap() == t
+                 }
+            ) ).map( |i| *i )
+            .collect();
+        left = after;
+    }
+    /*
     let sure_left: HashSet<&usize> = all.iter()
         .filter( |i| all.iter()
             .any( |j| *j != **i && get_collision_time( d[**i], d[*j] ).is_some() ) == false
         ).collect();
-
-    (a1, sure_left.len())
+    (a1, sure_left.len() )
+    */
+    (a1, left.len() )
 }
