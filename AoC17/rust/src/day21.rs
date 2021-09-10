e58c77aab8d9e5fb2f53b399c7075a0f9c60fd88
Yury Fedorov <yury.fedorov@gmail.com>
2021-09-10 06:25:01 +0200

    AoC17 Day21 Part 1 draft

--- AoC17/rust/src/day21.rs
+++ AoC17/rust/src/day21.rs
@@ -1,3 +1,39 @@
+/*
+use std::collections::HashMap;
+
+type Point = (i32,i32);
+type Point2 = (Point,Point); // segment, within segment
+type Image = HashMap<Point,char>;
+type Image2 = HashMap<Point2,char>;
+
+fn rotate_left( image : Image ) -> Image { image } // TODO
+fn flip_vertically( image : Image ) -> Image { image } // TODO
+fn flip_horizontally( image : Image ) -> Image { image } // TODO
+
+fn equal( a : &Image, b : &Image) -> bool { false }  // TODO
+
+fn to_image( data : &str ) -> Image {
+    let lines : Vec<&str> = data.split_terminator( '/' ).collect();
+    let mut result : Image = Image::new();
+    let n = i.len();
+    for x in 0 .. n {
+        for y in 0 .. n {
+            let c = lines[y].chars().nth(x).unwrap();
+            result.insert( (x as i32,y as i32), c );
+        }
+    }
+    result
+}
+*/
 pub fn task12( data : &str ) -> (i32,i32) {
+    /*
+    let init = ".#./..#/###";
+    let init_image = to_image( init );
+    let mut image = init_image;
+    for _ in 0 .. 2 { //  TODO 5 iterations
+        let n = sqrt( image.len() ) as usize;
+        let is_by_2 = n % 2 == 0;
+    }
+    */
     (0,0)
 }
