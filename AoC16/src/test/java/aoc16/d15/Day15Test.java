ad86dedf3cdbc42fa8277b21012c3791e9e320db
Yury Fedorov <yury.fedorov@gmail.com>
2021-09-21 15:02:22 +0200

    AoC16 - migration from Java 11 to Java 17 LTS (records)

--- AoC16/src/test/java/aoc16/d15/Day15Test.java
+++ AoC16/src/test/java/aoc16/d15/Day15Test.java
@@ -1,7 +1,7 @@
 package aoc16.d15;
 
 import aoc16.common.IOUtil;
-import org.javatuples.Pair;
+import aoc16.common.Point;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -17,19 +17,19 @@ public class Day15Test {
         return position == 0;
     }
 
-    static Optional<Integer> whereBouncedAway( final int t0, List<Pair<Integer,Integer>> disks ) {
+    static Optional<Integer> whereBouncedAway( final int t0, List<Point> disks ) {
         for ( int i = 0; i < disks.size(); i++ ) {
             final var disk = disks.get(i);
-            if ( !fallsThroughSlot( t0 + i + 1, disk.getValue0(), disk.getValue1() ) ) return Optional.of(i);
+            if ( !fallsThroughSlot( t0 + i + 1, disk.x(), disk.y() ) ) return Optional.of(i);
         }
         return Optional.empty();
     }
 
     @Test
     public void test() {
-        final var disks = new ArrayList<Pair<Integer,Integer>>();
-        disks.add( Pair.with( 5, 4 ) );
-        disks.add( Pair.with( 2, 1 ) );
+        final var disks = new ArrayList<Point>();
+        disks.add( new Point( 5, 4 ) );
+        disks.add( new Point( 2, 1 ) );
         Assert.assertEquals( 5, getT(disks) );
     }
 
@@ -38,18 +38,18 @@ public class Day15Test {
         final var input = IOUtil.input("d15");
         final var pattern = Pattern.compile(
                 "^Disc #(\\d+) has (\\d+) positions; at time=0, it is at position (\\d+).$");
-        final var disks = new ArrayList<Pair<Integer,Integer>>();
+        final var disks = new ArrayList<Point>();
         for ( final var l : input ) {
             final var m = pattern.matcher(l);
             Assert.assertTrue( "Unexpected format: " + l, m.find() );
-            disks.add( Pair.with( Integer.parseInt( m.group(2) ), Integer.parseInt( m.group(3) ) ) );
+            disks.add( new Point( Integer.parseInt( m.group(2) ), Integer.parseInt( m.group(3) ) ) );
         }
         Assert.assertEquals( "answer 1", 148737, getT(disks) );
-        disks.add( Pair.with(11,0) );
+        disks.add( new Point(11,0) );
         Assert.assertEquals( "answer 2", 2353212, getT(disks) );
     }
 
-    private int getT(ArrayList<Pair<Integer, Integer>> disks) {
+    private int getT(ArrayList<Point> disks) {
         for (  int t = 0; true; t++ ) { if ( whereBouncedAway( t, disks).isEmpty() ) return t; }
     }
 }
