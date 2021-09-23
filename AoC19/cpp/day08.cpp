bb2465e7cfa613803725d8ea1dd4ef78eda97c14
Yury Fedorov <yury.fedorov@gmail.com>
2021-09-23 23:33:52 +0200

    AoC19 - Day08 - Part2 solved

--- AoC19/cpp/day08.cpp
+++ AoC19/cpp/day08.cpp
@@ -1,4 +1,5 @@
 #include "common.h"
+#include <iostream>
 
 using namespace std;
 
@@ -13,24 +14,51 @@ namespace day08 {
         return result;
     }
 
+    constexpr int width = 25;
+    constexpr int height = 6;
+    constexpr int count_on_one_level = width * height;
+
     auto answer1(const auto & image ) {
-        const auto width = 25;
-        const auto height = 6;
-        const auto count_on_one_level = width * height;
         const int count_level = image.size() / count_on_one_level;
-        const auto level_id = rv::iota( 0, count_level ) | r::to_vector;
-        using LevelDataPair = pair<int,LevelData>;
-        const vector< LevelDataPair > data = level_id | rv::transform( [&image](const int level)
-            { return LevelDataPair( level, count_on_level(image, level * count_on_one_level, count_on_one_level ) ); } )
+        const vector< LevelData > data = rv::iota( 0, count_level )
+            | rv::transform( [&image](const int level)
+                { return count_on_level(image, level * count_on_one_level, count_on_one_level ); } )
             | r::to_vector;
         const auto min_zero = r::min_element(data,
-            [](const LevelDataPair & a, const LevelDataPair & b){ return a.second.at('0') < b.second.at('0'); });
-        const LevelData & m = min_zero->second;
+            [](const LevelData & a, const LevelData & b){ return a.at('0') < b.at('0'); });
+        const LevelData & m = *min_zero;
         return m.at('1') * m.at('2');
     }
 
-    int answer2(const auto &  ) {
-        return  0;
+    char at( const auto & image, int index ) {
+        constexpr char transparent = '2';
+        constexpr char black = '0';
+        constexpr char white = '1';
+        const int count_level = image.size() / count_on_one_level;
+        // The layers are rendered with the first layer in front and the last layer in back.
+        const auto level_id = rv::iota( 0, count_level );
+        for ( auto id : level_id )  {
+            const auto global_index = id * count_on_one_level + index;
+            const auto at_level = image[global_index];
+            if ( at_level == transparent ) continue;
+            assert( at_level == black || at_level == white );
+            return at_level == black ? ' ' : '#';
+        }
+        return '?';
+    }
+
+    vector<string> answer2(const auto & image ) {
+        const auto image_as_line = rv::iota( 0, count_on_one_level )
+                | rv::transform( [&image](const int index) { return at( image, index); } )
+                | r::to_vector;
+        return rv::iota(0, height)
+                | rv::transform( [&image_as_line](const  int line)
+                    {
+                        const auto i0 = image_as_line.begin() + ( line * width );
+                        const auto i1 = i0 + width;
+                        return string( i0, i1 );
+                    } )
+                | r::to_vector;
     }
 }
 
@@ -46,7 +74,8 @@ TEST_CASE( "Day08", "[08]" ) {
     }
 
     SECTION( "08-2" ) {
-        REQUIRE( answer2(line) == -2 );
+        const auto a2 = answer2(line);
+        r::for_each( a2, [](const auto &  l) { cout << l << endl; } ); // prints KCGEC
+        REQUIRE( true );
     }
 }
-
