da319325591ad3dff164aedd55cb564fa02de8d1
Yury Fedorov <yury.fedorov@gmail.com>
2021-09-26 12:51:23 +0200

    AoC19 - Day10 - Part1 - not working draft

--- AoC19/cpp/day10.cpp
+++ AoC19/cpp/day10.cpp
@@ -6,6 +6,7 @@ namespace day10 {
 
     using Point =  pair<int,int>;
     using Map = vector<string>;
+    using Positions = set<Point>;
 
     char at( const Map & map, int x, int y ) noexcept { return map[y][x]; }
     char at( const Map & map, const Point & p ) noexcept { return at(map, p.first, p.second); }
@@ -13,14 +14,15 @@ namespace day10 {
     // https://en.wikipedia.org/wiki/Prime_number
     constexpr array<int,10> primes = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29 };
 
-    auto common_denominator( int a, int b ) noexcept {
+    int common_denominator( int a, int b ) noexcept {
         const auto aa = abs(a);
         const auto ab = abs(b);
         const auto mi0 = min( aa, ab );
         auto mi = mi0;
         auto ma = max( aa, ab );
-        if ( mi < 2 ) return ma;
-        const int half_mi = mi >> 1;
+        if ( mi == 0 ) return ma;
+        if ( mi == 1 ) return 1;
+        const int half_mi = mi >> 1; // divide by two
         for ( const auto prime : primes
             | rv::filter( [half_mi](const auto p) { return p <= half_mi; } ) ) {
             while (true) {
@@ -36,17 +38,42 @@ namespace day10 {
         return ( mi0 == mi ) ? 1 : ( mi0 / mi );
     }
 
-    // constexpr char asteroid = '#';
+    constexpr char asteroid = '#';
 
     using Point = pair<int,int>;
 
-    auto answer1( const Map & /*map*/ ) {
-        // TODO - normalization x,y pair
-        // TODO - put normalized x,y pairs in set
-        // TODO - size of set
-        set<Point> passed;
-
+    int count_seen( const Positions & asteroids, Point from ) noexcept {
+        Positions passed;
+        for ( const auto & p : asteroids ) {
+            int dx = p.first - from.first;
+            int dy = p.second - from.second;
+            if ( dx == 0 && dy == 0 ) continue;
+            const int cd = common_denominator( dx, dy );
+            assert( dx <= cd );
+            assert( dx % cd == 0 );
+            assert( dy <= cd );
+            assert( dy % cd == 0 );
+            dx /= cd;
+            dy /= cd;
+            passed.insert( { dx, dy } );
+        }
         return (int)passed.size();
+    }
+
+    int answer1( const Map & map ) {
+        const int y_size = map.size();
+        const int x_size = map[0].size();
+        Positions all_asteriods;
+        for ( auto x : rv::iota(0, x_size) ) {
+            for ( auto y : rv::iota(0, y_size) ) {
+                const Point p {x,y};
+                if ( at(map, p ) == asteroid ) { all_asteriods.insert(p); }
+            }
+        }
+        const auto seen = all_asteriods
+            | rv::transform([&]( const auto & a ) { return count_seen( all_asteriods, a ); } )
+            | r::to_vector;
+        return r::max( seen );
     }
 
     auto answer2( const Map & ) {
