d664cd6ff6370a3648ca48eb2beecfe051212cc3
Yury Fedorov <yury.fedorov@gmail.com>
2021-11-26 21:47:51 +0100

    Create Day14Test.java

---
+++ AoC19/java/src/test/java/aoc19/Day14Test.java
@@ -0,0 +1,17 @@
+package aoc19;
+
+import org.junit.Test;
+
+import java.util.*;
+import java.util.stream.*;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class Day14Test {
+    @Test
+    public void solution() {
+        assertEquals( "answer 1", 0, 1);
+        assertEquals( "answer 2", 0, 2 );
+    }
+}
