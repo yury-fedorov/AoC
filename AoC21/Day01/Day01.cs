552363601223c0c0ed20b1a95f9dfeab471d512c
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-04 06:00:59 +0100

    AoC21 Day01 refactoring

--- AoC21/Day01/Day01.cs
+++ AoC21/Day01/Day01.cs
@@ -2,28 +2,26 @@
 
 public class Day01Test
 {
-    static int Count( int [] values, int n, Func<int,int> toValue ) {
-        Option<int> prev = None;
-        int count = 0;
-        for( var i = 0; i < n; i++ ) {
-            var value = toValue(i);
-            count += prev.Match( 
-                Some: p  => value > p ? 1 : 0,
-                None: () => 0 
-            );
-            prev = Some(value);
-        }
-        return count;
-    }
+    record CountState( int Count, Option<int> Prev ) {}
+    static int Count<TE>( IEnumerable<TE> values, Func<TE,int> toValue ) =>
+        values.Aggregate( new CountState(0, None), (a,v) => new CountState( 
+            a.Count + a.Prev.Match( Some: p  => toValue(v) > p ? 1 : 0, None: () => 0 ),
+            Some( toValue(v) ) ) 
+        ).Count;
+
+    record Triple( int A, int B, int C ) {}
 
     [TestCase("Day01/input.txt")]
     public async Task Test(string file) {
         var lines = await App.ReadLines(file);
         var values = lines.Select( int.Parse ).ToArray();
 
-        Count(values, values.Length, (i) => values[i]).Should().Be(1301, "answer 1");
+        Count<int>(values, _ => _ ).Should().Be(1301, "answer 1");
 
-        var f = (int i) => values[i] + values[i+1] + values[i+2];
-        Count(values, values.Length - 2, f ).Should().Be(1346, "answer 2");
+        IEnumerable<Triple> zip3 = values.Zip( values.Skip(1) ).Zip( values.Skip(2) )
+        .Select( t => new Triple( t.Item1.Item1, t.Item1.Item2,  t.Item2 ) );
+
+        var f = ( Triple t ) => t.A + t.B + t.C;
+        Count<Triple>(zip3, f ).Should().Be(1346, "answer 2");
     }
 }
