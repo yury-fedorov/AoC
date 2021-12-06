68c20283a6d1b07c477e4d0e63b88d77683ae805
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-06 07:45:39 +0100

    AoC21 Day06 refactored

--- AoC21/Day06/Day06.cs
+++ AoC21/Day06/Day06.cs
@@ -3,24 +3,25 @@
 public class Day06Test
 {
     record Pair( int Age, long Count ) {}
-    static IEnumerable<Pair> Next( Pair pair ) 
-        => pair.Age == 0 
+    static IEnumerable<Pair> Next( Pair pair ) => pair.Age == 0 
         ? new [] { new Pair( 6, pair.Count ), new Pair( 8, pair.Count ) } 
         : new [] { new Pair( pair.Age - 1, pair.Count ) };
+
+    static long Answer( IEnumerable<Pair> dictionary, int days ) {
+        for ( int day = 0; day < days; day++ ) {
+            dictionary = dictionary.SelectMany( Next )
+                .GroupBy( _ => _.Age, (age, agg) => new Pair( age, agg.Select(_ => _.Count).Sum() ) );
+        }
+        return dictionary.Select(_ => _.Count).Sum();
+    }
 
     [TestCase("Day06/input.txt")]
     public async Task Test(string file) {
         var lines = await App.ReadLines(file);
         var dictionary = lines.First().Split(",").Select( int.Parse ).GroupBy( _ => _ )
-            .Select( _ => new Pair( _.Key, _.Count() ) );
-        Console.WriteLine( string.Join( ',',  dictionary.Select(  p => $"{p.Age} -> {p.Count}" ).ToArray() ) );
-        for ( int day = 0; day < 256; day++ ) {
-            dictionary = dictionary.SelectMany( Next )
-                .GroupBy( _ => _.Age, (age, agg) => new Pair( age, agg.Select(_ => _.Count).Sum() ) );
-            //Console.WriteLine( string.Join( ',',  dictionary.Select(  p => $"{p.Age} -> {p.Count}" ).ToArray() ) );
-        }
-        // 80 // dictionary.Select(_ => _.Count).Sum().Should().Be(346063, "answer 1");
+            .Select( _ => new Pair( _.Key, _.Count() ) ).ToList();
 
-        dictionary.Select(_ => _.Count).Sum().Should().Be(0, "answer 2");
+        Answer(dictionary, 80).Should().Be(346063, "answer 1");
+        Answer(dictionary, 256).Should().Be(1572358335990L, "answer 2");
     }
 }
