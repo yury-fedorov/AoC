6d9432b4d28eda4114d9dc2ba18b60d134960405
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-11 14:46:12 +0100

    AoC19 universal data path detection

--- AoC19/java/src/test/java/aoc19/IOUtil.java
+++ AoC19/java/src/test/java/aoc19/IOUtil.java
@@ -1,17 +1,23 @@
 package aoc19;
 
-import org.junit.Assert;
-
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public abstract class IOUtil {
     public static List<String> input(String day ) { return inputByPath(day + ".txt"); }
     public static List<String> inputByPath(String file ) {
         try {
-            return Files.readAllLines( Path.of( "/Users/fedoy/projects/AoC/AoC19/data", file ) );
+            final var rootDir = System.getProperty("user.dir");
+            var path = Paths.get( rootDir ).toAbsolutePath();
+            while ( true ) {
+                var path1 = path.getParent();
+                if ( !path1.toString().contains("AoC19") ) break;
+                path = path1;
+            }
+            return Files.readAllLines( Path.of( path.toString(),"data", file ) );
         } catch (IOException e) {
             throw new RuntimeException("Error reading file: " + file, e );
         }
