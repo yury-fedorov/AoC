81e17909fea83c32d8f5729097a21f87681ffde8
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-11 07:26:31 +0100

    AoC21 Day11 solved

--- AoC21/Day11/Day11.cs
+++ AoC21/Day11/Day11.cs
@@ -6,10 +6,8 @@ public class Day11Test
     record Point( int X, int Y ) {}
 
     static IEnumerable<Point> Adjusent( int [][] map, Point point ) {
-        var xn = map.First().Length;
-        var yn = map.Length;
-        var isXBelow = (point.X + 1) < xn;
-        var isYBelow = (point.Y + 1) < yn;
+        var isXBelow = (point.X + 1) < map.First().Length;
+        var isYBelow = (point.Y + 1) < map.Length;
 
         if ( point.X > 0 ) yield return new Point( point.X - 1, point.Y );
         if ( point.Y > 0 ) yield return new Point( point.X, point.Y - 1 );
@@ -24,15 +22,14 @@ public class Day11Test
 
     static int Step( int [][] map ) {
         var flashed = new HashSet<Point>(); 
-        var toAdjust = new HashSet<Point>();
+        var toAdjust = new List<Point>();
         var xn = map.First().Length;
         var yn = map.Length;
         for ( int y = 0; y < yn; y++ ) {
             for ( int x = 0; x < xn; x++ ) {
-                var level = map[y][x];
-                var newLevel = ( level + 1 > 9 ) ? 0 : ( level + 1 );
-                map[y][x] = newLevel;
-                if ( newLevel == 0 ) {
+                var level = map[y][x] + 1;
+                map[y][x] = level > 9 ? 0 : level;
+                if ( level > 9 ) {
                     var p = new Point( x, y );
                     if ( flashed.Add( p ) ) {
                         // An octopus can only flash at most once per step
@@ -42,13 +39,12 @@ public class Day11Test
             }
         }
         while ( toAdjust.Any() ) {
-            var nextWave = new HashSet<Point>();
+            var nextWave = new List<Point>();
             foreach ( var p in toAdjust ) {
                 if ( flashed.Contains(p) ) continue;
-                var level = map[p.Y][p.X];
-                var newLevel = ( level + 1 > 9 ) ? 0 : ( level + 1 );
-                map[p.Y][p.X] = newLevel;
-                if ( newLevel == 0 ) {
+                var level = map[p.Y][p.X] + 1;
+                map[p.Y][p.X] = level > 9 ? 0 : level;
+                if ( level > 9 ) {
                     if ( flashed.Add( p ) ) {
                         // An octopus can only flash at most once per step
                         Adjusent( map, p ).ToList().ForEach( _ => nextWave.Add( _ ) );
@@ -61,21 +57,22 @@ public class Day11Test
         return flashed.Count();
     }
 
-    [TestCase("Day11/sample1.txt", 1)]
-    //  [TestCase("Day11/input.txt", 100)]
+    // [TestCase("Day11/sample1.txt", 2)]
+    [TestCase("Day11/input.txt", 100)]
     public async Task Test(string file, int steps) {
         var lines = await App.ReadLines(file);
         var map = lines.Select( _ => _.ToCharArray().Select( ch => ch - '0' ).ToArray() ).ToArray();
-        long flashes = 0;
-        for ( int i = 0; i < steps; i++ ) {
-            flashes += Step(map);
+        var size = map.Length * map.First().Length; // total size of the map
+        int flashes = 0;
+        for ( int step = 0; true; step++ ) {
+            int justFlashed = Step(map);
+            if ( step < steps ) flashes += justFlashed;
+            if ( justFlashed == size ) {
+                ( step + 1 ).Should().Be(329, "answer 2");
+                break;
+            }
         }
 
-        Console.WriteLine();
-        Enumerable.Range( 0, map.Length ).ToList()
-            .ForEach( y => Console.WriteLine( String.Concat( Enumerable.Range( 0, map.First().Length)
-                .Select( x => map[y][x] ) ) ) );
-
-        flashes.Should().Be(-1, "answer 1"); // 2281 - not right
+        flashes.Should().Be(1627, "answer 1");
     }
 }
