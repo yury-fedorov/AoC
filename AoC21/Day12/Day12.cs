aeeb3ce22d1533566215bfef55605a900b79a2c6
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-12 06:06:42 +0100

    AoC21 Day12 start

---
+++ AoC21/Day12/Day12.cs
@@ -0,0 +1,14 @@
+﻿using LanguageExt;
+
+namespace AoC21;
+
+public class Day12Test
+{
+    [TestCase("Day12/input.txt")]
+    public async Task Test(string file) {
+        var lines = await App.ReadLines(file);
+        var values = lines.Select( int.Parse ).ToArray();
+
+        1.Should().Be(0, "answer 1");
+    }
+}
