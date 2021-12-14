41bf4999562ed8d475a0e93af9c46bbc4dca52ab
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-14 08:54:51 +0100

    AoC21 Day14 - part1 solved but rewritten and new implementation does not work

--- AoC21/Day14/Day14.cs
+++ AoC21/Day14/Day14.cs
@@ -1,49 +1,83 @@
 namespace AoC21;
 
-public class Day02Test
+public class Day14Test
 {
-    enum Direction { Down, Forward, Up }
+    record Rule( string Pair, char Element ) {} 
 
-    static readonly IDictionary<string, Direction> Map = new Dictionary<string, Direction> {
-        {@"down (\d+)", Direction.Down}, {@"forward (\d+)", Direction.Forward}, {@"up (\d+)", Direction.Up}
-    };
+    static Rule ParseRule( string line ) {
+        var p = line.Split( " -> " );
+        return new Rule( p[0], p[1].First() );
+    }
+/*
+    static string Next1( string polymer, IDictionary<string,Rule> rules ) {
+        var parts = polymer.Zip( polymer.Skip(1) )
+            .Select( p => { 
+                var pair = $"{p.Item1}{p.Item2}"; 
+                var isPair = rules.TryGetValue( pair, out var rule );
+                return isPair ? $"{rule.Element}{p.Item2}" : $"{p.Item2}";
+            } );
+        return polymer[0] + string.Concat( parts );
+    }
+    */
 
-    static (Direction direction, int distance) Read( string line ) {
-        foreach ( var map in Map ) {
-            var matches = Regex.Matches(line, map.Key);
-            foreach (Match match in matches) {
-                return ( map.Value, Convert.ToInt32(match.Groups[1].Value) );
+    static IEnumerable<( string Pair, long Count) > Split( string pair, long count, IDictionary<string,Rule> rules ) {
+        var isPair = rules.TryGetValue( pair, out var rule );
+            if ( isPair ) {
+                yield return ( $"{pair[0]}{rule.Element}", count);
+                yield return ( $"{rule.Element}{pair[1]}", count);
             }
-        }
-        throw new Exception( $"wrong input {line}" );
+            yield return ( pair, count );
     }
 
-    [TestCase("Day02/input.txt")]
+    static IDictionary<string,long> NextD( IDictionary<string,long> polymer, IDictionary<string,Rule> rules ) 
+        => polymer.SelectMany( pair => Split( pair.Key, pair.Value, rules ) )
+         .GroupBy( _ => _.Item1 )
+         .ToDictionary( _ => _.Key, _ => _.Select( p => p.Item2 ).Sum() );
+
+/*
+    static IEnumerable<char> Next( IEnumerable<char> polymer, IDictionary<string,Rule> rules ) {
+        var parts = polymer.Zip( polymer.Skip(1) )
+            .SelectMany( p => { 
+                var pair = $"{p.Item1}{p.Item2}"; 
+                var isPair = rules.TryGetValue( pair, out var rule );
+                return isPair ? new [] { rule.Element, p.Item2 } : new [] { p.Item2 } ;
+            } );
+        return Enumerable.Concat( polymer.Take(1), parts );
+    }
+    */
+
+    static long Result(string start, IDictionary<string,Rule> rules, int steps) {
+        /*
+        IEnumerable<char> polymer = start;
+        for ( int i = 0; i < steps; i++ ) {
+            polymer = Next(polymer, rules);           
+        }
+        var frequency =polymer.GroupBy(_ => _).ToDictionary( _ => _.Key, _ => _.LongCount() );
+        var max = frequency.Values.Max();
+        var min = frequency.Values.Min();
+        return (max - min);
+        */
+
+        IDictionary<string,long> polymer = start.Zip( start.Skip(1) ).Select( p => $"{p.Item1}{p.Item2}" )
+            .GroupBy( _ => _ ).ToDictionary( _ => _.Key, _ => _.LongCount() );
+
+        for ( int i = 0; i < steps; i++ ) {
+            polymer = NextD(polymer, rules);           
+        }
+        var frequency = Enumerable.Concat( new [] { ( start.First(), 1L ) },
+                polymer.Select( p => ( p.Key[1], p.Value ) ) )
+            .GroupBy(_ => _).ToDictionary( _ => _.Key, _ => _.LongCount() );
+        var max = frequency.Values.Max();
+        var min = frequency.Values.Min();
+        return (max - min);
+    }
+
+    [TestCase("Day14/input.txt")]
     public async Task Test(string file) {
         var lines = await App.ReadLines(file);
-        var instructions = lines.Select( Read ).ToArray();
-
-        int x = 0; 
-        int y = 0;
-        foreach ( var i in instructions ) {
-            switch( i.direction ) {
-                case Direction.Down: y += i.distance; break;
-                case Direction.Up: y -= i.distance; break;
-                case Direction.Forward: x += i.distance; break;
-            }
-        }
-        ( x * y ).Should().Be(1698735, "answer 1");
-
-        x = 0; 
-        y = 0;
-        int aim = 0;
-        foreach ( var i in instructions ) {
-            switch( i.direction ) {
-                case Direction.Down: aim += i.distance; break;
-                case Direction.Up: aim -= i.distance; break;
-                case Direction.Forward: x += i.distance; y += i.distance * aim; break;
-            }
-        }
-        ( x * y ).Should().Be(1594785890, "answer 2");
+        var polymer = lines.First();
+        var rules = lines.Skip(2).Select( ParseRule ).ToDictionary( _ => _.Pair, _ => _ );
+        Result( polymer, rules, 10 ).Should().Be( 2509, "answer 1" );
+        Result( polymer, rules, 40 ).Should().Be( -1, "answer 2" ); //  2691399
     }
 }
