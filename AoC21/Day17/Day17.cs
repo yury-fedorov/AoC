226950285f8d9f90093491447d73cc2941242403
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-17 08:52:53 +0100

    AoC21 Day17 draft

---
+++ AoC21/Day17/Day17.cs
@@ -0,0 +1,12 @@
+﻿namespace AoC21;
+
+public class Day17Test
+{
+    // sample: target area: x=20..30, y=-10..-5
+    // my: target area: x=79..137, y=-176..-117
+    // [TestCase("x=20..30, y=-10..-5")] // sample
+    [TestCase("x=79..137, y=-176..-117")] // my input
+    public async Task Test(string targetArea) {
+        1.Should().Be(0, "answer 1");
+    }
+}
