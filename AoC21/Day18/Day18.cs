0e237d537b2d8e0d124146eaecbd1ae134ebe6ef
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-18 15:39:13 +0100

    AoC21 Day18 draft written

--- AoC21/Day18/Day18.cs
+++ AoC21/Day18/Day18.cs
@@ -2,22 +2,26 @@
 
 public class Day18Test
 {
-    class Option{
-        public Pair? Pair; 
-        public int? Value;
-        public bool IsValue => Value != null; 
+    abstract class Object {
+        public Pair? Pair => this as Pair;
+        public int? Number => ( this as Number )?.IntValue;
+        public bool IsNumber => this is Number; 
     }
-    class Pair { 
-        public Option Left;
-        public Option Right;
-        public bool IsSimple => Left.IsValue && Right.IsValue; 
+    class Number : Object {
+        public int IntValue;
     }
-    static long Magnitude(Option pair) => pair.Value != null ? pair.Value.Value 
-        : pair.Pair != null ? Magnitude( pair.Pair ) : throw new Exception( "bad pair");
+    class Pair : Object { 
+        public Object Left;
+        public Object Right;
+        public Object Get( bool isLeft ) => isLeft ? Left : Right;
+        public bool IsSimple => Left.IsNumber && Right.IsNumber; 
+    }
+    static long Magnitude(Object obj) => obj is Number number ? number.IntValue  : obj is Pair pair ? Magnitude( pair ) 
+        : throw new Exception( "bad pair");
     static long Magnitude(Pair pair) => ( 3 * Magnitude( pair.Left ) ) + ( 2 * Magnitude( pair.Right ) );
 
-    static Option MakeO(int value) => new Option { Value = value };
-    static Option MakeO(Pair pair) => new Option { Pair = pair };
+    static Object MakeO(int value) => new Number { IntValue = value };
+    static Object MakeO(Pair pair) => pair;
     static Pair Make(int Left, int Right) => new Pair { Left = MakeO(Left), Right = MakeO(Right) };
     static Pair Make(Pair Left, int Right) => new Pair { Left = MakeO(Left), Right = MakeO(Right) };
     static Pair Make(int Left, Pair Right) => new Pair { Left = MakeO(Left), Right = MakeO(Right) };
@@ -34,7 +38,7 @@ public class Day18Test
 
     enum Phase { Start, Term1, Term2 }
 
-    static Option ParseOption( string text ) => IsPair(text) ? MakeO( ParsePair(text) ) : MakeO( int.Parse(text) );
+    static Object ParseOption( string text ) => IsPair(text) ? MakeO( ParsePair(text) ) : MakeO( int.Parse(text) );
 
     static int MaxNested( Pair p ) => p.IsSimple ? 0 
         : 1 + Math.Max( p.Left.Pair  != null ? MaxNested( p.Left.Pair  ) : 0, 
@@ -48,7 +52,7 @@ public class Day18Test
     [TestCase("[[[[[9,8],1],2],3],4]", 4)]
     public void TestMaxNested(string pair, int max ) => MaxNested(ParsePair(pair)).Should().Be(max);
 
-    static string ToString( Option value ) => $"{value.Value?.ToString() ?? ToString(value.Pair)}";
+    static string ToString( Object value ) => $"{value.Number?.ToString() ?? ToString(value.Pair)}";
 
     static string ToString( Pair pair ) => $"[{ToString(pair.Left)},{ToString(pair.Right)}]";
 
@@ -58,7 +62,7 @@ public class Day18Test
     [TestCase(11, "[5,6]")]
     public void TestSplit( int value, string split ) => ToString(Split(value)).Should().Be(split);
 
-    static bool Is10OrAbove(Option value) => value.IsValue ? value.Value >= 10 : Is10OrAbove(value.Pair); 
+    static bool Is10OrAbove(Object value) => value.IsNumber ? value.Number >= 10 : Is10OrAbove(value.Pair); 
     static bool Is10OrAbove(Pair pair) => Is10OrAbove(pair.Left) || Is10OrAbove(pair.Right);
 
     [TestCase("[[[[0,7],4],[15,[0,13]]],[1,1]]", true)]
@@ -84,8 +88,12 @@ public class Day18Test
                     else term += ch;
                     break;
                 case Phase.Term2:
-                    if ( ch == ']' && level == 0 )
-                        return new Pair { Left = ParseOption(leftTerm), Right = ParseOption(term) };
+                    if ( ch == ']' && level == 0 )  {
+                        var left = ParseOption(leftTerm);
+                        var right = ParseOption(term);
+                        var pair = new Pair { Left = left, Right = right };
+                        return pair;
+                    }
                     level += dLevel;
                     term += ch;
                     break;
@@ -93,19 +101,110 @@ public class Day18Test
         }
         throw new Exception( "bad pair" );
     }
+    static Number? LeftMost10OrAbove( Object value ) {
+        if (value is Number number ) { if ( number.IntValue >= 10 ) return number; }
+        else { if ( Is10OrAbove( value ) ) return LeftMost10OrAbove( value.Pair ); }
+        return null;
+    }
+
+    static Number? LeftMost10OrAbove( Pair pair ) {
+        if ( pair.IsSimple ) return Is10OrAbove(pair) ? ( pair.Left.Number >= 10 ? pair.Left : pair.Right ) as Number : null;
+        return LeftMost10OrAbove( pair.Left ) ?? LeftMost10OrAbove( pair.Right );
+    }
+
+    static Object? LeftMostExploding( Pair tree ) => null; // it is always a pair
+
+    // XXX: navigation
+    record ParentResult( Object Parent, bool IsLeft ) {}
+
+    static ParentResult? Parent( Object tree, Object obj ) {
+        if ( tree == obj ) return null; // if object is head of the tree, there is no parent
+        if ( tree is Pair treeAsPair ) {
+            if ( treeAsPair.Left == obj ) return new ParentResult( tree, true );
+            if ( treeAsPair.Right == obj ) return new ParentResult( tree, false );
+            if ( treeAsPair.Left is Pair pair ) {
+                var result = Parent( pair, obj );
+                if ( result != null ) return result;
+            }
+            if ( treeAsPair.Right is Pair rightPair ) return Parent( rightPair, obj );
+            return null;
+        }
+        return null; // if tree is a number and does not match the object (checked already) - no solution
+    }
+
+    static bool IsLeft(Pair parent, Object node) => parent.Left == node;
+    static bool IsRight(Pair parent, Object node) => parent.Right == node;
+
+    // left / right access by enum
+    // inverse left to right and back
+    static Number? FindValue( Pair tree, bool isLeft ) {
+        if ( isLeft ) {
+            if ( tree.Left is Number leftNumber ) return leftNumber;
+            var left = FindValue( tree.Left.Pair, isLeft );
+            if ( left != null ) return left;
+            if ( tree.Right is Number rightNumber ) return rightNumber;
+            return FindValue( tree.Right.Pair, isLeft );
+        } else {
+            if ( tree.Right is Number rightNumber ) return rightNumber;
+            var right = FindValue( tree.Right.Pair, isLeft );
+            if ( right != null ) return right;
+            if ( tree.Left is Number leftNumber ) return leftNumber;
+            return FindValue( tree.Left.Pair, isLeft );
+        }
+    }
+
+    static Number? FirstOn( Pair tree, Object exploding, bool isOnLeft ) {
+        while ( true )  {
+            var parent = Parent(tree, exploding);
+            if ( parent == null ) return null; // not found anything
+            Pair parentPair = (Pair)parent.Parent;
+            if ( parent.IsLeft != isOnLeft ) {  // exploding node is the right node of the parent
+                // left side down could be value or tree
+                // first down as it is closer then up
+                var down = FindValue( ( isOnLeft ? parentPair.Left : parentPair.Right ).Pair, isOnLeft );
+                if ( down != null ) return down;
+            }
+            // if we did not find somewhere withing the direct parent of exploding
+            // we go one level above (if parent node is right - we search the left one down)
+            // if not we skip and go one level above
+            exploding = parentPair;
+        }
+    }
+
+    static Object Replace(Object tree, Object oldObj, Object newObj ) {
+        var parent = Parent( tree, oldObj );
+        if (parent == null) {
+            // is it the head
+            if ( tree == oldObj ) return newObj;
+            throw new Exception( "parent was not found" );
+        }
+        return tree;
+    } 
 
     static Pair SumPairs( Pair left, Pair right ) {
-        var result = Make( left, right );
-        // TODO: reduce a snailfish number
-        while ( IsNestedInside4Pairs(result) ) {
-            // TODO: the leftmost such pair explodes
+        Pair result = Make( left, right );
+        // reduce a snailfish number
+        while ( true ) {
+            // the leftmost such pair explodes
             //  To explode a pair, the pair's left value is added to the first regular number to the left of the exploding pair 
             // (if any), and the pair's right value is added to the first regular number to the right of the exploding pair 
-            // (if any). Exploding pairs will always consist of two regular numbers. Then, the entire exploding pair is 
-            // replaced with the regular number 0.
+            // (if any). Exploding pairs will always consist of two regular numbers.
+            var exploding = LeftMostExploding(result); // option that is always a pair
+            if ( exploding == null ) break;
+            Pair pair = exploding.Pair;
+            var leftOption = FirstOn(result, exploding, true );
+            if ( leftOption != null ) leftOption.IntValue += ((Number)pair.Left).IntValue;
+            var rightOption = FirstOn(result, exploding, false );
+            if ( rightOption != null ) rightOption.IntValue += ((Number)pair.Right).IntValue;
+            // Then, the entire exploding pair is replaced with the regular number 0.
+            result = (Pair)Replace(result, exploding, MakeO(0));
         }
-        while (Is10OrAbove(result)) {
-            // TODO: the leftmost such regular number splits
+        while (true) {
+            // the leftmost such regular number splits
+            Number ? value = LeftMost10OrAbove(result);
+            if ( value == null ) break;
+            int intValue = value.IntValue;
+            result = (Pair)Replace(result, value, Split(intValue));
         } 
         return result;
     }
