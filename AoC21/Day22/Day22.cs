3b11dab76b670f4d393306e1fe0cf2c3a752a688
Yury Fedorov <yury.fedorov@gmail.com>
2021-12-24 10:55:56 +0100

    AoC21 Day22 changes

--- AoC21/Day22/Day22.cs
+++ AoC21/Day22/Day22.cs
@@ -4,7 +4,9 @@ public class Day22Test
 {
     enum Axis {X,Y,Z}
     static readonly Axis[] AxisList = { Axis.X, Axis.Y, Axis.Z };
-    record Point( int X, int Y, int Z ) {}
+    record Point( int X, int Y, int Z ) {
+        public int Get( Axis a ) => a switch { Axis.X => X, Axis.Y => Y, Axis.Z => Z }; 
+    }
     record Cube( Point Min, Point Max ) {
         public int Size(Axis a) {
             var r = Range(a);
@@ -50,7 +52,7 @@ public class Day22Test
             .Select( z => new Point( x, y, z ) ) ) );
 
     static bool Within( Range r, int x ) => x >= r.Min &&  x <= r.Max;
-    static (int Min, int Max)? Intersect( Range a, Range b ) {
+    static Range? Intersect( Range a, Range b ) {
         var isAOnLeft = a.Min <= b.Min;  // a starts on the left
         var a1 = isAOnLeft ? a : b;
         var b1 = isAOnLeft ? b : a;
@@ -60,7 +62,24 @@ public class Day22Test
         if ( a1.Max >= b1.Min ) {
             // intersec
         } else return null;
-        return ( b1.Min, Math.Min( a1.Max, b1.Max ) );
+        return new Range( b1.Min, Math.Min( a1.Max, b1.Max ) );
+    }
+
+    record RangeOn( Range Range, bool On ) {}
+
+    static IEnumerable<RangeOn> Cut(RangeOn a, RangeOn b) {
+        var i = Intersect( a.Range, b.Range ); // what is in b
+        if ( i != null ) {
+            var left = Math.Min( a.Range.Min, b.Range.Min );
+            var right = Math.Max( a.Range.Max, b.Range.Max );
+            var rl = new Range( left, i.Min - 1 );
+            var rr = new Range( 1 + i.Max, right );
+            var isBOnLeft = Within( b.Range, rl.Min );
+            yield return new RangeOn( rl, isBOnLeft ? b.On : a.On );
+            yield return new RangeOn( i, b.On );
+            var isBOnRight = Within( b.Range, rr.Min );
+            yield return new RangeOn(rr, isBOnRight ? b.On : a.On );
+        }
     }
 
     static bool IsInside( Range big, Range small ) => big.Min <= small.Min && big.Max >= small.Max;
@@ -84,9 +103,27 @@ public class Day22Test
         if ( IsNotTouching(before, newCube) )
             return newOn  ? new [] { before, newCube } // no issue they are not touching each other
                           : new [] { before }; // they are not touching and newCube is off
-        
-        // TODO - implement
-        throw new Exception("TODO implement");
+
+        Func<Axis,IEnumerable<RangeOn> > cut = (a) => Cut( 
+            new RangeOn( new Range( before.Min.Get(a), before.Max.Get(a) ), true ), 
+            new RangeOn( new Range( newCube.Min.Get(a), newCube.Max.Get(a) ), newOn ) )
+            .Where( _ => _.Range.Min > _.Range.Max ) // only valid ranges
+            .ToArray();
+
+        var x = cut(Axis.X);
+        var y = cut(Axis.Y);
+        var z = cut(Axis.Z);
+
+        var result = new List<Cube>();
+        foreach ( var xi in x ) 
+            foreach ( var yi in y )
+                foreach ( var zi in z ) {
+                    var on = xi.On && yi.On && zi.On;
+                    result.Add( new Cube( new Point( xi.Range.Min, yi.Range.Min, zi.Range.Min ),
+                        new Point( xi.Range.Max, yi.Range.Max, zi.Range.Max ) ) );
+                }
+
+        return result;
     }
 
     static void Execute1( bool on, Cube range, HashSet<Point> setOn ) {
@@ -98,6 +135,9 @@ public class Day22Test
         RangePoints(limitedCube).ToList().ForEach( a );
     }
 
+    // 2,758,514,936,282,235
+    // 9,223,372,036,854,775,807
+
     [TestCase("Day22/input.txt")]
     public async Task Test(string file) {
         var lines = await App.ReadLines(file);
@@ -107,6 +147,11 @@ public class Day22Test
         instructions.ForEach( i => Execute1( i.On, i.Cube, setOn ) );
         setOn.Count().Should().Be(603661, "answer 1");
         */
+        var regions = new [] { instructions.First() };
+        foreach ( var i in instructions ) {
+            // ??? when many small regions applied on a big one
+            // if big contains entire small -> 
+        }
         0.Should().Be(-2, "answer 2");
     }
 }
