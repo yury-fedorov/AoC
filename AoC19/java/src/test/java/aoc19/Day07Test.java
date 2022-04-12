70dde4bc28d3e26157bf420b7634ffd50a43401a
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-12 23:19:22 +0200

    AoC19 Day 7 Part 1 working in Java

--- AoC19/java/src/test/java/aoc19/Day07Test.java
+++ AoC19/java/src/test/java/aoc19/Day07Test.java
@@ -2,18 +2,67 @@ package aoc19;
 
 import aoc19.computer.IntcodeComputer;
 import org.junit.Test;
+import org.apache.commons.collections4.iterators.PermutationIterator;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static org.junit.Assert.assertEquals;
 
 public class Day07Test {
-    //  @Test
-    public void solution() {
-        final var memory = IntcodeComputer.loadMemory(IOUtil.input("day07").get(0));
+    record SingleRunResult( long result, boolean isHalt ) {}
+
+    static SingleRunResult singleRun(ArrayList<Long> code, long setting, long input) {
+        var in = new LinkedBlockingQueue<Long>();
+        in.add(setting);
+        in.add(input);
         var out = new LinkedBlockingQueue<Long>();
-        IntcodeComputer.run( memory, 1L, out);
-        assertEquals( "answer 1", -1L, out.remove().longValue() ); //1102 too low
+        final var isHalt = IntcodeComputer.run(code, in, out);
+        return new SingleRunResult( out.poll(), isHalt );
+    }
+
+    static long allRuns( ArrayList<Long> code, List<Long> settings, boolean isTask1 ) {
+        long input = 0;
+        long maxInput = Long.MIN_VALUE;
+        do {
+            boolean isHalt = false;
+            for ( var setting : settings ) {
+                var result = singleRun( code, setting, input );
+                input = result.result;
+                isHalt = result.isHalt;
+            }
+            maxInput = Math.max( input, maxInput );
+            if (isHalt) break; // halt
+        } while ( !isTask1 ); // task 2 till halt
+        return isTask1 ? input : maxInput;
+    }
+
+    static long solution( ArrayList<Long> code, List<Long> settings, boolean isTask1 ) {
+        long result = -1L;
+        var i = new PermutationIterator(settings);
+        while (i.hasNext()) {
+            var curSettings = i.next();
+            final long r = allRuns(code, curSettings, isTask1);
+            result = Math.max( result, r );
+        }
+        return result;
+    }
+
+    static long answer1( ArrayList<Long> code ) {
+        var settings = List.of(0L,1L,2L,3L,4L);
+        return solution(code, settings, true);
+    }
+
+    static long answer2( ArrayList<Long> code ) {
+        var settings = List.of(5L,6L,7L,8L,9L);
+        return solution(code, settings, false);
+    }
+
+    @Test
+    public void solution() {
+        final var code = IntcodeComputer.loadMemory(IOUtil.input("day07").get(0));
+        assertEquals( "answer 1", 70597L, answer1(code) );
+        // TODO - assertEquals( "answer 2", -2L, answer2(code) );
     }
 }
--- AoC19/java/src/test/java/aoc19/computer/IntcodeComputer.java
+++ AoC19/java/src/test/java/aoc19/computer/IntcodeComputer.java
@@ -5,7 +5,6 @@ import aoc19.computer.operation.NothingOnEvent;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.stream.Collectors;
