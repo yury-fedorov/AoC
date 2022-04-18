604cef52e4e0bae70df66f3dbf87b132a8f3b196
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-18 08:48:26 +0200

    AoC19 Day 24 Part 1 code draft - rating ready #33

--- AoC19/java/src/test/java/aoc19/Day24Test.java
+++ AoC19/java/src/test/java/aoc19/Day24Test.java
@@ -1,11 +1,18 @@
 package aoc19;
 
 import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.junit.Assert.assertEquals;
 
 public class Day24Test {
 
     public final static int SIZE = 5; // field dimension (x or y)
+
+    public final static char BUG = '#';
+    public final static char SPACE = '.'; // empty space
 
     record Point( int x, int y ) {
         public long getBiodiversityRating() {
@@ -14,12 +21,30 @@ public class Day24Test {
         }
     }
 
+    static long getBiodiversityRating( Map<Point,Character> map ) {
+        return map.entrySet().stream()
+                .filter( e -> e.getValue() == BUG )
+                .mapToLong( e -> e.getKey().getBiodiversityRating() )
+                .sum();
+    }
+
     @Test
     public void solution() {
         assertEquals( 1, new Point(0,0).getBiodiversityRating() );
         assertEquals( 2, new Point(1,0).getBiodiversityRating() );
         assertEquals( 4, new Point(2,0).getBiodiversityRating() );
         assertEquals( 32768, new Point(0,3).getBiodiversityRating() );
-        assertEquals( "answer 1", -1, 0 );
+
+        var input = IOUtil.input("day24");
+        var map = new HashMap<Point,Character>();
+        for (int x = 0; x < SIZE; x++ ) {
+            for (int y = 0; y < SIZE; y++ ) {
+                map.put( new Point(x,y), input.get(y).charAt(x) );
+            }
+        }
+
+        var rating = getBiodiversityRating(map);
+
+        assertEquals( "answer 1", -1, rating );
     }
 }
