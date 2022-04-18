107397def117e2840999d608e168112aa89963a4
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-18 08:22:20 +0200

    AoC19 Day 24 Part 1 code draft #33

---
+++ AoC19/java/src/test/java/aoc19/Day24Test.java
@@ -0,0 +1,11 @@
+package aoc19;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Day24Test {
+    @Test
+    public void solution() {
+        assertEquals( "answer 1", -1, 0 );
+    }
+}
