c2427e796c30bf483fb524e974a24fb0e2d60a70
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-18 08:36:06 +0200

    AoC19 Day 24 Part 1 code draft - rating ready #33

--- AoC19/java/src/test/java/aoc19/Day24Test.java
+++ AoC19/java/src/test/java/aoc19/Day24Test.java
@@ -1,20 +1,28 @@
 package aoc19;
 
 import org.junit.Test;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 public class Day24Test {
 
-    // TODO - to cover with tests?
+    public final static int SIZE = 5; // field dimension (x or y)
+
     record Point( int x, int y ) {
         public long getBiodiversityRating(int maxX) {
-            var index = ( maxX * y ) + x + 1;
-            return (long)Math.pow( index, 2 );
+            var index = ( maxX * y ) + x;
+            return (long)Math.pow( 2, index );
+        }
+        public long getBiodiversityRating() {
+            return getBiodiversityRating(SIZE);
         }
     }
 
     @Test
     public void solution() {
+        assertEquals( 1, new Point(0,0).getBiodiversityRating() );
+        assertEquals( 2, new Point(1,0).getBiodiversityRating() );
+        assertEquals( 4, new Point(2,0).getBiodiversityRating() );
+        assertEquals( 32768, new Point(0,3).getBiodiversityRating() );
         assertEquals( "answer 1", -1, 0 );
     }
 }
