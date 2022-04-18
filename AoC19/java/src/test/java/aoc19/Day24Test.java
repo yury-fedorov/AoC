979fb5e3856a1075976867d94e81f0494a2c6079
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-18 08:27:13 +0200

    AoC19 Day 24 Part 1 code draft #33

--- AoC19/java/src/test/java/aoc19/Day24Test.java
+++ AoC19/java/src/test/java/aoc19/Day24Test.java
@@ -4,6 +4,15 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class Day24Test {
+
+    // TODO - to cover with tests?
+    record Point( int x, int y ) {
+        public long getBiodiversityRating(int maxX) {
+            var index = ( maxX * y ) + x + 1;
+            return (long)Math.pow( index, 2 );
+        }
+    }
+
     @Test
     public void solution() {
         assertEquals( "answer 1", -1, 0 );
