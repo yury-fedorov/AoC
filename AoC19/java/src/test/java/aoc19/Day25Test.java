5fc2cad893964d73e652cc51728b1f9bef6ef568
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-26 22:25:29 +0200

    AoC19 Day 25 console app solved #39

--- AoC19/java/src/test/java/aoc19/Day25Test.java
+++ AoC19/java/src/test/java/aoc19/Day25Test.java
@@ -61,11 +61,20 @@ take astronaut ice cream
 south
 inv
 """;
-        for ( var c : navigation.toCharArray() ) {
-            in.add( (long)c );
-        }
+        toIn( in, navigation );
         final var things = getThings(navigation);
         // A loud, robotic voice says "Alert! Droids on this ship are lighter than the detected value!" and you are ejected back to the checkpoint.
+
+        final var flagsLimits = (long)Math.pow(2, things.size());
+        for ( var flags = 0L; flags < flagsLimits; flags++ ) {
+            toIn( in, generate(things, flags) );
+            comp.run();
+            var lastOut = readOut(out).toString();
+            if ( !lastOut.contains( "Droids on this ship are") ) {
+                break;
+                // Oh, hello! You should be able to get in by typing 33624080 on the keypad at the main airlock.
+            }
+        }
 
         while ( comp.run() != IntcodeComputer.RunPhase.HALT ) {
             out(out);
@@ -79,6 +88,12 @@ inv
         out(out);
     }
 
+    static void toIn( Queue<Long> in, CharSequence commands ) {
+        for ( var c : commands.toString().toCharArray() ) {
+            in.add( (long)c );
+        }
+    }
+
     static List<String> getThings(String navigation) {
         return Arrays.stream(navigation.split( "\n" ))
                 .filter(s -> s.contains("take "))
@@ -86,12 +101,29 @@ inv
                 .toList();
     }
 
-    static void out( Queue<Long> out ) {
+    static CharSequence readOut(Queue<Long> out ) {
         final var text = new StringBuilder();
         while ( !out.isEmpty() ) {
             final long c = out.poll();
             text.append((char)c);
         }
-        System.out.println(text);
+        return text;
+    }
+
+    static void out( Queue<Long> out ) {
+        System.out.println( readOut(out) );
+    }
+
+    static CharSequence generate( List<String> things, long flags ) {
+        final var text = new StringBuilder();
+        for ( var t : things ) text.append( "drop ").append( t ).append( '\n' ); // drop everything
+        for ( var i = things.size(); --i >= 0; ) {
+            final var t = things.get(i);
+            final var flag = 1 << i;
+            if ( ( flags & flag ) != 0 )
+                text.append("take ").append(t).append('\n');
+        }
+        text.append("south\n");
+        return text;
     }
 }
