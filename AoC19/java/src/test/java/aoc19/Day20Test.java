f8a244ad8c7b19047ee0485e51b139d8d81777a4
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-26 23:12:46 +0200

    AoC19 Day 20 draft #34

---
+++ AoC19/java/src/test/java/aoc19/Day20Test.java
@@ -0,0 +1,15 @@
+package aoc19;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class Day20Test {
+
+    @Test
+    public void solution() {
+        final var input = IOUtil.input("day20");
+        assertEquals( "answer 1", -1, 0);
+        assertEquals( "answer 2", -2, 0 );
+    }
+}
