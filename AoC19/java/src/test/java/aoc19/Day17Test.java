566664c0a3985735e9b8770a1c5242601ee35480
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-27 21:11:50 +0200

    AoC19 Day 17 Part 1 solved #31

--- AoC19/java/src/test/java/aoc19/Day17Test.java
+++ AoC19/java/src/test/java/aoc19/Day17Test.java
@@ -3,9 +3,12 @@ package aoc19;
 import aoc19.computer.IntcodeComputer;
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
 
 import static aoc19.computer.AsciiUtil.out;
+import static aoc19.computer.AsciiUtil.readOut;
 import static org.junit.Assert.assertEquals;
 
 public class Day17Test {
@@ -17,8 +20,34 @@ public class Day17Test {
         final var out = new LinkedBlockingQueue<Long>();
         final var comp = new IntcodeComputer(memory, in, out);
         comp.run();
-        out(out);
-        assertEquals( "answer 1", -1, 0);
+        final var mapAsString = readOut(out).toString();
+        final var map = Arrays.stream(mapAsString.split( "\n" )).map( s -> s.trim() ).toList();
+        final var maxX = map.get(0).length();
+        final var maxY = map.size();
+        long answer1 = 0;
+        for ( var y = 0; y < maxY; y++ ) {
+            for ( var x = 0; x < maxX; x++ ) {
+                answer1 += isIntersection(map, x, y) ? x * y : 0;
+            }
+        }
+
+        assertEquals( "answer 1", 4600, answer1);
         assertEquals( "answer 2", -2, 0 );
+    }
+
+    static char get( List<String> map, int x, int y ) {
+        if ( y < 0 || y >= map.size() ) return OPEN_SPACE;
+        final var line = map.get(y);
+        if ( x < 0 || x >= line.length() ) return OPEN_SPACE;
+        return line.charAt(x);
+    }
+    static final char SCAFFOLD = '#';
+    static final char OPEN_SPACE = '.';
+    static boolean isIntersection( List<String> map, int x, int y ) {
+        if ( get( map, x, y ) != SCAFFOLD ) return false;
+        if ( get( map, x + 1, y ) != SCAFFOLD ) return false;
+        if ( get( map, x - 1, y ) != SCAFFOLD ) return false;
+        if ( get( map, x, y + 1 ) != SCAFFOLD ) return false;
+        return get( map, x, y - 1 ) == SCAFFOLD;
     }
 }
