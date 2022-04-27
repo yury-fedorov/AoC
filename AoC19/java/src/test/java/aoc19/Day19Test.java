083f34796850af839297899fc0749a468044c98a
Yury Fedorov <yury.fedorov@gmail.com>
2022-04-27 22:12:40 +0200

    AoC19 Day 19 Part 1 solved #35

--- AoC19/java/src/test/java/aoc19/Day19Test.java
+++ AoC19/java/src/test/java/aoc19/Day19Test.java
@@ -12,13 +12,30 @@ public class Day19Test {
 
     @Test
     public void solution() {
+
+        final var N = 50;
+        var answer1 = 0L;
+        for ( var y = 0; y < N; y++ ) {
+            for ( var x = 0; x < N; x++ ) {
+                final var r = get( x, y );
+                answer1 += r == 1 ? 1 : 0;
+            }
+        }
+
+        assertEquals( "answer 1", 131, answer1);
+        assertEquals( "answer 2", -2, 0 );
+    }
+
+    static long get( long x, long y ) {
         final var  memory = IntcodeComputer.loadMemory(IOUtil.input("day19").get(0));
         final var in = new LinkedBlockingQueue<Long>();
         final var out = new LinkedBlockingQueue<Long>();
-        final var comp = new IntcodeComputer(memory, in, out);
-        comp.run();
-        out(out);
-        assertEquals( "answer 1", -1, 0);
-        assertEquals( "answer 2", -2, 0 );
+        final var c = new IntcodeComputer(memory, in, out);
+        c.in( x );
+        c.in( y );
+        var r = c.run();
+        if ( c.outSize() == 0 )
+            throw new RuntimeException( "Bad pair!" );
+        return c.out();
     }
 }
