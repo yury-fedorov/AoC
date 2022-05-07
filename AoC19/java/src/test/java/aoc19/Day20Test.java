cf2a71b92a6f9d586e4e4951ce9e896ff2d18a4c
Yury Fedorov <yury.fedorov@gmail.com>
2022-05-07 23:41:07 +0200

    AoC19 Day 20 Part 1 draft - doors read #34

--- AoC19/java/src/test/java/aoc19/Day20Test.java
+++ AoC19/java/src/test/java/aoc19/Day20Test.java
@@ -37,8 +37,42 @@ public class Day20Test {
                 else if ( Character.isUpperCase(c) ) letters.put( new Point(x,y), c );
             }
         }
-        // TODO based on letters and walkable - construct doors
+        initDoors(letters, doors, walkable);
         return new Maze(walkable, doors);
+    }
+
+    static boolean isClose(Point a, Point b) { return step(a).contains(b); }
+    static Point getPoint(Point a, Point b, Set<Point> walkable) {
+        final var s = new HashSet<Point>(step(a));
+        s.addAll(step(b));
+        s.removeAll(walkable);
+        return s.iterator().next();
+    }
+
+    static String getName(Point ap, char ac, Point bp, char bc) {
+        final var isSameX = ap.x == bp.x;
+        final var isAb = isSameX ? ap.y < bp.y : ap.x < bp.x;
+        return isAb ? ( "" + ac ) + bc : ( "" + bc ) + ac;
+    }
+
+    static void initDoors(Map<Point,Character> letters,
+                          Map<String,List<Point>> doors, Set<Point> walkable) {
+        final var points = new ArrayList<>( letters.keySet() );
+        while ( !points.isEmpty() ) {
+            final var a = points.remove(0);
+            Point b = null;
+            for ( var i : points ) {
+                if ( isClose(a, i) ) {
+                    b = i;
+                    points.remove(b);
+                    break;
+                }
+            }
+            final var name = getName( a, letters.get(a), b, letters.get(b) );
+            final var d = doors.getOrDefault( name, new ArrayList<>(2) );
+            d.add( getPoint( a, b, walkable ) );
+            doors.put(name, d);
+        }
     }
 
     @Test
