82757b5d39c9d3d7c91b5bf3124ab69d8cc5f9b1
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2022-10-01 22:17:54 +0200

    AoC19 Day20 Part2 - level introduction
    level is used in part 2 but to keep the algorithm as much generic, let's introduce it also for the solution of part 1

--- AoC19/java/src/test/java/aoc19/Day20Test.java
+++ AoC19/java/src/test/java/aoc19/Day20Test.java
@@ -9,6 +9,7 @@ import static org.junit.Assert.assertEquals;
 public class Day20Test {
 
     record Point( int x, int y ) {}
+    record Point3D(Point point, int level) {} // used in part 2
 
     static Set<Point> step(Point p) {
         return Set.of( new Point(p.x, p.y+1), new Point(p.x, p.y-1),
@@ -72,15 +73,17 @@ public class Day20Test {
 
     static int solution(boolean isPart1, Maze maze) {
         // TODO implement the algorithm
-        var next = Map.of( maze.doors.get("AA").get(0), 0 );
-        var minDistance = new HashMap<Point,Integer>();
+        int level = 0; // 0 - the outermost level
+        final var aa = maze.doors.get("AA").get(0);
+        var next = Map.of( new Point3D(aa, level), 0); // generalized next
+        var minDistance = new HashMap<Point3D,Integer>(); // generalized min distance
         final var zz = maze.doors.get("ZZ").get(0);
         // 1 is it possible to walk from AA to ZZ directly without portals?
         // this is the maximum path required to arrive
         // after this options are: look in the range of minimal path all reachable portals
         // try to go measure paths using them
         while (!next.isEmpty()) {
-            final var next1 = new HashMap<Point, Integer>();
+            final var next1 = new HashMap<Point3D, Integer>();
             for ( var n : next.entrySet() ) {
                 final var p = n.getKey();
                 final var minD = minDistance.getOrDefault( p, Integer.MAX_VALUE ).intValue();
@@ -90,27 +93,27 @@ public class Day20Test {
                 } else {
                     continue; // no sense to continue
                 }
-                final var p1 = new HashSet<>( step(p) );
+                final var p1 = new HashSet<>( step(p.point) );
                 p1.retainAll(maze.walkable);
                 // TODO
                 final var di = maze.doors.values().stream()
-                        .filter(c -> c.size() == 2 && c.contains(p) ).iterator();
+                        .filter(c -> c.size() == 2 && c.contains(p.point) ).iterator();
                 if ( di.hasNext() ) {
                     // we found a portal
                     p1.addAll( di.next() );
-                    p1.remove(p);
+                    p1.remove(p.point);
                 }
                 for ( var p1i : p1 ) {
                     final var d1 = d+1;
                     final var d1i = next1.getOrDefault(p1i, Integer.MAX_VALUE);
                     if ( d1 < d1i ){
-                        next1.put( p1i, d1 );
+                        next1.put( new Point3D(p1i,level), d1 );
                     }
                 }
             }
             next = next1;
         }
-        return minDistance.get(zz).intValue();
+        return minDistance.get( new Point3D( zz, 0 ) ).intValue();
     }
 
     @Test
