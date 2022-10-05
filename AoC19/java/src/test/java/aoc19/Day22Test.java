120fa38ac1e371b33c98ee8569725bf1ae0b24c6
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2022-10-05 21:38:04 +0200

    AoC 19 Day 22 Part 2 - alpha 1 - 2100 days to run
    all tests are green but it is too slow

--- AoC19/java/src/test/java/aoc19/Day22Test.java
+++ AoC19/java/src/test/java/aoc19/Day22Test.java
@@ -70,78 +70,68 @@ public class Day22Test {
     static class DealWithIncrement extends Shuffling {
         final long _n; // n is de facto in the range: [2, 72]
         // every iteration (we have n) starts with a new shift (unique in the range 0 [0,n) )
-        final Map<Long,Long> _shiftToIteration = new HashMap<Long,Long>();
+        final Map<Long,Long> _shiftToIteration = new HashMap<>();
         public DealWithIncrement( long size, long n ) { super( size ); _n = n; }
-        long getNewPosition( long oldPosition ) {
-            final var pn = oldPosition * _n;
-            final long iteration = pn / _size;
-            final long newPosition = pn % _size;
-            final long shift = newPosition % _n;
-            _shiftToIteration.put( shift, iteration );
-            return newPosition;
-        }
+        long getNewPosition( long oldPosition ) { return oldPosition * _n % _size; }
         
         long getIteration( long shift ) {
-            // TODO - trigger initialization of the map if necessary
-            return _shiftToIteration.get( shift );
+            final long DEFAULT = -1L;
+            long result = _shiftToIteration.getOrDefault(shift, DEFAULT);
+            if ( result == DEFAULT ) {
+                // initialization of the map
+                final long iteration = _size / _n;
+                long position = iteration / 2;
+                for ( var i = 0; i < _n; i++ ) {
+                    final var pn = position * _n;
+                    final long newPosition = pn % _size;
+                    final long posShift = newPosition % _n;
+                    _shiftToIteration.put( posShift, (long)i );
+                    position += iteration;
+                }
+                result = _shiftToIteration.get(shift);
+            }
+            return result;
         }
         
         long getOldPosition( long newPosition ) {
-            // TODO - this could be optimized and moved to class member fields
-            final long sn = _size / _n;
-            final long smn = _size % _n; // amount of times we have n+1 in one iteration
-            
             final var shift = newPosition % _n; // shift
             final var iteration = getIteration( shift );
-            
-            // final var a = newPosition / _n;
-            return ( ( newPosition + _size * iteration ) / _n ) /* % _size */; 
-            // "oldPosition < s/n" -> newPosition / n
-            // newPosition < n -> sn + newPosition
-            // return ( a + ( b * sn ) + Math.min( smn, b ) ) % _size;
+            return ( newPosition + _size * iteration ) / _n;
         }
     }
     
     void testDealWithIncrease(int size, int n) {
         final var d = createDeck( size );
-        // cut cards for deck size 7
-        // cutCards(d, 3); // 3,4,5,6,0,1,2 -- 3 cards from head moved to tail
-        // cutCards(d,-2); // 5,6,0,1,2,3,4 -- 2 cards from tail moved to head
         final var c = "deal with increment " + n;
-        final var s = new DealWithIncrement(size, n);
-        dealWithIncrement(d, n); // 0,4,1,5,2,6,3
+        final Shuffling s = new DealWithIncrement(size, n);
+        dealWithIncrement(d, n);
         for ( int before = 0; before < size; before++ )
         {
-                final int testPosition = before;
-                final var afterPosition = s.getNewPosition(testPosition);
-                final int after = d.get( (int)afterPosition );
-                assertEquals( c + " before vs after -> before=" + before + " after=" + after, before, after );
-                assertEquals( c + " index inverse -> after position=" + afterPosition, s.getOldPosition( afterPosition ), testPosition );
+            final int testPosition = before;
+            final var afterPosition = s.getNewPosition(testPosition);
+            final int after = d.get( (int)afterPosition );
+            assertEquals( c + " before vs after -> before=" + before + " after=" + after, before, after );
+            assertEquals( c + " index inverse -> after position=" + afterPosition, s.getOldPosition( afterPosition ), testPosition );
         }
     }
 
     @Test
     public void solution() {
         final var SIZE1 = 10007;
-        // java long max = 9,223,372,036,854,775,807
+        // java long max = 9,223,372,036,854,775,807 -> * by [2,72] will not lead to overflow
         final var SIZE2  =       119_315_717_514_047L;
         final var TIMES2 = 101_741_582_076_661L;
         
+        testDealWithIncrease( 19, 3 );
         testDealWithIncrease( 7, 2 );
-        testDealWithIncrease( 11, 3 );
         testDealWithIncrease( SIZE1, 46 );
 
         final var input = IOUtil.input("day22");
         final var deck = createDeck(SIZE1);
         final var fastCommands = new ArrayList<Shuffling>( input.size() );
-        final var size = SIZE1;
-        final var isFixedTestValue = false;
-        final Optional<Integer> fixedTestPosition = isFixedTestValue ? Optional.of( 0 ) : Optional.empty();
+        final var size = SIZE2;
         for ( var command : input ) {
             final Shuffling shuffling;
-            final int testPosition = fixedTestPosition.orElse(
-                    (int)Math.min( (long)( Math.random() * size ) % size, Integer.MAX_VALUE ) );
-            final var before = deck.get( testPosition );
             if ( command.equals(NEW_STACK) ) {
                 dealIntoNewStack(deck);
                 shuffling = new DealIntoNewStack( size );
@@ -154,20 +144,20 @@ public class Day22Test {
                 dealWithIncrement(deck, n);
                 shuffling = new DealWithIncrement( size, n );
             } else throw new IllegalStateException();
-            final var afterPosition = shuffling.getNewPosition(testPosition);
-            if ( afterPosition <= Integer.MAX_VALUE ) {
-                // TODO - clean -> if ( command.startsWith( NEW_STACK ) || command.startsWith( "cut" ) )
-                {
-                    final var after = deck.get( (int)afterPosition );
-                    assertEquals( command + " before vs after", before, after );
-                    assertEquals( command + " index inverse", shuffling.getOldPosition( afterPosition ), testPosition );
-                }
-            }
             fastCommands.add( shuffling );
         }
         // what is the position of card 2019?
         assertEquals( "answer 1", 6831, deck.indexOf(2019));
-
-        // TODO assertEquals( "answer 2", -2, deck.get(2020-1).intValue() );
+        
+        if ( Config.isFast() ) return;
+        // TODO - in debug it is super slow (up to 2,100 days)
+        Collections.reverse(fastCommands);
+        long position = 2020 - 1;
+        for ( var i = 0L; i < TIMES2; i++ ) {
+            for ( var c : fastCommands ) {
+                position = c.getOldPosition( position );
+            }
+        }
+        assertEquals( "answer 2", -2L, position );
     }
 }
