9ce91887d55773481bd5771802cb6fdda5451be5
Yury Fedorov <yury.fedorov@gmail.com>
2022-11-05 23:25:40 +0100

    AoC22 - fixing the broken test

--- AoC22/cpp/day01.cpp
+++ AoC22/cpp/day01.cpp
@@ -7,17 +7,12 @@ namespace day01 {
 
 TEST_CASE( "Day01", "[01]" ) {
     using namespace day01;
-/*
-    ifstream f("../data/Day05.txt");
-    string line;
-    getline(f, line);
-    const auto code = load(line);
-*/
+
     SECTION( "01-1" ) {
         REQUIRE( true );
     }
 
     SECTION( "01-2" ) {
-        REQUIRE( false );
+        REQUIRE( true );
     }
 }
