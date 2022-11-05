bae40f9b25dc78f1a75e56c09022f4ee464367d4
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2022-11-05 23:36:32 +0100

    AoC22  - draft 1 (#69)
    New folder for AoC22 with the latest Ubuntu 22.04 LTS. Additional details about duration during test execution.

---
+++ AoC22/cpp/day01.cpp
@@ -0,0 +1,18 @@
+#include "common.h"
+
+using namespace std;
+
+namespace day01 {
+}
+
+TEST_CASE( "Day01", "[01]" ) {
+    using namespace day01;
+
+    SECTION( "01-1" ) {
+        REQUIRE( true );
+    }
+
+    SECTION( "01-2" ) {
+        REQUIRE( true );
+    }
+}
