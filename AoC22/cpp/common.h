8dbbdeda38c4b3b4419ca029d1a246e5efa60999
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-01 21:06:23 +0100

    AoC22 - Abseil - StrCat & StrSplit

--- AoC22/cpp/common.h
+++ AoC22/cpp/common.h
@@ -10,14 +10,12 @@
 #include <string>
 #include <string_view>
 #include <vector>
-// #include <format>
 
+#include "absl/strings/str_cat.h"
+#include "absl/strings/str_split.h"
 #include "gtest/gtest.h"
 
 // to hide the external dependencies
-// #include "fmt/format.h" // CLang does not support C++20 format library
-// #include "gsl/gsl_assert"
-// #include "gsl/span"
 // #include "range/v3/all.hpp" // CLang 12 does not support C++20 ranges
 // namespace r = std::ranges; // standard is std::ranges
 // namespace rv = std::ranges::views;
@@ -27,19 +25,9 @@
 inline bool is_fast_only() noexcept { return true; } // to filter out slow tests
 
 inline std::vector<std::string> ReadData(std::string_view file) noexcept {
-  // could be optimized with absl
-  std::string filename("../data/");
-  filename += file;
-  filename += ".txt";
-
-  // const auto filename = std::format( "../data/{}.txt", file ); // Clang does
-  // not support
-  std::ifstream f(filename);
-  std::string line;
-  std::vector<std::string> list;
-  while (!f.eof()) {
-    std::getline(f, line);
-    list.emplace_back(line);
-  }
-  return list;
+  const std::string filepath = absl::StrCat("../data/", file, ".txt");
+  std::ifstream f(filepath);
+  std::string content;
+  std::getline(f, content, '\0'); // read all file
+  return absl::StrSplit(content, '\n');
 }
