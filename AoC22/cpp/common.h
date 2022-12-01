802d940c90fb047d3d331ac9c5bc6b2694e60ae9
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-01 21:36:16 +0100

    AoC22 - common.h cleaning

--- AoC22/cpp/common.h
+++ AoC22/cpp/common.h
@@ -2,11 +2,8 @@
 
 #include <algorithm>
 #include <fstream>
-#include <iostream>
-#include <map>
 #include <numeric>
 #include <ranges>
-#include <span>
 #include <string>
 #include <string_view>
 #include <vector>
@@ -14,13 +11,6 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_split.h"
 #include "gtest/gtest.h"
-
-// to hide the external dependencies
-// #include "range/v3/all.hpp" // CLang 12 does not support C++20 ranges
-// namespace r = std::ranges; // standard is std::ranges
-// namespace rv = std::ranges::views;
-// namespace s = std; // namespace for span (defined in 3 different places:
-// C++20, ranges v3, GSL)
 
 inline bool is_fast_only() noexcept { return true; } // to filter out slow tests
 
