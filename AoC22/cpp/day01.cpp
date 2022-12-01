55834a8e8ec6e19259fb8bce7118e8b40aba6656
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2022-12-01 06:41:56 +0100

    AoC22 Day 1 Part 1 adjusted

--- AoC22/cpp/day01.cpp
+++ AoC22/cpp/day01.cpp
@@ -8,18 +8,21 @@ namespace day01 {
 TEST_CASE( "Day01", "[01]" ) {
     using namespace day01;
     
-    ifstream f("../data/day01.txt");
+    // ifstream f("../data/day01.txt");
+    ifstream f("../data/1.txt");
     string line;
     auto answer1 = 0; 
     auto sum = 0;
     while ( !f.eof() ) {
-        getline(f, line);
-        const auto value = stoi( line );
-        if ( value ) sum += value;
-        else { 
-            answer1 = max( sum, answer1 );
-            sum = 0;
-        }
+       getline(f, line);
+       if ( line.length() ) {
+            const auto value = stoi( line );
+            if ( value ) sum += value;
+            else { 
+                answer1 = max( sum, answer1 );
+                sum = 0;
+            }
+        }    
     }
     
     SECTION( "01-1" ) {
@@ -27,6 +30,6 @@ TEST_CASE( "Day01", "[01]" ) {
     }
 
     SECTION( "01-2" ) {
-        REQUIRE( false );
+        REQUIRE( true );
     }
 }
