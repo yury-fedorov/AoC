1b6254168d83bb014f342b355dbdb740ce20286b
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-04 06:45:38 +0100

    Update day04.cpp

--- AoC22/cpp/day04.cpp
+++ AoC22/cpp/day04.cpp
@@ -3,7 +3,7 @@
 
 TEST(AoC22, Day04) {
   const auto data = ReadData("04");
-// 67-84,66-87
+  // 67-84,66-87
 
   long answer1{0};
   int a, b, x, y;
@@ -11,15 +11,14 @@ TEST(AoC22, Day04) {
     if (line.length() <7) continua;
     sscanf( line.c_str(), "%d-%d,%d-%d", &a, &b, &c, &d);
     if (a > x) {
-std::swap(a, x);
-std::swap(b, y);
-} 
-// now a <= x
+       std::swap(a, x);
+       std::swap(b, y);
+    } 
+    // now a <= x
     // we search for: a...x...y...b
-    answer 1 += (y<=b)? 1:0;
+    answer1 += (y<=b)? 1:0;
   }
   
   EXPECT_EQ(answer1, 0);
   // EXPECT_EQ(sum_highest_n(3), 206643);
-
 }
