9f53b4fbf201633bd0f36227c7d350924a9201a7
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-04 08:03:36 +0100

    AoC22 Day4 solved from cell phone in Stuttgart

--- AoC22/cpp/day04.cpp
+++ AoC22/cpp/day04.cpp
@@ -1,15 +1,10 @@
-#include "absl/strings/numbers.h"
 #include "common.h"
-#include <set>
 
 TEST(AoC22, Day04) {
   const auto data = ReadData("04");
-  // 67-84,66-87
-
-  long answer1{0};
+  long answer1=0;
   long answer2=0;
   int a, b, x, y;
-  std::set<int> overlap;
   for (const std::string &line : data) {
     if (line.length() <7) continue;
     sscanf( line.c_str(), "%d-%d,%d-%d", &a, &b, &x, &y);
@@ -19,12 +14,9 @@ TEST(AoC22, Day04) {
     } 
     // now a <= x
     // we search for: a...x...y...b
-    answer1 += (a<x)? ( (y<=b)? 1:0 ) : (1) ;
-    for ( int i = x; i <= std::min(b, y); i++) 
-        overlap.insert(i);
+    answer1 += (a<x) ? ( y<=b ? 1:0 ) : 1;
     answer2 += (b >= x) ? 1 : 0;
   }
-  
   EXPECT_EQ(answer1, 582);
-  EXPECT_EQ(answer2,0);
+  EXPECT_EQ(answer2, 893);
 }
