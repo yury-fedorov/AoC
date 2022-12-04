51b21d06097c32e442667ca405403b11399caed5
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-04 07:39:31 +0100

    Update day04.cpp
    Part 2 draft

--- AoC22/cpp/day04.cpp
+++ AoC22/cpp/day04.cpp
@@ -1,5 +1,6 @@
 #include "absl/strings/numbers.h"
 #include "common.h"
+#include <set>
 
 TEST(AoC22, Day04) {
   const auto data = ReadData("04");
@@ -7,6 +8,7 @@ TEST(AoC22, Day04) {
 
   long answer1{0};
   int a, b, x, y;
+  std::set<int> overlap;
   for (const std::string &line : data) {
     if (line.length() <7) continue;
     sscanf( line.c_str(), "%d-%d,%d-%d", &a, &b, &x, &y);
@@ -17,8 +19,10 @@ TEST(AoC22, Day04) {
     // now a <= x
     // we search for: a...x...y...b
     answer1 += (a<x)? ( (y<=b)? 1:0 ) : (1) ;
+    for ( int i = x; i <= min(b, y); i++) 
+        overlap.insert(i);
   }
   
-  EXPECT_EQ(answer1, 0);
-  // EXPECT_EQ(sum_highest_n(3), 206643);
+  EXPECT_EQ(answer1, 582);
+  EXPECT_EQ(overlap.size(),0);
 }
