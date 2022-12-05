ab9de33f818f810e97adbaba3252a94882f3004c
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-05 06:11:51 +0100

    AoC22 Day04 reformat and simplify

--- AoC22/cpp/day04.cpp
+++ AoC22/cpp/day04.cpp
@@ -2,19 +2,20 @@
 
 TEST(AoC22, Day04) {
   const auto data = ReadData("04");
-  long answer1=0;
-  long answer2=0;
+  long answer1 = 0;
+  long answer2 = 0;
   int a, b, x, y;
   for (const std::string &line : data) {
-    if (line.length() <7) continue;
-    sscanf( line.c_str(), "%d-%d,%d-%d", &a, &b, &x, &y);
+    if (line.length() < 7)
+      continue;
+    sscanf(line.c_str(), "%d-%d,%d-%d", &a, &b, &x, &y);
     if (a > x) {
-       std::swap(a, x);
-       std::swap(b, y);
-    } 
+      std::swap(a, x);
+      std::swap(b, y);
+    }
     // now a <= x
     // we search for: a...x...y...b
-    answer1 += (a<x) ? ( y<=b ? 1:0 ) : 1;
+    answer1 += (y <= b) || (a == x) ? 1 : 0;
     answer2 += (b >= x) ? 1 : 0;
   }
   EXPECT_EQ(answer1, 582);
