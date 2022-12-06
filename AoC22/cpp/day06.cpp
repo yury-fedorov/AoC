41674a74b7386fc46f7380d6afcb2c5cac5e3855
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-06 07:29:06 +0100

    AoC22 Day06 solved with multiple solutions: vector, set, unordered_set and absl::flat_hash_set

--- AoC22/cpp/day06.cpp
+++ AoC22/cpp/day06.cpp
@@ -1,5 +1,8 @@
 #include "common.h"
-#include <set>
+#include <set> // 11 ms
+// #include <flat_set> // C++23 not supported yet
+// #include <unordered_set> // 23-11 ms
+// #include "absl/container/flat_hash_set.h" // takes 25 ms stable
 
 TEST(AoC22, Day06) {
   const auto data = ReadData("06")[0];
@@ -8,10 +11,29 @@ TEST(AoC22, Day06) {
                             size_t start) {
     const auto b = data.cbegin() + start;
     const auto e = b + sequence_length;
-    std::set<char> s(b, e);
+
+    // 7 ms
+    std::vector<char> s(sequence_length);
+    for ( auto i = b; i < e; i++ ) {
+        const auto j = std::ranges::upper_bound(s, *i); // first greater
+        if ( j > s.cbegin() ) {
+            // we have something to compare with
+            if ( *i == *(j - 1) ) return false;
+        }
+        s.insert(j, *i);
+    }
+    return true;
+
+    /* std::vector<char> s(b,e);
+    std::ranges::sort( s );
+    return s.cend() == std::adjacent_find( s.cbegin(), s.cend() ); // 10-12 ms
+    */
+    // std::set<char> s(b, e); // 11 ms
+    // std::unordered_set<char> s(b, e); // 12 ms
+    // absl::flat_hash_set<char> s(b, e); 25 ms
     return s.size() == sequence_length;
   };
-  const auto solution = [&is_unique] (std::string_view data,
+  const auto solution = [&is_unique](std::string_view data,
                                      size_t sequence_length) {
     const auto l = data.length() - sequence_length + 1;
     for (auto i = 0; i < l; i++) {
