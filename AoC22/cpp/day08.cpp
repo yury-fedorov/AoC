874f535537f226f8a62bc0ce7b119d3b5cfda118
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-08 07:12:43 +0100

    AoC22 Day08 Part1 solved

---
+++ AoC22/cpp/day08.cpp
@@ -0,0 +1,68 @@
+#include "absl/container/flat_hash_map.h"
+#include "absl/strings/numbers.h"
+#include "common.h"
+
+namespace day08 {
+bool is_border(int x, int y, int max_x, int max_y) {
+  return x == 0 || y == 0 || (x == max_x - 1) || (y == max_y - 1);
+};
+} // namespace day08
+
+TEST(AoC22, Day08) {
+  const auto data = ReadData("08");
+  using Point = std::pair<int, int>;
+  absl::flat_hash_map<Point, int> map;
+  int y = 0;
+  int max_x = 0;
+  for (const std::string &line : data) {
+    const auto ll = line.length();
+    if (ll == 0)
+      continue;
+    max_x = ll;
+    for (int x = 0; x < max_x; x++) {
+      map[{x, y}] = static_cast<int>(line[x] - '0');
+    }
+    y++;
+  }
+  const int max_y = y;
+  EXPECT_EQ(map.size(), max_x * max_y);
+  const auto is_visible_outside = [&map, max_x, max_y](const Point &p) {
+    // any tree on a border is visible
+    const int x = p.first;
+    const int y = p.second;
+    if (day08::is_border(x, y, max_x, max_y))
+      return true;
+    // now only internal trees to consider, so we need to look from 4 sides
+    const int h = map[p];
+    const std::array dxy = {std::make_pair(0, 1), std::make_pair(0, -1),
+                            std::make_pair(1, 0), std::make_pair(-1, 0)};
+    for (const auto &[dx, dy] : dxy) {
+      auto xi = x;
+      auto yi = y;
+      const auto hi = h;
+      // if the height goes down (it is still visible)
+      bool is_visible_in_this_direction = true;
+      while (true) {
+        xi += dx;
+        yi += dy;
+        const auto hj = map[{xi, yi}];
+        if (hj >= hi) {
+          is_visible_in_this_direction = false;
+          break;
+        }
+        // hi = hj;
+        if (day08::is_border(xi, yi, max_x, max_y))
+          break;
+      }
+      if (is_visible_in_this_direction)
+        return true;
+    }
+    return false;
+  };
+  long answer1{0};
+  for (const auto &[p, _] : map) {
+    answer1 += is_visible_outside(p) ? 1 : 0;
+  }
+  EXPECT_EQ(answer1, 1647);
+  // EXPECT_EQ(sum_highest_n(3), 206643);
+}
