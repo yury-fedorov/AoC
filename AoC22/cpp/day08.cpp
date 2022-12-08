19adf733430c15deb2a0a819ebcc09fd1d892c2d
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2022-12-08 09:40:56 +0100

    AoC22 Day08 solved

--- AoC22/cpp/day08.cpp
+++ AoC22/cpp/day08.cpp
@@ -1,26 +1,24 @@
 #include "absl/container/flat_hash_map.h"
-#include "absl/strings/numbers.h"
 #include "common.h"
 
 namespace day08 {
 bool is_border(int x, int y, int max_x, int max_y) {
   return x == 0 || y == 0 || (x == max_x - 1) || (y == max_y - 1);
 };
-} // namespace day08
+}  // namespace day08
 
 TEST(AoC22, Day08) {
-  const auto data = ReadData("08-sample");
+  const auto data = ReadData("08");
   using Point = std::pair<int, int>;
-  absl::flat_hash_map<Point, int> map;
+  absl::flat_hash_map<Point, char> map;
   int y = 0;
   int max_x = 0;
   for (const std::string &line : data) {
     const auto ll = line.length();
-    if (ll == 0)
-      continue;
+    if (ll == 0) continue;
     max_x = ll;
     for (int x = 0; x < max_x; x++) {
-      map[{x, y}] = static_cast<int>(line[x] - '0');
+      map[{x, y}] = line[x];
     }
     y++;
   }
@@ -30,8 +28,7 @@ TEST(AoC22, Day08) {
     // any tree on a border is visible
     const int x = p.first;
     const int y = p.second;
-    if (day08::is_border(x, y, max_x, max_y))
-      return true;
+    if (day08::is_border(x, y, max_x, max_y)) return true;
     // now only internal trees to consider, so we need to look from 4 sides
     const int h = map[p];
     const std::array dxy = {std::make_pair(0, 1), std::make_pair(0, -1),
@@ -50,52 +47,45 @@ TEST(AoC22, Day08) {
           is_visible_in_this_direction = false;
           break;
         }
-        // hi = hj;
-        if (day08::is_border(xi, yi, max_x, max_y))
-          break;
+        if (day08::is_border(xi, yi, max_x, max_y)) break;
       }
-      if (is_visible_in_this_direction)
-        return true;
+      if (is_visible_in_this_direction) return true;
     }
     return false;
   };
 
-    const auto get_scenic_score = [&map, max_x, max_y](const Point &p) -> long long {
-      // any tree on a border is visible
-      const int x = p.first;
-      const int y = p.second;
-      if ( day08::is_border(x, y, max_x, max_y) ) return 0;
-      const int h = map[p];
-      const std::array dxy = {std::make_pair(0, 1), std::make_pair(0, -1),
-                              std::make_pair(1, 0), std::make_pair(-1, 0)};
-      long long score {1};
-      for (const auto &[dx, dy] : dxy) {
-        auto xi = x;
-        auto yi = y;
-        long viewing_distance {0};
-        while (true) {
-          xi += dx;
-          yi += dy;
-          // if ( xi < 0 || yi < 0 || xi >= max_x || yi >= max_y ) break;
-          const auto hi = map[{xi, yi}];
-          if (hi <= h) {
-            viewing_distance++;
-          }
-          if ( hi >= h || day08::is_border(xi, yi, max_x, max_y))
-            break;
-        }
-        score *= viewing_distance;
+  const auto get_scenic_score = [&map, max_x,
+                                 max_y](const Point &p) -> long long {
+    // any tree on a border is visible
+    const int x = p.first;
+    const int y = p.second;
+    if (day08::is_border(x, y, max_x, max_y)) return 0;
+    const int h = map.at(p);
+    const std::array dxy = {std::make_pair(0, 1), std::make_pair(0, -1),
+                            std::make_pair(1, 0), std::make_pair(-1, 0)};
+    long long score{1};
+    for (const auto &[dx, dy] : dxy) {
+      auto xi = x;
+      auto yi = y;
+      long viewing_distance{0};
+      while (true) {
+        xi += dx;
+        yi += dy;
+        const auto hi = map.at({xi, yi});
+        viewing_distance++;
+        if (hi >= h || day08::is_border(xi, yi, max_x, max_y)) break;
       }
-      return score;
-    };
-
+      score *= viewing_distance;
+    }
+    return score;
+  };
 
   long answer1{0};
   long long answer2{0};
   for (const auto &[p, _] : map) {
     answer1 += is_visible_outside(p) ? 1 : 0;
-    answer2 = std::max(answer2, get_scenic_score(p)); // 126 - too low, 361920 -- too low
+    answer2 = std::max(answer2, get_scenic_score(p));
   }
-  // EXPECT_EQ(answer1, 1647);
-  EXPECT_EQ(answer2, 0);
+  EXPECT_EQ(answer1, 1647);
+  EXPECT_EQ(answer2, 392080);
 }
