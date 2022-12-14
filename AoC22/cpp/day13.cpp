ce09faf205ab6708c9c2b18e8b9236c731808522
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2022-12-15 00:01:13 +0100

    AoC22 Day13 Part1 early draft
    Comparison of lists is not yet implemented

--- AoC22/cpp/day13.cpp
+++ AoC22/cpp/day13.cpp
@@ -2,24 +2,47 @@
 
 namespace day13 {
 
+// list always first char [ and last ]
 std::vector<std::string_view> SplitList(std::string_view list) noexcept {
   return {};  // TODO implement
 }
 
-bool IsValue(std::string_view text) noexcept {
-  return text.find(',') < 0;  // TODO - cover with tests
+std::optional<int> GetValue(std::string_view text) noexcept {
+  int value{0};
+  return absl::SimpleAtoi(text, &value) ? std::optional<int>(value)
+                                        : std::optional<int>();
 }
 
+enum class Order { Right, Wrong, Continue };
+
 // in all cases highest level is always a list
-bool IsRightOrder(std::string_view a, std::string_view b) noexcept {
-  const auto al = SplitList(a);
-  const auto bl = SplitList(b);
-  const auto as = al.size();
-  const auto bs = bl.size();
+Order GetOrder(std::string_view l, std::string_view r) noexcept {
+  const auto lo = GetValue(l);
+  const auto ro = GetValue(r);
+
+  if (lo.has_value() && ro.has_value()) {
+    // simple case: 2 values to compare
+    return lo.value() < ro.value()   ? Order::Right
+           : lo.value() > ro.value() ? Order::Wrong
+                                     : Order::Continue;
+  }
+  // all other cases we compare lists (but we could need to convert a value to a
+  // list)
+  const auto ll = SplitList(l);
+  const auto rl = SplitList(r);
+  const auto ls = ll.size();
+  const auto rs = rl.size();
   // If the right list runs out of items first, the inputs are not in the right
   // order.
-  if (bs < as) return false;  // right list is shorter
-  return false;               // TODO implement
+  if (rs < ls) return Order::Wrong;  // right list is shorter
+  for (int i = 0; i < ls; i++) {
+    const auto oi = GetOrder(ll[i], rl[i]);
+    if (oi == Order::Continue) continue;
+    return oi;
+  }
+  // If the left list runs out of items first, the inputs are in the right
+  // order.
+  return ls == rs ? Order::Continue : Order::Right;
 }
 
 int Answer1(std::string_view file) noexcept {
@@ -31,7 +54,7 @@ int Answer1(std::string_view file) noexcept {
     const auto& b = data[i + 1];
     if (!a.empty() && !b.empty()) {
       index++;
-      answer1 += IsRightOrder(a, b) ? index : 0;
+      answer1 += GetOrder(a, b) == Order::Right ? index : 0;
     }
   }
   return answer1;
@@ -40,6 +63,13 @@ int Answer1(std::string_view file) noexcept {
 }  // namespace day13
 
 TEST(AoC22, Day13) {
+  // basic tests
+  EXPECT_EQ(day13::GetValue("13").value(), 13);
+  EXPECT_FALSE(day13::GetValue("[1,2]").has_value());
+  EXPECT_EQ(day13::GetOrder("13", "15"), day13::Order::Right);
+  EXPECT_EQ(day13::GetOrder("20", "15"), day13::Order::Wrong);
+  EXPECT_EQ(day13::GetOrder("1", "1"), day13::Order::Continue);
+
   const auto answer1 = day13::Answer1("13-sample");
   EXPECT_EQ(answer1, 13);
 }
