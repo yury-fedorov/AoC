f24f395a3110cfb37313bac2d11859ccbff42be0
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-16 06:31:20 +0100

    AoC22 Day16 start

---
+++ AoC22/cpp/day16.cpp
@@ -0,0 +1,23 @@
+// #include "absl/strings/numbers.h"
+#include "common.h"
+
+TEST(AoC22, Day16) {
+  const auto data = ReadData("16");
+  /*
+  std::vector<long> sums;
+  long sum{0};
+  for (const std::string &line : data) {
+    if (long value{0}; absl::SimpleAtoi(line, &value)) {
+      sum += value;
+    } else {
+      sums.insert(std::ranges::upper_bound(sums, sum), sum);
+      sum = 0;
+    }
+  }
+  const auto sum_highest_n = [&sums](size_t n) {
+    return std::accumulate(sums.rbegin(), sums.rbegin() + n, 0);
+  };
+  EXPECT_EQ(sum_highest_n(1), 70698);
+  EXPECT_EQ(sum_highest_n(3), 206643);
+  */
+}
