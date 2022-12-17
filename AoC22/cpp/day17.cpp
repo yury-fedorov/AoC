b91aa0b1200426fbe88dbb6b6419de6764c8737d
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-17 11:40:02 +0100

    AoC22 Day17 early draft

--- AoC22/cpp/day17.cpp
+++ AoC22/cpp/day17.cpp
@@ -1,59 +1,51 @@
-// #include "absl/strings/numbers.h"
 #include "common.h"
 namespace day17 {
-  using Long = long long;
-  using Point = std::pair<int,int>;
-  using Chamber = std::vector<std::string>;
-  constexpr int k_chamber_width = 7;
+using Long = long long;
+using Point = std::pair<int, int>;
+using Chamber = std::vector<std::string>;
+constexpr int k_chamber_width = 7;
 
-  struct JetPattern {
-    std::string_view pattern;
-    size_t position;
-    char next() noexcept {
-      const char next = pattern[position];
-      position = (position + 1) % pattern.length(); 
-      return next;
-    }
-  };
-
-  // TODO: do we need it or it could be data driven?
-  enum class RockType { H_LINE, PLUS, L_SHAPE, V_LINE, SQUARE };
-  
-  constexpr std::array k_h_line = { Point{ 0, 0 }, Point{1,0}, Point{2,0}, Point{3,0} };
-  // TODO other 4 rocks
-  constexpr std::array k_rocks = { k_h_line }; // TODO rocks here
-
-  void FallOnePiece( Chamber & chamber, JetPattern & jet ) noexcept {
+struct JetPattern {
+  std::string_view pattern;
+  size_t position;
+  char next() noexcept {
+    const char next = pattern[position];
+    position = (position + 1) % pattern.length();
+    return next;
   }
+};
 
-  size_t Answer1( std::string_view jet_pattern_file ) noexcept {
-    const auto data = ReadData(jet_pattern_file).at(0);
-    JetPattern jet { data, 0 };
-    Chamber chamber;
-    for ( int rock_id = 0; rock_id < 2022; rock_id++ ) {
-      FallOnePiece(chamber, jet);
-    }
-    return chamber.size(); // assumption that chamber lines are only filled by figures
+// TODO: do we need it or it could be data driven?
+enum class RockType { H_LINE, PLUS, L_SHAPE, V_LINE, SQUARE };
+
+const std::vector k_h_line = {Point{0, 0}, Point{1, 0}, Point{2, 0},
+                                 Point{3, 0}};
+const std::vector k_plus = {Point{1, 0}, Point{0, 1}, Point{1, 1},
+                               Point{2, 1}, Point{1, 2}}; // TODO
+const std::vector k_l_shape = {Point{2, 0}, Point{2, 1}, Point{2, 2},
+                                  Point{0, 2}, Point{1, 2}};
+const std::vector k_v_line = {Point{0, 0}, Point{0, 1}, Point{0, 2},
+                                 Point{0, 3}};
+const std::vector k_square = {Point{0, 0}, Point{0, 1}, Point{1, 0},
+                                 Point{1, 1}};
+const std::vector< std::vector<Point> > k_rocks = {k_h_line, k_plus, k_l_shape, k_v_line,
+                                k_square};
+
+void FallOnePiece(Chamber &chamber, JetPattern &jet) noexcept {}
+
+size_t Answer1(std::string_view jet_pattern_file) noexcept {
+  const auto data = ReadData(jet_pattern_file).at(0);
+  JetPattern jet{data, 0};
+  Chamber chamber;
+  for (int rock_id = 0; rock_id < 2022; rock_id++) {
+    FallOnePiece(chamber, jet);
   }
+  return chamber
+      .size(); // assumption that chamber lines are only filled by figures
 }
+} // namespace day17
 
 TEST(AoC22, Day17) {
   const auto answer1 = day17::Answer1("17-sample");
-  /*
-  std::vector<long> sums;
-  long sum{0};
-  for (const std::string &line : data) {
-    if (long value{0}; absl::SimpleAtoi(line, &value)) {
-      sum += value;
-    } else {
-      sums.insert(std::ranges::upper_bound(sums, sum), sum);
-      sum = 0;
-    }
-  }
-  const auto sum_highest_n = [&sums](size_t n) {
-    return std::accumulate(sums.rbegin(), sums.rbegin() + n, 0);
-  };
-  EXPECT_EQ(sum_highest_n(3), 206643);
-  */
   EXPECT_EQ(answer1, 3068);
 }
