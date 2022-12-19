eea327fc1dd1a8355a47f022bf95cbcbab10e9b2
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-19 09:10:46 +0100

    AoC22 Day15 early draft

--- AoC22/cpp/day15.cpp
+++ AoC22/cpp/day15.cpp
@@ -1,23 +1,33 @@
-// #include "absl/strings/numbers.h"
 #include "common.h"
+#include <re2/re2.h>
+// Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 
-TEST(AoC22, Day15) {
-  const auto data = ReadData("15");
-  /*
-  std::vector<long> sums;
-  long sum{0};
+namespace day15 {
+using Point = std::pair<int, int>;
+using SensorBeacon = std::pair<Point, Point>;
+
+[[nodiscard]] std::vector<SensorBeacon> Read(std::string_view file) noexcept {
+  // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+  std::vector<SensorBeacon> result;
+  const auto data = ReadData(file);
+  re2::RE2 re(
+      "Sensor at x=(\\d+), y=(\\d+): closest beacon is at x=(\\d+), y=(\\d+)");
   for (const std::string &line : data) {
-    if (long value{0}; absl::SimpleAtoi(line, &value)) {
-      sum += value;
+    if (line.empty())
+      continue;
+    re2::StringPiece input(line);
+    int sx, sy, bx, by;
+    if (re2::RE2::FullMatch(input, re, &sx, &sy, &bx, &by)) {
+      result.emplace_back(SensorBeacon{{sx, sy}, {bx, by}});
     } else {
-      sums.insert(std::ranges::upper_bound(sums, sum), sum);
-      sum = 0;
+      EXPECT_TRUE(false) << line;
     }
   }
-  const auto sum_highest_n = [&sums](size_t n) {
-    return std::accumulate(sums.rbegin(), sums.rbegin() + n, 0);
-  };
-  EXPECT_EQ(sum_highest_n(1), 70698);
-  EXPECT_EQ(sum_highest_n(3), 206643);
-  */
+  return result;
+}
+} // namespace day15
+
+TEST(AoC22, Day15) {
+  const auto data = day15::Read("15");
+  EXPECT_EQ(data.size(), 0);
 }
