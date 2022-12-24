99b00b8a18f51b94248c4ac643367480ee808f47
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-24 08:01:37 +0100

    AoC22 Day25 early draft
    Do not know what is the task yet. Just skeleton.

---
+++ AoC22/cpp/day25.cpp
@@ -0,0 +1,10 @@
+#include "common.h"
+
+namespaces day25 {
+} 
+
+TEST(AoC22, Day25) {
+  // TODO not arrived yet, this is just a draft
+  // const auto data = ReadData("25"); 
+  // EXPECT_EQ(day24::Answer1(map), 0);
+}
