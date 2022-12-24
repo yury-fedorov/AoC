2598433878cc1a9f4dabc05cc27058d866a479b1
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-24 07:57:45 +0100

    AoC22 Day24 early draft
    Only skeleton

---
+++ AoC22/cpp/day24.cpp
@@ -0,0 +1,15 @@
+#include "common.h"
+
+namespaces day24 {
+
+using Map = std::vector<std::string>;
+[[nodiscard]] size_t Answer1(const Map& map) noexcept {
+  return 0; // TODO
+} 
+
+} 
+
+TEST(AoC22, Day24) {
+  const day24::Map map = ReadData("24"); 
+  EXPECT_EQ(day24::Answer1(map), 0);
+}
