6e68d7c45600185db0262bf8614f7b41bc9670aa
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-25 06:50:49 +0100

    AoC22 Day25 early draft
    First test of ToDecimal implementation

--- AoC22/cpp/day25.cpp
+++ AoC22/cpp/day25.cpp
@@ -1,10 +1,22 @@
 #include "common.h"
 
 namespace day25 {
+  using Int = long long;
+  using Snafu = std::string;
+  Int ToDecimal(std::string_view snafu) noexcept {
+    auto it = snafu.rbegin();
+    const auto end = snafu.rend();
+    Int sum {0};
+    for (int v=1; it != end; v*=5, it++) {
+      const char c { *it };
+      const int k = c == '=' ? -2 : c == '-' ? -1 : ( c - '0' );
+      sum += k * v;
+    }
+    return sum;
+  }
 } 
 
 TEST(AoC22, Day25) {
-  // TODO not arrived yet, this is just a draft
-  // const auto data = ReadData("25"); 
-  // EXPECT_EQ(day24::Answer1(map), 0);
+  const auto data = ReadData("25"); 
+  EXPECT_EQ(day25::ToDecimal("1=0"), 15);
 }
