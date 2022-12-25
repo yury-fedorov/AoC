352e4bd87ca2a568fef4581cbb0d157d3b09bdce
Yury Fedorov <yury.fedorov@gmail.com>
2022-12-25 23:55:42 +0100

    AoC22 Day25 solved half manually

--- AoC22/cpp/day25.cpp
+++ AoC22/cpp/day25.cpp
@@ -7,7 +7,7 @@ Int ToDecimal(std::string_view snafu) noexcept {
   auto it = snafu.rbegin();
   const auto end = snafu.rend();
   Int sum{0};
-  for (int v = 1; it != end; v *= 5, it++) {
+  for (Int v = 1; it != end; v *= 5, it++) {
     const char c{*it};
     const int k = c == '=' ? -2 : c == '-' ? -1 : (c - '0');
     sum += k * v;
@@ -26,31 +26,58 @@ constexpr inline bool IsGood(Int value, int position) noexcept {
   return (3 * ref_value > value) && (value <= (ref_value - (2 * ref_low)));
 }
 
+// from Zug (CH)
+constexpr inline int Position(Int value) noexcept {
+  int position = 0;
+  for (; true; position++) {
+    if (3 * std::pow(5, position) > value)
+      return position;
+  }
+}
+
 Snafu ToSnafu(Int value) noexcept {
   Snafu result;
-  constexpr double kRoot5 = 1.0 / 5.0;
-  auto start = std::pow(value, kRoot5);
-  int position = std::ceil(start);
-  if (!IsGood(value, position))
-    position--;
+  auto position = Position(value);
   while (value != 0) {
     EXPECT_TRUE(IsGood(value, position)) << "Wrong position " << position;
     break; // todo
   }
   return result;
 }
-} // namespace day25
 
-// from Domodossola (Italy)
-TEST(AoC22, Day25) {
-  const auto data = ReadData("25");
+Int Sum(std::string_view file) noexcept {
+  const auto data = ReadData(file);
   day25::Int sum = 0;
   for (const auto &s : data) {
     sum += day25::ToDecimal(s);
   }
+  return sum;
+}
+
+} // namespace day25
+
+// from Domodossola (Italy)
+TEST(AoC22, Day25) {
+  EXPECT_EQ(day25::Sum("25-sample"), 4890);
+  const auto sum = day25::Sum("25");
   EXPECT_EQ(day25::ToDecimal("1=0"), 15);
-  EXPECT_EQ(sum, 5481165789LL);
-  EXPECT_EQ(sum, day25::ToDecimal("4216113430113-")); // 4216113430113-
-  EXPECT_EQ(day25::ToSnafu(3), "1=");
-  EXPECT_EQ(day25::ToSnafu(sum), "4216113430113-"); // what is right?
+  EXPECT_EQ(day25::ToDecimal("1=0"), 15);
+  EXPECT_EQ(sum, 36671616971741LL); // 36671616971741
+
+  using Pair = std::pair<int, std::string_view>;
+  constexpr std::array kTests = {
+      Pair{3, "1="},          Pair{4, "1-"},
+      Pair{5, "10"},          Pair{15, "1=0"},
+      Pair{20, "1-0"},        Pair{2022, "1=11-2"},
+      Pair{12345, "1-0---0"}, Pair{314159265, "1121-1110-1=0"}};
+  for (const auto &[decimal, snafu] : kTests) {
+    // EXPECT_EQ(day25::ToSnafu(decimal), snafu)
+    //     << "Bad encoding " << decimal << " to " << snafu;
+    EXPECT_EQ(day25::ToDecimal(snafu), decimal)
+        << "Bad encoding " << decimal << " to " << snafu;
+  }
+  // calculated using Google Sheet
+  // TODO - finish the algorithm later?
+  EXPECT_EQ(sum, day25::ToDecimal("20=02=120-=-2110-0=1"));
+  // EXPECT_EQ(day25::ToSnafu(sum), "4216113430113-"); // what is right?
 }
