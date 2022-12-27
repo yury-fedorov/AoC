0c9380aaae6ff3a0b4cd2474a336f3cf77c16ed9
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2022-12-27 17:11:10 +0100

    AoC22 Day18 nodiscard, naming and reformatting

--- AoC22/cpp/day18.cpp
+++ AoC22/cpp/day18.cpp
@@ -1,16 +1,16 @@
 #include "absl/container/flat_hash_set.h"
 #include "common.h"
-#include <re2/re2.h>
+#include "re2/re2.h"
 
 namespace day18 {
 using Point3 = std::tuple<int, int, int>;
 using PointSet = absl::flat_hash_set<Point3>;
 
-constexpr std::array<Point3, 6> k_shifts = {
+constexpr std::array<Point3, 6> kShifts = {
     Point3{-1, 0, 0}, {1, 0, 0}, {0, -1, 0}, {0, 1, 0}, {0, 0, -1}, {0, 0, 1}};
 
-Point3 Limit(const PointSet &list,
-             std::function<int(int, int)> limit) noexcept {
+[[nodiscard]] Point3 Limit(const PointSet &list,
+                           std::function<int(int, int)> limit) noexcept {
   auto [a, b, c] = *list.begin();
   for (const auto [x, y, z] : list) {
     a = limit(a, x);
@@ -20,11 +20,11 @@ Point3 Limit(const PointSet &list,
   return {a, b, c};
 }
 
-bool Contains(const PointSet &space, const Point3 &p) noexcept {
+[[nodiscard]] bool Contains(const PointSet &space, const Point3 &p) noexcept {
   return space.find(p) != space.end();
 }
 
-std::pair<int, int> Solution(std::string_view file) {
+[[nodiscard]] std::pair<int, int> Solution(std::string_view file) noexcept {
   const auto data = ReadData(file);
   PointSet positions;
   re2::RE2 re("(\\d+),(\\d+),(\\d+)");
@@ -39,7 +39,7 @@ std::pair<int, int> Solution(std::string_view file) {
 
   int answer1{0};
   for (const auto [x, y, z] : positions) {
-    for (const auto [dx, dy, dz] : k_shifts) {
+    for (const auto [dx, dy, dz] : kShifts) {
       const Point3 p = {x + dx, y + dy, z + dz};
       answer1 += Contains(positions, p) ? 0 : 1;
     }
@@ -76,7 +76,7 @@ std::pair<int, int> Solution(std::string_view file) {
     for (const auto pn : next) {
       visited.insert(pn);
       const auto [x, y, z] = pn;
-      for (const auto [dx, dy, dz] : k_shifts) {
+      for (const auto [dx, dy, dz] : kShifts) {
         const Point3 p = {x + dx, y + dy, z + dz};
         const auto [px, py, pz] = p;
         if (px < ax || px > bx || py < ay || py > by || pz < az || pz > bz) {
@@ -84,8 +84,7 @@ std::pair<int, int> Solution(std::string_view file) {
           all_empty.insert(p);
           continue;
         }
-        if (Contains(visited, p))
-          continue;
+        if (Contains(visited, p)) continue;
         if (Contains(positions, p)) {
           // this position is inside lava
           border.insert(p);
@@ -102,7 +101,7 @@ std::pair<int, int> Solution(std::string_view file) {
   int answer2 = 0;
   for (const auto &bp : border) {
     const auto [x, y, z] = bp;
-    for (const auto [dx, dy, dz] : k_shifts) {
+    for (const auto [dx, dy, dz] : kShifts) {
       const Point3 p = {x + dx, y + dy, z + dz};
       answer2 += Contains(all_empty, p) ? 1 : 0;
     }
@@ -110,7 +109,7 @@ std::pair<int, int> Solution(std::string_view file) {
 
   return {answer1, answer2};
 }
-} // namespace day18
+}  // namespace day18
 
 TEST(AoC22, Day18) {
   const auto [t1, t2] = day18::Solution("18-sample");
