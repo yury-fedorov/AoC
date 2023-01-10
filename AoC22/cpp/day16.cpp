01bb57912a00ed8dd3878b6696282d238acf6a5d
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-01-10 23:07:06 +0100

    AoC22 Day16 solution 2 early draft

--- AoC22/cpp/day16.cpp
+++ AoC22/cpp/day16.cpp
@@ -12,10 +12,10 @@ struct Conf {
   std::vector<std::string> next;
 };
 using Map = absl::flat_hash_map<std::string, Conf>;
-using Doors = std::vector<std::string_view>; // open doors
+using Doors = std::vector<std::string_view>;  // open doors
 using StateKey =
-    std::tuple<std::string, std::string, int>; // open doors, position, time
-using StateCache = absl::flat_hash_map<StateKey, long>; // state - pressure
+    std::tuple<std::string, std::string, int>;  // open doors, position, time
+using StateCache = absl::flat_hash_map<StateKey, long>;  // state - pressure
 
 [[nodiscard]] long PressureInMinute(const Map &map,
                                     const Doors &open) noexcept {
@@ -34,8 +34,7 @@ constexpr auto kDoorSeparator = " ";
 }
 
 [[nodiscard]] Doors StrToDoors(std::string_view doors) noexcept {
-  if (doors.empty())
-    return Doors{};
+  if (doors.empty()) return Doors{};
   return absl::StrSplit(doors, kDoorSeparator);
 }
 
@@ -50,8 +49,7 @@ bool RememberState(StateCache &states, const StateKey &key,
     // the key exists and we found better pressure value
     is_to_insert = true;
   }
-  if (is_to_insert)
-    states.insert({key, pressure});
+  if (is_to_insert) states.insert({key, pressure});
   return is_to_insert;
 }
 
@@ -75,8 +73,7 @@ constexpr int kT = 30;
 
     const auto [open_doors, position, t0] = next;
     const int t1 = t0 + 1;
-    if (t1 > kT)
-      continue; // we end this request here
+    if (t1 > kT) continue;  // we end this request here
 
     const auto doors = StrToDoors(open_doors);
     const long pressure_t1 = PressureInMinute(map, doors) + states.at(next);
@@ -89,9 +86,9 @@ constexpr int kT = 30;
       if (i < 0) {
         // opening valve?
         if (c.rate <= 0)
-          continue; // no sense to spend time opening this broken valve
+          continue;  // no sense to spend time opening this broken valve
         if (open_doors.find(position) != std::string::npos)
-          continue; // this one is already open
+          continue;  // this one is already open
         auto doors_t1 = doors;
         doors_t1.push_back(position);
         r::sort(doors_t1);
@@ -130,43 +127,46 @@ constexpr int kT = 30;
 // insert all
 // cached
 // shortest path
-[[nodiscard]] int Distance( const Map &map, std::string_view from, std::string_view to ) noexcept {
-  if ( from == to ) return 0;
+[[nodiscard]] int Distance(const Map &map, std::string_view from,
+                           std::string_view to) noexcept {
+  if (from == to) return 0;
   const auto &next = map.at(from).next;
-  if ( r::find( next, to ) != next.end() ) return 1;
-  
-  absl::flat_hash_set<std::string_view> visited { next.begin(), next.end() };
-  visited.insert( from );
+  if (r::find(next, to) != next.end()) return 1;
+
+  absl::flat_hash_set<std::string_view> visited{next.begin(), next.end()};
+  visited.insert(from);
   int distance = 2;
-  absl::flat_hash_set<std::string_view> next_from { next.begin(), next.end() };
-  absl::flat_hash_set<std::string_view> next_from_1 {};
-  for ( const auto & from_i : next_from ) {
-  	const auto &next_i = map.at(from_i).next;
-  	if ( r::find( next_i, to ) != next.end() ) {
-  		return distance;
-  	}
-  	visited.insert( next_i.begin(), next_i.end() );
-  	for ( const auto & j : next_i ) {
-		if ( visited.find( j ) != visited.end() ) continue;
-  	}
-  	distance++;
-  } 
+  absl::flat_hash_set<std::string_view> next_from{next.begin(), next.end()};
+  absl::flat_hash_set<std::string_view> next_from_1{};
+  for (const auto &from_i : next_from) {
+    const auto &next_i = map.at(from_i).next;
+    if (r::find(next_i, to) != next.end()) {
+      return distance;
+    }
+    visited.insert(next_i.begin(), next_i.end());
+    for (const auto &j : next_i) {
+      if (visited.find(j) != visited.end()) continue;
+    }
+    distance++;
+  }
   return -1;
 }
 
 // second attempt
-[[nodiscard]] long Pressure(const Map &map, std::string_view start, const Doors &open, int t_left, std::optional<std::string_view> target ) noexcept {
-  if ( t_left <= 0 ) return 0;
-  if ( t_left == 1 ) {
-  	// no sense to open anything, just calculate what is opened
-  	return PressureInMinute(map, open);
+[[nodiscard]] long Pressure(const Map &map, std::string_view start,
+                            const Doors &open, int t_left,
+                            std::optional<std::string_view> target) noexcept {
+  if (t_left <= 0) return 0;
+  if (t_left == 1) {
+    // no sense to open anything, just calculate what is opened
+    return PressureInMinute(map, open);
   }
   // we've more then 1 step to go, opening a door could make sense
-  if ( target.has_value() ) {
-  	// we go to the selected target
-  	// TODO
+  if (target.has_value()) {
+    // we go to the selected target
+    // TODO
   }
-  
+
   // we do not have a selected target, we choose among the doors to open
   // 1. get all doors
   // 2. remove opened doors
@@ -174,7 +174,6 @@ constexpr int kT = 30;
   // 4. order first high rated doors (could try to optimize: top 50%)
   return 0;
 }
-
 
 // open doors in format ,AA, ... ,ZZ,...
 // state - cur position - AA + open doors + time_passed --> maximum
@@ -198,10 +197,10 @@ constexpr int kT = 30;
   }
   return Pressure(map);
 }
-} // namespace day16
+}  // namespace day16
 
 TEST(AoC22, Day16) {
-  return; // TODO - wrong answer
+  return;  // TODO - wrong answer
   const auto a1 = day16::Answer1("16-sample");
   EXPECT_EQ(a1, 1651);
 }
