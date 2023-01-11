29a1e30f245c54df63bd5d1f7dbf04695b4f6c46
Yury Fedorov <yury.fedorov@gmail.com>
2023-01-11 22:30:42 +0100

    AoC22 Day15 Part2 solved (takes 50 seconds)

--- AoC22/cpp/day15.cpp
+++ AoC22/cpp/day15.cpp
@@ -1,4 +1,4 @@
-#include "absl/container/flat_hash_set.h"  // not efficient
+#include "absl/container/flat_hash_set.h" // not efficient
 #include "common.h"
 #include "re2/re2.h"
 
@@ -12,9 +12,8 @@ using Set = absl::flat_hash_set<int>;
   std::vector<SensorBeacon> result;
   const auto data = ReadData(file);
   // Sensor at x=2, y=18: closest beacon is at x=-2, y=15
-  re2::RE2 re(
-      "Sensor at x=([-\\d]+), y=([-\\d]+): closest beacon is at "
-      "x=([-\\d]+), y=([-\\d]+)");
+  re2::RE2 re("Sensor at x=([-\\d]+), y=([-\\d]+): closest beacon is at "
+              "x=([-\\d]+), y=([-\\d]+)");
   for (const std::string &line : data) {
     re2::StringPiece input(line);
     int sx, sy, bx, by;
@@ -30,14 +29,15 @@ using Set = absl::flat_hash_set<int>;
 // Intersection operations
 [[nodiscard]] constexpr inline bool IsIntersection(Range a, Range b) noexcept {
   if (a.first > b.first)
-    std::swap(a, b);  // now a.first <= b.first
-                      // [a.first, a.second]
-                      //      [b.first
+    std::swap(a, b); // now a.first <= b.first
+                     // [a.first, a.second]
+                     //      [b.first
   return a.second >= b.first;
 }
 
 [[nodiscard]] std::optional<Range> Intersection(Range a, Range b) noexcept {
-  if (!IsIntersection(a, b)) return std::nullopt;
+  if (!IsIntersection(a, b))
+    return std::nullopt;
   return Range{std::min(a.first, b.first), std::max(a.second, b.second)};
 }
 
@@ -63,6 +63,15 @@ void AddRange(std::vector<Range> &ranges, Range r) noexcept {
       return;
     }
   }
+  // otherwise we need to insert a piece between
+  for (auto i = ranges.begin(); i != ranges.end(); i++) {
+    if (i->first < r.first)
+      continue;
+    ranges.insert(i, r);
+    return;
+  }
+  ranges.emplace_back(std::move(r));
+  return;
 }
 
 [[nodiscard]] constexpr inline bool IsInside(Range r, int value) noexcept {
@@ -80,21 +89,15 @@ void RemovePoint(std::vector<Range> &ranges, int value) noexcept {
       const Range r1{r.first, value - 1};
       const Range r2{value + 1, r.second};
       ranges.erase(ranges.begin() + i);
-      if (IsValid(r1)) ranges.emplace_back(std::move(r1));
-      if (IsValid(r2)) ranges.emplace_back(std::move(r2));
+      if (IsValid(r1))
+        ranges.emplace_back(std::move(r1));
+      if (IsValid(r2))
+        ranges.emplace_back(std::move(r2));
       Sort(ranges);
       return;
     }
   }
 }
-/*
-[[nodiscard]] Set Minus( Range ab, const std::vector<Range> &ranges ) noexcept {
-    Set result;
-    for ( const auto & r : ranges ) {
-    }
-    return result;
-}
-*/
 
 [[nodiscard]] inline int Distance(const Point &a, const Point &b) noexcept {
   return abs(a.first - b.first) + abs(a.second - b.second);
@@ -107,9 +110,10 @@ constexpr int k_y = 2000000;
   const auto [sensor, beacon] = sb;
   const auto distance = Distance(sensor, beacon);
   const auto [sx, sy] = sensor;
-  const auto dy = abs(sy - y);         // how distant sensor is from y
-  const auto range_x = distance - dy;  // how much freedom we have on x
-  if (range_x < 0) return std::optional<Range>();
+  const auto dy = abs(sy - y);        // how distant sensor is from y
+  const auto range_x = distance - dy; // how much freedom we have on x
+  if (range_x < 0)
+    return std::optional<Range>();
   return Range{sx - range_x, sx + range_x};
 }
 
@@ -118,7 +122,8 @@ constexpr int k_y = 2000000;
   day15::Set set;
   for (const auto &sb : data) {
     const auto opt_range = GetRange(sb, y);
-    if (!opt_range.has_value()) continue;
+    if (!opt_range.has_value())
+      continue;
     const auto [min, max] = opt_range.value();
     for (int x = min; x <= max; x++) {
       set.insert(x);
@@ -129,7 +134,8 @@ constexpr int k_y = 2000000;
     const auto [bx, by] = beacon;
     if (by == y) {
       auto i = set.find(bx);
-      if (i != set.end()) set.erase(i);
+      if (i != set.end())
+        set.erase(i);
     }
   }
   return set.size();
@@ -140,43 +146,46 @@ constexpr int k_y = 2000000;
   std::vector<Range> result;
   for (const auto &sb : data) {
     const auto opt_range = GetRange(sb, y);
-    if (!opt_range.has_value()) continue;
+    if (!opt_range.has_value())
+      continue;
     AddRange(result, opt_range.value());
   }
   return result;
 }
 
+[[nodiscard]] inline int64_t TurningFrequency(const Point &p) noexcept {
+  const auto [x, y] = p;
+  return (x * 4'000'000LL) + y;
+}
+
 // part2
 using RangeList = std::vector<Range>;
-[[nodiscard]] int Answer2(std::string_view file, Range min_max) noexcept {
+[[nodiscard]] int64_t Answer2(std::string_view file, Range min_max) noexcept {
   const auto data = Read(file);
   const auto [y0, y1] = min_max;
   for (int y = y0; y <= y1; y++) {
     const auto ranges = GetSlice(data, y);
-    if (ranges.size() > 1) return y;
+    if (ranges.size() > 1) {
+      const int x = ranges[0].second + 1;
+      return TurningFrequency({x, y});
+    }
   }
   return -1;
 }
 
-[[nodiscard]] inline long long TurningFrequency(const Point &p) noexcept {
-  const auto [x, y] = p;
-  return (x * 4000000) + y;
-}
-
-}  // namespace day15
+} // namespace day15
 
 TEST(AoC22, Day15) {
   EXPECT_EQ(day15::Answer1("15-sample", 10), 26);
   EXPECT_EQ(day15::TurningFrequency({14, 11}), 56000011);
-  // TODO - fixme --
-  return;
-  EXPECT_EQ(day15::Answer2("15-sample", {0, 20}), -2);
-  constexpr int kLowest = 0;
-  constexpr int kLargest = 4000000;
-  EXPECT_EQ(day15::Answer2("15", {kLowest, kLargest}), -2);
-  if (IsFastOnly()) {
+  EXPECT_EQ(day15::Answer2("15-sample", {0, 20}), 56000011);
+  EXPECT_EQ(day15::TurningFrequency({3129625, 2636475}), 12518502636475LL);
+  if (IsFastOnly())
     return;
-  }
   // takes 14 seconds
   EXPECT_EQ(day15::Answer1("15", day15::k_y), 5127797);
+  // takes 50 seconds
+  constexpr int kLowest = 0;
+  constexpr int kLargest = 4000000;
+  EXPECT_EQ(day15::Answer2("15", {kLowest, kLargest}), 12518502636475LL);
 }
