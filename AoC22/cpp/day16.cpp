8ff3f543b8e3cc7ce82dfc9cfd3113fb82eb3e20
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-01-16 14:57:11 +0100

    AoC22 Day16 Part1 alpha - cleaning
    First slow implementation is removed.
    Minor bug fix in calculus of time.
    Disabled sorting as it is not used.

--- AoC22/cpp/day16.cpp
+++ AoC22/cpp/day16.cpp
@@ -16,11 +16,8 @@ struct Conf {
 using Map = absl::flat_hash_map<std::string, Conf>;
 using Doors = std::vector<std::string_view>;  // open doors
 using DoorSet = absl::flat_hash_set<std::string_view>;
-/*
-using StateKey =
-    std::tuple<std::string, std::string, int>;  // open doors, position, time
-using StateCache = absl::flat_hash_map<StateKey, long>;  // state - pressure
-*/
+
+constexpr int kT = 30;
 
 [[nodiscard]] long PressureInMinute(const Map &map,
                                     const Doors &open) noexcept {
@@ -30,108 +27,6 @@ using StateCache = absl::flat_hash_map<StateKey, long>;  // state - pressure
   }
   return out;
 }
-
-/*
-constexpr auto kDoorSeparator = " ";
-
-[[nodiscard]] std::string DoorsToStr(Doors doors) noexcept {
-  r::sort(doors);
-  return absl::StrJoin(doors, kDoorSeparator);
-}
-
-[[nodiscard]] Doors StrToDoors(std::string_view doors) noexcept {
-  if (doors.empty()) return Doors{};
-  return absl::StrSplit(doors, kDoorSeparator);
-}
-
-bool RememberState(StateCache &states, const StateKey &key,
-                   long pressure) noexcept {
-  bool is_to_insert = false;
-  auto i = states.find(key);
-  if (i == states.end()) {
-    // the key does not yet exist, we just save it
-    is_to_insert = true;
-  } else if (i->second < pressure) {
-    // the key exists and we found better pressure value
-    is_to_insert = true;
-  }
-  if (is_to_insert) states.insert({key, pressure});
-  return is_to_insert;
-}
-*/
-
-constexpr int kT = 30;
-
-/*
-[[nodiscard]] long Pressure(const Map &map) noexcept {
-  const auto kStartState = StateKey{"", "AA", 0};
-  auto states = StateCache{{kStartState, 0}};
-  std::vector<StateKey> to_process = {kStartState};
-  const auto priority = [&states](const StateKey &key) {
-    const auto [_, __, t] = key;
-    return states.at(key) / (0.1 + t);
-  };
-  const auto less = [&priority](const StateKey &a, const StateKey &b) {
-    return priority(a) < priority(b);
-  };
-  std::priority_queue queue(to_process.begin(), to_process.end(), less);
-  while (!queue.empty()) {
-    auto next = queue.top();
-    queue.pop();
-
-    const auto [open_doors, position, t0] = next;
-    const int t1 = t0 + 1;
-    if (t1 > kT) continue;  // we end this request here
-
-    const auto doors = StrToDoors(open_doors);
-    const long pressure_t1 = PressureInMinute(map, doors) + states.at(next);
-    // everything is ready but position
-
-    const auto &c = map.at(position);
-    const int in = static_cast<int>(c.next.size());
-    for (int i = -1; i < in; i++) {
-      std::optional<StateKey> key_t1;
-      if (i < 0) {
-        // opening valve?
-        if (c.rate <= 0)
-          continue;  // no sense to spend time opening this broken valve
-        if (open_doors.find(position) != std::string::npos)
-          continue;  // this one is already open
-        auto doors_t1 = doors;
-        doors_t1.push_back(position);
-        r::sort(doors_t1);
-        key_t1 = StateKey{DoorsToStr(doors_t1), position, t1};
-      } else {
-        // moving to another?
-        const auto &position_t1 = c.next[i];
-        key_t1 = StateKey{open_doors, position_t1, t1};
-      }
-      if (key_t1.has_value()) {
-        // wrong - we may have temporal less pressure but it is ok
-        // good optimizer: have understanding where to go next: one of non
-        // opened doors and how much it costs us (in steps)
-        if (RememberState(states, key_t1.value(), pressure_t1)) {
-          // this state is valuable, let's insert it to the processing queue
-          queue.push(key_t1.value());
-        }
-      }
-    }
-  }
-  long max_pressure = 0;
-  StateKey best_key;
-  for (const auto &[state, pressure] : states) {
-    const auto &[_, __, t] = state;
-    if (t == kT) {
-      if (max_pressure < pressure) {
-        best_key = state;
-        max_pressure = pressure;
-      }
-      // max_pressure = std::max(max_pressure, pressure);
-    }
-  }
-  return max_pressure;
-}
-*/
 
 [[nodiscard]] Doors AllDoors(const Map &map) noexcept {
   Doors doors;
@@ -176,10 +71,13 @@ constexpr int kT = 30;
     if (absl::c_find(next_doors, to) != next_doors.end()) return Doors{to};
     // we are here because one level depth was not enough
     for (const auto &door : next_doors) {
-      auto s = Sequence(map, door, to, depth - 1);
-      if (!s.empty() && s.back() == to) {
-        s.insert(s.begin(), door);
-        return s;
+      const auto d1 = depth - 1;
+      if (d1 > 0) {
+        auto s = Sequence(map, door, to, d1);
+        if (!s.empty() && s.back() == to) {
+          s.insert(s.begin(), door);
+          return s;
+        }
       }
     }
   }
@@ -215,6 +113,7 @@ void RemoveNoPressure(Doors &doors, const Map &map) noexcept {
   });
 }
 
+/*
 // highest rates at the beginning with shortest path to it
 void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
   absl::c_sort(doors, [&map, from](const auto &a, const auto &b) {
@@ -223,8 +122,8 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
     return (map.at(a).rate - ad) > (map.at(b).rate - bd);
   });
 }
+*/
 
-// second attempt
 [[nodiscard]] long Pressure(const Map &map, std::string_view start,
                             const Doors &open, int t_left,
                             std::optional<std::string_view> target) noexcept {
@@ -238,13 +137,13 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
   if (target.has_value()) {
     // we go to the selected target
     const auto path = SequenceFast(map, start, target.value());
-    const auto dt = path.size();
+    const auto dt = path.size() + 1;  // 1 - to open the door
     const auto t1 = t_left - dt;
     Doors open1{open};
     open1.push_back(target.value());
     const auto pressure =
         Pressure(map, target.value(), open1, t1, std::nullopt);
-    return pressure + dt * last_minute;
+    return pressure + (dt * last_minute);
   }
 
   // we do not have a selected target, we choose among the doors to open
@@ -255,7 +154,7 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
   // 3. remove no pressure doors
   RemoveNoPressure(doors, map);
   // 4. order first high rated doors (could try to optimize: top 50%)
-  Order(map, doors, start);
+  // Order(map, doors, start);
   long pressure{0};
   for (const auto &target : doors) {
     const long cur_pressure = Pressure(map, start, open, t_left, target);
@@ -265,8 +164,6 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
 }
 
 // open doors in format ,AA, ... ,ZZ,...
-// state - cur position - AA + open doors + time_passed --> maximum
-
 [[nodiscard]] long Answer1(std::string_view file) noexcept {
   const auto data = ReadData(file);
   // Valve HH has flow rate=22; tunnel leads to valve GG
@@ -284,7 +181,6 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
     } else
       EXPECT_TRUE(false) << line;
   }
-  // XXX - old return Pressure(map);
   return Pressure(map, "AA", Doors{}, kT, std::nullopt);
 }
 
