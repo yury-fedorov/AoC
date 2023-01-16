a41a5cf57fc6254a1266ded59b3e65fcf2a28020
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-01-16 18:32:15 +0100

    AoC22 Day22 Part2 work in progress - refactoring
    Preparation refactoring to introduce logic for part 2 in less intrusive way

--- AoC22/cpp/day22.cpp
+++ AoC22/cpp/day22.cpp
@@ -5,7 +5,9 @@ namespace day22 {
 
 using Point = std::pair<int, int>;
 using Map = std::vector<std::string>;
+using Input = std::pair<Map, std::string>;
 enum class Direction : int { kRight = 0, kDown, kLeft, kUp };
+using Position = std::pair<Point, Direction>;
 
 constexpr std::array kShifts = {Point{1, 0}, Point{0, 1}, Point{-1, 0},
                                 Point{0, -1}};
@@ -19,15 +21,70 @@ constexpr char TILE = '.';
   return (1000 * row) + (4 * column) + static_cast<int>(direction);
 }
 
-[[nodiscard]] std::pair<Map, std::string> Load(std::string_view file) noexcept {
+[[nodiscard]] Input Load(std::string_view file) noexcept {
   const auto data = ReadData(file);
   const auto map = data | rv::filter([](const std::string &s) {
-                     return s.find( TILE ) != std::string::npos;
+                     return s.find(TILE) != std::string::npos;
                    }) |
                    r::to<std::vector>();
   const std::string path = *(data.rbegin());
   return {map, path};
 }
+
+class Navigator {
+ protected:
+  const Map &map_;
+
+  Navigator(const Map &map) : map_(map) {}
+
+  char At(int x, int y) const noexcept {
+    if (y < 0 || x < 0 || y >= map_.size()) return EMPTY;
+    const auto &line = map_[y];
+    if (x >= line.size()) return EMPTY;
+    // EXPECT_TRUE( line.size() == x_n ) << line.size() << " " << x_n << " " <<
+    // line;
+    return line[x];
+  }
+
+  virtual Position Overlap(Position from_direction) noexcept = 0;
+
+ public:
+  // basic navigation
+  [[nodiscard]] Position To(Position from_direction) noexcept {
+    auto [from, direction] = from_direction;
+    const auto [dx, dy] = kShifts[static_cast<int>(direction)];
+    const auto [x0, y0] = from;
+    auto x1 = x0 + dx;
+    auto y1 = y0 + dy;
+    char c1 = At(x1, y1);
+    if (c1 == ' ') {
+      const auto [p, d] = Overlap(from_direction);
+      x1 = p.first;
+      y1 = p.second;
+      direction = d;
+      c1 = At(x1, y1);
+    }
+    if (c1 == WALL) return {from, direction};
+    return {{x1, y1}, direction};
+  }
+};
+
+class Navigator1 : public Navigator {
+ protected:
+  Position Overlap(Position from_direction) noexcept override {
+    auto [from, direction] = from_direction;
+    auto [x1, y1] = from;
+    const auto [dx, dy] = kShifts[static_cast<int>(direction)];
+    while (At(x1 - dx, y1 - dy) != EMPTY) {
+      x1 -= dx;
+      y1 -= dy;
+    }
+    return {{x1, y1}, direction};
+  }
+
+ public:
+  Navigator1(const Map &map) : Navigator(map) {}
+};
 
 [[nodiscard]] int Answer1(std::string_view file) noexcept {
   const auto [map_binding, path] = day22::Load(file);
@@ -42,36 +99,10 @@ constexpr char TILE = '.';
   const int y_n = map.size();
   const int x_n = line.size();
   for (; x < x_n; x++) {
-    if (line[x] == TILE)
-      break;
+    if (line[x] == TILE) break;
   }
-  const auto at = [&map, y_n](int x, int y) -> char {
-    if (y < 0 || x < 0 || y >= y_n) return EMPTY;
-    const auto & line = map[y];
-    if ( x >= line.size() ) return EMPTY;
-    // EXPECT_TRUE( line.size() == x_n ) << line.size() << " " << x_n << " " << line; 
-    return line[x];
-  };
-  const auto to = [&map, &at](Point from, Direction direction) -> Point {
-    const auto [dx, dy] = kShifts[static_cast<int>(direction)];
-    const auto [x0, y0] = from;
-    auto x1 = x0 + dx;
-    auto y1 = y0 + dy;
-    char c1 = at(x1, y1);
-    if (c1 == ' ') {
-      // opposite side
-      x1 = x0;
-      y1 = y0;
-      while (at(x1- dx, y1 - dy) != EMPTY ) {
-        x1 -= dx;
-        y1 -= dy;
-      }
-      c1 = at(x1, y1);
-    }
-    if (c1 == WALL )
-      return from;
-    return {x1, y1};
-  };
+
+  Navigator1 navigator(map);
 
   // found the start
   auto position = Point{x, y};
@@ -85,7 +116,10 @@ constexpr char TILE = '.';
     if (re2::RE2::FullMatch(input, re_digit, &moves, &tail)) {
       // moves in direction
       for (int i = 0; i < moves; i++) {
-        position = to(position, facing);
+        const auto [new_position, new_direction] =
+            navigator.To({position, facing});
+        position = new_position;
+        facing = new_direction;
       }
     } else if (re2::RE2::FullMatch(input, re_turn, &turn, &tail)) {
       // turn
@@ -110,7 +144,7 @@ constexpr char TILE = '.';
 // 7R-2R 7D-9R
 // 6U-4L 6L-1L
 
-} // namespace day22
+}  // namespace day22
 
 TEST(AoC22, Day22) {
   EXPECT_EQ(day22::FinalPassword(6, 8, day22::Direction::kRight), 6032);
