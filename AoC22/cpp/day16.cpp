b342d45252a20a3db88073a3be522a1ba0d56493
Yury Fedorov <yury.fedorov@gmail.com>
2023-02-13 22:45:50 +0100

    AoC22 Day16 Part1 alpha

--- AoC22/cpp/day16.cpp
+++ AoC22/cpp/day16.cpp
@@ -15,10 +15,9 @@ struct Conf {
 };
 
 using Map = absl::flat_hash_map<std::string, Conf>;
-using Doors = std::vector<std::string_view>;  // open doors
-using DoorSet = absl::flat_hash_set<std::string_view>;
-
-constexpr int kT = 30;
+using Door = std::string_view;
+using Doors = std::vector<Door>;
+using DoorSet = absl::flat_hash_set<Door>;
 
 [[nodiscard]] long PressureInMinute(const Map &map,
                                     const Doors &open) noexcept {
@@ -35,8 +34,7 @@ constexpr int kT = 30;
   return doors;
 }
 
-[[nodiscard]] int Distance(const Map &map, std::string_view from,
-                           std::string_view to) noexcept {
+[[nodiscard]] int Distance(const Map &map, Door from, Door to) noexcept {
   int distance{0};
   DoorSet frontline{from};
   while (frontline.find(to) == frontline.end()) {
@@ -52,24 +50,25 @@ constexpr int kT = 30;
 }
 
 // to avoid costly path searches over and over again
-[[nodiscard]] int DistanceFast(const Map &map, std::string_view from,
-                               std::string_view to) noexcept {
-  using Input = std::pair<std::string_view, std::string_view>;
+[[nodiscard]] int DistanceFast(const Map &map, Door from, Door to) noexcept {
+  using Input = std::pair<Door, Door>;
   using Cache = absl::flat_hash_map<Input, int>;
   static Cache cache;
   const Input input{from, to};
   const auto i = cache.find(input);
-  if (i != cache.end()) return i->second;  // cache hitted
+  if (i != cache.end())
+    return i->second; // cache hitted
   const int distance = Distance(map, from, to);
   cache[input] = distance;
   return distance;
 }
 
-[[nodiscard]] Doors Sequence(const Map &map, std::string_view from,
-                             std::string_view to, int depth) noexcept {
+[[nodiscard]] Doors Sequence(const Map &map, Door from, Door to,
+                             int depth) noexcept {
   if (from != to && depth > 0) {
     const auto &next_doors = map.at(from).next;
-    if (absl::c_find(next_doors, to) != next_doors.end()) return Doors{to};
+    if (absl::c_find(next_doors, to) != next_doors.end())
+      return Doors{to};
     // we are here because one level depth was not enough
     for (const auto &door : next_doors) {
       const auto d1 = depth - 1;
@@ -85,14 +84,14 @@ constexpr int kT = 30;
   return Doors{};
 }
 
-[[nodiscard]] Doors SequenceFast(const Map &map, std::string_view from,
-                                 std::string_view to) noexcept {
-  using Input = std::pair<std::string_view, std::string_view>;
+[[nodiscard]] Doors SequenceFast(const Map &map, Door from, Door to) noexcept {
+  using Input = std::pair<Door, Door>;
   using Cache = absl::flat_hash_map<Input, Doors>;
   static Cache cache;
   const Input input{from, to};
   const auto i = cache.find(input);
-  if (i != cache.end()) return i->second;  // cache hitted
+  if (i != cache.end())
+    return i->second; // cache hitted
   const int distance = DistanceFast(map, from, to);
   const auto s = Sequence(map, from, to, distance);
   cache[input] = s;
@@ -102,7 +101,10 @@ constexpr int kT = 30;
 void RemoveOpened(Doors &doors, const Doors &open) noexcept {
   // erase-remove idiom
   absl::c_for_each(open, [&doors](const auto &door) {
-    doors.erase(std::remove(doors.begin(), doors.end(), door), doors.end());
+    //    doors.erase(std::remove(doors.begin(), doors.end(), door),
+    //    doors.end());
+    const auto i = absl::c_find(doors, door);
+    if ( i != doors.end() ) doors.erase(i);
   });
 }
 
@@ -114,6 +116,7 @@ void RemoveNoPressure(Doors &doors, const Map &map) noexcept {
   });
 }
 
+/*
 // highest rates at the beginning with shortest path to it
 void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
   absl::c_sort(doors, [&map, from](const auto &a, const auto &b) {
@@ -122,78 +125,12 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
     return (map.at(a).rate - ad) > (map.at(b).rate - bd);
   });
 }
+*/
 
-[[nodiscard]] long Pressure(const Map &map, std::string_view start,
-                            const Doors &open, int t_left,
-                            std::optional<std::string_view> target) noexcept {
-  if (t_left <= 0) return 0;
-  const auto last_minute = PressureInMinute(map, open);
-  if (t_left == 1) {
-    // no sense to open anything, just calculate what is opened
-    return last_minute;
-  }
-  // we've more then 1 step to go, opening a door could make sense
-  if (target.has_value()) {
-    // we go to the selected target
-    const auto path = SequenceFast(map, start, target.value());
-    const auto dt = path.size() + 1;  // 1 - to open the door
-    const auto t1 = t_left - dt;
-    if (t1 <= 0) return t_left * last_minute;
-    Doors open1{open};
-    open1.push_back(target.value());
-    const auto pressure =
-        Pressure(map, target.value(), open1, t1, std::nullopt);
-    return pressure + (dt * last_minute);
-  }
-
-  // we do not have a selected target, we choose among the doors to open
-  // 1. get all doors
-  Doors doors = AllDoors(map);
-  // 2. remove opened doors
-  RemoveOpened(doors, open);
-  // 3. remove no pressure doors
-  RemoveNoPressure(doors, map);
-  // 4. order first high rated doors (could try to optimize: top 50%)
-  Order(map, doors, start);
-  long pressure{t_left * last_minute};
-  for (const auto &target : doors) {
-    const long cur_pressure = Pressure(map, start, open, t_left, target);
-    pressure = std::max(pressure, cur_pressure);
-  }
-  return pressure;
-}
-
-[[nodiscard]] long Pressure(const Map &map, std::string_view cur_door,
-                            std::deque<std::string_view> sequence,
+[[nodiscard]] long Pressure(const Map &map, Door cur_door, Doors open,
                             int t_max) noexcept {
-  long pressure{0};
-  Doors open;
-  for (int t{0}; t < t_max;) {
-    const auto last_minute = PressureInMinute(map, open);
-    int dt;
-    if (sequence.empty()) {
-      dt = t_max;
-    } else {
-      const auto target = sequence.front();
-      sequence.pop_front();
-      const auto path = SequenceFast(map, cur_door, target);
-      dt = path.size() + 1;  // 1 - to open the door
-      open.push_back(target);
-      cur_door = target;
-    }
-    const auto t1{t + dt};
-    const auto edt = t1 < t_max ? dt : (t_max - t);
-    pressure += edt * last_minute;
-    t += edt;
-    // EXPECT_TRUE(false) << "t=" << t << " pressure= " << pressure << " cur
-    // door = " << target;
-  }
-  return pressure;
-}
-
-[[nodiscard]] long Pressure(const Map &map, std::string_view cur_door,
-                            Doors open, int t_max) noexcept {
-  if (t_max < 0) return 0;
+  if (t_max < 0)
+    return 0;
   const auto last_minute = PressureInMinute(map, open);
   // 1. get all doors
   Doors doors = AllDoors(map);
@@ -201,30 +138,111 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
   RemoveOpened(doors, open);
   // 3. remove no pressure doors
   RemoveNoPressure(doors, map);
+  /*
   constexpr bool kIsOptimized = false;
   if (kIsOptimized) {
     // 4. order first high rated doors (could try to optimize: top 50%)
     Order(map, doors, cur_door);
     const auto n{doors.size()};
-    // 4379 - not right answer
+    // 4379 - not right answer -- 639s
     constexpr size_t kMaxHead =
         5;  // with 3 - 3735 (too high) 4 - 4149 (too high), 5 - 4149, no
-            // restrict - 4442 -- too high?
+            // restrict - 4442 -- too high? - 635s
     if (n > kMaxHead) {
       doors.resize(kMaxHead);
     }
   }
+  */
   long pressure{t_max * last_minute};
   for (const auto &target : doors) {
-    const auto path = SequenceFast(map, cur_door, target);
-    const auto dt = path.size() + 1;  // 1 - to open the door
+    const auto dt = DistanceFast(map, cur_door, target) + 1;
     const auto t1 = t_max - dt;
-    if (t1 < 0) continue;  // no sense to go
+    if (t1 < 0)
+      continue; // no sense to go
     long cur_pressure = dt * last_minute;
     Doors open1{open};
     open1.push_back(target);
     cur_pressure += Pressure(map, target, open1, t1);
     pressure = std::max(pressure, cur_pressure);
+  }
+  return pressure;
+}
+
+using State = std::pair<Door, std::optional<Door>>; // position, target
+constexpr Door kNoDoor = "??";
+[[nodiscard]] long Pressure2(const Map &map, State actor_a, State actor_b,
+                             Doors open, int t) noexcept {
+  if (t < 0)
+    return 0;
+  const auto last_minute = PressureInMinute(map, open);
+  if (t == 0) return last_minute;
+
+  const auto [pos_a, target_a] = actor_a;
+  const auto [pos_b, target_b] = actor_b;
+
+  // 1. get all doors
+  Doors doors = AllDoors(map);
+  // 2. remove opened doors
+  RemoveOpened(doors, open);
+  // 3. remove no pressure doors
+  RemoveNoPressure(doors, map);
+
+  if ( doors.empty() ) return t * last_minute;
+
+  // no sense to provide among options doors we already go
+  // XXX - really? if no more doors are available, then target of other actor is
+  // also good
+  Doors targets;
+  if (target_a.has_value())
+    targets.push_back(target_a.value());
+  if (target_b.has_value())
+    targets.push_back(target_b.value());
+  RemoveOpened(doors, targets);
+
+  const auto init_targets = [&doors](std::optional<Door> target) -> Doors {
+    return target.has_value() ? Doors{target.value()}
+           : doors.empty()    ? Doors{kNoDoor}
+                              : doors;
+  };
+
+  const auto seq = [&map](Door pos, Door target) {
+    return target == kNoDoor ? Doors{} : SequenceFast(map, pos, target);
+  };
+
+  const Doors targets_a = init_targets(target_a);
+  const Doors targets_b = init_targets(target_b);
+
+  long pressure{t * last_minute};
+  for (const auto &target_ai : targets_a) {
+    for (const auto &target_bi : targets_b) {
+      // no sense to follow for both the same door
+      if ( targets_a.size() == 1 && targets_b.size() == 1 ) {
+        // if only one option in every target, it is ok to go in the same direction
+      } else if (target_ai == target_bi && target_ai != kNoDoor)
+        continue;
+      Doors open1{open};
+      const auto step = [&open1, &seq](Door pos, Door target) -> State {
+        const Doors &path = seq(pos, target);
+        if (path.empty()) {
+          if  ( pos == target ) {
+            // we do not need to go anywhere but just open
+            if (absl::c_find(open1, pos) == open1.end())
+              open1.push_back(pos);
+            return State{pos, std::nullopt};
+          }
+          if ( target == kNoDoor )
+            return State{pos, std::nullopt};
+          EXPECT_TRUE(false) << "we are not expected to happen here";
+        }
+        // we need to make the next step
+        return State{path.front(), target};
+      };
+      const auto actor_a1 = step(pos_a, target_ai);
+      const auto actor_b1 = step(pos_b, target_bi);
+      const auto cur_pressure =
+          last_minute + Pressure2(map, actor_a1, actor_b1, open1, t - 1);
+      pressure = std::max(pressure, cur_pressure);
+    }
   }
   return pressure;
 }
@@ -249,26 +267,25 @@ void Order(const Map &map, Doors &doors, std::string_view from) noexcept {
   return map;
 }
 
-// open doors in format ,AA, ... ,ZZ,...
-[[nodiscard]] long Answer1(std::string_view file) noexcept {
-  const auto map = ReadMap(file);
-  return Pressure(map, "AA", Doors{}, kT, std::nullopt);
-}
+} // namespace day16
 
-}  // namespace day16
+constexpr day16::Door kStart = "AA";
 
 TEST(AoC22, Day16) {
-  std::deque<std::string_view> test_seq = {"DD", "BB", "JJ", "HH", "EE", "CC"};
-  const auto test_map = day16::ReadMap("16-sample");
-  EXPECT_EQ(day16::Pressure(test_map, "AA", test_seq, day16::kT), 1651);
-  const auto t1 =
-      day16::Pressure(test_map, "AA", day16::Doors{}, day16::kT, std::nullopt);
-  EXPECT_EQ(t1, 1651);
-  EXPECT_EQ(day16::Pressure(test_map, "AA", day16::Doors{}, day16::kT), 1651);
-  return;  // slow
-  const auto map = day16::ReadMap("16");
-  const auto a1 =
-      day16::Pressure(map, "AA", day16::Doors{}, day16::kT, std::nullopt);
-  EXPECT_EQ(a1, 0);
-  // EXPECT_EQ( day16::Pressure( map, "AA", day16::Doors{}, day16::kT ) , 0);
+  const auto answer1 = [](const day16::Map &map) {
+    // return day16::Pressure(map, "AA", day16::Doors{}, day16::kT,
+    // std::nullopt);
+    return day16::Pressure(map, kStart, day16::Doors{}, 30);
+  };
+  const auto answer2 = [](const day16::Map &map) {
+    // return day16::Pressure(map, "AA", day16::Doors{}, day16::kT,
+    // std::nullopt);
+    day16::State start{kStart, std::nullopt};
+    return day16::Pressure2(map, start, start, day16::Doors{}, 24);
+  };
+  const bool is_test = IsFastOnly(); // takes 56 seconds
+  const auto file = is_test ? "16-sample" : "16";
+  const auto map = day16::ReadMap(file);
+  EXPECT_EQ(answer1(map), is_test ? 1651 : 1775);
+  EXPECT_EQ(answer2(map), is_test ? 1707 : 0);
 }
