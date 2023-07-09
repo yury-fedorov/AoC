8371035329d9a9da12966baccaa41411d2ad9c9a
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-07-09 20:40:57 +0200

    Filter out red tests
    Disabling red tests in the pipeline.

--- AoC22/cpp/common.h
+++ AoC22/cpp/common.h
@@ -11,8 +11,11 @@
 #include "absl/strings/str_split.h"
 #include "gtest/gtest.h"
 
-// to filter out slow tests
+// To filter out slow tests.
 [[nodiscard]] inline bool IsFastOnly() noexcept { return true; }
+
+// Avoid running red tests. It helps to keep pipeline green.
+[[nodiscard]] inline bool IsGreenOnly() noexcept { return true; }
 
 [[nodiscard]] inline constexpr int Sign(auto x) noexcept {
   return x > 0 ? 1 : x < 0 ? -1 : 0;
