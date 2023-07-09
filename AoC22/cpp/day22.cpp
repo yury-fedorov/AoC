8090a4c2b321ad9d8e8fbe9420366f1f743ae9fb
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-07-09 21:03:23 +0200

    Update day22.cpp

--- AoC22/cpp/day22.cpp
+++ AoC22/cpp/day22.cpp
@@ -204,11 +204,12 @@ TEST(AoC22, Day22) {
   EXPECT_EQ(day22::FinalPassword(6, 8, day22::Direction::kRight), 6032);
   const auto test = day22::Solution("22-sample");
   EXPECT_EQ(test[0], 6032);
-  EXPECT_EQ(test[1], 5031);
   const auto answers = day22::Solution("22");
   EXPECT_EQ(answers[0], 27436);
   
   // TODO - Day22 Part2 to be solved
-  if ( !IsGreenOnly() )
-    EXPECT_EQ(answers[1], 0);
+  if ( IsGreenOnly() ) return;
+  
+  EXPECT_EQ(test[1], 5031);
+  EXPECT_EQ(answers[1], 0);
 }
