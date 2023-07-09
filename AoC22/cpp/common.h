04529b2e656e1274e3e168676b87db164217e584
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-07-09 16:55:01 +0200

    Update common.h
    Getting rid of dependency on rangev3 from common.h header.

--- AoC22/cpp/common.h
+++ AoC22/cpp/common.h
@@ -3,8 +3,6 @@
 #include <algorithm>
 #include <fstream>
 #include <numeric>
-// #include <ranges>
-#include <range/v3/all.hpp> // get everything for external implementation
 #include <string>
 #include <string_view>
 #include <vector>
@@ -12,10 +10,6 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_split.h"
 #include "gtest/gtest.h"
-
-// mapping to hide external dependency on ranges
-namespace r = ranges;
-namespace rv = ranges::views;
 
 // to filter out slow tests
 [[nodiscard]] inline bool IsFastOnly() noexcept { return true; }
