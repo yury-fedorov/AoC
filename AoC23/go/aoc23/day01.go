aae420c75e779d6a6d43a4ea70d187f727516800
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-01 09:47:09 +0000

    AoC23 Day01 refactoring.

--- AoC23/go/aoc23/day01.go
+++ AoC23/go/aoc23/day01.go
@@ -8,55 +8,54 @@ import (
 
 type Day01 struct{}
 
-func (d Day01) Line(part Part, line string) int {
-	return (10 * d.FirstDigit(part, line)) + d.LastDigit(part, line)
+func (d Day01) calibrationValue(part Part, line string) int {
+	return (10 * d.firstDigit(part, line)) + d.lastDigit(part, line)
 }
 
-func (d Day01) FirstDigit(part Part, line string) int {
-	a := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-	ac := []rune(line)
-	for i, letter := range ac {
-		if unicode.IsDigit(letter) {
-			return int(letter - '0')
+func (d Day01) textDigit(line string) (found bool, digit int) {
+	Day01TextDigits := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
+	for i, text := range Day01TextDigits {
+		if strings.HasPrefix(line, text) {
+			return true, i + 1
 		}
-		if part == Part1 {
-			continue
-		}
-		for j, text := range a {
-			if strings.HasPrefix(line[i:], text) {
-				return j + 1
-			}
+	}
+	return false, 0
+}
+
+func (d Day01) positionDigit(part Part, line string, i int) (found bool, digit int) {
+	letter := []rune(line)[i]
+	if unicode.IsDigit(letter) {
+		return true, int(letter - '0')
+	}
+	if part == Part1 {
+		return false, 0
+	}
+	return d.textDigit(line[i:])
+}
+
+func (d Day01) firstDigit(part Part, line string) int {
+	for i := range line {
+		if found, digit := d.positionDigit(part, line, i); found {
+			return digit
 		}
 	}
 	return 0
 }
 
-func (d Day01) LastDigit(part Part, line string) int {
-	a := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-	ac := []rune(line)
-	for i := len(ac) - 1; i >= 0; i-- {
-		letter := ac[i]
-		if unicode.IsDigit(letter) {
-			return int(letter - '0')
-		}
-		if part == Part1 {
-			continue
-		}
-		for j, text := range a {
-			if strings.HasPrefix(line[i:], text) {
-				return j + 1
-			}
+func (d Day01) lastDigit(part Part, line string) int {
+	for i := len(line) - 1; i >= 0; i-- {
+		if found, digit := d.positionDigit(part, line, i); found {
+			return digit
 		}
 	}
 	return 0
 }
 
 func (d Day01) Solve() Solution {
-	part1 := 0
-	part2 := 0
+	var part1, part2 int
 	for _, line := range ReadFile("01") {
-		part1 += d.Line(Part1, line)
-		part2 += d.Line(Part2, line)
+		part1 += d.calibrationValue(Part1, line)
+		part2 += d.calibrationValue(Part2, line)
 	}
 	return Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
