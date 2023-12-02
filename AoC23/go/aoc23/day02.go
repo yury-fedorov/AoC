335572bb1b0dba25f6308d1720349025d61fc78d
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-02 07:06:54 +0100

    AoC23 Day02 Part1 solved

--- AoC23/go/aoc23/day02.go
+++ AoC23/go/aoc23/day02.go
@@ -1,7 +1,91 @@
 package aoc23
 
+import (
+	"regexp"
+	"strconv"
+	"strings"
+)
+
 type Day02 struct{}
 
+type color string
+
+const (
+	red   color = "red"
+	blue  color = "blue"
+	green color = "green"
+)
+
+var colors = []color{red, blue, green}
+
+type extraction map[color]int
+
+type game struct {
+	id          int
+	extractions []extraction
+}
+
+var r1 = regexp.MustCompile(`Game (\d+): (.+)`)
+var r2 = regexp.MustCompile(`(\d+) ([a-z]+)`)
+
+// Game 6: 3 blue, 10 green, 2 red; 5 green; 6 blue, 3 red
+func (d Day02) parseGame(line string) game {
+	match := r1.FindStringSubmatch(line)
+	id, err := strconv.Atoi(match[1])
+	if err != nil {
+		panic(err)
+	}
+	var extractions []extraction
+	gamesStr := strings.Split(match[2], ";")
+	for _, gameStr := range gamesStr {
+		e := make(extraction)
+		es := strings.Split(gameStr, ",")
+		for _, esi := range es {
+			match = r2.FindStringSubmatch(esi)
+			qty, err := strconv.Atoi(match[1])
+			if err != nil {
+				panic(err)
+			}
+			e[color(match[2])] = qty
+		}
+		extractions = append(extractions, e)
+	}
+	return game{id: id, extractions: extractions}
+}
+
+func (d Day02) rgb(r int, g int, b int) extraction {
+	return extraction{red: r, green: g, blue: b}
+}
+
+func (d Day02) match(requirement, test extraction) bool {
+	for _, c := range colors {
+		maxQty := requirement[c]
+		testQty := test[c]
+		if testQty > maxQty {
+			return false
+		}
+	}
+	return true
+}
+
+func (d Day02) matchGame(requirement extraction, g game) bool {
+	for _, e := range g.extractions {
+		if !d.match(requirement, e) {
+			return false
+		}
+	}
+	return true
+}
+
 func (d Day02) Solve() Solution {
-	return Solution{"", ""}
+	requirement := d.rgb(12, 13, 14)
+	var part1, part2 int
+	for _, line := range ReadFile("02") {
+		g := d.parseGame(line)
+		if d.matchGame(requirement, g) {
+			part1 += g.id
+		}
+		part2 += 1
+	}
+	return Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
--- AoC23/go/main_test.go
+++ AoC23/go/main_test.go
@@ -9,7 +9,7 @@ import (
 func TestDays(t *testing.T) {
 	solutions := []aoc23.Solution{
 		{Part1: "54968", Part2: "54094"},
-		{Part1: "", Part2: ""},
+		{Part1: "2283", Part2: ""},
 	}
 	for i, d := range aoc23.Days() {
 		got := d.Solve()
