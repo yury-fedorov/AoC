fd7715a99abc0b6ad0dc0a3fef1e95e676c733f3
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-03 07:23:26 +0100

    AoC23 Day03 Part2 solved.

--- AoC23/go/day03/day03.go
+++ AoC23/go/day03/day03.go
@@ -34,10 +34,14 @@ func isPartialNumber(engine []string, startX, endX, y int) bool {
 	return false
 }
 
-func sumPartNumbers(engine []string) int {
+type partNumber struct {
+	number, x0, x1, y int
+}
+
+func identifyPartNumbers(engine []string) []partNumber {
 	maxY := len(engine)
 	maxX := len(engine[0])
-	var result int
+	var result []partNumber
 	for y := 0; y < maxY; y++ {
 		const NotStarted int = -1
 		var startAt = NotStarted
@@ -56,7 +60,12 @@ func sumPartNumbers(engine []string) int {
 					if err != nil {
 						panic(partialNumber)
 					}
-					result += partialNumber
+					result = append(result, partNumber{
+						number: partialNumber,
+						x0:     startAt,
+						x1:     x - 1,
+						y:      y,
+					})
 				}
 				startAt = NotStarted
 			}
@@ -66,10 +75,59 @@ func sumPartNumbers(engine []string) int {
 	return result
 }
 
+func sumPartNumbers(list []partNumber) int {
+	var result int
+	for _, pn := range list {
+		result += pn.number
+	}
+	return result
+}
+
+func abs(x int) int {
+	if x >= 0 {
+		return x
+	}
+	return -x
+}
+
+func inRange(x, min, max int) bool {
+	return x >= min && x <= max
+}
+
+func isAdjacent(n partNumber, x, y int) bool {
+	return abs(n.y-y) <= 1 && inRange(x, n.x0-1, n.x1+1)
+}
+
+func sumGearRatios(engine []string, numberList []partNumber) int {
+	var result int
+	const Star rune = '*'
+	maxY := len(engine)
+	maxX := len(engine[0])
+	for y := 0; y < maxY; y++ {
+		for x := 0; x < maxX; x++ {
+			chi := at(engine, x, y)
+			if chi == Star {
+				// A gear is any * symbol that is adjacent to exactly two part numbers.
+				var found []partNumber
+				for _, pni := range numberList {
+					if isAdjacent(pni, x, y) {
+						found = append(found, pni)
+					}
+				}
+				if len(found) == 2 {
+					result += found[0].number * found[1].number
+				}
+			}
+		}
+	}
+	return result
+}
+
 func (d Day03) Solve() aoc.Solution {
 	var part1, part2 int
 	engine := aoc.ReadFile("03")
-	part1 = sumPartNumbers(engine)
-	part2 = len(engine[0])
+	partNumberList := identifyPartNumbers(engine)
+	part1 = sumPartNumbers(partNumberList)
+	part2 = sumGearRatios(engine, partNumberList)
 	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
