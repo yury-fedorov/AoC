6efa8a3853df5643f79b98f6f4b931ac043a7e5d
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-05 20:56:05 +0100

    AoC23 slow/fast tests orchestration.

--- AoC23/go/main_test.go
+++ AoC23/go/main_test.go
@@ -12,24 +12,34 @@ import (
 	"github.com/yury-fedorov/AoC/AoC23/day06"
 )
 
+const runSlowTests = false
+
+type Test struct {
+	day  aoc.Day
+	want aoc.Solution
+}
+
 func TestDays(t *testing.T) {
-	tests := []struct {
-		day  aoc.Day
-		want aoc.Solution
-	}{
+	fastTests := []Test{
 		{day: &day01.Day01{}, want: aoc.Solution{Part1: "54968", Part2: "54094"}},
 		{day: &day02.Day02{}, want: aoc.Solution{Part1: "2283", Part2: "78669"}},
 		{day: &day03.Day03{}, want: aoc.Solution{Part1: "540212", Part2: "87605697"}},
 		{day: &day04.Day04{}, want: aoc.Solution{Part1: "21821", Part2: "5539496"}},
-		{day: &day05.Day05{}, want: aoc.Solution{Part1: "174137457", Part2: "1493866"}},
 		{day: &day06.Day06{}, want: aoc.Solution{Part1: "0", Part2: "0"}},
 	}
-	for i, test := range tests {
+	slowTests := []Test{
+		// takes 8 minutes
+		{day: &day05.Day05{}, want: aoc.Solution{Part1: "174137457", Part2: "1493866"}},
+	}
+	tests := fastTests
+	if runSlowTests {
+		tests = append(tests, slowTests...)
+	}
+	for _, test := range tests {
 		got := test.day.Solve()
 		want := test.want
 		if got != want {
-			day := i + 1 // zero based index
-			t.Fatalf(`Day %d returned %v, want %v`, day, got, want)
+			t.Fatalf(`%T returned %v, want %v`, test.day, got, want)
 		}
 	}
 }
