5b22d58797b648c8c1320013a161dde33daf4fbe
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-05 21:39:08 +0100

    AoC23 Day07 draft.

---
+++ AoC23/go/day07/day07.go
@@ -0,0 +1,15 @@
+package day07
+
+import (
+	"github.com/yury-fedorov/AoC/AoC23/aoc"
+	"strconv"
+)
+
+type Day07 struct{}
+
+func (d Day07) Solve() aoc.Solution {
+	var part1, part2 int
+	// for _, line := range ReadFile("07") {
+	// }
+	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
+}
