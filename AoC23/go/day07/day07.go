f06f369601135384a62985af05c0d96d986db2eb
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-12-08 08:59:00 +0100

    Update day07.go

--- AoC23/go/day07/day07.go
+++ AoC23/go/day07/day07.go
@@ -10,14 +10,14 @@ const Cards = "AKQJT98765432"
 
 type HandType int
 
-const(
-FiveOfKind HandType = iota, // where all five cards have the same label: AAAAA
-FourOfKind, // where four cards have the same label and one card has a different label: AA8AA
-FullHouse, // where three cards have the same label, and the remaining two cards share a different label: 23332
-ThreeOfKind, // where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-TwoPair, // where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-OnePair, // where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-HighCard // where all cards are different
+const (
+	FiveOfKind  HandType = iota // where all five cards have the same label: AAAAA
+	FourOfKind                  // where four cards have the same label and one card has a different label: AA8AA
+	FullHouse                   // where three cards have the same label, and the remaining two cards share a different label: 23332
+	ThreeOfKind                 // where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+	TwoPair                     // where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+	OnePair                     // where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+	HighCard                    // where all cards are different
 )
 
 type Day07 struct{}
