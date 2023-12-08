020af2568f64d6f08804f6b1247cd068a3c745a9
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-12-08 09:18:05 +0100

    AoC23 Day07 solved (#131)
    * AoC23 Day07 solution
    
    * AoC23 Day07 update main_test.go with results
    
    * slices as exp import - day07.go
    
    * Cleaning from debug info day07.go

--- AoC23/go/day07/day07.go
+++ AoC23/go/day07/day07.go
@@ -2,11 +2,15 @@ package day07
 
 import (
 	"github.com/yury-fedorov/AoC/AoC23/aoc"
+	"golang.org/x/exp/maps"
+	slices "golang.org/x/exp/slices" // Due to 1.20
+	"sort"
 	"strconv"
+	"strings"
 )
 
 // Order of the cards (lower index, more value)
-const Cards = "AKQJT98765432" 
+const Cards = "AKQJT98765432"
 
 type HandType int
 
@@ -20,10 +24,165 @@ const (
 	HighCard                    // where all cards are different
 )
 
+func getType(hand []rune) HandType {
+	m := make(map[rune]int)
+	for _, c := range hand {
+		m[c]++
+	}
+	counts := maps.Values(m)
+	slices.Sort(counts)
+	maxCount := counts[len(counts)-1]
+	differentLabels := len(m)
+	if differentLabels == 1 {
+		return FiveOfKind
+	}
+	if differentLabels == 2 {
+		// AAAAB or AAABB
+		return aoc.Ifelse(maxCount == 4, FourOfKind, FullHouse)
+	}
+	if maxCount == 3 {
+		return ThreeOfKind
+	}
+	if maxCount == 2 {
+		return aoc.Ifelse(differentLabels == 3, TwoPair, OnePair)
+	}
+	return HighCard
+}
+
+func getType2(hand []rune) HandType {
+	t1 := getType(hand)
+	if t1 == FiveOfKind {
+		return FiveOfKind
+	}
+	m := make(map[rune]int)
+	for _, c := range hand {
+		m[c]++
+	}
+	jokerCount := m['J']
+	if jokerCount == 0 {
+		return t1
+	}
+	differentLabels := len(m)
+	if jokerCount == 4 || differentLabels == 2 {
+		// 4 jokers + any card
+		// or only jokers and any single card
+		return FiveOfKind
+	}
+
+	counts := maps.Values(m)
+	slices.Sort(counts)
+	maxCount := counts[len(counts)-1]
+
+	if jokerCount == 3 {
+		// min: 4 same + 1 diff - FourOfKind
+		return FourOfKind
+	}
+
+	if jokerCount == 2 {
+
+		// 2 cards same label - FourOfKind
+		// all different (ABC) - AaaBC - ThreeOfKind or AaBbC - TwoPairs, more precious is ThreeOfKind
+		return aoc.Ifelse(differentLabels == 3, FourOfKind, ThreeOfKind)
+	}
+
+	// the case with a single joker
+	switch maxCount {
+	case 3:
+		return FourOfKind
+	case 2:
+		return aoc.Ifelse(differentLabels == 3, FullHouse, ThreeOfKind)
+	case 1:
+		return OnePair
+	}
+
+	panic("Unexpected flow")
+}
+
+func compareCards(a, b rune) int {
+	if a == b {
+		return 0
+	}
+	ai := strings.IndexRune(Cards, a)
+	bi := strings.IndexRune(Cards, b)
+	return aoc.Ifelse(ai < bi, 1, -1)
+}
+
+func compareHands(a, b []rune) int {
+	hta := getType(a)
+	htb := getType(b)
+	if hta != htb {
+		return aoc.Ifelse(hta < htb, 1, -1)
+	}
+	for i := 0; i < len(a); i++ {
+		cc := compareCards(a[i], b[i])
+		if cc != 0 {
+			return cc
+		}
+	}
+	return 0
+}
+
+func compareCards2(a, b rune) int {
+	if a == b {
+		return 0
+	}
+	if a == 'J' {
+		return -1
+	}
+	if b == 'J' {
+		return 1
+	}
+	return compareCards(a, b)
+}
+
+func compareHands2(a, b []rune) int {
+	hta := getType2(a)
+	htb := getType2(b)
+	if hta != htb {
+		return aoc.Ifelse(hta < htb, 1, -1)
+	}
+	for i := 0; i < len(a); i++ {
+		cc := compareCards2(a[i], b[i])
+		if cc != 0 {
+			return cc
+		}
+	}
+	return 0
+}
+
+type HandBet struct {
+	hand []rune
+	bet  int
+}
+
+func parse(line string) HandBet {
+	a := strings.Split(line, " ")
+	hand := a[0]
+	return HandBet{hand: []rune(hand), bet: aoc.Atoi(a[1])}
+}
+
 type Day07 struct{}
 
 func (d Day07) Solve() aoc.Solution {
 	var part1, part2 int
-	// data := ReadFile("07-1")
+  data := aoc.ReadFile("07")
+	var list []HandBet
+	for _, line := range data {
+		hb := parse(line)
+		list = append(list, hb)
+	}
+
+	sort.SliceStable(list, func(i, j int) bool { return compareHands(list[i].hand, list[j].hand) < 0 })
+	for i, hb := range list {
+		rank := i + 1
+		part1 += rank * hb.bet
+	}
+
+	sort.SliceStable(list, func(i, j int) bool { return compareHands2(list[i].hand, list[j].hand) < 0 })
+	for i, hb := range list {
+		rank := i + 1
+		part2 += rank * hb.bet
+	}
+
 	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
