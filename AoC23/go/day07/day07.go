1b0e1346c07f2e485c6c018ea9b583ce5323fc85
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-12-08 08:55:24 +0100

    Temporal fix of day07.go

--- AoC23/go/day07/day07.go
+++ AoC23/go/day07/day07.go
@@ -17,13 +17,13 @@ FullHouse, // where three cards have the same label, and the remaining two cards
 ThreeOfKind, // where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
 TwoPair, // where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
 OnePair, // where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-HighCard, // where all cards are different
+HighCard // where all cards are different
 )
 
 type Day07 struct{}
 
 func (d Day07) Solve() aoc.Solution {
 	var part1, part2 int
-	data := ReadFile("07-1")
+	// data := ReadFile("07-1")
 	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
