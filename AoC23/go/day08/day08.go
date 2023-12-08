ee08b8b520f64c4dcc289477d281fd83f017dc5e
Yury Fedorov <114264320+yuryf-google@users.noreply.github.com>
2023-12-08 09:36:52 +0100

    Initial day08 draft

---
+++ AoC23/go/day08/day08.go
@@ -0,0 +1,57 @@
+package day08
+
+import (
+	"github.com/yury-fedorov/AoC/AoC23/aoc"
+	"regexp"
+	"strconv"
+)
+
+type NodeName string
+
+type Node struct {
+   name NodeName
+   left NodeName
+   right NodeName
+}
+
+type Network struct {
+   navigation string
+   nodes map[NodeName]Node
+}
+
+const (
+   StartNode NodeName = "AAA"
+   EndNode NodeName = "ZZZ"
+)
+
+func parse(data []string) Network {
+	var re = regexp.MustCompile( `([A-Z]+) = \((A-Z]+), (A-Z]+)\)` )
+	var result Network
+	result.nodes = make(map[NodeName]Node)
+	result.navigation = data[0]
+	for _, line := range data[2:] {
+		m := re.FindStringSubmatch(line)
+		node := Node{ name: NodeName(m[0]), left: NodeName(m[1]), right: NodeName(m[2]) }
+		result.nodes[node.name] = node
+	}
+	return result
+}
+
+type Day08 struct{}
+
+func (d Day08) Solve() aoc.Solution {
+	var part1, part2 int
+	data := aoc.ReadFile("08-1")
+	n := parse(data)
+	navIndex := 0
+	navIndexLength := len(n.navigation)
+	var curNode Node
+	curNode = n.nodes[StartNode]
+	for curNode.name != EndNode {
+		navInstruction := n.navigation[navIndex]
+		curNode = n.nodes[aoc.Ifelse(navInstruction == 'L', curNode.left, curNode.right)]
+		navIndex = ( navIndex + 1 ) % navIndexLength
+		part1++
+	}
+	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
+}
