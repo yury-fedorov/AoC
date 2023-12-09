26599d532db22209e37ace2000069467a65bdfcf
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-09 07:08:27 +0100

    AoC23 Day25 day draft (for coping).

---
+++ AoC23/go/day25/day25.go
@@ -0,0 +1,15 @@
+package day25
+
+import (
+	"github.com/yury-fedorov/AoC/AoC23/aoc"
+	"strconv"
+)
+
+type Day25 struct{}
+
+func (d Day25) Solve() aoc.Solution {
+	var part1, part2 int
+	// for _, line := range aoc.ReadFile("25") {
+	// }
+	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
+}
