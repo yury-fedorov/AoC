8934dcf0fcc4fe081d4c20ba63c76a34c1235bba
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-09 06:18:35 +0100

    AoC23 Day08 Part1 solution.

--- AoC23/go/day08/day08.go
+++ AoC23/go/day08/day08.go
@@ -11,33 +11,33 @@ import (
 type NodeName string
 
 type Node struct {
-   name NodeName
-   left NodeName
-   right NodeName
+	name  NodeName
+	left  NodeName
+	right NodeName
 }
 
 type Network struct {
-   navigation string
-   nodes map[NodeName]Node
+	navigation string
+	nodes      map[NodeName]Node
 }
 
 const (
-   StartNode NodeName = "AAA"
-   EndNode NodeName = "ZZZ"
+	StartNode NodeName = "AAA"
+	EndNode   NodeName = "ZZZ"
 )
 
 func parse(data []string) Network {
 	// AAA = (BBB, CCC)
-	var re = regexp.MustCompile( `([0-9A-Z]+) = \(([0-9A-Z]+), ([0-9A-Z]+)\)` )
+	var re = regexp.MustCompile(`([0-9A-Z]+) = \(([0-9A-Z]+), ([0-9A-Z]+)\)`)
 	var result Network
 	result.nodes = make(map[NodeName]Node)
 	result.navigation = data[0]
 	for _, line := range data[2:] {
 		m := re.FindStringSubmatch(line)
 		if m == nil {
-			panic( line )
+			panic(line)
 		}
-		node := Node{ name: NodeName(m[1]), left: NodeName(m[2]), right: NodeName(m[3]) }
+		node := Node{name: NodeName(m[1]), left: NodeName(m[2]), right: NodeName(m[3])}
 		result.nodes[node.name] = node
 	}
 	return result
@@ -45,7 +45,7 @@ func parse(data []string) Network {
 
 func isEnd(nodes []Node) bool {
 	for _, n := range nodes {
-		if !strings.HasSuffix( string(n.name), "Z" ) {
+		if !strings.HasSuffix(string(n.name), "Z") {
 			return false
 		}
 	}
@@ -56,7 +56,7 @@ type Day08 struct{}
 
 func (d Day08) Solve() aoc.Solution {
 	var part1, part2 int
-	data := aoc.ReadFile("08-2")
+	data := aoc.ReadFile("08")
 	n := parse(data)
 	navIndex := 0
 	navIndexLength := len(n.navigation)
@@ -65,22 +65,22 @@ func (d Day08) Solve() aoc.Solution {
 	for curNode.name != EndNode {
 		navInstruction := n.navigation[navIndex]
 		curNode = n.nodes[aoc.Ifelse(navInstruction == 'L', curNode.left, curNode.right)]
-		navIndex = ( navIndex + 1 ) % navIndexLength
+		navIndex = (navIndex + 1) % navIndexLength
 		part1++
 	}
 
 	var curNodes []Node
 	for _, name := range maps.Keys(n.nodes) {
-		if strings.HasSuffix( string(name), "A" ) {
+		if strings.HasSuffix(string(name), "A") {
 			curNodes = append(curNodes, n.nodes[name])
-		}		
+		}
 	}
 
 	// part 2 is a draft - to solve later
 	navIndex = 0
 	for !isEnd(curNodes) {
 		navInstruction := n.navigation[navIndex]
-		navIndex = ( navIndex + 1 ) % navIndexLength
+		navIndex = (navIndex + 1) % navIndexLength
 		part2++
 		newVersion := curNodes
 		for i, cn := range curNodes {
@@ -88,8 +88,8 @@ func (d Day08) Solve() aoc.Solution {
 
 		}
 		curNodes = newVersion
-		break; // not working implementation, to remove at home
-	}	
-	
+		break // TODO - slow implementation
+	}
+
 	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
