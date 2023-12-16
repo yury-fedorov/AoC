541a7cb819d7ca04e42279a33b79cad5c658906f
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-16 18:06:05 +0100

    AoC23 Day15 Part1 solved.

--- AoC23/go/day15/day15.go
+++ AoC23/go/day15/day15.go
@@ -3,13 +3,28 @@ package day15
 import (
 	"github.com/yury-fedorov/AoC/AoC23/aoc"
 	"strconv"
+	"strings"
 )
 
 type Day15 struct{}
 
+func hash(seq string) int {
+	var result int
+	for _, c := range seq {
+		ascii := int(c)
+		result += ascii
+		result *= 17
+		result = result % 256
+	}
+	return result
+}
+
 func (d Day15) Solve() aoc.Solution {
 	var part1, part2 int
-	// for _, line := range aoc.ReadFile("25") {
-	// }
+	initSeq := aoc.ReadFile("15")
+	ss := strings.Split(initSeq[0], ",")
+	for _, seq := range ss {
+		part1 += hash(seq)
+	}
 	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
