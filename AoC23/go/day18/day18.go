12349d0ca4ad8914666ebf44184dba22d1fd5f52
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-18 17:31:51 +0100

    AoC23 Day18 Part2 wip.

--- AoC23/go/day18/day18.go
+++ AoC23/go/day18/day18.go
@@ -3,6 +3,7 @@ package day18
 import (
 	"github.com/yury-fedorov/AoC/AoC23/aoc"
 	"golang.org/x/exp/maps"
+	"sort"
 	"strconv"
 	"strings"
 )
@@ -47,19 +48,6 @@ func minMax(points []Point) (min Point, max Point) {
 		maxP.y = aoc.Max(maxP.y, p.y)
 	}
 	return minP, maxP
-}
-
-// -- part 2 --
-
-type Line struct {
-	a, b Point
-}
-
-var Directions2 = map[rune]Point{
-	'0': Right,
-	'1': Down,
-	'2': Left,
-	'3': Up,
 }
 
 // Solution of the part 1.
@@ -119,31 +107,61 @@ func part1(m map[Point]rune) int {
 	return part1
 }
 
-func (day Day18) Solve() aoc.Solution {
-	var part2 int
+// -- part 2 --
 
-	// Initialization starts.
+type Line struct {
+	a, b Point
+}
+
+var Directions2 = map[rune]Point{
+	'0': Right,
+	'1': Down,
+	'2': Left,
+	'3': Up,
+}
+
+func orderedGrid(m2 []Line, getZ func(Point) int) []int {
+	set := make(map[int]bool)
+	for _, l := range m2 {
+		set[getZ(l.a)] = true
+		set[getZ(l.b)] = true
+	}
+	result := maps.Keys(set)
+	sort.Ints(result)
+	return result
+}
+
+func part2(m2 []Line) int {
+	var part2 int
+	xGrid := orderedGrid(m2, func(p Point) int { return p.x })
+	yGrid := orderedGrid(m2, func(p Point) int { return p.y })
+	part2 = len(xGrid) * len(yGrid)
+	return part2
+}
+
+func (day Day18) Solve() aoc.Solution {
+	// Initialization requires different maps for part1 and part 2.
 	var digPlan = aoc.ReadFile("18")
-	m := make(map[Point]rune)
-	var m2 []Line // part 2 version of the map
-	var p, p2 Point
+	m := make(map[Point]rune) // part 1 version fo the map
+	var m2 []Line             // part 2 version of the map
+	var p, p2 Point           // starting points
 	m[p] = Dug
 	for _, line := range digPlan {
 		dq := strings.Split(line, " ")
+		// part 1 map initialization
 		d := Directions[dq[0]]
 		q := aoc.Atoi(dq[1])
 		for i := 0; i < q; i++ {
 			p = move(p, d)
 			m[p] = Dug
 		}
-		// part 2
-		// (#489852)
-		code := dq[2]
-		code = code[2 : len(code)-1]
-		d2code := []rune(code)[len(code)-1]
-		d2 := Directions2[d2code]
-		code = code[:len(code)-1]
-		q2, err := strconv.ParseInt(code, 16, 64)
+		// part 2 map initialization
+		code := dq[2]                             // (#489852)
+		code = code[2 : len(code)-1]              // 489852
+		d2code := []rune(code)[len(code)-1]       // last digit is direction code
+		d2 := Directions2[d2code]                 // conversion to the direction
+		code = code[:len(code)-1]                 // code without last digit: 489852 -> 48985
+		q2, err := strconv.ParseInt(code, 16, 64) // from hex to int
 		if err != nil {
 			panic(err)
 		}
@@ -151,6 +169,5 @@ func (day Day18) Solve() aoc.Solution {
 		m2 = append(m2, Line{p2, p2b})
 		p2 = p2b
 	}
-
-	return aoc.Solution{strconv.Itoa(part1(m)), strconv.Itoa(part2)}
+	return aoc.Solution{strconv.Itoa(part1(m)), strconv.Itoa(part2(m2))}
 }
