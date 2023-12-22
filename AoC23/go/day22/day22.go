91b55aeafdc5de567ab8016f071f2c56ef77ae7e
Yury Fedorov <yury.fedorov@gmail.com>
2023-12-22 08:03:39 +0100

    AoC23 Day22 wip.

--- AoC23/go/day22/day22.go
+++ AoC23/go/day22/day22.go
@@ -3,13 +3,35 @@ package day22
 import (
 	"github.com/yury-fedorov/AoC/AoC23/aoc"
 	"strconv"
+	"strings"
 )
 
 type Day22 struct{}
 
+type Point3 struct {
+	x, y, z int
+}
+
+type Brick struct {
+	a, b Point3
+}
+
+func parseBricks(data []string) []Brick {
+	var result []Brick
+	pp := func(p string) Point3 {
+		a := aoc.ToSlice(strings.ReplaceAll(p, ",", " "))
+		return Point3{x: int(a[0]), y: int(a[1]), z: int(a[2])}
+	}
+	for _, line := range data {
+		points := strings.Split(line, "~")
+		result = append(result, Brick{pp(points[0]), pp(points[1])})
+	}
+	return result
+}
+
 func (day Day22) Solve() aoc.Solution {
 	var part1, part2 int
-	// for _, line := range aoc.ReadFile("25") {
-	// }
+	bricks := parseBricks(aoc.ReadFile("22"))
+	part1 = len(bricks)
 	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
