d8aebd235406d41fc53f1e27c6da491b64bce1d7
Yury Fedorov <yury.fedorov@gmail.com>
2024-01-05 18:17:34 +0100

    AoC23 Day24 wip.

--- AoC23/go/day24/day24.go
+++ AoC23/go/day24/day24.go
@@ -7,9 +7,50 @@ import (
 
 type Day24 struct{}
 
+type Point struct{ x, y, z float64 }
+
+// 2D basics: https://www.cuemath.com/geometry/intersection-of-two-lines/
+// Line in form: ax + by + cz + d = 0
+type LineCoefficients struct{ a, b, c, d float64 }
+
+// Input
+type PointShift struct{ point, shift Point }
+
+func toLineCoefficients(ps PointShift) LineCoefficients {
+	return LineCoefficients{} // TODO implement
+}
+
+func intersectionPoint(one, two LineCoefficients) Point {
+	base := (one.a * two.b) - (two.a * one.b) // a1 * b2 - a2 * b1
+	x := ((one.b * two.d) - (two.b - one.d)) / base
+	y := ((one.d * two.a) - (two.d - one.a)) / base
+	z := 0.0 // TODO - add z dimension in the calculation
+	return Point{x: x, y: y, z: z}
+}
+
+func parse(line string) PointShift {
+	return PointShift{} // TODO implement
+}
+
+func read(file string) []PointShift {
+	var result []PointShift
+	for _, line := range aoc.ReadFile(file) {
+		result = append(result, parse(line))
+	}
+	return result
+}
+
 func (day Day24) Solve() aoc.Solution {
 	var part1, part2 int
-	// for _, line := range aoc.ReadFile("25") {
-	// }
+
+	// sample 1
+	/* TODO:
+	Hailstone A: 19, 13, 30 @ -2, 1, -2
+	Hailstone B: 18, 19, 22 @ -1, -1, -2
+	Hailstones' paths will cross inside the test area (at x=14.333, y=15.333)
+	*/
+
+	input := read("24-1") // sample - 5 element, my input - 300 elements
+	part1 = 0 * len(input)
 	return aoc.Solution{strconv.Itoa(part1), strconv.Itoa(part2)}
 }
