3a6820153b4b63d0e534c6d1a469f24fb13f38c8
Yury Fedorov <yury.fedorov@gmail.com>
2024-11-30 20:10:50 +0100

    AoC24 - initial commit for Python 3.10

---
+++ AoC24/python/01.py
@@ -0,0 +1,3 @@
+import common as c
+with c.open_file("01") as f:
+    print(f.readline())
---
+++ AoC24/python/common.py
@@ -0,0 +1,4 @@
+# common functions
+
+def open_file(data_file : str):
+    return open("../data/" + data_file + ".txt", "r")
