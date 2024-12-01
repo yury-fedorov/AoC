571dec17df76d7c3a838101ea833d708cb89fa87
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-01 09:32:56 +0100

    AoC18 - dockerized - Microsoft .NET 6

--- AoC18/App.cs
+++ AoC18/App.cs
@@ -3,9 +3,13 @@
         public static string Directory {
             get {
                 var cd = System.IO.Directory.GetCurrentDirectory();
-                while (!cd.EndsWith("AoC18") ) {
-                    var pd = System.IO.Directory.GetParent(cd);
-                    cd = pd.FullName;
+                if (cd.Contains("AoC18")) {
+                    while (!cd.EndsWith("AoC18") ) {
+                        var pd = System.IO.Directory.GetParent(cd);
+                        cd = pd.FullName;
+                    }
+                } else {
+                    cd = "/usr/src/aoc";
                 }
                 return cd;
             }
