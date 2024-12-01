9e4fd4ed5d7103870c7758d15f502c454d498f7f
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-01 06:48:57 +0100

    AoC24 - Day 1 - refactoring (with functions)

--- AoC24/python/01.py
+++ AoC24/python/01.py
@@ -1,25 +1,29 @@
 import common as c
 
-left = []
-right = []
-with c.open_file("01") as f:
-    for line in f:
-        x = line.split()
-        left.append(int(x[0]))
-        right.append(int(x[1]))
+def read_input(data:str):
+    left = []
+    right = []
+    with c.open_file(data) as f:
+        for line in f:
+            x = line.split()
+            left.append(int(x[0]))
+            right.append(int(x[1]))
+    left.sort()
+    right.sort()
+    return left, right
 
-left.sort()
-right.sort()
+def answer1(left, right) -> int:
+    diff = 0
+    for i in range(0, len(left)):
+        diff += abs(right[i] - left[i])
+    return diff
 
-n = len(left)
-diff = []
-for i in range(0, n):
-    diff.append(abs(right[i] - left[i]))
+def answer2(left, right) -> int:
+    score = 0  # similarity score
+    for i in range(0, len(left)):
+        score += (left[i] * right.count(left[i]))
+    return score
 
-assert sum(diff) == 2344935, "answer 1"
-
-score = []  # similarity score
-for i in range(0, n):
-    score.append(left[i] * right.count(left[i]))
-
-assert sum(score) == 27647262, "answer 2"
+left, right = read_input("01")
+assert answer1(left, right) == 2344935, "answer 1"
+assert answer2(left, right) == 27647262, "answer 2"
