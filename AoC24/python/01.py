89aee0b84777dbafb99d0b8528df01137a4b06fa
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-01 06:32:51 +0100

    AoC24 - Day 1 solved

--- AoC24/python/01.py
+++ AoC24/python/01.py
@@ -1,3 +1,26 @@
 import common as c
+
+left = []
+right = []
 with c.open_file("01") as f:
-    print(f.readline())
+    for line in f:
+        x = line.split()
+        left.append(int(x[0]))
+        right.append(int(x[1]))
+
+left.sort()
+right.sort()
+
+n = len(left)
+diff = []
+for i in range(0, n):
+    diff.append(abs(right[i] - left[i]))
+
+answer1 = sum(diff)
+assert answer1 == 2344935, "answer 1"
+
+score = []  # similarity score
+for i in range(0, n):
+    score.append(left[i] * right.count(left[i]))
+
+assert sum(score) == 27647262, "answer 2"
