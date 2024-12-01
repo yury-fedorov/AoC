74ad168299ceb0e2404a23a6edc1a72f45af7857
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-01 08:53:58 +0100

    AoC21 - dockerized

--- AoC21/App.cs
+++ AoC21/App.cs
@@ -3,10 +3,16 @@ public class App {
     public static string Directory {
         get {
             var cd = System.IO.Directory.GetCurrentDirectory();
-            while (!cd.EndsWith("AoC21") ) {
-                var pd = System.IO.Directory.GetParent(cd);
-                if (pd == null) throw new Exception("failed to identify the directory");
-                cd = pd.FullName;
+            if (cd.Contains("AoC21"))  {
+                // For the container case, we don't find this folder
+                while (!cd.EndsWith("AoC21") ) {
+                    var pd = System.IO.Directory.GetParent(cd);
+                    if (pd == null) throw new Exception("failed to identify the directory");
+                    cd = pd.FullName;
+                }
+            } else {
+                // The container case:
+                return "/usr/src/aoc/";
             }
             return cd;
         }
