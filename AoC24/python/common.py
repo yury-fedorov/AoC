773e05b4182117e9f0b490cc44dee277adf47114
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-02 10:46:25 +0100

    AoC24 - adoptation for Python GitHub action

--- AoC24/python/common.py
+++ AoC24/python/common.py
@@ -1,4 +1,10 @@
 # common functions
+import os.path
 
-def open_file(data_file : str):
-    return open("../data/" + data_file + ".txt", "r")
+
+def open_file(data_file: str):
+    path = "../data/" + data_file + ".txt"
+    if not os.path.exists(path):
+        # Execution from GitHub actions
+        path = "AoC24/python/data" + data_file + ".txt"
+    return open(path, "r")
