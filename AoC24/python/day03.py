95eb7fffc999eb05ed8a364ec34d4cf5771e6286
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-03 06:44:32 +0100

    AoC24 - Day03 - solved

--- AoC24/python/day03.py
+++ AoC24/python/day03.py
@@ -3,45 +3,62 @@ import re
 import unittest
 
 
-def read_input(data: str):
-    lines = []
+def read_input(data: str) -> str:
+    result = ''
     with c.open_file(data) as f:
         for line in f:
-            lines.append(line)
-    return lines
+            result += line
+    return result
+
 
 def find_all_matches(string):
-    pat = re.compile(r'mul(.)(\d+),(\d+)(.)')
+    pat = re.compile(r'mul\((\d+),(\d+)\)')
     pos = 0
     out = []
     while m := pat.search(string, pos):
         pos = m.start() + 1
-        if m[1] == '(' and m[4] == ')':
-            out.append(int(m[2]) * int(m[3]))
+        out.append(int(m[1]) * int(m[2]))
     return out
 
-def answer1(lines) -> int:
-    result = 0
 
-    for line in lines:
-        for m in find_all_matches(line):
-            result += m
+def answer1(line) -> int:
+    result = 0
+    for m in find_all_matches(line):
+        result += m
     return result
 
 
-def answer2(lines) -> int:
-    return 0
+def answer2(line) -> int:
+    result = 0
+
+    while len(line) > 0:
+        line_ok: str
+        index_end = line.find("don't()")
+        if index_end >= 0:
+            line_ok = line[:index_end]
+            index_start = line.find("do()", index_end)
+            line = line[index_start:] if index_start >= 0 else ""
+        else:
+            line_ok = line
+            line = ""
+        for m in find_all_matches(line_ok):
+            result += m
+
+    return result
 
 
 class Day03(unittest.TestCase):
 
     def x_test(self, data: str, a1: int, a2: int):
-        lines = read_input(data)
-        self.assertEqual(answer1(lines), a1, "answer 1")
-        # self.assertEqual(answer2(lines), a2, "answer 2")
+        code = read_input(data)
+        self.assertEqual(answer1(code), a1, "answer 1")
+        self.assertEqual(answer2(code), a2, "answer 2")
 
     def test_sample(self):
-        self.x_test("03-1", 161, 0)
+        self.x_test("03-1", 161, 161)
+
+    def test_sample2(self):
+        self.x_test("03-2", 161, 48)
 
     def test_day(self):
-        self.x_test("03", 161289189, 0)
+        self.x_test("03", 161289189, 83595109)
