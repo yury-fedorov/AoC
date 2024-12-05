d78e495cb4a7ccbc24dea061dc1f3910836ddba1
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-05 07:28:57 +0100

    AoC24 - Day05 - solved

--- AoC24/python/day05.py
+++ AoC24/python/day05.py
@@ -30,25 +30,39 @@ def _is_correct(rules: [], update: []) -> bool:
 
     for before, after in rules:
         if after == page:
-            if any(x == before for x in  later):
-                print(f"Rule {before}, {after} is broken for page {page} and later {later}")
+            if any(x == before for x in later):
                 return False
 
     return _is_correct(rules, later)
 
+
 def _answer1(rules, updates) -> int:
-    result = 0
-    for u in updates:
-        if _is_correct(rules, u):
-            result += _middle_page(u)
-        else:
-            print(f"Bad rule: {u}")
-    return result
-    # return sum([_middle_page(u) for u in updates if _is_correct(rules, u)])
+    return sum([_middle_page(u) for u in updates if _is_correct(rules, u)])
 
 
-def _answer2(left, right) -> int:
-    return 0
+def _correct(rules: [], update: []) -> []:
+    def sub_correct(rules: [], corrected: [], to_correct: str) -> []:
+        n = len(corrected)
+        for i in range(n + 1):
+            version = corrected[:i] + [to_correct] + corrected[i:]
+            if _is_correct(rules, version):
+                return version
+        raise RuntimeError(f"Bad sequence {corrected} for {to_correct}")
+
+    corrected = [update[0]]
+    to_correct = update[1:]
+    while len(to_correct) > 0:
+        next_to_correct = to_correct[0]
+        to_correct = to_correct[1:]
+        corrected = sub_correct(rules, corrected, next_to_correct)
+
+    return corrected
+
+
+def _answer2(rules, updates) -> int:
+    incorrect = [u for u in updates if not _is_correct(rules, u)]
+
+    return sum([_middle_page(_correct(rules, u)) for u in incorrect])
 
 
 class Day05(unittest.TestCase):
@@ -56,7 +70,7 @@ class Day05(unittest.TestCase):
     def __solution(self, data: str, a1: int, a2: int):
         rules, updates = _read_input(data)
         self.assertEqual(_answer1(rules, updates), a1, "answer 1")
-        # self.assertEqual(_answer2(left, right), a2, "answer 2")
+        self.assertEqual(_answer2(rules, updates), a2, "answer 2")
 
     def test_middle_page(self):
         self.assertEqual(_middle_page([75, 47, 61, 53, 29]), 61, "of five")
@@ -64,7 +78,7 @@ class Day05(unittest.TestCase):
         self.assertEqual(_middle_page([75, 29, 13]), 29, "of three")
 
     def test_sample(self):
-        self.__solution("05-1", 143, 0)
+        self.__solution("05-1", 143, 123)
 
     def test_day(self):
-        self.__solution("05", 7307, 0)
+        self.__solution("05", 7307, 4713)  # 4672 -- too low
