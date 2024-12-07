66a953a7b29668de9505725b382e3760a250e31b
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-07 20:56:51 +0100

    AoC24 - Day08 - initial draft commit.

--- AoC24/python/day08.py
+++ AoC24/python/day08.py
@@ -2,7 +2,6 @@ import common as c
 import unittest
 
 
-# TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
 def _answer1(lines: []) -> int:
     return 0
 
@@ -11,7 +10,7 @@ def _answer2(lines: []) -> int:
     return 0
 
 
-class Day24(unittest.TestCase):
+class Day08(unittest.TestCase):
 
     def __solution(self, data: str, a1: int, a2: int):
         lines = c.read_lines(data)
@@ -19,4 +18,4 @@ class Day24(unittest.TestCase):
         self.assertEqual(_answer2(lines), a2, "answer 2")
 
     def test_day(self):
-        self.__solution("01", 0, 0)
+        self.__solution("08", 0, 0)
