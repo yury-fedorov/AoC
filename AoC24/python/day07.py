4733d97879e01b76d7a2365c61ae5319274d757c
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-07 07:01:49 +0100

    AoC24 - Day07 - Part1 - tests are green, answer is not right

--- AoC24/python/day07.py
+++ AoC24/python/day07.py
@@ -1,41 +1,40 @@
 import common as c
 import unittest
 
+
 def _parse_equation(line: str) -> (int, []):
     a = line.split(': ')
     test_value = int(a[0])
     numbers = [int(n) for n in a[1].split()]
     return test_value, numbers
 
-def _is_equation2(test_value: int, numbers: []) -> bool:
-    if len(numbers) == 2:
+
+def _is_equation3(test_value: int, partial: int, numbers: []) -> bool:
+    if partial > test_value:
+        return False
+    if len(numbers) == 1:
         a = numbers[0]
-        b = numbers[1]
-        return (test_value == (a + b)) or (test_value == (a * b))
-    elif len(numbers) > 2:
+        return (test_value == (a + partial)) or (test_value == (a * partial))
+    elif len(numbers) >= 2:
         a = numbers[0]
         n = numbers[1:]
-        # for sum
-        tvs = test_value - a
-        if tvs >= 0:
-            if _is_equation2(tvs, n):
-                return True
-        # for multiplication
-        remainder = test_value % a
-        if remainder == 0:
-            tvm = test_value / a
-            if _is_equation2(tvm, n):
-                return True
+        if _is_equation3(test_value, partial + a, n):
+            return True
+        if _is_equation3(test_value, partial * a, n):
+            return True
     return False
 
-def _is_equation(line: str) -> bool :
-    test_value, numbers = _parse_equation(line)
-    return _is_equation2(test_value, numbers)
 
-def _answer1(lines:[]) -> int:
+def _is_equation(line: str) -> bool:
+    test_value, numbers = _parse_equation(line)
+    return _is_equation3(test_value, 0, numbers)
+
+
+def _answer1(lines: []) -> int:
     return sum([_parse_equation(l)[0] for l in lines if _is_equation(l)])
 
-def _answer2(lines:[]) -> int:
+
+def _answer2(lines: []) -> int:
     return 0
 
 
@@ -46,8 +45,12 @@ class Day07(unittest.TestCase):
         self.assertEqual(_answer1(lines), a1, "answer 1")
         # self.assertEqual(_answer2(lines), a2, "answer 2")
 
+    def test_wrong(self):
+        self.assertEqual(_is_equation("292: 11 6 16 20"), True, "answer 1")
+
     def test_sample(self):
         self.__solution("07-1", 3749, 0)
 
+    # 5030892096384 - That's not the right answer.
     def test_day(self):
         self.__solution("07", 0, 0)
