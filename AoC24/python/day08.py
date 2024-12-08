4a8c404b51e112b793b34a236b9ef3112070fc47
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-08 06:32:44 +0100

    AoC24 - Day08 - initial draft

--- AoC24/python/day08.py
+++ AoC24/python/day08.py
@@ -2,20 +2,23 @@ import common as c
 import unittest
 
 
-def _answer1(lines: []) -> int:
+def _answer1(the_map: []) -> int:
     return 0
 
 
-def _answer2(lines: []) -> int:
+def _answer2(the_map: []) -> int:
     return 0
 
 
 class Day08(unittest.TestCase):
 
     def __solution(self, data: str, a1: int, a2: int):
-        lines = c.read_lines(data)
-        self.assertEqual(_answer1(lines), a1, "answer 1")
-        self.assertEqual(_answer2(lines), a2, "answer 2")
+        the_map = c.read_lines(data)
+        self.assertEqual(_answer1(the_map), a1, "answer 1")
+        self.assertEqual(_answer2(the_map), a2, "answer 2")
+
+    def test_sample(self):
+        self.__solution("08-1", 14, 0)
 
     def test_day(self):
         self.__solution("08", 0, 0)
