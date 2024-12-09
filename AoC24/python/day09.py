e7f1a0d9db842baeb3ee5a96943cbf245097803f
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-09 07:07:01 +0100

    AoC24 - Day09 - Part1 solved.

--- AoC24/python/day09.py
+++ AoC24/python/day09.py
@@ -1,25 +1,69 @@
 import common as c
 import unittest
 
+EMPTY_SPACE = -1
 
-# TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
-def _answer1(lines: []) -> int:
-    return 0
+def _unpack_disk(disk: str) -> {}:
+    '''Returns map where key is global position and value is the file id or empty space marker.'''
+    unpacked = {} # common vector will supper on performance during the degfragmentation, alternative is linked list
+    file_id = 0
+    global_index = 0
+    for i, length in enumerate(disk):
+        length = int(length)
+        is_file = (i % 2 == 0) # zero based index, first goes a file
+        content = file_id if is_file else EMPTY_SPACE # file id or empty space
+        for _ in range(length):
+            unpacked[global_index] = content
+            global_index += 1
+        file_id += 1 if is_file else 0 # next file id
+    return unpacked
+
+def _defrag(disk_map:{}):
+    disk_end = max(disk_map.keys())
+    position = disk_end
+    low_bound_empty = 0 # before it there is no free space
+    while position >= 0:
+        content = disk_map[position]
+        is_file = content != EMPTY_SPACE
+        if is_file:
+            # let's find empty space
+            while low_bound_empty < position:
+                if disk_map[low_bound_empty] == EMPTY_SPACE:
+                    # found!
+                    disk_map[low_bound_empty] = content
+                    disk_map[position] = EMPTY_SPACE
+                    break
+                else:
+                    # not found, let's move bound ahead
+                    low_bound_empty += 1
+        position -= 1
+    return disk_map
+
+# TODO may rewrite in shorter form
+def _checksum(disk_map:{}) -> int:
+    result = 0
+    for position, file_id in disk_map.items():
+        if file_id != EMPTY_SPACE:
+            result += (position * file_id)
+    return result
+
+def _answer1(disk: str) -> {}:
+    return _checksum(_defrag(_unpack_disk(disk)))
 
 
-def _answer2(lines: []) -> int:
+def _answer2(disk: str) -> {}:
     return 0
 
 
 class Day09(unittest.TestCase):
 
     def __solution(self, data: str, a1: int, a2: int):
-        lines = c.read_lines(data)
-        self.assertEqual(a1, _answer1(lines), "answer 1")
-        self.assertEqual(a2, _answer2(lines), "answer 2")
+        disk = c.read_lines(data)[0]
+        self.assertEqual(a1, _answer1(disk), "answer 1")
+        self.assertEqual(a2, _answer2(disk), "answer 2")
 
     def test_sample(self):
-        self.__solution("01-1", 0, 0)
+        self.__solution("09-1", 1928, 0)
 
     def test_day(self):
-        self.__solution("01", 0, 0)
+        self.__solution("09", 6370402949053, 0)
