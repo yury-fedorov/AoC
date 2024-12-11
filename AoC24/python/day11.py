2e6aec8f13ea5d8cf9c28a6bfcbed56f298f174d
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-11 10:10:05 +0100

    AoC24 - Day11 - fast and short

--- AoC24/python/day11.py
+++ AoC24/python/day11.py
@@ -1,7 +1,6 @@
 import common as c
 import unittest
 from collections import Counter
-import time
 
 
 def _blink(stones: [], times: int) -> []:
@@ -26,39 +25,37 @@ def _blink(stones: [], times: int) -> []:
     return _blink(new_stones, times - 1)
 
 
-def _brute_force(stones: [], times: int) -> int:
-    return len(_blink(stones, times))
-
-
-# TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
 def _answer1(stones: []) -> int:
-    return _brute_force(stones, 25)
+    return _a2(stones, 25)
 
 
-MAX_LENGTH = 40  # perfect length for performance (4 times faster on 25 times)
-MAX_DEPTH = 5
+def _answer2(stones: []) -> int:
+    return _a2(stones, 75)
+
+
+# (value,times) - amount of stones
 CACHE = {}
 
 
 def _a2(stones: [], times: int) -> int:
-    n = len(stones)
     if times <= 0:
-        return n
+        return len(stones)
+    if times == 1:
+        return len(_blink(stones, times))
 
-    if n == 1 and times >= MAX_DEPTH:
-        # single element we cache
+    if len(stones) == 1:
+        # single stone case
         s = stones[0]
+        # the cache key is stone and times
+        key = (s, times)
         try:
-            next_five = CACHE[s]
+            result = CACHE[key]
         except KeyError:
-            next_five = _blink(stones, MAX_DEPTH)
-            CACHE[s] = next_five
-        return _a2(next_five, times - MAX_DEPTH)
+            result = _a2(_blink(stones, 1), times - 1)
+            CACHE[key] = result
+        return result
 
-    elif n < MAX_LENGTH:
-        return _a2(_blink(stones, 1), times - 1)
-
-    # we need to split
+    # splitting on stones one by one
     counter = Counter(stones)
     result = 0
     for s_i in counter.keys():
@@ -67,122 +64,18 @@ def _a2(stones: [], times: int) -> int:
     return result
 
 
-def _a2_v3(stones: [], times: int) -> int:
-    n = len(stones)
-    if times <= 0:
-        return n
-
-    if n == 1 and times >= MAX_DEPTH:
-        # single element we cache
-        s = stones[0]
-        try:
-            next_five = CACHE[s]
-        except KeyError:
-            next_five = _blink(stones, MAX_DEPTH)
-            CACHE[s] = next_five
-        return _a2(next_five, times - MAX_DEPTH)
-
-    # we need to split
-    counter = Counter(stones)
-    result = 0
-    for s_i in counter.keys():
-        s_i_n = counter[s_i]
-
-        try:
-            next_25 = CACHE25[s_i]
-        except KeyError:
-            next_25 = _blink([s_i], 25)
-            CACHE[s_i] = next_25
-
-        result += s_i_n * _a2(next_25, times - 25)
-    return result
-
-
-def _answer2(stones: []) -> int:
-    return _a2_v5(stones, 75)
-
-
-CACHE25 = {}
-
-
-def _a2_v4(stones: [], times: int) -> int:
-    if times <= 0:
-        return len(stones)
-
-    DELTA = 25
-    if len(stones) == 1:
-        s = stones[0]
-        try:
-            next_25 = CACHE25[s]
-        except KeyError:
-            next_25 = _blink([s], DELTA)
-            CACHE[s] = next_25
-        if times == 25:
-            return len(next_25)
-        result = next_25
-    else:
-        result = _blink(stones, DELTA)
-
-    counter = Counter(result)
-    result = 0
-    for s_i in counter.keys():
-        s_i_n = counter[s_i]
-        result += s_i_n * _a2_v4([s_i], times - DELTA)
-    return result
-
-# (value,times) - amount of stones
-CACHE_V_T = {}
-
-def _a2_v5(stones: [], times: int) -> int:
-    if times <= 0:
-        return len(stones)
-    if times == 1:
-        return len(_blink(stones, times))
-
-    if len(stones) == 1:
-        s = stones[0]
-        key = (s, times)
-        try:
-            result = CACHE_V_T[key]
-        except KeyError:
-            result = _a2_v5(_blink(stones, 1), times - 1)
-            CACHE_V_T[key] = result
-        return result
-
-    counter = Counter(stones)
-    result = 0
-    for s_i in counter.keys():
-        s_i_n = counter[s_i]
-        result += s_i_n * _a2_v5([s_i], times)
-    return result
-
-
 class Day11(unittest.TestCase):
 
     def __solution(self, data: str, a1: int, a2: int):
         stones = [int(x) for x in c.read_lines(data)[0].split()]
         self.assertEqual(a1, _answer1(stones), "answer 1")
-        # if not c.is_fast_only():
         self.assertEqual(a2, _answer2(stones), "answer 2")
 
-    def test_sample1(self):
+    def test_answer1(self):
         self.assertEqual(55312, _answer1([125, 17]), "answer 1")
 
-    def test_sample(self):
-        self.assertEqual(7, _brute_force([0, 1, 10, 99, 999], 1), "answer 1")
-
-    def test_sample2(self):
-        s = [125, 17]
-        ts0 = time.time()
-        a1 = _answer1(s)
-        ts1 = time.time()
-        a2 = _a2_v5(s, 25)
-        ts2 = time.time()
-        dt1 = ts1 - ts0
-        dt2 = ts2 - ts1
-        self.assertLess(dt2, dt1, "answer 2 must be faster")
-        self.assertEqual(a1, a2, "answer 1 vs answer 2")
-
+    def test_blink(self):
+        self.assertEqual(7, len(_blink([0, 1, 10, 99, 999], 1)), "blink")
 
     def test_day(self):
         self.__solution("11", 207683, 244782991106220)
