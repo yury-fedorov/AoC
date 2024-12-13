21f2223d54d45d280d3bf9e592582a7845df9b96
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-13 07:53:45 +0100

    AoC24 - Day14 - initial draft and test verbosity (at least names of the tests are shown)

--- AoC24/python/aoc24.py
+++ AoC24/python/aoc24.py
@@ -11,13 +11,14 @@ import day10
 import day11
 import day12
 import day13
+import day14
 import unittest
 
 
 def suite():
     suite = unittest.TestSuite()
     days = [day01.Day01, day02.Day02, day03.Day03, day04.Day04, day05.Day05, day06.Day06, day07.Day07, day08.Day08,
-            day09.Day09, day10.Day10, day11.Day11, day12.Day12, day13.Day13]
+            day09.Day09, day10.Day10, day11.Day11, day12.Day12, day13.Day13, day14.Day14]
     _IS_ALL = True  # all tests are executed or only the major ones ("test_day")
     for d in days:
         t = unittest.defaultTestLoader.loadTestsFromTestCase(d) if _IS_ALL else d("test_day")
@@ -26,5 +27,5 @@ def suite():
 
 
 if __name__ == '__main__':
-    runner = unittest.TextTestRunner()
+    runner = unittest.TextTestRunner(verbosity=2)
     runner.run(suite())
