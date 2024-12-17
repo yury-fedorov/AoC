3502c1ead7338c65294e8417336daf1a12ea5638
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-17 22:44:01 +0100

    AoC24 - Day18 - early draft

--- AoC24/python/aoc24.py
+++ AoC24/python/aoc24.py
@@ -15,6 +15,7 @@ import day14
 import day15
 import day16
 import day17
+import day18
 import unittest
 
 
@@ -22,7 +23,7 @@ def suite():
     suite = unittest.TestSuite()
     days = [day01.Day01, day02.Day02, day03.Day03, day04.Day04, day05.Day05, day06.Day06, day07.Day07, day08.Day08,
             day09.Day09, day10.Day10, day11.Day11, day12.Day12, day13.Day13, day14.Day14, day15.Day15, day16.Day16,
-            day17.Day17]
+            day17.Day17, day18.Day18]
     _IS_ALL = True  # all tests are executed or only the major ones ("test_day")
     for d in days:
         t = unittest.defaultTestLoader.loadTestsFromTestCase(d) if _IS_ALL else d("test_day")
