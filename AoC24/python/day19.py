672722a643a6694527580b4057f39d961432666e
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-19 06:24:35 +0100

    AoC24 - Day19 - Part1 alpha

--- AoC24/python/day19.py
+++ AoC24/python/day19.py
@@ -1,13 +1,24 @@
 import common as c
 import unittest
 
+def _is_possible(patterns: [str], to_make: str, made: str) -> bool:
+    if len(to_make) == 0 or to_make in patterns:
+        return True
+    appliable = [p for p in patterns if to_make.find(p) == 0]
+    for a in appliable:
+        to_make1 = to_make[len(a):]
+        made1 = made + a
+        ok = _is_possible(patterns, to_make1, made1)
+        if ok:
+            return True
+    return False
 
 # TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
-def _answer1(lines: []) -> int:
-    return 0
+def _answer1(patterns: [str], designs: [str]) -> int:
+    return sum(1 for d in designs if _is_possible(patterns, d, ""))
 
 
-def _answer2(lines: []) -> int:
+def _answer2(patterns: [str], designs: [str]) -> int:
     return 0
 
 
@@ -15,11 +26,13 @@ class Day19(unittest.TestCase):
 
     def __solution(self, data: str, a1: int, a2: int):
         lines = c.read_lines(data)
-        self.assertEqual(a1, _answer1(lines), "answer 1")
-        self.assertEqual(a2, _answer2(lines), "answer 2")
+        patterns = lines[0].split(", ")
+        designs = lines[2:]
+        self.assertEqual(a1, _answer1(patterns, designs), "answer 1")
+        self.assertEqual(a2, _answer2(patterns, designs), "answer 2")
 
     def test_sample(self):
-        self.__solution("01-1", 0, 0)
+        self.__solution("19-1", 6, 0)
 
     def test_day(self):
-        self.__solution("01", 0, 0)
+        self.__solution("19", 0, 0)
