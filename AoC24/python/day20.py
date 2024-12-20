0e0c165cd22f92e3e12ca768448068954e97930e
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-20 06:07:16 +0100

    AoC24 - Day20 - input

--- AoC24/python/day20.py
+++ AoC24/python/day20.py
@@ -3,23 +3,23 @@ import unittest
 
 
 # TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
-def _answer1(lines: []) -> int:
+def _answer1(the_map: [str]) -> int:
     return 0
 
 
-def _answer2(lines: []) -> int:
+def _answer2(the_map: [str]) -> int:
     return 0
 
 
 class Day20(unittest.TestCase):
 
     def __solution(self, data: str, a1: int, a2: int):
-        lines = c.read_lines(data)
-        self.assertEqual(a1, _answer1(lines), "answer 1")
-        self.assertEqual(a2, _answer2(lines), "answer 2")
+        the_map = c.read_lines(data)
+        self.assertEqual(a1, _answer1(the_map), "answer 1")
+        self.assertEqual(a2, _answer2(the_map), "answer 2")
 
     def test_sample(self):
-        self.__solution("01-1", 0, 0)
+        self.__solution("20-1", 0, 0)
 
     def test_day(self):
-        self.__solution("01", 0, 0)
+        self.__solution("20", 0, 0)
