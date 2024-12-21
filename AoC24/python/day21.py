c4307198b56da3a0b89800980d9fda0038bec07f
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-21 07:47:25 +0100

    AoC24 - Day21 - Part1 - WIP - navigate is ready

--- AoC24/python/day21.py
+++ AoC24/python/day21.py
@@ -1,8 +1,7 @@
-from tensorflow.python.ops.numpy_ops.np_array_ops import dsplit
-
 import common as c
 import unittest
 
+ND = ' '  # not defined key
 A = 'A'
 UP = '^'
 DOWN = 'v'
@@ -14,30 +13,49 @@ NUMERIC_KEYPAD = {'7': c.Point(0, 0), '8': c.Point(1, 0), '9': c.Point(2, 0),
                   '1': c.Point(0, 2), '2': c.Point(1, 2), '3': c.Point(2, 2),
                   '0': c.Point(1, 3), 'A': c.Point(2, 3)}
 
+POINT_NUMERIC_KEYPAD = {v: k for k, v in NUMERIC_KEYPAD.items()}
+
 DIRECTIONAL_KEYPAD = {UP: c.Point(1, 0), 'A': c.Point(2, 0),
                       LEFT: c.Point(0, 1), DOWN: c.Point(1, 1), RIGHT: c.Point(2, 1)}
 
+POINT_DIRECTIONAL_KEYPAD = {v: k for k, v in DIRECTIONAL_KEYPAD.items()}
+
 DIRECTIONS = {c.Point(-1, 0): LEFT, c.Point(0, -1): UP, c.Point(1, 0): RIGHT, c.Point(0, 1): DOWN}
 
-def _navigate_to(start, end:c.Point, keypad: {str: c.Point}) -> str:
+
+def sign(value: int) -> int:
+    return 0 if value == 0 else 1 if value > 0 else -1
+
+
+def _navigate_to(start, end: c.Point, point_keypad: {c.Point: str}) -> str:
     p = start
     result = ""
     while p != end:
-        dx, dy = end.x - p.x, end.y - p.y
+        dx, dy = sign(end.x - p.x), sign(end.y - p.y)
+        found = False
         if dx != 0:
             # let's try to move on x axis
             p1 = c.Point(p.x + dx, p.y)
-
-
-    result += A # once properly positioned the arm, we need to press A
+            digit = point_keypad.get(p1, ND)
+            if digit != ND:
+                p = p1
+                result += DIRECTIONS[c.Point(dx, 0)]
+                found = True
+        if not found:
+            # let's try to move on y axis
+            p = c.Point(p.x, p.y + dy)
+            result += DIRECTIONS[c.Point(0, dy)]
+    result += A  # once properly positioned the arm, we need to press A
     return result
+
 
 def _navigate(code: str, keypad: {str: c.Point}) -> str:
     position = keypad[A]
     result = ""
+    point_keypad = {v: k for k, v in keypad.items()}
     for digit in code:
         target = keypad[digit]
-        result += _navigate_to(position, target, keypad)
+        result += _navigate_to(position, target, point_keypad)
         position = target
     return result
 
@@ -69,6 +87,13 @@ class Day21(unittest.TestCase):
     def test_complexity(self):
         seq = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
         self.assertEqual(68 * 29, _complexity("029A", len(seq)), "answer 1")
+
+    def test_navigate_a0(self):
+        pad = NUMERIC_KEYPAD
+        self.assertEqual("<A", _navigate_to(pad[A], pad['0'], POINT_NUMERIC_KEYPAD), "navigate A->0")
+
+    def test_navigate(self):
+        self.assertEqual("<A^A>^^AvvvA", _navigate("029A", NUMERIC_KEYPAD), "navigate 029A")
 
     def test_sample(self):
         self.__solution("21-1", 0, 0)
