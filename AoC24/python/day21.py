d609eabf15c93ab045085d7ff5d5a70439bac66e
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-21 06:20:24 +0100

    AoC24 - Day21 - Part1 - WIP

--- AoC24/python/day21.py
+++ AoC24/python/day21.py
@@ -2,6 +2,14 @@ import common as c
 import unittest
 
 
+def _shortest_sequence(code: str) -> str:
+    return code
+
+
+def _complexity(code: str, length: int) -> int:
+    return int(code[:-1]) * length
+
+
 # TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
 def _answer1(lines: [str]) -> int:
     return 0
@@ -17,6 +25,10 @@ class Day21(unittest.TestCase):
         codes = c.read_lines(data)
         self.assertEqual(a1, _answer1(codes), "answer 1")
         self.assertEqual(a2, _answer2(codes), "answer 2")
+
+    def test_complexity(self):
+        seq = "<vA<AA>>^AvAA<^A>A<v<A>>^AvA^A<vA>^A<v<A>^A>AAvA^A<v<A>A>^AAAvA<^A>A"
+        self.assertEqual(68 * 29, _complexity("029A", len(seq)), "answer 1")
 
     def test_sample(self):
         self.__solution("21-1", 0, 0)
