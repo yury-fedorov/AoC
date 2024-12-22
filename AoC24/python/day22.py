ceb6e00a509a94ce3b9ddbb732ea0522836b2890
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-22 07:30:34 +0100

    AoC24 - Day22 - Part2 solved in 16.5 seconds

--- AoC24/python/day22.py
+++ AoC24/python/day22.py
@@ -1,5 +1,5 @@
 from math import floor
-from typing import NamedTuple
+from typing import NamedTuple, Counter
 
 import common as c
 import unittest
@@ -42,13 +42,29 @@ def _price_change(secret, n: int) -> [PriceChange]:
     return result
 
 
+def _sequences(price_change: [PriceChange]) -> {(int, int, int, int): int}:
+    # all sequences
+    window_size = 4
+    result = {}
+    for i in range(len(price_change) - window_size + 1):
+        s = tuple([pc.change for pc in price_change[i: i + window_size]])
+        if not s in result:
+            result[s] = price_change[i + window_size - 1].price
+    return result
+
+
 def _answer1(secrets: [int]) -> int:
     return sum(_next_n(secret, 2000) for secret in secrets)
 
 
 def _answer2(secrets: [int]) -> int:
     price_change_list = [_price_change(secret, 2000) for secret in secrets]
-    return 0
+    sequences = [_sequences(pcl) for pcl in price_change_list]
+    counter = Counter()
+    for si in sequences:
+        for seq, price in si.items():
+            counter[seq] = counter.get(seq, 0) + price
+    return counter.most_common(1)[0][1]  # top 1 most common, first tuple of (sequence,price), price
 
 
 class Day22(unittest.TestCase):
@@ -59,8 +75,15 @@ class Day22(unittest.TestCase):
         self.assertEqual(a1, _answer1(secrets), "answer 1")
         self.assertEqual(a2, _answer2(secrets), "answer 2")
 
-    def test_sample(self):
-        self.__solution("22-1", 37327623, 0)
+    def test_sample1(self):
+        lines = c.read_lines("22-1")
+        secrets = [int(l) for l in lines]
+        self.assertEqual(37327623, _answer1(secrets), "answer 1")
+
+    def test_sample2(self):
+        lines = c.read_lines("22-2")
+        secrets = [int(l) for l in lines]
+        self.assertEqual(23, _answer2(secrets), "answer 2")
 
     def test_prices(self):
         test = _price_change(123, 9)
@@ -70,4 +93,4 @@ class Day22(unittest.TestCase):
         self.assertEqual([-3, 6, -1, -1, 0, 2, -2, 0, -2], changes, "changes")
 
     def test_day(self):
-        self.__solution("22", 14691757043, 0)
+        self.__solution("22", 14691757043, 1831)  # 16.5 seconds
