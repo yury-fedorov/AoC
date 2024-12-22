e44ee068337d3285a4be160b4f3f2dec69c1938b
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-22 06:47:37 +0100

    AoC24 - Day22 - Part2 WIP

--- AoC24/python/day22.py
+++ AoC24/python/day22.py
@@ -1,4 +1,5 @@
 from math import floor
+from typing import NamedTuple
 
 import common as c
 import unittest
@@ -22,12 +23,31 @@ def _next_n(secret: int, n: int) -> int:
     return secret
 
 
-# TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
+def _price(secret: int) -> int: return secret % 10
+
+
+class PriceChange(NamedTuple):
+    price: int
+    change: int
+
+
+def _price_change(secret, n: int) -> [PriceChange]:
+    previous = _price(secret)
+    result = []
+    for _ in range(n):
+        secret = _next(secret)
+        price = _price(secret)
+        result.append(PriceChange(price, price - previous))
+        previous = price
+    return result
+
+
 def _answer1(secrets: [int]) -> int:
     return sum(_next_n(secret, 2000) for secret in secrets)
 
 
 def _answer2(secrets: [int]) -> int:
+    price_change_list = [_price_change(secret, 2000) for secret in secrets]
     return 0
 
 
@@ -41,6 +61,13 @@ class Day22(unittest.TestCase):
 
     def test_sample(self):
         self.__solution("22-1", 37327623, 0)
+
+    def test_prices(self):
+        test = _price_change(123, 9)
+        prices = [pc.price for pc in test]
+        changes = [pc.change for pc in test]
+        self.assertEqual([0, 6, 5, 4, 4, 6, 4, 4, 2], prices, "prices")
+        self.assertEqual([-3, 6, -1, -1, 0, 2, -2, 0, -2], changes, "changes")
 
     def test_day(self):
         self.__solution("22", 14691757043, 0)
