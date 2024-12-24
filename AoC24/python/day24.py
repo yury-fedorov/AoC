06fe99c8fe8f8f47691552b0ffd9d6ebed847eb0
Yury Fedorov <yury.fedorov@gmail.com>
2024-12-24 06:35:24 +0100

    AoC24 - Day 24 - Part1 - WIP - z, bin2int

--- AoC24/python/day24.py
+++ AoC24/python/day24.py
@@ -1,5 +1,25 @@
+from typing import NamedTuple
+
 import common as c
 import unittest
+
+
+class Formula(NamedTuple):
+    a: str
+    b: str
+    op: str
+    r: str
+
+
+def _bin2int(value: str) -> int: return int(value, 2)
+
+
+def _z(registers: {str: bool}) -> int:
+    n = sum(1 for ri in registers.keys() if ri.startswith("z"))
+    result = ""
+    for i in range(n):
+        result = ("1" if registers[f"z{i:02d}"] else "0") + result
+    return _bin2int(result)
 
 
 # TODO - template to use for daily solutions, don't forget to add the solution to aoc24.py
@@ -17,6 +37,10 @@ class Day24(unittest.TestCase):
         lines = c.read_lines(data)
         self.assertEqual(a1, _answer1(lines), "answer 1")
         self.assertEqual(a2, _answer2(lines), "answer 2")
+
+    def test_z(self):
+        self.assertEqual(4, _z({"z00": 0, "z01": 0, "z02": 1}), "z case 1")
+        self.assertEqual(2024, _bin2int("0011111101000"), "_bin2int")
 
     def test_sample1(self):
         self.__solution("24-1", 0, 0)
