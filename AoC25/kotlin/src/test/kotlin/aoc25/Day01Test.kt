6d73b6672702838c64473c6780736893fdd1e540
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-01 08:11:27 +0100

    AoC25 - Day01 solved

--- AoC25/kotlin/src/test/kotlin/aoc25/Day01Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day01Test.kt
@@ -1,60 +1,71 @@
 package aoc25
 
-import kotlin.math.absoluteValue
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day01Test {
 
-    fun move(counter: Int, move: String) : Pair<Int, Int> {
-        val direction = move.substring(0, 1)
-        val directionSign : Int = if ( direction == "R" ) { 1 } else { -1 }
-        val shift : Int = move.substring(1).toInt()
-        val rowResult : Int = counter + (directionSign * shift)
-        val modResult = rowResult / 100
-        val newCounter = rowResult % 100
-        val isNegative = rowResult < 0
-        val clicks = modResult.absoluteValue
-        if (isNegative) {
-            return newCounter + 100 to clicks + 1
+    fun move(counter: Int, move: String): Pair<Int, Int> {
+        val direction = move.take(1)
+        val directionSign: Int = if (direction == "R") {
+            1
+        } else {
+            -1
         }
-        return newCounter to clicks
-    }
-
-    @Test
-    fun test() {
-        val list = IOUtil.input("01-1")
-        var counter = 50
-        var zeroCount = 0
-        for (aMove in list) {
-            val counterPair = move(counter, aMove)
-            counter = counterPair.first
-            if (counter == 0) {
-                zeroCount++
+        val shift: Int = move.substring(1).toInt()
+        var clicks = 0
+        var result = counter
+        for (i in 1..shift) {
+            val newResult = result + directionSign
+            result = when (newResult) {
+                100 -> 0
+                -1 -> 99
+                else -> newResult
+            }
+            if (result == 0) {
+                // count the number of times any click causes the dial to point at 0
+                clicks++
             }
         }
-        assertEquals(3, zeroCount)
+        return result to clicks
     }
 
-    @Test
-    fun solution() {
-        val list = IOUtil.input("01")
+    fun solution(case: String): Pair<Int, Int> {
+        val list = IOUtil.input(case)
         var counter = 50
         var zeroCount1 = 0
         var zeroCount2 = 0
         for (aMove in list) {
-            println( "Before: $counter shift: $aMove")
+            // print( "Before: $counter shift: $aMove ")
             val counterPair = move(counter, aMove)
             counter = counterPair.first
-            var clicks = counterPair.second
-            println( "After: $counter clicks: $clicks")
+            val clicks = counterPair.second
+            // println( "After: $counter clicks: $clicks")
             if (counter == 0) {
                 zeroCount1++
-                clicks--
             }
             zeroCount2 += clicks
         }
-        // assertEquals(1141, zeroCount1)
-        assertEquals(0, zeroCount1+zeroCount2)
+        return zeroCount1 to zeroCount2
+    }
+
+    @Test
+    fun test() {
+        val answer = solution("01-1")
+        assertEquals(3, answer.first)
+        assertEquals(6, answer.second)
+    }
+
+    @Test
+    fun testR1000() {
+        val result = move(50, "R1000")
+        assertEquals(10, result.second)
+    }
+
+    @Test
+    fun solution() {
+        val answer = solution("01")
+        assertEquals(1141, answer.first)
+        assertEquals(6634, answer.second)
     }
 }
