65efb1a3a25d44dac204f47bcf5063b81f0dd2f0
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-02 06:56:45 +0100

    AoC25 - Day02 solved

--- AoC25/kotlin/src/test/kotlin/aoc25/Day02Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day02Test.kt
@@ -5,7 +5,7 @@ import kotlin.test.assertEquals
 
 class Day02Test {
 
-    fun isValid(id: String): Boolean {
+    fun isValid1(id: String): Boolean {
         // you can find the invalid IDs by looking for any ID which is made only of some sequence of digits repeated twice.
         // So, 55 (5 twice), 6464 (64 twice), and 123123 (123 twice) would all be invalid IDs.
         if (id.length % 2 == 1) return true
@@ -17,20 +17,20 @@ class Day02Test {
 
     @Test
     fun invalidIds() {
-        assertEquals(true, isValid("101"))
-        assertEquals(false, isValid("55"))
-        assertEquals(false, isValid("6464"))
-        assertEquals(false, isValid("123123"))
+        assertEquals(true, isValid1("101"))
+        assertEquals(false, isValid1("55"))
+        assertEquals(false, isValid1("6464"))
+        assertEquals(false, isValid1("123123"))
     }
 
-    fun solution(data: String): List<Long> {
+    fun solution(data: String, isValid: (id: String) -> Boolean): List<Long> {
         var invalid = mutableListOf<Long>()
         val ranges = IOUtil.input(data).first().split(",")
-        for ( range in ranges ) {
+        for (range in ranges) {
             val list = range.split("-")
             val min = list[0].toLong()
-            var max = list[1].toLong()
-            for ( i in min .. max ) {
+            val max = list[1].toLong()
+            for (i in min..max) {
                 val id = i.toString()
                 if (!isValid(id)) {
                     invalid += i
@@ -43,16 +43,53 @@ class Day02Test {
 
     @Test
     fun answer1Sample() {
-        val invalidIdList : List<Long> = solution("02-1")
-        val answer : Long = invalidIdList.sum()
+        val invalidIdList: List<Long> = solution("02-1") { id: String -> isValid1(id) }
+        val answer: Long = invalidIdList.sum()
         assertEquals(1227775554L, answer)
     }
 
     @Test
-    fun solution() {
-        val invalidIdList : List<Long> = solution("02")
-        val answer : Long = invalidIdList.sum()
+    fun solution1() {
+        val invalidIdList: List<Long> = solution("02") { id: String -> isValid1(id) }
+        val answer: Long = invalidIdList.sum()
         assertEquals(40214376723L, answer)
     }
 
+    fun isValid2(id: String): Boolean {
+        // an ID is invalid if it is made only of some sequence of digits repeated at least twice.
+        // So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times),
+        // and 1111111 (1 seven times) are all invalid IDs.
+        val maxN = id.length
+        for (numberOfChuncks in 2..maxN) {
+            if (id.length % numberOfChuncks != 0) continue
+            val chunckSize = id.length / numberOfChuncks
+            val chunks = id.chunked(chunckSize)
+            val left = chunks[0]
+            val invalid = chunks.all { it == left }
+            if (invalid) return false
+        }
+        return true
+    }
+
+    @Test
+    fun invalid2Ids() {
+        assertEquals(false, isValid2("12341234"))
+        assertEquals(false, isValid2("123123123"))
+        assertEquals(false, isValid2("1212121212"))
+        assertEquals(false, isValid2("1111111"))
+    }
+
+    @Test
+    fun answer2Sample() {
+        val invalidIdList: List<Long> = solution("02-1") { id: String -> isValid2(id) }
+        val answer: Long = invalidIdList.sum()
+        assertEquals(4174379265L, answer)
+    }
+
+    @Test
+    fun solution2() {
+        val invalidIdList: List<Long> = solution("02") { id: String -> isValid2(id) }
+        val answer: Long = invalidIdList.sum()
+        assertEquals(50793864718L, answer)
+    }
 }
