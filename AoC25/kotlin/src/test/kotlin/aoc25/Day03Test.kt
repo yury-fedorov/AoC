9b7db7375e2ff82ee99fa98660fe8dbb8d50c15c
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-03 08:37:44 +0100

    AoC25 - Day03 - Part1 - WIP

--- AoC25/kotlin/src/test/kotlin/aoc25/Day03Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day03Test.kt
@@ -5,17 +5,32 @@ import kotlin.test.assertEquals
 
 class Day03Test {
 
+    fun largestVoltage(bank:String): Int {
+        return "${bank[0]}${bank[1]}".toInt()
+    }
+
+    @Test
+    fun largestTest() {
+        assertEquals(98, largestVoltage("987654321111111"))
+    }
+
+    fun solution(case: String): Pair<Int, Int> {
+        val list = IOUtil.input(case)
+        val answer1 = list.sumOf { bank -> largestVoltage(bank) }
+        return answer1 to 0
+    }
+
     @Test
     fun test() {
-        val answer = solution("01-1")
-        assertEquals(3, answer.first)
-        assertEquals(6, answer.second)
+        val answer = solution("03-1")
+        assertEquals(357, answer.first)
+        assertEquals(0, answer.second)
     }
 
     @Test
     fun solution() {
-        val answer = solution("01")
-        assertEquals(1141, answer.first)
-        assertEquals(6634, answer.second)
+        val answer = solution("03")
+        assertEquals(0, answer.first)
+        assertEquals(0, answer.second)
     }
 }
