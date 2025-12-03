61164eba90ca9ea2bf3d4f32bd7278832ddb15a1
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-03 09:09:52 +0100

    AoC25 - Day03 - Part2 slow and uncertain

--- AoC25/kotlin/src/test/kotlin/aoc25/Day03Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day03Test.kt
@@ -6,10 +6,10 @@ import kotlin.test.assertEquals
 
 class Day03Test {
 
-    fun largestVoltage(bank:String): Int {
+    fun largestVoltage(bank: String): Int {
         var result = 0
-        for ( a in 0 .. bank.length - 2) {
-            for ( b in a + 1 .. bank.length - 1) {
+        for (a in 0..bank.length - 2) {
+            for (b in a + 1..bank.length - 1) {
                 result = max(result, "${bank[a]}${bank[b]}".toInt())
             }
         }
@@ -22,17 +22,58 @@ class Day03Test {
         assertEquals(89, largestVoltage("811111111111119"))
     }
 
-    fun solution(case: String): Pair<Int, Int> {
+    fun largestVoltage2(bank: String): Long {
+        var result = 0L
+        val n = bank.length - 1
+        for (i1 in 0..n - 11) {
+            for (i2 in i1..n - 10) {
+                for (i3 in i2..n - 9) {
+                    for (i4 in i3..n - 8) {
+                        for (i5 in i4..n - 7) {
+                            for (i6 in i5..n - 6) {
+                                for (i7 in i6..n - 5) {
+                                    for (i8 in i7..n - 4) {
+                                        for (i9 in i8..n - 3) {
+                                            for (i10 in i9..n - 2) {
+                                                for (i11 in i10..n - 1) {
+                                                    for (i12 in i11..n) {
+                                                        val seq =
+                                                            listOf(i1, i2, i3, i4, i5, i6, i7, i8, i9, i10, i11, i12)
+                                                        if (!isIndexesOk(seq)) continue
+                                                        val newResult =
+                                                            seq.map { i -> bank[i] }.toCharArray().joinToString("")
+                                                                .toLong()
+                                                        result = max(result, newResult)
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return result
+    }
+
+    fun isIndexesOk(list: List<Int>): Boolean =
+        list.zip(list.drop(1)).all { p -> p.first < p.second }
+
+    fun solution(case: String): Pair<Int, Long> {
         val list = IOUtil.input(case)
         val answer1 = list.sumOf { bank -> largestVoltage(bank) }
-        return answer1 to 0
+        val answer2 = list.sumOf { bank -> largestVoltage2(bank) }
+        return answer1 to answer2
     }
 
     @Test
     fun test() {
         val answer = solution("03-1")
         assertEquals(357, answer.first)
-        assertEquals(0, answer.second)
+        assertEquals(3121910778619L, answer.second)
     }
 
     @Test
