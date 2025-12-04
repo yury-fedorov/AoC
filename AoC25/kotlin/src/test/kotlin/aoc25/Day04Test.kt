2efbc3346e4b1261815adaad8c2ee634307e6474
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-04 06:55:21 +0100

    AoC25 - Day04 - Part1

---
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day04Test.kt
@@ -0,0 +1,59 @@
+package aoc25
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+
+class Day04Test {
+    data class Position(val x: Int, val y: Int)
+
+    val adjacentShifts = setOf<Position>(
+        Position(-1, -1), Position(-1, 0), Position(-1, 1),
+        Position(0, -1), Position(0, 1), Position(1, -1), Position(1, 0),
+        Position(1, 1)
+    )
+
+    val roll = '@'
+
+    fun shift(p: Position, shift: Position) : Position = Position(p.x + shift.x, p.y + shift.y)
+
+    fun at(map: List<String>, p: Position): Char {
+        if (p.y < 0 || p.y >= map.size) return '.'
+        val line = map[p.y]
+        if (p.x < 0 || p.x >= line.length) return '.'
+        return line[p.x]
+    }
+
+    fun countRolls(map: List<String>, p: Position) : Int =
+        adjacentShifts.map { s -> at(map, shift(p, s)) }.count { c -> c == roll }
+
+    fun countAccessibleRolls(map: List<String>): Int {
+        var count = 0
+        for (y in 0..<map.size) {
+            val line = map[y]
+            for (x in 0..<line.length) {
+                val p = Position(x,y)
+                val me = at(map, p)
+                if ( me != roll ) continue
+                val adjCount = countRolls(map, p)
+                if (adjCount<=3) {
+                    count++
+                }
+            }
+        }
+        return count
+    }
+
+    @Test
+    fun test() {
+        val map = IOUtil.input("04-1")
+        assertEquals(13, countAccessibleRolls(map))
+    }
+
+
+    @Test
+    fun solution() {
+        val map = IOUtil.input("04")
+        assertEquals(1346, countAccessibleRolls(map))
+    }
+}
