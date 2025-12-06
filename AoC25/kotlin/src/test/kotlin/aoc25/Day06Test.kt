6c1c1569c865cbed35866b80473b50c136c5dd23
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-06 06:39:34 +0100

    AoC25 - Day06 - Part1 solved

--- AoC25/kotlin/src/test/kotlin/aoc25/Day06Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day06Test.kt
@@ -5,24 +5,41 @@ import kotlin.test.assertEquals
 
 class Day06Test {
 
+    fun parseLine(line: String): List<String> = line.split("\\s+".toRegex()).filter { it.isNotBlank() }.toList()
+
     fun solution(data: String): Pair<Long, Long> {
         val list = IOUtil.input(data)
-        for (line in list) {
+        val splitted :List<List<String>> = list.map { parseLine(it) }.toList()
+        val sizes = splitted.map{ it.size }.groupBy { it }
+        assertEquals(1, sizes.size)
+        var answer1 = 0L
+        val n : Int = sizes.keys.first()
+        for (i in 0 ..< n ) {
+            val operation = splitted.last()[i]
+            var total = splitted.first()[i].toLong()
+            for ( j in splitted.drop(1).dropLast(1) ) {
+                val value = j[i].toLong()
+                when (operation) {
+                    "*" -> total *= value
+                    "+" -> total += value
+                }
+            }
+            answer1 += total
         }
-        return 0L to 0L
+        return answer1 to 0L
     }
 
     @Test
     fun test() {
         val result = solution("06-1")
-        assertEquals(0L, result.first)
+        assertEquals(4277556L, result.first)
         assertEquals(0L, result.second)
     }
 
     @Test
     fun solution() {
-        val result = solution("06-1")
-        assertEquals(0L, result.first)
+        val result = solution("06")
+        assertEquals(5381996914800L, result.first)
         assertEquals(0L, result.second)
     }
 }
