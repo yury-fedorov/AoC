cc7128cf521e641e9c2cd3d28923b93879efd1ff
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-07 06:35:41 +0100

    AoC25 - Day07 - Part1 - solved

--- AoC25/kotlin/src/test/kotlin/aoc25/Day07Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day07Test.kt
@@ -7,7 +7,25 @@ class Day07Test {
 
     fun solution(data: String): Pair<Long, Long> {
         val list = IOUtil.input(data)
-        val answer1 = list.sumOf { line -> line.filter { c -> c == '^' }.length }.toLong() // fast approx
+        val splitterCount = list.sumOf { line -> line.filter { c -> c == '^' }.length }.toLong() // fast approx
+        var answer1 = 0L
+        var sources = mutableSetOf( list.first().indexOf('S') )
+        for (line in list.drop(1)) {
+            var newSources = mutableSetOf<Int>()
+            var hitSplitterCount = mutableSetOf<Int>()
+            for (src in sources) {
+                val ch = line[src]
+                if (ch == '^') {
+                    newSources += src - 1
+                    newSources += src + 1
+                    hitSplitterCount += src
+                } else {
+                    newSources += src
+                }
+            }
+            answer1 += hitSplitterCount.size
+            sources = newSources
+        }
         return answer1 to 0L
     }
 
@@ -21,7 +39,7 @@ class Day07Test {
     @Test
     fun solution() {
         val result = solution("07") // TODO
-        assertEquals(0L, result.first) // max - 1640
+        assertEquals(1516L, result.first) // max - 1640
         assertEquals(0L, result.second)
     }
 }
