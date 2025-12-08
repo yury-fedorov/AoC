cfdf9bc5a76512b0cca62362eaf89792b2db2a1a
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-08 08:07:28 +0100

    AoC25 - Day08 - WIP

--- AoC25/kotlin/src/test/kotlin/aoc25/Day08Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day08Test.kt
@@ -1,13 +1,39 @@
 package aoc25
 
+import kotlin.math.pow
+import kotlin.math.sqrt
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day08Test {
 
+    data class Point(val x: Int, val y: Int, val z: Int) {}
+
+    fun p2(n: Number): Double = n.toDouble().pow(2.0)
+
+    fun distance(a: Point, b: Point): Double = sqrt(p2(a.x - b.x) + p2(a.y - b.y) + p2(a.z - b.z))
+
+    fun parse(line: String): Point {
+        val xyz = line.split(",").map { n -> n.toInt() }
+        return Point(xyz[0], xyz[1], xyz[2])
+    }
+
+    fun compare(a: Point, b: Point): Int {
+        val x = a.x.compareTo(b.x)
+        if (x != 0) return x
+        val y = a.y.compareTo(b.y)
+        if (y != 0) return y
+        return a.z.compareTo(b.z)
+    }
+
+    fun sort(a: Point, b: Point): Pair<Point, Point> = if (compare(a, b) < 0) Pair(a, b) else Pair(b, a)
+
     fun solution(data: String): Pair<Long, Long> {
-        val list = IOUtil.input(data)
-        for (line in list) { // TODO
+        val list = IOUtil.input(data).map { l -> parse(l) }.toList()
+        for (a in list) {
+            for (b in list) {
+                if (a == b) continue
+            }
         }
         return 0L to 0L
     }
