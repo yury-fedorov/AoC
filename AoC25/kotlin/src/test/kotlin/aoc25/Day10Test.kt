4e2f8d4f0d5be4ff6a976298d84f730b64641303
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-09 07:37:53 +0100

    AoC25 - Day10 and Day11 - initial templates

---
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day10Test.kt
@@ -0,0 +1,28 @@
+package aoc25
+
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class Day10Test {
+
+    fun solution(data: String): Pair<Long, Long> {
+        val list = IOUtil.input(data)
+        for (line in list) { // TODO
+        }
+        return 0L to 0L
+    }
+
+    @Test
+    fun test() {
+        val result = solution("01-1") // TODO
+        assertEquals(0L, result.first)
+        assertEquals(0L, result.second)
+    }
+
+    @Test
+    fun solution() {
+        val result = solution("01") // TODO
+        assertEquals(0L, result.first)
+        assertEquals(0L, result.second)
+    }
+}
