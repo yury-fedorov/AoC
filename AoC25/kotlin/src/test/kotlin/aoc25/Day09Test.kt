b69e04ea025386121ef8727982112605814279ce
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-09 06:19:14 +0100

    AoC25 - Day09 - Part1 - solved

--- AoC25/kotlin/src/test/kotlin/aoc25/Day09Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day09Test.kt
@@ -1,28 +1,37 @@
 package aoc25
 
+import kotlin.math.abs
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day09Test {
 
+    data class Point(val x: Int, val y: Int) {}
+
+    fun area(a: Point, b: Point): Long = (abs(a.x - b.x) + 1L) * (abs(a.y - b.y) + 1L)
+
+    fun parse(line:String):Point {
+        val ab = line.split(",").map { n -> n.toInt() }
+        return Point(ab[0], ab[1])
+    }
+
     fun solution(data: String): Pair<Long, Long> {
-        val list = IOUtil.input(data)
-        for (line in list) { // TODO
-        }
-        return 0L to 0L
+        val list = IOUtil.input(data).map{l -> parse(l)}
+        val answer1 = list.flatMap { a -> list.map{ b -> area(a, b) } }.max()
+        return answer1 to 0L
     }
 
     @Test
     fun test() {
         val result = solution("09-1")
-        assertEquals(0L, result.first)
+        assertEquals(50L, result.first)
         assertEquals(0L, result.second)
     }
 
     @Test
     fun solution() {
         val result = solution("09")
-        assertEquals(0L, result.first)
+        assertEquals(4725826296L, result.first)
         assertEquals(0L, result.second)
     }
 }
