45ad11d06bda0ccc6348cee5e23eb14aec029e71
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-10 06:49:29 +0100

    AoC25 - Day10 - Part1 - WIP

--- AoC25/kotlin/src/test/kotlin/aoc25/Day10Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day10Test.kt
@@ -5,9 +5,31 @@ import kotlin.test.assertEquals
 
 class Day10Test {
 
+    typealias Button = List<Int>
+
+    data class Machine(val start: String, val buttons: List<Button>, val numbers: List<Int>) {}
+
+    fun parse(line: String): Machine {
+        val startRegex = Regex("^\\[(.*?)\\]")
+        val buttonsRegex = Regex("\\(([^)]+)\\)")
+        val numbersRegex = Regex("\\{([^}]+)\\}$")
+        val startMatch = startRegex.find(line)
+        val startString = startMatch?.groupValues?.get(1)
+        val buttons = buttonsRegex.findAll(line)
+            .map { r -> r.groupValues.get(1).split(',').map { it.toInt() } }.toList()
+        val numbersMatch = numbersRegex.find(line)
+        val numbersContent = numbersMatch?.groupValues?.get(1)
+        val numbers = numbersContent?.split(',')?.map { it.toInt() }
+        return Machine(start = startString!!, buttons = buttons, numbers = numbers!!)
+    }
+
+    fun shortest(machine: Machine): Long = 0L
+
     fun solution(data: String): Pair<Long, Long> {
-        val list = IOUtil.input(data)
-        for (line in list) { // TODO
+        val list = IOUtil.input(data).map { l -> parse(l) }
+        var answer1 = 0L
+        for (machine in list) {
+            answer1 += shortest(machine)
         }
         return 0L to 0L
     }
@@ -15,7 +37,7 @@ class Day10Test {
     @Test
     fun test() {
         val result = solution("10-1")
-        assertEquals(0L, result.first) // TODO
+        assertEquals(7L, result.first) // TODO
         assertEquals(0L, result.second)
     }
 
