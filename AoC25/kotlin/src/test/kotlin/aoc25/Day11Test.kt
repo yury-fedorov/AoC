1b75eefaf8e4da47bd5e048cea31ebabb4dd25c4
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-11 06:48:17 +0100

    AoC25 - Day11 - Part1 solved

--- AoC25/kotlin/src/test/kotlin/aoc25/Day11Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day11Test.kt
@@ -5,24 +5,52 @@ import kotlin.test.assertEquals
 
 class Day11Test {
 
-    fun solution(data: String): Pair<Long, Long> {
-        val list = IOUtil.input(data)
-        for (line in list) { // TODO
+    typealias Node = String
+    typealias Links = Pair<Node, Set<Node>>
+    typealias Graph = Map<Node, Set<Node>>
+    typealias Path = List<Node>
+
+    val YOU = "you"
+    val OUT = "out"
+
+    fun parse(line: String): Links {
+        val fromOther = line.split(':')
+        val to = fromOther.last().trim().split(' ').toSet()
+        return Links(fromOther.first(), to)
+    }
+
+    fun explore(graph: Graph, from: Node, path: Path, found: MutableList<Path>) {
+        val nextList = graph[from] ?: return
+        for (next in nextList) {
+            val path1 = path + next
+            if (next == OUT) {
+                found += path1
+            } else if (path.contains(next)) {
+                // we have already been in this node
+            } else {
+                explore(graph, next, path1, found)
+            }
         }
-        return 0L to 0L
+    }
+
+    fun solution(data: String): Pair<Long, Long> {
+        val graph = IOUtil.input(data).associate { parse(it) }
+        val result = mutableListOf<Path>()
+        explore(graph, YOU, listOf(), result)
+        return result.size.toLong() to 0L
     }
 
     @Test
     fun test() {
         val result = solution("11-1") // TODO
-        assertEquals(0L, result.first)
+        assertEquals(5L, result.first)
         assertEquals(0L, result.second)
     }
 
     @Test
     fun solution() {
         val result = solution("11") // TODO
-        assertEquals(0L, result.first)
+        assertEquals(643L, result.first)
         assertEquals(0L, result.second)
     }
 }
