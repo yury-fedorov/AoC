4fd34062ab2ea653ced6d29b03bd3dfd6fc2d062
Yury Fedorov <yury.fedorov@gmail.com>
2025-12-12 07:40:43 +0100

    AoC25 - Day12 - initial draft

--- AoC25/kotlin/src/test/kotlin/aoc25/Day12Test.kt
+++ AoC25/kotlin/src/test/kotlin/aoc25/Day12Test.kt
@@ -4,7 +4,13 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 
 class Day12Test {
+    // rotation
+    // flipping
 
+    // Presents can be rotated and flipped as necessary to make them fit in the available space, but they have to always
+    // be placed perfectly on the grid. Shapes can't overlap (that is, the # part from two different presents can't go
+    // in the same place on the grid), but they can fit together (that is, the . part in a present's shape's diagram
+    // does not block another present from occupying that space on the grid).
     fun solution(data: String): Pair<Long, Long> {
         val list = IOUtil.input(data)
         for (line in list) { // TODO
@@ -14,14 +20,14 @@ class Day12Test {
 
     @Test
     fun test() {
-        val result = solution("01-1") // TODO
+        val result = solution("12-1") // TODO
         assertEquals(0L, result.first)
         assertEquals(0L, result.second)
     }
 
     @Test
     fun solution() {
-        val result = solution("01") // TODO
+        val result = solution("12") // TODO
         assertEquals(0L, result.first)
         assertEquals(0L, result.second)
     }
