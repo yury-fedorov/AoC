3b7a68c857c1b6a5d9d2518deb758dac249ac5d4
yury-fedorov <yury.fedorov@gmail.com>
2026-08-20 20:52:38 +0200

    AoC24 - Day20 - Part2 - solved.

--- AoC24/python/day20.py
+++ AoC24/python/day20.py
@@ -18,155 +18,56 @@ def _find_location(the_map: [str], what: str) -> c.Point:
     raise ValueError()
 
 
-def _at(the_map: [str], x, y: int) -> str:
-    if x < 0 or y < 0 or y >= len(the_map) or x >= len(the_map[0]): return WALL
-    return the_map[y][x]
+def _get_track_path(the_map: [str], start: c.Point, end: c.Point) -> dict[c.Point, int]:
+    dist = {start: 0}
+    curr = start
+    while curr != end:
+        for s in SHIFTS:
+            nxt = c.Point(curr.x + s.x, curr.y + s.y)
+            if 0 <= nxt.y < len(the_map) and 0 <= nxt.x < len(the_map[0]):
+                if the_map[nxt.y][nxt.x] != WALL and nxt not in dist:
+                    dist[nxt] = dist[curr] + 1
+                    curr = nxt
+                    break
+    return dist
 
 
-def _walls(the_map: [str]) -> [c.Point]:
-    return [c.Point(x, y) for y in range(1, len(the_map))
-            for x in range(1, len(the_map[0]))
-            if _at(the_map, x, y) == WALL]
+def _shortest_distance(the_map: [str], start: c.Point, end: c.Point, max_distance: int | None = None) -> int | None:
+    dist = _get_track_path(the_map, start, end)
+    return dist.get(end, None)
 
 
-def _shortest_distance(the_map: [str], start, end: c.Point, max_distance: int | None = None) -> int | None:
-    if start == end: return 0
+def _count_cheats(the_map: [str], max_cheat_length: int, min_save: int) -> int:
+    start = _find_location(the_map, START)
+    end = _find_location(the_map, END)
+    dist = _get_track_path(the_map, start, end)
 
-    distance = 0
-    visited = set({})
-    frontline = {start}
-    while len(frontline):
-        next_frontline = set({})
-        for pos in frontline:
-            if pos == end: return distance
-            visited.add(pos)
-            for s in SHIFTS:
-                x1 = pos.x + s.x
-                y1 = pos.y + s.y
-                p1 = c.Point(x1, y1)
-                if p1 == end: return distance + 1  # wall can be destination
-                what = _at(the_map, x1, y1)
-                if what != WALL and (not p1 in visited):
-                    next_frontline.add(p1)
-        frontline = next_frontline
-        distance += 1
-        if max_distance is not None and distance > max_distance: return None
-
-    return None
+    count = 0
+    for p1, d1 in dist.items():
+        for dx in range(-max_cheat_length, max_cheat_length + 1):
+            rem = max_cheat_length - abs(dx)
+            for dy in range(-rem, rem + 1):
+                if dx == 0 and dy == 0:
+                    continue
+                d = abs(dx) + abs(dy)
+                p2 = c.Point(p1.x + dx, p1.y + dy)
+                if p2 in dist:
+                    d2 = dist[p2]
+                    if d2 - d1 - d >= min_save:
+                        count += 1
+    return count
 
 
 def _answer1(the_map: [str], max_desired_shortest: int) -> int:
-    if False:
-        return _answer2(the_map, 2, max_desired_shortest)
-    start = _find_location(the_map, START)
-    end = _find_location(the_map, END)
-    walls = _walls(the_map)
-    count = 0
-    for i, w in enumerate(walls):
-        start_to_wall = _shortest_distance(the_map, start, w, max_desired_shortest)
-        if start_to_wall is None or start_to_wall >= max_desired_shortest: continue
-        wall_to_end = _shortest_distance(the_map, w, end, max_desired_shortest - start_to_wall)
-        if wall_to_end is not None and start_to_wall + wall_to_end <= max_desired_shortest:
-            count += 1
-    return count
-
-
-def _distance_through_walls(jump_start, jump_end: c.Point) -> int:
-    return abs(jump_start.x - jump_end.x) + abs(jump_start.y - jump_end.y)
-
-
-def _jump_distance(the_map: [str], start, jump_start, jump_end, end: c.Point, max_desired_shortest: int) -> int | None:
-    start_to_wall = _shortest_distance(the_map, start, jump_start, max_desired_shortest)
-    if start_to_wall is None or start_to_wall >= max_desired_shortest: return None
-    distance_through_walls = _distance_through_walls(jump_start, jump_end)
-    remaining_max_distance = max_desired_shortest - start_to_wall - distance_through_walls
-    wall_to_end = _shortest_distance(the_map, jump_end, end, remaining_max_distance)
-    if wall_to_end is None: return None
-    total_distance = start_to_wall + distance_through_walls + wall_to_end
-    return total_distance if total_distance <= max_desired_shortest else None
-
-
-def _all_paths(the_map: [str], start, end: c.Point, max_steps: int) -> {c.Point}:
-    result = {start}
-    frontline = {start}
-    distance = 0
-    while len(frontline):
-        new_frontline = set({})
-        for p in frontline:
-            for s in SHIFTS:
-                x1 = p.x + s.x
-                y1 = p.y + s.y
-                what = _at(the_map, x1, y1)
-                p1 = c.Point(x1, y1)
-                if what == WALL or (p1 in result): continue
-                d1 = _fast_shortest(the_map, p1, end)
-                if d1 + distance > max_steps: continue
-                result.add(p1)
-                new_frontline.add(p1)
-        distance += 1
-        frontline = new_frontline
-    return result
-
-
-CACHE = {}
-
-
-def _fast_shortest(the_map: [str], start, end: c.Point) -> int:
-    key = (start, end)
-    result = CACHE.get(key, -1)
-    if result > -1: return result
-    result = _shortest_distance(the_map, start, end)
-    CACHE[key] = result
-    return result
-
-
-def _sign(value: int) -> int: return 0 if value == 0 else 1 if value > 0 else -1
-
-
-def _is_cheat(the_map: [str], p1, p2: c.Point) -> bool:
-    x1, y1 = p1
-    x2, y2 = p2
-    dx, dy = x2 - x1, y2 - y1
-    if abs(dx) + abs(dy) <= 1: return False  # no wall possible between two adjacent points
-    if dx == 0:
-        # pure y move
-        # points on the y-axis after p1 and before p2 have to be walls to be a cheat
-        return (_at(the_map, x1, y1 + _sign(dy)) == WALL
-                and _at(the_map, x2, y2 - _sign(dy)) == WALL)
-    if dy == 0:
-        # pure x move
-        # points on the x-axis after p1 and before p2 have to be walls to be a cheat
-        return (_at(the_map, x1 + _sign(dx), y1) == WALL
-                and _at(the_map, x2 - _sign(dy), y2) == WALL)
-
-    # it is ok to be a cheat if only on one direction versus destination there is a wall
-    # TODO - need to be implemented
-    return True
-
-
-def _answer2(the_map: [str], max_cheat_length: int, max_desired_shortest: int) -> int:
     start = _find_location(the_map, START)
     end = _find_location(the_map, END)
     shortest = _shortest_distance(the_map, start, end)
-    paths = list(_all_paths(the_map, start, end, shortest))
-    count = 0
-    for i, p1 in enumerate(paths):
-        dist_to_end1 = _fast_shortest(the_map, p1, end)
-        for j, p2 in enumerate(paths):
-            if not (j > i): continue
-            jump_distance = _distance_through_walls(p1, p2)
-            if jump_distance > max_cheat_length: continue
-            if _fast_shortest(the_map, p1, p2) == jump_distance: continue  # no obstacles already
-            if not _is_cheat(the_map, p1, p2): continue  # there are normal step around p1 or p2
-            dist_to_end2 = _fast_shortest(the_map, p2, end)
-            if dist_to_end1 < dist_to_end2:
-                # p1 is closer to end, we swap them
-                p1, p2 = p2, p1
-                dist_to_end2 = dist_to_end1
-            ds1 = _fast_shortest(the_map, start, p1)
-            if (ds1 + dist_to_end2 + jump_distance) <= max_desired_shortest:
-                count += 1
-    return count
+    min_save = shortest - max_desired_shortest
+    return _count_cheats(the_map, 2, min_save)
+
+
+def _answer2(the_map: [str], max_cheat_length: int, min_save: int) -> int:
+    return _count_cheats(the_map, max_cheat_length, min_save)
 
 
 class Day20(unittest.TestCase):
@@ -184,9 +85,8 @@ class Day20(unittest.TestCase):
         shortest = _shortest_distance(the_map, start, end)
         max_desired_shortest = shortest - 20
         self.assertEqual(5, _answer1(the_map, max_desired_shortest), "answer 1")
-        # 12 + 22 + 4
-        # self.assertEqual(3, _answer2(the_map, 6, shortest - 76), "answer 2")
-        # self.assertEqual(4 + 3, _answer2(the_map, 6, shortest - 74), "answer 2")
+        self.assertEqual(3, _answer2(the_map, 20, 76), "answer 2 (>=76)")
+        self.assertEqual(285, _answer2(the_map, 20, 50), "answer 2 (>=50)")
 
     def test_day(self):
         the_map = c.read_lines("20")
@@ -194,8 +94,9 @@ class Day20(unittest.TestCase):
         end = _find_location(the_map, END)
         shortest = _shortest_distance(the_map, start, end)
         max_desired_shortest = shortest - MIN_SAFE
-        if not c.is_fast_only():
-            # takes 39 minutes
-            self.assertEqual(1415, _answer1(the_map, max_desired_shortest), "answer 1")
-        # TODO answer 2 - 684977 - too low, took 38 minutes 17 seconds
-        # self.assertEqual(0, _answer2(the_map, MIN_SAFE, max_desired_shortest), "answer 2")
+        self.assertEqual(1415, _answer1(the_map, max_desired_shortest), "answer 1")
+        self.assertEqual(1022577, _answer2(the_map, 20, 100), "answer 2")
+
+
+if __name__ == '__main__':
+    unittest.main()
